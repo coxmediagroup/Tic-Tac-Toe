d00d60e1c8dda05c5a9c90bb38c343c80af907c8
Cameron Webb <cwebb02@gmail.com>
2010-12-17 08:27:42 -0600

    Adding game and computer logic file

---
+++ logic.js
@@ -0,0 +1,70 @@
+/* This file is for anything and everything dealing with actual game play */
+
+function determineIfWin() {
+	//Right, so the below logic isn't enitrely awesome.
+	//Should improve on it at some point
+	
+	/* The "each" stuff essentially is checking if there are any
+	 * matches horizontally while also building up a list to check
+	 * for other directional matches later */
+	var gameboard = [];
+	//Don't initialize a variable each time through the loop! Go through and fix!
+	$("#board tr").each(function() {
+		var rowData = $(this).find('td');
+		var firstCell = rowData.eq(0).text();
+		var secondCell = rowData.eq(1).text();
+		var thirdCell = rowData.eq(2).text();
+		
+		gameboard.push(firstCell);
+		gameboard.push(secondCell);
+		gameboard.push(thirdCell);
+		
+		if (firstCell == secondCell &&
+			secondCell == thirdCell &&
+			thirdCell != "") {
+				endOfGame = true;
+			}
+		
+	});
+	
+	//Vertical and diaganol checking... why can't we do this above? C'Mon! Think!
+	if ((gameboard[0]== gameboard[3] && gameboard[3] == gameboard[6] && gameboard[0] != "")
+		|| (gameboard[1]== gameboard[4] && gameboard[4] == gameboard[7] && gameboard[1] != "") 
+		|| (gameboard[2]== gameboard[5] && gameboard[5] == gameboard[8] && gameboard[2] != "") 
+		|| (gameboard[0]== gameboard[4] && gameboard[4] == gameboard[8] && gameboard[0] != "")
+		|| (gameboard[2]== gameboard[4] && gameboard[4] == gameboard[6] && gameboard[2] != "")){
+		endOfGame = true;
+	}
+}
+
+function getComputerMove(){
+	var gameboard = [];
+	
+	var winning_combos = [[0, 1, 2], [3, 4, 5], [6, 7, 8], [0, 3, 6], [1, 4, 7], [2, 5, 8],
+        [0, 4, 8], [2, 4, 6]];
+	
+	$("#board tr").each(function() {
+		var rowData = $(this).find('td');
+		var firstCell = rowData.eq(0).text();
+		var secondCell = rowData.eq(1).text();
+		var thirdCell = rowData.eq(2).text();
+		
+		gameboard.push(firstCell);
+		gameboard.push(secondCell);
+		gameboard.push(thirdCell);
+		
+	});
+	//TODO: Figure out how to fill in the Spot Directly!!
+	
+	//First, is the center open? If so, Mr. Computer wants that!
+	gameboard[4] = (gameboard[4] == "" ? piece.O : gameboard[4]);
+	if (gameboard[4] == "") {
+		//Ok, it is open. Mr. Computer would like to take this spot!
+		gameboard == piece.O;
+		
+		//Let's not continue to beat the bush and just return
+		return true;
+	}
+	
+	
+}
