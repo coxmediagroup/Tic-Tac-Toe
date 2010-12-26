781e3eedc05b07e9b5d862d8b17c3de45facf8f0
Brit Butler <redline6561@gmail.com>
2010-12-26 12:58:09 -0500

    A class for board was quite silly. Let's try a different representation. We'll use a game object holding scores and a 2-dimensional array for the board.

--- lisp/tic-tac-toe.lisp
+++ lisp/tic-tac-toe.lisp
@@ -3,33 +3,51 @@
 
 (in-package :tic-tac-toe)
 
-(defclass board ()
-  ((row1 :initform '(" " " " " ")
-         :accessor row1)
-   (row2 :initform '(" " " " " ")
-         :accessor row2)
-   (row3 :initform '(" " " " " ")
-         :accessor row3)))
+(defclass tic-tac-toe ()
+  ((score-human :initform 0
+                :accessor score-human)
+   (score-ai :initform 0
+             :accessor score-ai)
+   (board :initform (make-array '(3 3) :initial-element " ")
+          :accessor board)))
 
-(defmethod print-object ((board board) stream)
-  "This will ensure that the board object is always printed
-with a reasonably nice representation and the options for
-future moves will also be displayed."
-  (let ((move-count 0))
-    (flet ((print-row (row &key moves)
-             (if moves
-                 (with-output-to-string (result)
-                   (loop for place in row do
-                        (if (string= " " place)
-                            (format result " ~A" (incf move-count))
-                            (format result " ~A" place))))
-                 (format nil "~{ ~A~}" row))))
-    (format stream "[~A ]~%[~A ]~%[~A ]~%"
-            (print-row (row1 board))
-            (print-row (row2 board))
-            (print-row (row3 board)))
-    (format stream "~%Your potential moves are:~%")
-    (format stream "[~A ]~%[~A ]~%[~A ]~%"
-            (print-row (row1 board) :moves t)
-            (print-row (row2 board) :moves t)
-            (print-row (row3 board) :moves t)))))
+(defparameter *current-game* (make-instance 'tic-tac-toe))
+
+(defmethod print-board ((game tic-tac-toe) &key moves)
+  "Print each row of the board inside square brackets.
+If moves is T, blank spaces (i.e. available moves) will
+be numbered starting from 1."
+  (let ((move-count 0)
+        (board (board game)))
+    (flet ((print-row (row-num &key moves)
+             (with-output-to-string (result)
+               (loop for i in '(0 1 2) do
+                    (if (and moves
+                             (string= (aref board row-num i) " "))
+                        (format result " ~A" (incf move-count))
+                        (format result " ~A" (aref board row-num i)))))))
+      (when moves
+        (format t "Your potential moves are:~%"))
+      (format t "[~A ]~%[~A ]~%[~A ]~%"
+              (print-row 0 :moves moves)
+              (print-row 1 :moves moves)
+              (print-row 2 :moves moves)))))
+
+(defmethod print-score ((game tic-tac-toe))
+  "Print the score of the computer and player in GAME."
+  (format t "The score is... Scary Robots: ~A   Puny Humans: ~A~%"
+          (score-ai game) (score-human game)))
+
+(defmethod reset-board ((game tic-tac-toe))
+  "Reset the board for a new game."
+  (setf (board game) (make-array '(3 3) :initial-element " ")))
+
+(defun main ()
+  (loop
+     (print-score (score *current-game*))
+     (reset-board (board *current-game*))
+     (print-board (board *current-game*))
+     (take-turns *current-game*)))
+
+(defmethod take-turns ((game tic-tac-toe))
+  )
