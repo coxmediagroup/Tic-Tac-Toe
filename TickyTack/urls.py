1a72a4c6a94fd9badd035061b4c6515512770ab1
sabren <michal.wallace@gmail.com>
2011-01-19 20:25:35 -0500

    user login page

--- TickyTack/urls.py
+++ TickyTack/urls.py
@@ -1,16 +1,18 @@
 from django.conf.urls.defaults import *
-
-# Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
-urlpatterns = patterns('',
-    # Example:
-    # (r'^TickyTack/', include('TickyTack.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+import DJTickyTack
+
+urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
+    (r'^accounts/login/$', 'django.contrib.auth.views.login',
+        {'template_name': 'login.html'}),
+
+
+    # mount the app:
+    (r'^$', 'DJTickyTack.views.index'),
 )
