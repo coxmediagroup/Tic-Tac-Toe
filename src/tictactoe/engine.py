94de80177171225fe830d3526b7a887af0e4e0b8
Krzysztof Tarnowski <krzysztof.tarnowski@ymail.com>
2011-01-29 10:01:53 +0100

    The board is now represented by numpy.array object. Previous representation was invalid (created list of list refrences!).
    Updated control flow within NegamaxEngine class. The actual Negamax algorithm is left to do.

--- src/tictactoe/engine.py
+++ src/tictactoe/engine.py
@@ -14,7 +14,7 @@ class Engine(object):
         Constructor
         '''
         
-    def next_move(self, board):
+    def next_move(self, board, player):
         ''' doc '''
         pass
     
@@ -26,5 +26,15 @@ class NegamaxEngine(Engine):
     classdocs
     '''
     
-    def __init__(self):
-        Engine.__init__(self) 
+    def __init__(self, max_depth=float('Infinity')):
+        Engine.__init__(self)
+        self.max_depth = max_depth
+        self.move = None
+        
+    def next_move(self, board, player):
+        self._negamax(board, 0, player)
+        
+        return self.move
+        
+    def _negamax(self, board, depth, player):
+        pass
--- src/tictactoe/game.py
+++ src/tictactoe/game.py
@@ -4,6 +4,8 @@ Created on Jan 29, 2011
 @author: Krzysztof Tarnowski
 '''
 
+import numpy
+
 from engine import NegamaxEngine
 
 class Game(object):
@@ -13,14 +15,15 @@ class Game(object):
     
     def __init__(self, engine=NegamaxEngine()):
         self._engine = engine
-        self._board = [[0]*3]*3
+        self._board = numpy.zeros((3, 3), dtype=numpy.int16)
+        self.turn = Game.P1
         
     def start(self):
         pass
     
-    def play(self, move):
+    def play(self, move=None):
         if move == None:
-            move = self._engine.next_move(self._board)
+            move = self._engine.next_move(self._board, self.turn)
         
         #TODO: Update state
     
