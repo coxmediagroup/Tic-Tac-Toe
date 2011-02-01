9d94282af8da1981368dd354cef0fd8f1c43156f
Fred <stake75@hotmail.com>
2011-01-31 23:29:25 -0500

    Resolved a few mistakes in the recursive ai test and added a few empty functions as well to the ai test. Next step will be to finish this out and test it. If this goes well a similar algorithm will be implemented in the computer player class.

--- temp/TestRecursiveAlgorithm.py
+++ temp/TestRecursiveAlgorithm.py
@@ -22,13 +22,15 @@ class Node(object):
 def foo(current_state, player):
     open_positions = findRemainingPositions(current_state)
     print "Open positions: " + str(open_positions)
-    if len(current_state) == 1:
-        new_state = copy.deepcopy(currentState)
+    if len(open_positions) == 1:
+        position = open_positions.pop()
+        new_state = copy.deepcopy(current_state)
         new_state[position] = player
-        return [Node(open_positions.pop())]
+        # TODO => test for score and add to node
+        return [Node(position)]
 
     nodes = []
-    for i, position in enumerate(open_positions):
+    for position in open_positions:
         new_state = copy.deepcopy(current_state)
         new_state[position] = player
         new_player = None
@@ -37,17 +39,24 @@ def foo(current_state, player):
         else:
             new_player = 'x'
         links = foo(new_state, new_player)
+        # TODO => find the values from the links to calulate the score
         node = Node(position, 0, links)
         nodes.append(node)
     
     return nodes
 
 def findRemainingPositions(current_state):
-    open_positions = set()
+    open_positions = []
     for i, state in enumerate(current_state):
         if state == '-':
-            open_positions.add(i)
+            open_positions.append(i)
     return open_positions
+    
+def testForWinner(state):
+    pass
+    
+def findScore(links):
+    pass
 
 if __name__ == '__main__':
     state = [ 'x', '-', '-',
