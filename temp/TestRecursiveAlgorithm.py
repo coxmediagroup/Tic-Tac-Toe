1be19f6d2f5a9bfd1d2f50363f064fec3d9f77f9
Fred <stake75@hotmail.com>
2011-02-02 19:56:33 -0500

    Worked on finishing the recursive algorithm. Currently it is complete and needs to be tested before being added into the main code.

--- temp/TestRecursiveAlgorithm.py
+++ temp/TestRecursiveAlgorithm.py
@@ -14,20 +14,30 @@ import copy
 
 class Node(object):
    
-    def __init__(self, position=0, score=0, links=[]):
+    def __init__(self, position=0, state=[], score=0, links=[]):
         self.position = position
+        self.state = state
         self.score = score
         self.links = links
         
+    def __str__(self):
+        return str(self.position) + " :: " + str(self.score)
+        
 def foo(current_state, player):
     open_positions = findRemainingPositions(current_state)
-    print "Open positions: " + str(open_positions)
     if len(open_positions) == 1:
         position = open_positions.pop()
         new_state = copy.deepcopy(current_state)
         new_state[position] = player
-        # TODO => test for score and add to node
-        return [Node(position)]
+        score = 0
+        if ( player == 'x' and testForWinner(new_state, 'x') == True ) or \
+           ( player =='o' and testForWinner(new_state, 'o') == True ):
+            score = 1
+        elif ( player == 'x' and testForWinner(new_state, 'o') == True ) or \
+             ( player =='o' and testForWinner(new_state, 'x') == True ):
+            score = -1
+        node = Node(position, new_state, score)
+        return [node]
 
     nodes = []
     for position in open_positions:
@@ -39,8 +49,11 @@ def foo(current_state, player):
         else:
             new_player = 'x'
         links = foo(new_state, new_player)
-        # TODO => find the values from the links to calulate the score
-        node = Node(position, 0, links)
+        score = 1
+        for link in links:
+            if link.score < score:
+                score == link.score
+        node = Node(position, new_state, score, links)
         nodes.append(node)
     
     return nodes
@@ -52,12 +65,26 @@ def findRemainingPositions(current_state):
             open_positions.append(i)
     return open_positions
     
-def testForWinner(state):
-    pass
+def testForWinner(state, player):
+    if state[0] == player and state[1] == player and state[2] == player:
+        return True
+    elif state[0] == player and state[4] == player and state[8] == player:
+        return True
+    elif state[0] == player and state[3] == player and state[6] == player:
+        return True
+    elif state[2] == player and state[4] == player and state[6] == player:
+        return True
+    elif state[2] == player and state[5] == player and state[8] == player:
+        return True
+    elif state[3] == player and state[4] == player and state[5] == player:
+        return True
+    elif state[6] == player and state[7] == player and state[8] == player:
+        return True
+    elif state[1] == player and state[4] == player and state[7] == player:
+        return True
     
-def findScore(links):
-    pass
-
+    return False
+     
 if __name__ == '__main__':
     state = [ 'x', '-', '-',
               '-', 'o', '-',
