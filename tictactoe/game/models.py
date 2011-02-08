3ce69dc911ccdcb4db81f157358f174a9e5be19c
Michael Hampton <mirhampt@gmail.com>
2011-02-07 20:46:21 -0500

    Add TicTacToeBoardField and Game model

--- tictactoe/game/models.py
+++ tictactoe/game/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from game.fields import TicTacToeBoardField
 
-# Create your models here.
+class Game(models.Model):
+    """A tic-tac-toe game."""
+    board_state = TicTacToeBoardField()
+
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __init__(self, *args, **kwargs):
+        super(Game, self).__init__(*args, **kwargs)
