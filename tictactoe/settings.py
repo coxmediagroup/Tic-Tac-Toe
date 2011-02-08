9069a2b50213344b75c93798acf70c996c2496d0
Michael Hampton <mirhampt@gmail.com>
2011-02-07 19:55:22 -0500

    Add game app skeleton

--- tictactoe/settings.py
+++ tictactoe/settings.py
@@ -85,6 +85,7 @@ INSTALLED_APPS = (
     'django.contrib.sessions',
     'django.contrib.sites',
     'django.contrib.messages',
+    'game',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
