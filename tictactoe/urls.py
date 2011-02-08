7580f7ef9f151420073282f330bbb19c68ef9391
Michael Hampton <mirhampt@gmail.com>
2011-02-07 22:32:12 -0500

    Setup static file serving for debug; add board image

--- tictactoe/urls.py
+++ tictactoe/urls.py
@@ -1,8 +1,5 @@
 from django.conf.urls.defaults import *
-
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.conf import settings
 
 urlpatterns = patterns('',
     # Example:
@@ -14,3 +11,12 @@ urlpatterns = patterns('',
     # Uncomment the next line to enable the admin:
     # (r'^admin/', include(admin.site.urls)),
 )
+
+# Serve static content through Django if DEBUG = True.
+if settings.DEBUG:
+    urlpatterns += patterns('',
+        (
+            r'^site_media/(?P<path>.*)$', 'django.views.static.serve',
+            {'document_root': settings.MEDIA_ROOT}
+        ),
+    )
