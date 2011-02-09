e76fb6a03fe312a90807051442583285e98b1ccb
Michael Hampton <mirhampt@gmail.com>
2011-02-09 00:26:52 -0500

    Add index page with instructions

--- tictactoe/urls.py
+++ tictactoe/urls.py
@@ -2,6 +2,10 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 
 urlpatterns = patterns('',
+    # Index page.
+    url(r'^$', 'django.views.generic.simple.direct_to_template',
+        {'template': 'index.html'}, name="index"),
+
     # Game app URLs.
     (r'^game/', include('game.urls')),
 )
