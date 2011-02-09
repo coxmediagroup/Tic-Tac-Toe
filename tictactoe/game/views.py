b4414f0741b14cf87c158d265c7566ec80de719f
Michael Hampton <mirhampt@gmail.com>
2011-02-08 19:32:52 -0500

    Add basic ability for user to interact with the board

--- tictactoe/game/views.py
+++ tictactoe/game/views.py
@@ -10,3 +10,23 @@ def show_game(request, game_id):
     "Show a specific game."
     game = get_object_or_404(Game, pk=game_id)
     return render_to_response("game/show_game.html", { 'game': game })
+
+def make_move(request, game_id):
+    """
+    Make a move.
+
+    This method should be called asynchronously and will return the new game
+    board state rendered as HTML.
+    """
+    game = get_object_or_404(Game, pk=game_id)
+
+    if 'row' not in request.POST or 'column' not in request.POST:
+        # Can't do anything if we don't know which cell was clicked.
+        return render_to_response("game/_board.html", { 'game': game })
+
+    row, column = int(request.POST['row']), int(request.POST['column'])
+    if game.board_state[row][column] is None:
+        game.board_state[row][column] = 'O'
+        game.save();
+
+    return render_to_response("game/_board.html", { 'game': game })
--- tictactoe/urls.py
+++ tictactoe/urls.py
@@ -2,8 +2,9 @@ from django.conf.urls.defaults import *
 from django.conf import settings
 
 urlpatterns = patterns('',
-    url(r'^$',                      'game.views.new_game',  name="new_game"),
-    url(r'game/(?P<game_id>\d+)/$', 'game.views.show_game', name="show_game"),
+    url(r'^$',                          'game.views.new_game',  name="new_game"),
+    url(r'game/(?P<game_id>\d+)/$',     'game.views.show_game', name="show_game"),
+    url(r'game/(?P<game_id>\d+)/move$', 'game.views.make_move', name="make_move"),
 )
 
 # Serve static content through Django if DEBUG = True.
