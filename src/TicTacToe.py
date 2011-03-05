4f1a1aa0e2e9f87e6f7889b972871e2c97ac4a40
Sam Aparo <mirthquakes@gmail.com>
2011-03-05 13:03:53 -0600

    Implemented and tested game over function

--- src/TicTacToe.py
+++ src/TicTacToe.py
@@ -55,15 +55,49 @@ class Board:
             output +='\n'
         return output
 
+#Rotate Board Function. Returns a version of the board 'rotated' 90 degrees clockwise. 
+def RotateBoard(gameBoard):
+    newBoard = Board()
+    for i in range(3):
+        for j in range(3):
+            newBoard.updateBoard(i, j, gameBoard.readBoard((3-j)-1, i))
+    return newBoard
+
 #Make Next Move Function: Algorithm that makes computer player's next move, and modifies the board
 #accordingly. 
 def MakeNextMove(gameBoard):
     pass
 
-#Is Game Over Function: Algorithm for determining if the game is over.
-def isGameOver(gameBoard):
-    pass
-
+#Is Game Over Function: Algorithm for determining if the game is over. Returns 0 for no, 1 for X win
+#2 for 0 win, and -1 for a tie game. 
+def isGameOver(gameBoard):    
+    #Check for a vertical, horizontal, middle, or diagonal win. If no win was found, rotate board (4 times)
+    for i in range(4):
+        #Vertical Check
+        if((gameBoard.readBoard(0,0)!=0) and (gameBoard.readBoard(0,0)==gameBoard.readBoard(1,0)) and (gameBoard.readBoard(1,0)==gameBoard.readBoard(2,0))):
+            return gameBoard.readBoard(0,0)
+        #Horizontal Check
+        elif ((gameBoard.readBoard(0,0)!=0) and (gameBoard.readBoard(0,0)==gameBoard.readBoard(0,1)) and (gameBoard.readBoard(0,1)==gameBoard.readBoard(0,2))):
+            return gameBoard.readBoard(0,0)
+        #Middle Check
+        elif ((gameBoard.readBoard(1,0)!=0) and (gameBoard.readBoard(1,0)==gameBoard.readBoard(1,1)) and (gameBoard.readBoard(1,1)==gameBoard.readBoard(1,2))):
+            return gameBoard.readBoard(1,0)
+        #Diagonal Check
+        elif ((gameBoard.readBoard(0,0)!=0) and (gameBoard.readBoard(0,0)==gameBoard.readBoard(1,1)) and (gameBoard.readBoard(1,1)==gameBoard.readBoard(2,2))):
+            return gameBoard.readBoard(0,0)
+        #Rotate Board Otherwise
+        else:
+            gameBoard = RotateBoard(gameBoard)
+            
+    #If no win was found, check to see if board is full. If not, return a 'game not over' value
+    for i in range(3):
+        for j in range(3):
+            if(gameBoard.readBoard(i,j)==0):
+                return 0
+            
+    #Otherwise, return 'tie' value
+    return -1
+                
 #Main Function. Procedure for playing a game. Ties together Board, AI, and user input. 
 if __name__ == '__main__':
     pass
--- src/TicTacToe_Tests.py
+++ src/TicTacToe_Tests.py
@@ -13,7 +13,9 @@ class Test(unittest.TestCase):
     #Setup: Instantiate a Board
     def setUp(self):
         self.gameBoard = ttt.Board()
-        
+    
+##Tests for Board class
+
     #Test reading and writing to a board class (valid case)
     def test_validReadAndWriteToGameBoard(self):
         self.gameBoard.updateBoard(0, 0, 1)
@@ -32,6 +34,49 @@ class Test(unittest.TestCase):
         self.gameBoard.updateBoard(2, 2, 1)
         output = self.gameBoard.renderBoard()
         self.assertEqual(output, 'X|_|_|\n_|O|_|\n_|_|X|\n')
+        
+##Tests for other Functions
+    
+    #Test rotate board function
+    def test_rotateBoard(self):
+        self.gameBoard.updateBoard(0, 0, 1)
+        self.gameBoard.updateBoard(0, 1, 2)
+        self.gameBoard.updateBoard(0, 2, 1)
+        newBoard = ttt.RotateBoard(self.gameBoard)
+        rotatedString = newBoard.renderBoard()
+        self.assertEqual(rotatedString,'_|_|X|\n_|_|O|\n_|_|X|\n')
+    
+    #Test game over function (when game is actually over)
+    def test_gameOverWin(self):
+        
+        self.gameBoard.updateBoard(0, 2, 1)
+        self.gameBoard.updateBoard(1, 2, 1)
+        self.gameBoard.updateBoard(2, 2, 1)
+        isGameOverValue = ttt.isGameOver(self.gameBoard)
+        self.assertEqual(isGameOverValue, 1)
+    
+    #Test game over function (when game is NOT over)
+    def test_gameOverNotOver(self):
+        self.gameBoard.updateBoard(1, 0, 1)
+        self.gameBoard.updateBoard(1, 1, 1)
+        self.gameBoard.updateBoard(1, 2, 2)
+        isGameOverValue = ttt.isGameOver(self.gameBoard)
+        self.assertEqual(isGameOverValue, 0)
+    
+    #Test game over function (when game is a tie)
+    def test_gameOverTie(self):
+        self.gameBoard.updateBoard(0, 0, 1)
+        self.gameBoard.updateBoard(0, 1, 2)
+        self.gameBoard.updateBoard(0, 2, 1)
+        self.gameBoard.updateBoard(1, 0, 1)
+        self.gameBoard.updateBoard(1, 1, 2)
+        self.gameBoard.updateBoard(1, 2, 1)
+        self.gameBoard.updateBoard(2, 0, 2)
+        self.gameBoard.updateBoard(2, 1, 1)
+        self.gameBoard.updateBoard(2, 2, 2)
+        isGameOverValue = ttt.isGameOver(self.gameBoard)
+        self.assertEqual(isGameOverValue, -1)
+        
         
 if __name__ == "__main__":
     unittest.main()
