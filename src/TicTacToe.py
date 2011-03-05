4bcb2a9139405e3371f9031217cb301c5b22a0a0
Sam Aparo <mirthquakes@gmail.com>
2011-03-05 10:41:02 -0600

    Wrote a skeleton, containing class/function interfaces and comments explaining them.

---
+++ src/TicTacToe.py
@@ -0,0 +1,24 @@
+#
+#Cox Media Group 'Tic-Tac-Toe' Coding Challenge
+#Author: Samuel Aparo
+#Created on Mar 5, 2011
+#
+
+#Board Class: A class to represent a simple 3x3 tic-tac-toe board. Essentially, it provides an 
+#interface to the board data structure to ensure that only valid moves are read and written. 
+#It is also able to 'print' itself. 
+class Board:
+    pass
+
+#Make Next Move Function: Algorithm that makes computer player's next move, and modifies the board
+#accordingly. 
+def MakeNextMove(gameBoard):
+    pass
+
+#Is Game Over Function: Algorithm for determining if the game is over.
+def isGameOver(gameBoard):
+    pass
+
+#Main Function. Procedure for playing a game. Ties together Board, AI, and user input. 
+if __name__ == '__main__':
+    pass
