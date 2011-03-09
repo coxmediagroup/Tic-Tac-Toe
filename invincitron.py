cd4abf12395f9625a470cd86ed2309ee23547326
Gregg Lind <gregg.lind@gmail.com>
2011-03-08 20:14:52 -0600

    "RandomPlayers" game-state, functional 'tie' and 'won' detection
    invincitron
    - 'won' and 'tie'
    - interactive games can be played
    
    test_invincitron
    - (non-)test, showing random player games

--- invincitron.py
+++ invincitron.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python 
 '''
 INVINCITRON, an opponent that can win *all games* with 100% certainty [1]
 
 Note [1]: current implementation only plays tic-tac-toe, future versions will expand on this.
 
+Note: requires 2.5+ since we are going to use co-routines
 
 
 '''
@@ -35,16 +37,63 @@ The second player, whom we shall designate "O", must respond to X's opening mark
 
 '''
 
+import itertools
+import random
 
 # ttt has small state, so we can throw it around.  
 
-def choose_move(game,player):
-    pass
+triples = [
+    (0,1,2),
+    (3,4,5),
+    (6,7,8),
+    (0,3,6),
+    (1,4,7),
+    (2,5,8),
+    (0,4,8),
+    (2,4,6)
+]
 
+def won(game):
+    ''' return play who wins the game, or False 
 
-def game_is_won(game):
-    ''' return play who wins the game, or False '''
+    >>> print won(['X','X','X'] + [None,]*6)
+    X
+    >>> print won([None,]*9)
+    None
+
+    # 2 ways for x to win
+    >>> print won(['O', 'O', 'X', 'O', 'X', 'O', 'X', 'X', 'X'])
+    X
+
+    '''
+    winners = set()
+    for p in (('O','O','O'),('X','X','X')):
+        for (a,b,c) in triples:
+            run = (game[a],game[b],game[c]) 
+            if run == p:
+                winners.add(p[0])
+            else:
+                pass
+                #print run, p[0]
+
+    l = len(winners)
+    if l == 0:
+        return None
+    elif l == 1:
+        return list(winners)[0]
+    else:
+        raise Exception('more than one winner!?  oops!')
+
+def tie(game):
+    '''
+
+    >>> g = ['O', 'X', 'O', 'X', 'O', 'X', 'X', 'O', 'X']
+    >>> tie(g)
+    True
+    '''
+    return (not won(game)) and None not in game
     
+
 
 ## all of these are brute force.  We could save state along the way.
 def can_win(game,player):
@@ -78,22 +127,60 @@ def suggest_optimal_move(game,player):
     move = None
     move_fns = (can_win,can_block,can_fork,
         block_fork,center,
-        opposite_corner, empty_corner
+        opposite_corner, empty_corner,
         empty_side)
+     
     while not move:
         for move_fn in move_fns:
             move = move_fn(game,player)
 
     return move
 
-        
+def interactive(player1,player2):
+    '''
+    Play an interactive tic-tac-toe game.
+
+    Args:
+        player1, player2: 'players'
+            that have a 'get_move(game,player)'
+            method.  
+
+    Returns:
+        game, winner, moves
+    '''
+    game = [None,]*9
+    moves = []
+    players = itertools.cycle([('X',player1),('O',player2)])
+    while not (won(game) or tie(game)):
+        s, p = players.next()
+        move = p.next_move(game,s)
+        if move is None:
+            raise Exception, "no move chosen! %r %r %r" % (s,game,moves)
+        game[move] = s
+        moves.append(move)
+        # should have some exceptions here for illegal moves.  
+    
+    if tie(game):
+        winner = "TIE"
+    else:
+        winner = won(game)
+    
+    return winner,game,moves
 
 
 
-class Game(object):
-    def __init__(self,):
+class GoodPlayer(object):
+    def next_move(self,game,player):
+        pass
 
-    def move(player,position):
+class RandomPlayer(object):
+    def next_move(self,game,player):
+        moves = [ii for (ii,x) in enumerate(game) if x is None]
+        print "NEXT_MOVE:", game, player, moves
+        return random.choice(moves)
+
+class CliPlayer(object):
+    def next_move(self,game,player):
         
 
 
--- test_invincitron.py
+++ test_invincitron.py
@@ -5,11 +5,15 @@ tests for invincitron
 
 import random
 
-class Test_RandomGames(object):
+import invincitron as I
+
+class test_RandomGames(object):
     ''' as a first guess, show that in n games, played with random moves,
     invincitron always wins 
     '''
-    pass
+    def test_random(self):
+        for ii in range(20):
+            print I.interactive(I.RandomPlayer(),I.RandomPlayer())
 
        
 
