d8cab91e5478db0a51cc89c83dc0ae6458fd3424
Robert Genito <rgenito@proteushosting.net>
2011-04-25 22:34:39 -0400

    Added the tic-tac-toe file with basic framework.

---
+++ ttt.py
@@ -0,0 +1,43 @@
+# Tic Tac Toe
+# -- classic game :)
+
+board = ['-'] * 9
+
+def render_board():
+	'''
+	Renders the Tic Tac Toe game board to the screen.
+	'''
+	pass
+	
+def place_mark(mark, x, y):
+	'''
+	Place a mark on the game board. minimum to maximum coordinates range from 
+	(0, 0) to (2, 2)
+	'''
+	
+def winner(char = ''):
+	'''
+	Determines if someone won Tic Tac Toe :D
+	Returns X if the X player won; O if O player won.
+	'''
+	
+def computer_move():
+	'''
+	Makes a move onto the game board for the computer. This is essentially 
+	our Tic-Tac-Toe AI.
+	'''
+	
+def human_move():
+	'''
+	Get the human player's move.
+	'''
+	
+
+print '''
+	Hi. I'm a computer. I'm going to demonstrate to you how AWESOME I am by 
+	playing you a game of tic-tac-toe and never losing a single game! I'll 
+	make the first move though. Press the enter/return key when you are ready 
+	for a beating!
+'''
+
+render_board()
