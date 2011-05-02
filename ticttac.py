13415c813a26b80cddd5e4f31b5be92250212104
jcarter1 <dennis_backhaus@yahoo.de>
2011-05-02 15:46:08 -0700

    general program design

--- ticttac.py
+++ ticttac.py
@@ -1,1 +1,15 @@
-#finally starting
+Game
+1) start game (2)
+2) ask for player letter (3)
+3) ask who should go first (4,7)
+
+Players turn
+4) draw board (5)
+5) get player move (6)
+6) check win/tie conditions (7,9)
+
+Computers turn
+7) get computer move (8)
+8) check win/tie conditions (4,9)
+
+9) ask to play again (end,2)
