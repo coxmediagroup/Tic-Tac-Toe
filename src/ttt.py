a3d18310308b4af764607185151def62bfef8dde
Paul <paul@ethos.com.cn>
2011-05-09 15:07:21 +0800

    base method defintions for game

--- src/ttt.py
+++ src/ttt.py
@@ -3,10 +3,32 @@ class Board():
     def __init__(self):
         pass
 
+
 class Game:
     """ Class to control the game logic """
     def __init__(self):
         pass
+    
+    def run(self):
+        """ Cheesy main loop"""
+        pass
+    
+    def turnComplete(self):
+        """ Swap the active players, check to see if there was
+        a gameover event, and perform any required cleanup """
+        pass
+
+    def randStart(self):
+        """ Randomly choose who gets to go first """
+        pass
+
+    def checkGameOver(self):
+        """ Check to see if the game is a draw or someone has won """
+        pass
 
 if __name__ == "__main__":
-    pass
+    Storage()._game_board = Board()
+    Storage()._player_one = Ai()
+    Storage()._player_two = LocalHuman()
+    Storage()._game_instance = Game()
+    Storage()._game_instance.run()
