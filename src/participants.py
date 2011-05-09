28498ef5d9ed403c94bd92972aa71aebbcf4e8ee
Paul <paul@ethos.com.cn>
2011-05-09 14:56:06 +0800

    basic class definitions

---
+++ src/participants.py
@@ -0,0 +1,12 @@
+class Participant(object):
+    def __init__(self):
+        pass
+
+class LocalHuman(Participant):
+    """ Console player for a game """
+    def __init__(self):
+        pass
+
+class Ai(Participant):
+    def __init__(self):
+        pass
---
+++ src/ttt.py
@@ -0,0 +1,12 @@
+class Board():
+    """ Game board class that has basic game board utility functions """
+    def __init__(self):
+        pass
+
+class Game:
+    """ Class to control the game logic """
+    def __init__(self):
+        pass
+
+if __name__ == "__main__":
+    pass
