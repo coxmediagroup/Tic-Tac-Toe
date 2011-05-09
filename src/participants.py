35a19bc8c07283cb1abe99073155a4456c7804da
Paul <paul@ethos.com.cn>
2011-05-09 15:21:14 +0800

    more base method definitions, including shape (x,0) management

--- src/participants.py
+++ src/participants.py
@@ -1,6 +1,12 @@
 class Participant(object):
     def __init__(self):
-        pass
+        self.shape = EMPTY
+        self.opponent_shape = EMPTY
+    
+    def setShape(self, shape):
+        """ Set the shape we are using """
+        self.opponent_shape = 1 if shape == 2 else 2
+        self.shape = shape 
 
 class LocalHuman(Participant):
     """ Console player for a game """
--- src/ttt.py
+++ src/ttt.py
@@ -2,12 +2,31 @@ class Board():
     """ Game board class that has basic game board utility functions """
     def __init__(self):
         pass
+    
+    def winLists(self):
+        """ Returns lists in all directions. This
+        method should allow the board to be an arbitrary size
+        as long as it maintains an odd count, 1:1 row to column ratio
+        """
+        pass
 
+    def place(self, shape, index_list):
+        """ Attempt to place a piece on the board """
+        pass
 
+    def drawBoard(self):
+        """ Basic draw board function for debugging """
+        pass
+           
 class Game:
     """ Class to control the game logic """
     def __init__(self):
-        pass
+        self.players = [Storage()._player_one, Storage()._player_two]
+        self.active_player,self.idle_player = self.randStart()
+        self.active_player.setShape(NOUGHT)
+        self.idle_player.setShape(CROSS)
+        self.move_count = 0
+        self.running = True
     
     def run(self):
         """ Cheesy main loop"""
@@ -20,8 +39,10 @@ class Game:
 
     def randStart(self):
         """ Randomly choose who gets to go first """
-        pass
-
+        ap = self.players[choice((0,1))]
+        ip = self.players[1] if ap == self.players[0] else self.players[0]
+        return (ap, ip)
+    
     def checkGameOver(self):
         """ Check to see if the game is a draw or someone has won """
         pass
