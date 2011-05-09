b8b767064bf3628873c971ee1e317e5ce364d9a4
Paul <paul@ethos.com.cn>
2011-05-09 15:01:30 +0800

    storage class, quote-unquote constants and some helper decorators

---
+++ src/common.py
@@ -0,0 +1,35 @@
+'''
+Created on May 9, 2011
+
+@author: pmtemp
+'''
+EMPTY   = 0
+NOUGHT  = 1
+CROSS   = 2
+DRAW    = 3
+DEBUG   = True
+shape_map = {0: " ", 1 : 0, 2 : "x"}
+
+def singleton(cls):
+    """ Enforce single instance of decorated class """
+    instances = {}
+    def getInstance():
+        if not cls in instances.keys():
+            instances[cls] = cls()
+        return instances[cls]
+    return getInstance
+
+def debug(f):
+    """ Print a basic debug statement """
+    def wrapper(*args, **kwargs):
+        print "executing: ",f.func_name
+        f(*args, **kwargs)
+    return wrapper
+
+@singleton
+class Storage:
+    def __init__(self):
+        self._game_board = None
+        self._player_one = None
+        self._player_two = None
+        self._game_instance = None
