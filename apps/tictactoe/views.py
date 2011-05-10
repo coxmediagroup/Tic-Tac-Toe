ce09f222761644c5d2ee3c8892c589ed374120f6
Matthew Trump <matthew.trump@gmail.com>
2011-05-10 00:34:10 -0600

    removing extraneous alert upon receipt of ping confirmation from server

--- apps/tictactoe/views.py
+++ apps/tictactoe/views.py
@@ -4,7 +4,7 @@ from django.template import RequestContext, loader
 
 def greetings(request):
     t = loader.get_template("tictactoe/tictactoe.html")
-    return HttpResponse(t.render(RequestContext(request,{'gridwidth':50})))
+    return HttpResponse(t.render(RequestContext(request,{'gridwidth':50,'use_site_header':True})))
 
 def result(request):
     return HttpResponse("1");
