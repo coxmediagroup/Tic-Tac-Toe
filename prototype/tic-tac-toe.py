2a94e17c7c7a69d64dca4d9b4a4221b22166b644
Allan Davis <javaalley@gmail.com>
2011-05-13 17:13:54 -0400

    moved prototype game into directory

---
+++ prototype/tic-tac-toe.py
@@ -0,0 +1,19 @@
+#! /usr/bin/env python
+
+
+class Board(object):
+
+    def __init__(self):
+        self.__board = []
+
+
+class Player(object):
+    pass
+
+
+class ComputerPlayer(Player):
+    pass
+
+
+class Game(object):
+    pass
