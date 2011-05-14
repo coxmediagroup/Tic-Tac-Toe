051169d0aa589db06bc8ce354f1a7486700b5b0f
Allan Davis <javaalley@gmail.com>
2011-05-14 11:05:21 -0400

    Added move and get to board

--- prototype/tests.py
+++ prototype/tests.py
@@ -5,9 +5,13 @@ class Board_test(unittest.TestCase):
     def setUp(self):
         self.board = Board()
 
+    def test_board_size(self):
+        self.assertEquals(self.board.size(), 9)
 
-    def test_board(self):
-        self.assertEquals(self.board.size, 9)
+    def test_board_take_cell(self):
+        self.assertEquals(self.board.get_cell(0), None)
+        self.board.take_cell(0, "X")
+        self.assertEquals(self.board.get_cell(0), "X")
 
 if __name__ == "__main__":
     unittest.main()
--- prototype/tic_tac_toe.py
+++ prototype/tic_tac_toe.py
@@ -3,8 +3,23 @@
 
 class Board(object):
 
+    BOARD_SIZE = 9
+
     def __init__(self):
         self.__board = []
+        for i in range(Board.BOARD_SIZE):
+            self.__board.append(None)
+
+    def size(self):
+        return len(self.__board)
+
+    def take_cell(self, index, claim):
+        self.__board[index] = claim
+
+    def get_cell(self, index):
+        return self.__board[index]
+
+
 
 
 class Player(object):
