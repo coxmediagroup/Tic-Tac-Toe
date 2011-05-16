be55557e5dbb53bd937a3890fa97cc7fd74494d0
Allan Davis <javaalley@gmail.com>
2011-05-15 22:52:32 -0400

    got the index page displayed

--- game/views.py
+++ game/views.py
@@ -1,1 +1,39 @@
 # Create your views here.
+from models import BoardError, Board, Player, ComputerPlayer
+
+from django import forms
+from django.shortcuts import render, redirect
+
+MARKER_CHOICES = (("X", "X"), ("O", "O"),)
+MOVE_CHOICES = ("1","2","3","4","5", "6", "7", "8", "9", "C")
+
+class IntroForm(forms.Form):
+
+    name = forms.CharField(label="Please enter your name:")
+    marker = forms.ChoiceField(label= "Select which marker you would like to use:", choices = MARKER_CHOICES)
+
+class MoveForm(forms.Form):
+    move = forms.ChoiceField(label= "Select which area to place marker: ", choices=MOVE_CHOICES)
+
+def index(request):
+    params = {}
+    session = request.session
+    if request.method == 'POST':
+        form = IntroForm(request.POST)
+        if form.is_valid():
+            board = Board()
+            marker = form.cleaned_data["marker"]
+            name = form.cleaned_data["name"]
+            session["player"] = Player(marker, board, name)
+            if marker == "X":
+                computer_marker = "O"
+            else:
+                computer_marker = "X"
+            session["computer"] = ComputerPlayer(computer_marker, board)
+            session["board"] = board
+            redirect("board")
+    else:
+        form = IntroForm()
+    params["form"] = form
+    return render(request, "index.html", params )
+
--- urls.py
+++ urls.py
@@ -6,7 +6,7 @@ from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'tic_tac_toe.views.home', name='home'),
+    url(r'^$', 'tic_tac_toe.game.views.index', name='home'),
     # url(r'^tic_tac_toe/', include('tic_tac_toe.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
