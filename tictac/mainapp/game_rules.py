fe492073f978117f482ed2d568d3715d3c09e497
alex <ferrerdallas@gmail.com>
2011-05-17 15:11:27 -0500

    calc computer move

--- tictac/mainapp/game_rules.py
+++ tictac/mainapp/game_rules.py
@@ -1,5 +1,4 @@
-def  calc_computer_move(board):
-       return 4    
+import random
 
 def  calc_game_over(board):
        if board[0]==board[1]==board[2]!="_"  or \
@@ -14,4 +13,41 @@ def  calc_game_over(board):
           return True
        else: 
          return False
+         
+         
+def  calc_computer_move(board):
+       # brute force  for all posible moves :
+       #if next move makes *me* win, take it.
+       for x in range(0,8):
+          if board[x] == "_" :
+             temp = list(board)
+             temp[x] = "0"
+             if calc_game_over(temp):
+               print "rule win"
+               return x     
+       
+       # brute force  for all posible moves :
+       #if next move makes him win take it
+       for x in range(0,8):
+          print "(",x,")",
+          if board[x] == "_" :
+             temp = list(board)
+             temp[x] = "X"
+             print x,temp
+             if calc_game_over(temp):
+               print "rule not loose"
+               return x
+          print "skip"
+          
+       #if center not taken, take it.
+       if board[4] == "_":
+          print "rule center if not taken"
+          return 4    
 
+     #otherwise, randomly take first open space
+       while True:
+          i = random.randrange(0,8)
+          if board[i] == "_" :
+             print "rule random"
+             return i 
+     
