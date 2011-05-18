130f96390938afcb4ee14c05c511e904f5719cf6
Rob Misiorowski <rmisio@yahoo.com>
2011-05-17 22:55:51 -0600

    Each cell of the grid can be updated with an X or O.

--- tictactoe/views.py
+++ tictactoe/views.py
@@ -2,7 +2,20 @@ from django.template import Context, loader
 from django.http import HttpResponse
 
 def start(request):
+    if request.method == 'GET':
+        print request.GET.keys()
+        
+    EMPTY = 'not-selected'
+    USER = 'user-selected'
+    COMPUTER = 'computer-selected'
+    
+    # prime the board with empty selections
+    #board = [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY]
+    board = [EMPTY, COMPUTER, EMPTY, USER, USER, COMPUTER, USER, COMPUTER, EMPTY]
+    
     t = loader.get_template('home.html')
-    c = Context({})
+    c = Context({
+        'board': board,
+    })
     
     return HttpResponse(t.render(c))
