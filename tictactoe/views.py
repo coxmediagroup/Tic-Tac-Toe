978e5fec35702d3a5e1b9770f68b5cb942672e83
Rob Misiorowski <rmisio@yahoo.com>
2011-05-19 19:31:31 -0600

    Reworked AI logic to use minimax algorythm.

--- tictactoe/views.py
+++ tictactoe/views.py
@@ -3,124 +3,149 @@ from django.http import HttpResponse
 from django.core.cache import cache
 from random import choice
 
-def is_winner(p, b):
-    # p is for player (USER or COMPUTER);
-    # b is for the board
+class Computer:
+    MAX = 'MAX'
+    MIN = 'MIN'
     
-    return ((b[0] == p and b[1] == p and b[2] == p) or # across the top
-        (b[3] == p and b[4] == p and b[5] == p) or # across the middle
-        (b[6] == p and b[7] == p and b[8] == p) or # across the bottom
-        (b[0] == p and b[3] == p and b[6] == p) or # down the left side
-        (b[1] == p and b[4] == p and b[7] == p) or # down the middle
-        (b[2] == p and b[5] == p and b[8] == p) or # down the right
-        (b[0] == p and b[4] == p and b[8] == p) or # left diagonal
-        (b[2] == p and b[4] == p and b[6] == p)) # right diagonal
+    def make_move(self, board_instance):
+        move_pos, result = self.max_move(board_instance)
+        board_instance.set_mark(move_pos, Board.COMPUTER)
 
-def is_board_full(board, EMPTY):
-    for i in range(9):
-        if board[i] == EMPTY:
-            return false
-    
-    return true
+    def max_move(self, board_instance):
+        best_result = None
+        best_move = None
         
-def get_computer_move(board, USER, COMPUTER, EMPTY):
-    # logic for counter to opening move
-    user_moves = 0
-    
-    for i in range(9):
-        if board[i] == USER:
-            user_moves += 1
-
-    if user_moves == 1:
-        # special strategy to counter opening move
-        if board[0] == USER or board[2] == USER or \
-            board[6] == USER or board[8] == USER: 
-            # opening move was a corner
-            return 4
-        elif board[4] == USER:
-            # opening move was the center
-            return choice([0, 2, 6, 8])
-        else: # it was an edge opening
-            if board[1] == USER:
-                return choice([4, 2, 0, 7])
-            elif board[3] == USER:
-                return choice([4, 6, 0, 5])
-            elif board[5] == USER:
-                return choice([4, 8, 2, 3])
-            else: # board[7]
-                return choice([4, 8, 6, 1])
+        for m in board_instance.get_open_spaces():
+            board_instance.set_mark(m, Board.COMPUTER)
                 
-    # the remaining logic is for non-opening move counter... (i.e. the user has
-    # made at least two moves)
-    # see if a winning move is possible
-    for i in range(9):
-        board_copy = board[:]
-        if board_copy[i] == EMPTY:
-            board_copy[i] = COMPUTER
-        if is_winner(COMPUTER, board_copy):
-            return i
+            if board_instance.is_game_over():
+                result = self.get_result(board_instance)
+            else:
+                move_pos, result = self.min_move(board_instance)
 
-    # check if player could win on the next move and block them
-    for i in range(9):
-        board_copy = board[:]
-        if board_copy[i] == EMPTY:
-            board_copy[i] = USER
-            if is_winner(USER, board_copy):
-                return i
-                
-    # take a random corner, if it's free
-    corners = [0, 2, 6, 8]
-    while corners:
-        random_corner = choice(corners)
-        if board[random_corner] == EMPTY:
-            return random_corner
-        else:
-            corners = [value for value in corners[:] if value != random_corner]
+            board_instance.revert_last_move()
             
-    # take the center, if it's free
-    if board[4] == EMPTY:
-        return 4
+            if best_result == None or result > best_result:
+                best_result = result
+                best_move = m
+
+        return best_move, best_result
         
-    # randomly choose one of the remaining side space    
-    sides = [1, 3, 5, 7]
-    while sides:
-        random_side = choice(sides)
-        if board[random_side] == EMPTY:
-            return random_side
-        else:
-            sides = [value for value in sides[:] if value != random_side]
-    
-def start(request):
+    def min_move(self, board_instance):
+        best_result = None
+        best_move = None
+        
+        for m in board_instance.get_open_spaces():
+            board_instance.set_mark(m, Board.USER)
+                
+            if board_instance.is_game_over():
+                result = self.get_result(board_instance)
+            else:
+                move_pos, result = self.max_move(board_instance)
+
+            board_instance.revert_last_move()
+            
+            if best_result == None or result < best_result:
+                best_result = result
+                best_move = m
+
+        return best_move, best_result        
+        
+    def get_result(self, board_instance):
+        if board_instance.is_game_over():
+            if board_instance.winner == Board.COMPUTER:
+                return 1
+            elif board_instance.winner == Board.USER:
+                return -1
+        
+        return 0
+        
+class Board:
     EMPTY = 'not-selected'
     USER = 'user-selected'
     COMPUTER = 'computer-selected'
     
-    EMPTY_BOARD = [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY]
-    
-    selected_cell = request.GET.get('selected_cell', None)
+    EMPTY_BOARD = [EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY, EMPTY]    
+
+    def __init__(self, board = None):
+        if board:
+            self.board = board
+        else:
+            self.board = Board.EMPTY_BOARD
+            
+        self.moves = []
+        self.winner = None
+
+    def set_mark(self, position, user):
+        self.board[position] = user
+        self.moves.append(position)
         
+        b = self.board
+        
+        if (b[0] == b[1] == b[2] and not b[0] == self.EMPTY):
+            self.winner = b[0]
+        elif (b[3] == b[4] == b[5] and not b[3] == self.EMPTY):
+            self.winner = b[3]
+        elif (b[6] == b[7] == b[8] and not b[6] == self.EMPTY):
+            self.winner = b[6]            
+        elif (b[0] == b[3] == b[6] and not b[0] == self.EMPTY):
+             self.winner = b[0]            
+        elif (b[1] == b[4] == b[7] and not b[1] == self.EMPTY):
+             self.winner = b[1]            
+        elif (b[2] == b[5] == b[8] and not b[2] == self.EMPTY):
+             self.winner = b[2]            
+        elif (b[0] == b[4] == b[8] and not b[0] == self.EMPTY):
+             self.winner = b[0]            
+        elif (b[2] == b[4] == b[6] and not b[2] == self.EMPTY):
+             self.winner = b[2]                        
+        
+    def revert_last_move(self):
+        self.board[self.moves.pop()] = Board.EMPTY
+        self.winner = None
+    
+    def get_open_spaces(self):
+        open_spaces = []
+        
+        for i in range(9):
+            if self.board[i] == Board.EMPTY:
+                open_spaces.append(i)
+        
+        return open_spaces
+        
+    def is_game_over(self):
+        if self.winner:
+            return True
+        else:
+            for i in range(9):
+                if self.board[i] == Board.EMPTY:
+                    return False
+        
+        return True     
+
+def start(request):
+    computer = Computer()
+    selected_cell = request.GET.get('selected_cell', None)
+    current_board = cache.get('board')
+    
+    if current_board and selected_cell:
+        board_instance = Board(current_board)
+    else:
+        board_instance = Board()        
+
     if request.method == 'GET' and selected_cell:
         # ensure the value is not already set (i.e. refresh button was pressed
         # with the GET value in the URL)        
-        current_board = cache.get('board')
-        if current_board and not current_board[int(selected_cell)] in (USER, COMPUTER):
+        if not board_instance.board[int(selected_cell)] in (Board.USER, Board.COMPUTER):
             #update the current board with the user's move and a follow-up move from the computer
-            current_board[int(selected_cell)] = USER
-            current_board[get_computer_move(current_board, USER, COMPUTER, EMPTY)] = COMPUTER
-            board = current_board
-            cache.set('board', board)
-        else: # move already made - return the same board
-            board = current_board
-    else:
-        # prime the board with empty selections
-        board = EMPTY_BOARD 
-        
-        # store the board in cache
-        cache.set('board', board)
+            board_instance.set_mark(int(selected_cell), Board.USER)
+            computer.make_move(board_instance)
+      
+    # store the board in cache
+    cache.set('board', board_instance.board)
     
     t = loader.get_template('home.html')
     c = Context({
-        'board': board,
+        'board': board_instance.board,
     })
     
     return HttpResponse(t.render(c))
