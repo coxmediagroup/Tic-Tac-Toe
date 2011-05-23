0b46824563e27d8c4aa241dc986c91f89541e9a1
James Robey <circlecycle@gmail.com>
2011-05-22 23:02:00 -0400

    major update to move logic, changing the order in which moves are evaluated for win, very important, and the order in which those possible moves compete, also important

--- TicTacToe3DField.py
+++ TicTacToe3DField.py
@@ -35,9 +35,21 @@ class TicTacToe3DField:
         ],
     ]
     
-    #a list of all possible winning vectors through the 3x3x3 cube
+    #a list of all possible winning vectors through the 3x3x3 cube, in specific order! (diagonals first)
     vectors_to_check = (
         #2D vectors ------------
+        
+        #diagonals_board_0_2d
+        ((0, 0, 0), (1, 1, 0), (2, 2, 0)),
+        ((0, 2, 0), (1, 1, 0), (2, 0, 0)),
+
+        #diagonals_board_1_2d
+        ((0, 0, 1), (1, 1, 1), (2, 2, 1)),
+        ((0, 2, 1), (1, 1, 1), (2, 0, 1)),
+
+        #diagonals_board_2_2d
+        ((0, 0, 2), (1, 1, 2), (2, 2, 2)),
+        ((0, 2, 2), (1, 1, 2), (2, 0, 2)),
 
         #left_to_right_board_0_2d
         ((0, 0, 0), (1, 0, 0), (2, 0, 0)),
@@ -69,35 +81,8 @@ class TicTacToe3DField:
         ((1, 0, 2), (1, 1, 2), (1, 2, 2)),
         ((2, 0, 2), (2, 1, 2), (2, 2, 2)),
 
-        #diagonals_board_0_2d
-        ((0, 0, 0), (1, 1, 0), (2, 2, 0)),
-        ((0, 2, 0), (1, 1, 0), (2, 0, 0)),
-
-        #diagonals_board_1_2d
-        ((0, 0, 1), (1, 1, 1), (2, 2, 1)),
-        ((0, 2, 1), (1, 1, 1), (2, 0, 1)),
-
-        #diagonals_board_2_2d
-        ((0, 0, 2), (1, 1, 2), (2, 2, 2)),
-        ((0, 2, 2), (1, 1, 2), (2, 0, 2)),
-
-        #3d Vectors ------------
-
-        #up_to_down_left_to_right_top_3d
-        ((0, 0, 0), (0, 0, 1), (0, 0, 2)),
-        ((1, 0, 0), (1, 0, 1), (1, 0, 2)),
-        ((2, 0, 0), (2, 0, 1), (2, 0, 2)),
-
-        #up_to_down_left_to_right_middle_3d
-        ((0, 1, 0), (0, 1, 1), (0, 1, 2)),
-        ((1, 1, 0), (1, 1, 1), (1, 1, 2)),
-        ((2, 1, 0), (2, 1, 1), (2, 1, 2)),
-
-        #up_to_down_left_to_right_bottom_3d
-        ((0, 2, 0), (0, 2, 1), (0, 2, 2)),
-        ((1, 2, 0), (1, 2, 1), (1, 2, 2)),
-        ((2, 2, 0), (2, 2, 1), (2, 2, 2)),
-
+        #3D Vectors ------------
+        
         #up_to_down_diagonal_top_to_bottom_3d
         ((0, 0, 0), (0, 1, 1), (0, 2, 2)),
         ((1, 0, 0), (1, 1, 1), (1, 2, 2)),
@@ -121,6 +106,21 @@ class TicTacToe3DField:
         #up_to_down_diagonal_cross_center_3d
         ((0, 0, 0), (1, 1, 1), (2, 2, 2)),
         ((0, 2, 2), (1, 1, 1), (2, 0, 2)),
+
+        #up_to_down_left_to_right_top_3d
+        ((0, 0, 0), (0, 0, 1), (0, 0, 2)),
+        ((1, 0, 0), (1, 0, 1), (1, 0, 2)),
+        ((2, 0, 0), (2, 0, 1), (2, 0, 2)),
+
+        #up_to_down_left_to_right_middle_3d
+        ((0, 1, 0), (0, 1, 1), (0, 1, 2)),
+        ((1, 1, 0), (1, 1, 1), (1, 1, 2)),
+        ((2, 1, 0), (2, 1, 1), (2, 1, 2)),
+
+        #up_to_down_left_to_right_bottom_3d
+        ((0, 2, 0), (0, 2, 1), (0, 2, 2)),
+        ((1, 2, 0), (1, 2, 1), (1, 2, 2)),
+        ((2, 2, 0), (2, 2, 1), (2, 2, 2)),
     )
     
     def __init__(self, game=None):
@@ -140,9 +140,9 @@ class TicTacToe3DField:
             two, or a2 one if there are no ones.
         """
         
-        score_of_ones = []
-        score_of_twos = []
         score_of_neg_twos = []
+        score_of_twos = []
+        score_of_ones = []
         score_of_neg_ones = []
         for vector in self.vectors_to_check:
             #note that the array is [board, y, x] while vectors are x, y, board
@@ -155,57 +155,64 @@ class TicTacToe3DField:
             #if we find they can make a winning move next
             elif score == -2:   
                 score_of_neg_twos.append(vector)
-                
+             
             #if we find somewhere beneficial for us to block opponent
             elif score == -1:    
-                score_of_neg_ones.append(vector)
-            
+                score_of_neg_ones.append(vector)   
+                
             #if we find somewhere beneficial for us to go
-            elif score == -1:    
-                score_of_neg_ones.append(vector)        
+            elif score == 1:    
+                score_of_ones.append(vector)
 
         #analyze the scores of each vector evaluated, looking for the best match
+        vector_to_evaluate = None
+        moveToMake = None
         
-        if score_of_neg_twos:
-            vector_to_evaluate = score_of_neg_twos[0]
-        elif score_of_twos:
+        #any wins?
+        if score_of_twos:
+            print "score_of_twos"
             vector_to_evaluate = score_of_twos[0]
+            
+        #are they about to win?
+        elif score_of_neg_twos:
+            print "score_of_neg_twos"
+            vector_to_evaluate = score_of_neg_twos[0]
+            
         elif score_of_neg_ones:
-            vector_to_evaluate = score_of_neg_ones[0]
-        elif score_of_ones:
-            vector_to_evaluate = score_of_ones[0]
-        else:
-            vector_to_evaluate = None
+            print "score_of_neg_twos"
+            vector_to_evaluate = score_of_neg_twos[0]
             
-        #given we have the "best" vector, now determine in which spot in the 
-        #vector we must go.
+        elif score_of_one:
+            print "score_of_neg_twos"
+            vector_to_evaluate = score_of_one[0]
             
-        moveToMake = (1, 1, 1)
-        #if we have detected a good move
-        if vector_to_evaluate:
-            for i, point in enumerate(vector_to_evaluate):
-                if self.game[point[2]][point[1]][point[0]] == 0:
-                    moveToMake = point
-                    
-        #if there are none, pick a good one (move to center, or move to some open place)
+        #make the first open move we find. Apparently, we're not having any fun yet
         else:
+            print "center and misc logic"
             #center of middle board open?
             if self.game[1][1][1] == 0:    moveToMake = (1, 1, 1)
-            
+
             #center of top board open?
             elif self.game[0][1][1] == 0:  moveToMake = (1, 1, 0)
-            
+
             #center of bottom board open?
             elif self.game[2][1][1] == 0:  moveToMake = (1, 1, 2)
             
-            #make the first open move we find. Apparently, we're not having any fun yet
             else:
-                for z in self.game:
-                    for y in z:
-                        for x in y:
-                            if y[x] == 0: moveToMake = (x, y, z)
+                for z, a in enumerate(self.game):
+                    for y, b in enumerate(b):
+                        for x, c in enumerate(c):
+                            if c == 0: moveToMake = (x, y, z)
+                    
+    
+        #if we have detected a good move
+        if not moveToMake and vector_to_evaluate:
+            for i, point in enumerate(vector_to_evaluate):
+                if self.game[point[2]][point[1]][point[0]] == 0:
+                    moveToMake = point     
+            
+        print "vector_to_evaluate, moveToMake", vector_to_evaluate, moveToMake
                             
-        
         self.game[moveToMake[2]][moveToMake[1]][moveToMake[0]] = 1
         return self.game
 
