8c5c0b7d75b7c580886b1041086e3200896a2eb2
Lee Trout <lee@leetrout.com>
2011-05-26 21:35:33 -0400

    throwing down a quick foundation

--- xsos.py
+++ xsos.py
@@ -2,3 +2,17 @@ class Grid(object):
     """
     The grid for playing X's & O's (tic tac toe)
     """
+    # marks as constants
+    X = 'X'
+    O = 'O'
+    # the actual grid- blank at start
+    grid = [
+        ['','',''],
+        ['','',''],
+        ['','',''],
+    ]
+    
+    def __init__(self):
+        pass
+    
+    
