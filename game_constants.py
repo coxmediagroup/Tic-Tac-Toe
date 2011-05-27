6af62722a192603f8723786e16e382f5d4bb5906
Alex Kagioglu <alex.kagioglu@gmail.com>
2011-05-27 07:03:45 -0700

    computer player is working.

---
+++ game_constants.py
@@ -0,0 +1,12 @@
+player_symbol_converter = {0: ' ', 1: 'X', 2: 'O'}
+coordinate_representation_reverse_map = {
+    'a1': (0,0),
+    'a2': (0,1),
+    'a3': (0,2),
+    'b1': (1,0),
+    'b2': (1,1),
+    'b3': (1,2),
+    'c1': (2,0),
+    'c2': (2,1),
+    'c3': (2,2),
+}
