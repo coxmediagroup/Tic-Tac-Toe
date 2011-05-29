2011d6f53106b993a4fda04cb786088975fe241d
Jeremy Self <finder83@gmail.com>
2011-05-29 11:10:38 -0400

    Make it so that the game ends when the computer wins

--- tictactoe/media/control.js
+++ tictactoe/media/control.js
@@ -1,5 +1,9 @@
 $('document').ready(function() {
+    var game_over = false;
     $('td').click(function () {
+        if (game_over == true) {
+            return false;
+        }
         var td = $(this);
         if (td.html().indexOf('X') == -1 && td.html().indexOf('O') == -1) {
             td.html("<h1>" + window.AVATAR + "</h1>");
@@ -22,10 +26,12 @@ $('document').ready(function() {
             }
 
             if (res['condition'] == 'Tie') {
+                game_over = true;
                 msg.css('text-color', 'black').html('Tie!');
                 msg.show();
             }
             else if (res['condition'] == 'Win') {
+                game_over = true;
                 msg.css('text-color', 'black');
                 msg.html('Player ' + res['avatar'] + ' won!');
                 msg.show();
