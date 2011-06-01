fb11e49e733d935ac1b42647f644c1c10258961f
ben robinson <ben@gnotek.com>
2011-06-01 15:52:32 -0700

    cleaned up AI + better reporting, some logical looping bugs open bug:  check for no winner and no available moves, notify and exit

--- tictactoe.py
+++ tictactoe.py
@@ -6,6 +6,7 @@ class Grid:
         #create a dictionary with 9 members
         self.squares = [' ']*9
         self.moves = [0,1,2,3,4,5,6,7,8]
+        self.moved = False
         
     #obtain all available moves left  
     def getAvailableMoves(self):
@@ -26,6 +27,7 @@ class Grid:
         move = raw_input("Enter your move: (available: %s)" % (self.moves))
         selection = int(move)
         self.squares[selection] = 'X';
+        self.moved = False
         self.AnalyzeBoard()
         self.ShowBoard()
         self.PresentMoves()
@@ -47,33 +49,43 @@ class Grid:
         for item in row:
             #print 'item is %s' % item
             if self.squares[item] == ' ':
-                self.squares[item] = 'O'    
+                self.squares[item] = 'O' 
+                print 'computer made a strategy move on square %s ' % item
+                self.moved = True
+                #print 'filled item %s' % item 
+            
                 
+    #basic randomly generated bot move
     def BotMove(self):
         index = random.choice(self.moves)
-        print 'selcted index %s for computer move' % index
+        print 'computer made a random move on square %s ' % index
         self.squares[index] = 'O'       
         
     def AnalyzeBoard(self):
+        #update available moves 
+        self.getAvailableMoves()
         rows = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
         computer_moved = False
         for row in rows:
             count = self.CheckRow(row)
             #print count['player']
             if(count['player'] == 3):
+                self.ShowBoard();
                 print 'You have won!'
                 sys.exit()
             #if computer is about to win, finish the game
-            if count['bot'] == 2 and computer_moved == False:
+            if count['bot'] == 2 and self.moved == False:
                 self.FillRow(row)
-                computer_moved = True
-            if count['player'] == 2 and computer_moved == False:
+                print 'computer wins!'
+                sys.exit()
+            if count['player'] == 2 and self.moved == False:
+                print 'found 2 x\'s in this row %s ' % row
                 #prevent player from winning the row only if turn hasn't been taken
                 self.FillRow(row)
-                computer_moved = True
         #if computer still hasn't gone, make a move
-        if computer_moved == False:
-            self.BotMove()    
+        #print 'computer still hasnt moved yet??? %s ' % self.moved
+        if self.moved == False:
+            self.BotMove()  
                            
 game = Grid()
 game.PresentMoves()
