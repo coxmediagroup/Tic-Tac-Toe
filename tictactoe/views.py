18cfdf8c97bde78493cd93ebc3b3e92316ccda66
Lee Trout <lee@leetrout.com>
2011-06-03 01:31:13 -0400

    cleaned up grid display logic and divided between view and template as appropriate

--- tictactoe/views.py
+++ tictactoe/views.py
@@ -4,14 +4,18 @@ from django.template.response import TemplateResponse
 from django.core.urlresolvers import reverse
 import xsos
 
+SIZE = 3
+EMPTY = [[0]*SIZE]*SIZE
 def default(request):
     if 'xsos_game' not in request.session:
-        request.session['xsos_game'] = xsos.Grid()
+        request.session['xsos_game'] = xsos.Grid(SIZE)
     g = request.session['xsos_game']
     if 'reset' in request.GET:
         g.reset()
         request.session['xsos_game'] = g
-    if 'set_mark' in request.GET and g.grid == [[0]*3]*3:
+        del request.session["player_mark"]
+        return HttpResponseRedirect(reverse("default"))
+    if 'set_mark' in request.GET and g.grid == EMPTY:
         m = int(request.GET['set_mark'])
         if m in g.marks:
             request.session["player_mark"] = m
@@ -24,37 +28,35 @@ def play(request):
         return HttpResponseRedirect(reverse("default"))
     g = request.session['xsos_game']
     mark = request.session["player_mark"]
-    grid = '<table>'
-    size = g.size
-    for r in range(size):
-        grid += "<tr>" 
-        for c in range(size):
-            grid += "<td>"
-            cell_mark = g.grid[r][c]
-            if g.grid[r][c]:
-                grid += g.marks[cell_mark]
-            else:
-                grid += '<a href="' + reverse("move") + '?row=' + str(r) + \
-                "&col=" + str(c) + "&mark="+ str(mark) +'">_</a>'
-            grid += "</td>"
-        grid += "</tr>"    
-    grid += "</table>"
+    if mark == 2 and g.grid == EMPTY:
+        move_url = reverse("move") + "?cmp=True"
+        return HttpResponseRedirect(move_url)
+    # convert grid marks from int to str for the template
+    grid = [[g.marks[c] if c else '' for c in r] for r in g.grid]
     return TemplateResponse(request, 'tictactoe/play.html', {'grid': grid, \
-        'game':g})
+        'game':g, 'mark':mark})
 
 def move(request):
     q = request.GET.copy()
-    if 'xsos_game' not in request.session:
-        return HttpResponseBadRequest("Game not initialized")
+    if ('xsos_game' or 'player_mark') not in request.session:
+        return HttpResponseRedirect(reverse("default"))
+    g = request.session['xsos_game']
+    opmark = g._op(int(request.session["player_mark"]))
+    if 'cmp' in q:
+        g.move(opmark)
+        request.session['xsos_game'] = g
+        return HttpResponseRedirect(reverse("play"))
     if 'row' not in q or 'col' not in q or 'mark' not in q:
         return HttpResponseBadRequest("Missing parameters")
-    g = request.session['xsos_game']
     r = int(q["row"])
     c = int(q["col"])
     m = int(q["mark"])
     if m in g.marks:
         if not g.grid[r][c]:
             g.grid[r][c] = m
+            over, winner = g.game_over()
+            if not over:
+                g.move(opmark)
             request.session['xsos_game'] = g
             return HttpResponseRedirect(reverse("play"))
     else:
