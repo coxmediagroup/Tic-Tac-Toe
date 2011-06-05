53af6fc216ba791aff720d091ec86ede6bda9691
Intchanter <unlearned@gmail.com>
2011-06-04 23:16:38 -0600

    Allowed human player to make a move.

--- game.py
+++ game.py
@@ -6,24 +6,38 @@ def main():
     import os, sys
     b = board.Board()
     players = board.players
-    current_player = 0
+    player_number = 0
     winner = None
     movers = (
         get_human_move,
         get_human_move,
     )
-    while b.available_moves() and not winner:
-        current_move = movers[current_player]()
-        b.move(players[current_player], current_move)
+    possible_moves = b.available_moves()
+    while possible_moves and not winner:
+        player = players[player_number]
+        current_move = movers[player_number](b, player, possible_moves)
+        b.move(player, current_move)
         winner = b.winner()
-        current_player = (current_player + 1) % 2
+        player_number = (player_number + 1) % 2
+        possible_moves = b.available_moves()
+    print b
     if winner:
         print 'The winner is: %s' % winner
     else:
         print 'There was no winner.'
 
-def get_human_move():
-    raise NotImplementedError('Please implement get_human_move().')
+def get_human_move(board, player, possible_moves):
+    while True:
+        print board
+        try:
+            move = int(raw_input('Please enter your move %s: ' % possible_moves))
+        except ValueError:
+            print 'Please enter a number for your move.'
+            continue
+        if move not in possible_moves:
+            print 'Sorry, %s is not available.  Please try again.' % move
+            continue
+        return move
 
 def get_computer_move():
     raise NotImplementedError('Please implement get_computer_move().')
