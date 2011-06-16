685a18e844fc068751d878d16992a42121da05cc
Matthew McClelland <matthew@nuwork.com>
2011-06-16 18:22:38 -0400

    rework of tictactoe2.py to make unbeatable. computer plays first

--- tictactoe2.py
+++ tictactoe2.py
@@ -13,6 +13,9 @@ def checkwinner(wincombos, check):
         if combo.count(check) == 3:
             status = 1
             break
+        elif combo.count(check) == 2 and check == "X":
+            if combo.count("O") > 0:
+                status = 2
         else:
             status = 0
 
@@ -37,7 +40,7 @@ def make_play(board, spaces, wincombos, player, n):
         symbol = "O"
     # Check if block is used.
     while not is_valid_move(spaces, n):
-        print "\nInvalid position"
+        print "\n%s: Invalid position" % player
         n = get_player_input()
 
     # Remove block from spaces array
@@ -56,30 +59,58 @@ def make_play(board, spaces, wincombos, player, n):
     return board, status
 
 def evalPossibleMoves(spaces, wincombos, player):
-    win_found = False
+    outcomes = [[], [], []]
+    test_spaces = spaces[:]
     for s in spaces:
+        test_spaces.remove(s)
         test_wc = []
         for e in wincombos:
             if e != s:
                 test_wc.append(e)
             else:
                 test_wc.append(player)
-        #print test_wc
         status = checkwinner(test_wc, player)
-        if status == 1:
-            win_found = s
-    return win_found
+        outcomes[status].append(s)
+        recheck = evalPossibleMoves(test_spaces, test_wc, player)
+        if recheck[1]:
+            outcomes[2].append(s)
+    outcomes_cleaned = []
+    for o in outcomes:
+        outcomes_cleaned.append(list(set(o)))
+    return outcomes_cleaned
 
 def get_computer_input(board, spaces, wincombos):
     human_can_win = evalPossibleMoves(spaces, wincombos, "X")
     computer_can_win = evalPossibleMoves(spaces, wincombos, "O")
-    if computer_can_win:
-        return computer_can_win
-    if human_can_win:
-        return human_can_win
+    #print "hcw: %s" % human_can_win
+    #print "ccw: %s" % computer_can_win
+    if computer_can_win[1]:
+        # make winning move for computer
+        return choice(computer_can_win[1])
+    if human_can_win[1]:
+        # block winning move of human
+        return choice(human_can_win[1])
+    if computer_can_win[2]:
+        selection = find_elev_space(spaces, computer_can_win[2])
+    elif human_can_win[2]:
+        selection = find_elev_space(spaces, human_can_win[2])
+    else:
+        selection = find_elev_space(spaces, [])
+    return selection
+
+def find_elev_space(spaces, choices):
+    corners = [s for s in spaces if s in [1, 3, 7, 9]]
     if 5 in spaces:
         return 5
-    return choice(spaces)
+    elif len(corners) == 2:
+        return choice([s for s in spaces if s in [2,4,6,8]])
+    elif corners:
+        for e in corners:
+            if e in choices:
+                return e
+        return choice(corners)
+    else:
+        return choice(spaces)
 
 def get_player_input():
     try:
@@ -91,14 +122,12 @@ def get_player_input():
 
 def playttt():
     """main game play function"""
-    #print "NOTE: Numbers correspond to board play positions, left to right, top to bottom."
     board = " 1 | 2 | 3\n-----------\n 4 | 5 | 6\n-----------\n 7 | 8 | 9"
     wincombos=[1,2,3,4,5,6,7,8,9,1,4,7,2,5,8,3,6,9,1,5,9,3,5,7]
     spaces=range(1,10)
-    print "\n"+board
 
     while True:
-        player = len(spaces)%2 +1
+        player = len(spaces)%2
         if player == 1:
             player = 2
             position = get_computer_input(board, spaces, wincombos)
