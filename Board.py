233bbe3434fb0f704eea2cf8a375ce975ef005f2
Daniel Avila <sparkytwobillion@gmail.com>
2011-06-19 22:40:18 -0700

    Discovered small bug in Board fixed

--- Board.py
+++ Board.py
@@ -7,6 +7,15 @@ class Board(object):
         self.tokens = [None] * 9
 
     def addToken(self, token, idx):
+        """Add a token to the board or raise an exception
+
+        str token  "x" or "o"
+        int idx    0-8"""
+
+        if  idx > 8 or idx < 0:
+            msg = "Invalid index %s " % idx
+            raise TokenPlacementException(msg)
+
         if not self.tokens[idx]:
             self.tokens[idx] = token
         else:
