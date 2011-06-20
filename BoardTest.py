2f60ade2a21db428709417a9f621fbfd267963dc
Daniel Avila <sparkytwobillion@gmail.com>
2011-06-19 22:40:47 -0700

    BoardTests never got checked in and were wiped out. Rebuilding.

--- BoardTest.py
+++ BoardTest.py
@@ -1,15 +1,36 @@
 import unittest
+import random
 import Board
 
+from TicTacToeExceptions import *
 
-class TestBoard(unittest.TestCase):
+class BoardTest(unittest.TestCase):
     
     
     def setUp(self):
-        self.board = Board()
+        self.board = Board.Board()
 
     def tearDown(self):
-        self.board = null
+        self.board = None
 
 
 
+
+    def test_addToken(self):
+
+        """Can we add a token to a known empty space?"""
+        local_board = []
+        for i in range(9):
+            ran = random.randint(8,9)
+            token = ("x", "o")[ran % 2] 
+            self.board.addToken(token, i)
+            local_board.append(token)
+        self.assertEqual(local_board, self.board.getBoard())
+
+    def test_upperIndexRaisesException(self):
+        """Does an index higher than expected raise a TokenPlacementException?"""
+        self.assertRaises(TokenPlacementException, self.board.addToken, "x", 9)
+
+    def test_lowerIndexRaisesException(self):
+        """Does an index lower than expected raise a TokenPlacementException?"""
+        self.assertRaises(TokenPlacementException, self.board.addToken, "x", -1)
