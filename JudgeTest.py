33f9e6b309dd42cd7098eb42327ba10ae158769e
Daniel Avila <sparkytwobillion@gmail.com>
2011-06-19 23:15:02 -0700

    Reimplemented JudgeTests.

--- JudgeTest.py
+++ JudgeTest.py
@@ -1,17 +1,69 @@
 import unittest
 import Judge
+import Board
 
-
-class TestJudge(unittest.TestCase):
+class JudgeTest(unittest.TestCase):
     
     
     def setUp(self):
-        self.board = Board()
-        self.judge = Judge()
 
-    def tearDown(self):
-        self.board = null
-        self.judge = null
+        self.winners = [['x', 'x', 'x', None, None, None, None, None, None]
+                       ,[None, None, None, 'x', 'x', 'x', None, None, None]
+                       ,[None, None, None, None, None, None, 'x', 'x', 'x']
+                       ,['x', None, None, 'x', None, None, 'x', None, None]
+                       ,[None, 'x', None, None, 'x', None, None, 'x', None]
+                       ,[None, None, 'x', None, None, 'x', None, None, 'x']
+                       ,['x', None, None, None, 'x', None, None, None, 'x']
+                       ,[None, None, 'x', None, 'x', None, 'x', None, None]]
 
 
+    def test_Winners(self):
+        """Do all winning boards evaluate as a winner?"""
 
+        for w in self.winners:
+            board = Board.Board()
+            board.tokens = w
+            judge = Judge.Judge(board)
+            result = judge.isWinner()
+            self.assertEqual(result, "x")
+
+    def test_notWinnerNotDone(self):
+        """Do we get the correct response for an unfinished game?"""
+        tokens = ['x', 'o', None, None, None, None, None, None, None]
+        board = Board.Board()
+        board.tokens = tokens
+        judge = Judge.Judge(board)
+        result = judge.isWinner()
+        self.assertEqual(result, None)
+
+    def test_draw(self):
+        """Do we get the correct response for a draw game?"""
+        tokens = ['x', 'o', 'x', 'o', 'x', 'o', 'o', 'x', 'o']
+        board = Board.Board()
+        board.tokens = tokens
+        judge = Judge.Judge(board)
+        result = judge.isWinner()
+        self.assertEqual(result, None)
+
+    def test_evalGame(self):
+        """Is the game still going?"""
+        board = Board.Board()
+        judge = Judge.Judge(board)
+        result = judge.evalGame()
+        self.assertEqual(result, None)
+
+    def test_evalGameWinner(self):
+        """Is the game Over with a winner?"""
+        board = Board.Board()
+        board.tokens = ['x', 'x', 'x', 'o', 'x', 'o', 'o', 'x', 'o']
+        judge = Judge.Judge(board)
+        result = judge.evalGame()
+        self.assertEqual(result, ['x','done'])
+
+    def test_evalGameDraw(self):
+        """Is the game Over with a draw?"""
+        board = Board.Board()
+        board.tokens = ['x', 'o', 'x', 'o', 'x', 'o', 'o', 'x', 'o']
+        judge = Judge.Judge(board)
+        result = judge.evalGame()
+        self.assertEqual(result, [None,'done'])
