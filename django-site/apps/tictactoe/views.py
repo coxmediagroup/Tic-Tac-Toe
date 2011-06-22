eb1dd3d2623f14e83e3ae2a13012a0c36d558a01
grasshoppersunshine <joseph.n.king@gmail.com>
2011-06-22 02:53:13 -0600

    coded skeleton ajax interface for tictactoe app views (time for bed...zzzzzzz...)

--- django-site/apps/tictactoe/views.py
+++ django-site/apps/tictactoe/views.py
@@ -14,3 +14,14 @@ def newgame(request, xo):
         }
     
     return HttpResponse(json.dumps(response), mimetype="application/json")
+
+def makemove(request, x, y):
+    # check if spot is already taken
+    # yes? return error
+    # no? place mark, save in session, and check for win
+    return HttpResponse(json.dumps({'success':False,'message':'Not yet implemented.'}, mimetype="application/json"))
+
+def getmove(request):
+    # calculate next move based on session data
+    # place mark, save in session, and check for win
+    return HttpResponse(json.dumps({'success':False,'message':'Not yet implemented.'}, mimetype="application/json"))
