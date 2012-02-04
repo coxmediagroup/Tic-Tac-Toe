01d784973c42b0d4948e2b9a2105aecda8f7db36
Russell Bernhardt <russell.bernhardt@gmail.com>
2012-02-04 14:45:10 -0800

    Added some textual UI, with clearing the screen (not sure if it works in Windows yet), taking and validating the user input, applying the play, and generating a move. At this point the win flag isn't tripping. Should write that testXOwinEndGame test now...

--- ui.py
+++ ui.py
@@ -5,3 +5,59 @@
 screen.
 """
 
+import os
+from engine import TTTEngine, TTTError
+
+""" Slightly override TTTBoard to add UI methods.
+"""
+class GameUI(TTTEngine):
+    
+    """ Clears the screen and redraws the current board layout.
+    """ 
+    def redrawScreen(self):
+        # WARNING: untested on Windows:
+        _,cols = os.popen('stty size', 'r').read().split()
+        print '\n' * int(cols)
+        
+        # draw the gameboard
+        print '''
+           %s  |  %s  |  %s
+         -----------------
+           %s  |  %s  |  %s
+         -----------------
+           %s  |  %s  |  %s
+         ''' % tuple(self.board)
+
+
+# starter -- not ideal here, but not sure where else to put it yet.
+game = GameUI()
+game.redrawScreen()
+in_play = True
+while( in_play ):
+    var = raw_input(
+        'Enter the number of an open space or "q" to quit: '
+    )
+
+    if var == 'q':
+        print 'Thanks for playing!'
+        in_play = False
+        
+    elif var.isdigit() and len(var) == 1:
+        try:
+            game.applyMove( int(var) - 1 )
+        
+        except TTTError as e:
+            print e.message
+            _ = raw_input('Press ENTER to try again...')
+            
+        except TTTEndGame as e:
+            print e.message
+            in_play = False
+        
+        game.applyMove( game.getBestMove() )
+        game.redrawScreen()
+        
+    else:
+        # invalid input
+        print 'Please enter either a single digit or the letter "q"...'
+
