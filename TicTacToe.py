ce9a6fd44e28b0031fdaa4b46311cef1766cab48
Thomas Hjelm <thomas.edward.hjelm@gmail.com>
2012-02-04 12:50:43 -0600

    Added make_move() and move_possible() methods to TicTacToe class, plus unittests
    There are two new methods on the TicTacToe class.  One is move_possible()
    which takes a board position as an argument and determines if that
    position is free.  The other is make_move() which takes a player and board
    position as arguments and makes a move at the specified position for the
    player, if the move is possible.  Created unit tests testing these new
    methods.

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,8 +1,67 @@
 
 
 class TicTacToe(object):
+    """A simple class for creating unwinnable TicTacToe games.
+    
+    Class variables:
+    COMPUTER and HUMAN are constants used to index into instance
+    variables "squares" and "tokens".
+    
+    """
+    COMPUTER = 0
+    HUMAN = 1
+
     def __init__(self):
+        """Create victory conditions and clear the board."""
+        self.victories = [[0,1,2],[3,4,5],[6,7,8],[0,3,6],[1,4,7],[2,5,8],[0,4,8],[2,4,6]]
+        self.reset_board()
         print "You lose!"
         
+    def reset_board(self):
+        """(Re)set all properties to an initial pre-game state.
+        
+        Instance variables:
+        board -- an 9-element list representing the current board state
+        squares -- two lists representing squares claimed by the computer and human players
+        free_squares -- a list of available squares on the board
+        tokens -- our X and O. 
+        game_over -- a flag to keep track of the game state
+        board_control -- a flag indicating whose turn it is
+        turns -- number of turns that have elapsed
+        
+        """
+        self.board = ["."] * 9
+        self.squares = [[],[]]
+        self.free_squares = [x for x in range(9)]
+        self.tokens = ["x","o"]
+        self.game_over = False
+        self.board_control = 0
+        self.turns = 0
+        
+    def move_possible(self, pos):
+        """Determine if a particular square is free and return a boolean.
+        
+        Arguments:
+        pos -- the board position to check for vacancy
+        
+        """
+        return self.board[pos] == "."
+        
+    def make_move(self, player, pos):
+        """Make a move at pos for player.  Return True on success.
+        
+        Arguments:
+        player -- which player is moving.  Either self.COMPUTER or self.HUMAN
+        pos -- the board position player is attempting to move to
+        
+        """
+        if self.move_possible(pos):
+            self.board[pos] = self.tokens[player]
+            self.squares[player].append(pos)
+            self.free_squares.remove(pos)
+            self.turns = self.turns + 1
+            return True
+        return False
+
 if __name__ == '__main__':
     game = TicTacToe()
--- unittests.py
+++ unittests.py
@@ -18,5 +18,31 @@ class test_TicTacToe(unittest.TestCase):
         self.assertEqual(self.ttt.game_over, game_over)
         self.assertEqual(self.ttt.turns, turns)
         
+    def test_move_possible(self):
+        self.assertTrue(self.ttt.move_possible(3))
+        self.ttt.make_move(self.ttt.COMPUTER, 3)
+        self.assertFalse(self.ttt.move_possible(3))
+    
+    def test_make_move(self):
+        self.ttt.make_move(self.ttt.COMPUTER, 0)
+        self.ttt.make_move(self.ttt.HUMAN, 4)
+        
+        board = ["."] * 9
+        board[0] = "x"
+        board[4] = "o"
+        self.assertEqual(self.ttt.board, board)
+        
+        human = [4]
+        computer = [0]
+        self.assertEqual(human, self.ttt.squares[self.ttt.HUMAN])
+        self.assertEqual(computer, self.ttt.squares[self.ttt.COMPUTER])
+        
+        free_squares = [x for x in range(9)]
+        free_squares.remove(0)
+        free_squares.remove(4)
+        self.assertEqual(free_squares, self.ttt.free_squares)
+        
+        self.assertEqual(2, self.ttt.turns)
+        
 if __name__ == '__main__':
     unittest.main()
