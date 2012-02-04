bb7a4a655b1d6d20006236f1793b5334e34ef5a1
Russell Bernhardt <russell.bernhardt@gmail.com>
2012-02-03 17:58:43 -0800

    Built some basic test containers for what I'll likely need to test later.

--- tests.py
+++ tests.py
@@ -5,3 +5,32 @@
 """ This module contains all the unit tests.
 """ 
 
+import unittest
+
+class UnitTests(unittest.TestCase):
+    def setUp(self):
+        pass
+        
+    def testInvalidMove(self):
+        # tests to make sure a known invalid moves are caught
+        pass
+        
+    def testValidMove(self):
+        # simulate a move; should not result in errors and move counter would
+        # increment
+        pass
+        
+    def testXWinEndGame(self):
+        # simulate a game where X wins
+        pass
+        
+    def testOWinEndGame(self):
+        # simulate a game where O wins
+        pass
+        
+    def testStalemateEndGame(self):
+        # simulate a game where a stalemate occurs
+        pass
+        
+suite = unittest.TestLoader().loadTestsFromTestCase(UnitTests)
+unittest.TextTestRunner(verbosity=2).run(suite)
