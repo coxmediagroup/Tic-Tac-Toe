eeeded89951a49ed9f4e371c32f5a939499394d8
Spencer Chastain <sechastain@FoeHammer.launchmodem.com>
2012-02-07 12:09:28 -0500

    Basic game model created

--- game/models.py
+++ game/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    is_user_x    = models.BooleanField(default=True)
+    started      = models.DateTimeField(auto_now_add=True)
+    ended        = models.DateTimeField(null=True, default=None)
+
+class Board(models.Model):
+    game         = models.OneToOneField(Game)
+    upper_left   = models.SmallIntegerField(default=0)
+    upper_center = models.SmallIntegerField(default=0)
+    upper_right  = models.SmallIntegerField(default=0)
+    center_left  = models.SmallIntegerField(default=0)
+    center       = models.SmallIntegerField(default=0)
+    center_right = models.SmallIntegerField(default=0)
+    lower_left   = models.SmallIntegerField(default=0)
+    lower_center = models.SmallIntegerField(default=0)
+    lower_right  = models.SmallIntegerField(default=0)
+    last_played  = models.DateTimeField(auto_now=True)
+
