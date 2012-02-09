5d9182365330d3994698415d1332ff244c92ed7d
Spencer Chastain <sechastain@gmail.com>
2012-02-09 12:26:01 -0500

    An unbeatble computer\!\!\!

--- game/views.py
+++ game/views.py
@@ -80,13 +80,34 @@ def move(request, game_id):
             if move is None:
                 move = game.winning_move(for_player=player)
                 if move is None:
+                    # if there are no winning moves
                     if game[1][1] == PLAYER_NONE:
+                        # take the center if it's available
                         move = (1,1)
                     else:
+                        # since the center is taken,
+                        # look for the other player's token
+                        # and play next to it
                         for x,y in ((x,y) for y in range(0,3) for x in range(0,3)):
-                            if game[x][y] == PLAYER_NONE:
-                                move = (x,y)
-                                break
+                            if game[x][y] == player:
+                                if x == 1 and y == 1:
+                                    continue #ignore the center
+                                elif x < 2 and game[x+1][y] == PLAYER_NONE:
+                                    move = (x+1,y)
+                                    break
+                                elif y < 2 and game[x][y+1] == PLAYER_NONE:
+                                    move = (x,y+1)
+                                    break
+                                elif x > 0 and game[x-1][y] == PLAYER_NONE:
+                                    move = (x-1,y)
+                                    break
+                                elif y > 0 and game[x][y-1] == PLAYER_NONE:
+                                    move = (x,y-1)
+                                    break
+                        else:
+                            # we have arrived here because only 
+                            # the center is occupied
+                            move = (0,0)
             assert move is not None
             game[move[0]][move[1]] = comp
     
