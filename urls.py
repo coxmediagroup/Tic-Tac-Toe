030cf4132944f9c42145393be77db1d33747810f
Van Gale <vangale@ggtechnica.com>
2012-02-08 20:01:15 -0700

    First URLconf; hook up admin, lazysignup, and our new game app

--- urls.py
+++ urls.py
@@ -1,17 +1,19 @@
+from django.contrib import admin
 from django.conf.urls.defaults import patterns, include, url
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.views.generic.simple import direct_to_template, redirect_to
 
-# Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 't.views.home', name='home'),
-    # url(r'^t/', include('t.foo.urls')),
-
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+    url(r'^convert/', include('lazysignup.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^game/', include('game.urls', namespace='game')),
+    url(r'^$',
+        redirect_to,
+        { 'url': '/game/', 'permanent': True },
+        name='home'),
+    #url(r'^$', direct_to_template, {'template': 'index.html'}, "home"),
 )
+
+urlpatterns += staticfiles_urlpatterns()
