f918da7e83dd50c0a2c371de7508c8a79ae49caf
Bobby R. Ward <bobbyrward@gmail.com>
2012-02-09 13:38:37 -0600

    Add add_move to board

--- tests/test_board.py
+++ tests/test_board.py
@@ -6,3 +6,20 @@ def test_board_creation():
     board = tictactoe.Board()
 
 
+def test_add_move():
+    board = tictactoe.Board()
+    board.add_move(pos=(1,1), player=tictactoe.PLAYER_X)
+
+    try:
+        board.add_move(pos=(1,1), player=tictactoe.PLAYER_O)
+    except tictactoe.MoveNotAvailable:
+        pass
+    else:
+        assert False, 'Board allowed 2 moves at the same position'
+
+    try:
+        board.add_move(pos=(0,0), player='Unknown')
+    except tictactoe.InvalidPlayerError:
+        pass
+    else:
+        assert False, 'Board allowed a move by an unknown player'
--- tictactoe/__init__.py
+++ tictactoe/__init__.py
@@ -5,28 +5,58 @@ PLAYER_O = 'circle'
 PLAYER_X = 'cross'
 
 
+class MoveNotAvailable(Exception):
+    """Thrown when trying to make a move on a tile that was already played on"""
+    pass
+
+
+class InvalidPlayerError(Exception):
+    """Thrown when making a move to player other than PLAYER_O or PLAYER_X"""
+    pass
+
+
 class Board(object):
     """The Tic-Tac-Toe board"""
 
     def __init__(self):
-        pass
+        self.board = [
+                [None, None, None],
+                [None, None, None],
+                [None, None, None],
+            ]
 
-    def has_winner(self):
-        """Returns true if a player has won"""
-        return False
+        self.size = 3
 
     def get_winner(self):
-        """Returns the winning player if a player has won, otherwise None"""
-        return 'cross'
+        """Returns true if a player has won"""
+        # no winner found
+        return None
+
+    def get_move_at_position(self, pos):
+        """Returns the move at the specified position"""
+
+        if pos[0] >= self.size or pos[1] >= self.size:
+            raise IndexError()
+
+        return self.board[pos[0]][pos[1]]
 
     def add_move(self, pos, player):
         """Adds a move to the board at position `pos` by player `player`"""
-        pass
+
+        current_move_at_pos = self.get_move_at_position(pos)
+
+        if current_move_at_pos is not None:
+            raise MoveNotAvailable()
+
+        if player not in (PLAYER_X, PLAYER_O):
+            raise InvalidPlayerError()
+
+        self.board[pos[0]][pos[1]] = player
 
 
 class AIPlayer(object):
     """An AI Tic-Tac-Toe player
-    
+
     Guaranteed to always at least stalemate
     """
 
@@ -36,4 +66,3 @@ class AIPlayer(object):
     def get_next_move(self, board):
         """Returns the position of the next move"""
         pass
-
