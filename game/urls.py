0ae7c52e0ec999340374983466c6e9888430c83a
Van Gale <vangale@ggtechnica.com>
2012-02-10 04:30:21 -0700

    Add url's for starting new games

--- game/urls.py
+++ game/urls.py
@@ -4,6 +4,7 @@ from django.conf.urls.defaults import *
 from django.views.generic.simple import direct_to_template
 
 urlpatterns = patterns('game.views',
+    url(r'^new/X/$', 'new_X', name='newX'),
+    url(r'^new/O/$', 'new_O', name='newO'),
     url(r'^$', 'index', name='index'),
-    url(r'^current/$', 'index', name='current'),
 )
