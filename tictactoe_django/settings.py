a1700848d322d77a821f63f8954af49020d93ee4
Donald Morrone <larrik@gmail.com>
2012-02-12 22:28:27 -0500

    Cleaned up settings a bit.
    Change the ROOT_DIR when deploying.

--- tictactoe_django/settings.py
+++ tictactoe_django/settings.py
@@ -1,5 +1,8 @@
 # Django settings for tictactoe_django project.
 
+# Change this to be the actual root directory
+ROOT_DIR = '/media/MainData/Websites/tictactoe_django/tictactoe_django/'
+
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
@@ -9,24 +12,6 @@ ADMINS = (
 
 MANAGERS = ADMINS
 
-DATABASES = {
-    #'default': {
-    #    'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'postgresql', 'mysql', 'sqlite3' or 'oracle'.
-    #    'NAME': '',                      # Or path to database file if using sqlite3.
-    #    'USER': '',                      # Not used with sqlite3.
-    #    'PASSWORD': '',                  # Not used with sqlite3.
-    #    'HOST': '',                      # Set to empty string for localhost. Not used with sqlite3.
-    #    'PORT': '',                      # Set to empty string for default. Not used with sqlite3.
-    #}
-}
-
-# Local time zone for this installation. Choices can be found here:
-# http://en.wikipedia.org/wiki/List_of_tz_zones_by_name
-# although not all choices may be available on all operating systems.
-# On Unix systems, a value of None will cause Django to use the same
-# timezone as the operating system.
-# If running in a Windows environment this must be set to the same as your
-# system time zone.
 TIME_ZONE = None
 
 # Language code for this installation. All choices can be found here:
@@ -47,7 +32,7 @@ USE_L10N = True
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = '/media/MainData/Websites/tictactoe_django/tictactoe_django/static/'
+STATIC_ROOT = ROOT_DIR + 'static/'
 
 # URL prefix for static files.
 # Example: "http://media.lawrence.com/static/"
@@ -58,7 +43,6 @@ STATIC_URL = '/static/'
 STATICFILES_FINDERS = (
     'django.contrib.staticfiles.finders.FileSystemFinder',
     'django.contrib.staticfiles.finders.AppDirectoriesFinder',
-#    'django.contrib.staticfiles.finders.DefaultStorageFinder',
 )
 
 # Make this unique, and don't share it with anybody.
@@ -68,7 +52,6 @@ SECRET_KEY = 'qf!5yqr7bg--ml4cq07uwu!cyo-z*u)n+y#dh(o%)@-5nj6#-)'
 TEMPLATE_LOADERS = (
     'django.template.loaders.filesystem.Loader',
     'django.template.loaders.app_directories.Loader',
-#     'django.template.loaders.eggs.Loader',
 )
 
 SESSION_ENGINE = 'django.contrib.sessions.backends.file'
@@ -76,26 +59,16 @@ SESSION_ENGINE = 'django.contrib.sessions.backends.file'
 MIDDLEWARE_CLASSES = (
     'django.middleware.common.CommonMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
-    #'django.middleware.csrf.CsrfViewMiddleware',
-    #'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'tictactoe_django.tictactoe.middleware.GameSetupMiddleware'
 )
 
 ROOT_URLCONF = 'tictactoe_django.urls'
 
-TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
-    '/media/MainData/Websites/tictactoe_django/tictactoe_django/tictactoe/templates/'
-)
+TEMPLATE_DIRS = (ROOT_DIR + 'tictactoe/templates/')
 
 INSTALLED_APPS = (
-    #'django.contrib.auth',
-    #'django.contrib.contenttypes',
     'django.contrib.sessions',
-    #'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'tictactoe_django.tictactoe'
