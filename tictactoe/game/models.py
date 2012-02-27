59cbb381f72d12631863d235be0190a1cceffa79
unknown <Bluewolf@Bluewolf-2011PC.(none)>
2012-02-27 02:06:03 -0500

    Started adding functionality for the CPU movement based upon the Minimax algorithm.

--- tictactoe/game/models.py
+++ tictactoe/game/models.py
@@ -6,5 +6,103 @@ class TicTacToeModel(models.Model):
     gameID = models.AutoField(primary_key=True)
     sessionID = models.CharField(max_length=255, help_text="Used to prevent the user from affecting other games by form editing.")
     playerCharacter = models.CharField(max_length=1, help_text="Determines if they are X or O.")
+    cpuCharacter = models.CharField(max_length=1)
     boardSize = models.IntegerField()
     gameBoard = PickledObjectField()
+    winner = ' '
+
+    def putPlayerMove(self,row,col):
+        '''Set the player move'''
+        self.gameBoard[row][col] = self.playerCharacter
+
+    def putCPUMove(self,row,col):
+        '''Set the cpu move'''
+        self.gameBoard[row][col] = self.cpuCharacter
+
+    def clearMove(self,row,col):
+        '''Clear the current place on the board'''
+        self.gameBoard[row][col] = ' '
+
+    def calculateCPUMove(self):
+        '''Calculate and do the CPU Move'''
+        move_position, score = self.getMaximizedMove()
+        self.putCPUMove(move_position[0], move_position[1])
+
+    def getMaximizedMove(self):
+        ''' Find maximized move'''
+        bestScore = None
+        bestMove = None
+
+        for row_index, row_value in enumerate(self.gameBoard):
+            for column_index, column_value in enumerate(row_value):
+                if column_value == ' ':
+                    #Mark the position as now taken by the CPU
+                    self.putCPUMove(row_index, column_index)
+
+                    #Check opponent move possibilities
+                    if self.checkGameOver():
+                        score = self.calculateScore()
+                    else:
+                        move_position,score = self.getMinimizedMove()
+
+                    #Revert back from the move
+                    self.clearMove(row_index, column_index)
+
+                    if bestScore == None or score > bestScore:
+                        bestScore = score
+                        bestMove = [row_index, column_index]
+
+        return bestMove, bestScore
+
+    def getMinimizedMove(self):
+        ''' Find minimized move'''
+
+        bestScore = None
+        bestMove = None
+        for row_index, row_value in enumerate(self.gameBoard):
+            for column_index, column_value in enumerate(row_value):
+                if column_value == ' ':
+                    self.putPlayerMove(row_index, column_index)
+
+                    if self.checkGameOver():
+                        score = self.calculateScore()
+                    else:
+                        move_position,score = self.getMaximizedMove()
+
+                    #Revert back from the move
+                    self.clearMove(row_index, column_index)
+
+                    if bestScore == None or score < bestScore:
+                        bestScore = score
+                        bestMove = [row_index, column_index]
+
+        return bestMove, bestScore
+
+    def checkGameOver(self):
+        '''Check to see if we have a winner'''
+        for row_index, row_value in enumerate(self.gameBoard):
+
+            #Check for a horizontal winner
+            if row_value.count(' ') == 0:
+                self.winner = row_value[0]
+                return True
+
+            #Other win conditions later
+        return False
+
+    def calculateScore(self):
+        '''Get the score of the result'''
+        if self.checkGameOver():
+            if self.winner  == self.cpuCharacter:
+                return 1 # CPU Won
+            elif self.winner == self.playerCharacter:
+                return -1 # Person won
+        return 0 # Draw
+
+
+
+
+
+
+
+
--- tictactoe/game/views.py
+++ tictactoe/game/views.py
@@ -1,11 +1,9 @@
 from django.shortcuts import render
+from django.core.urlresolvers import reverse
+
 from tictactoe.game import forms as gameforms
 from tictactoe.game.models import TicTacToeModel
 from tictactoe.responses import HttpResponseSeeOtherRedirect
-
-from django.core.urlresolvers import reverse
-
-
 
 def index(request):
     '''This is the initial view that lists the few navigation options that this
@@ -22,10 +20,25 @@ def createGame(request):
         if sizeForm.is_valid():
             boardSize = int(request.POST['boardSize'])
 
+            #Setup the game board
+            gameBoard = []
+            for outer_value in range(0,boardSize):
+                inner_loop = []
+                for inner_value in range(0,boardSize):
+                    inner_loop.append(' ')
+                gameBoard.append(inner_loop)
+
+            #Get CPU character
+            if request.POST['playerCharacter'] == 'X':
+                cpuCharacter = 'O'
+            else:
+                cpuCharacter = 'X'
+
             game = TicTacToeModel.objects.create(
                 boardSize = boardSize,
-                gameBoard = [[' '] * boardSize,] * boardSize,
+                gameBoard = gameBoard,
                 playerCharacter = request.POST['playerCharacter'],
+                cpuCharacter = cpuCharacter,
                 sessionID = request.session.session_key
             )
             game.save()
@@ -34,14 +47,33 @@ def createGame(request):
 
             return render(request, 'game/playGame.html', {'gameObj': game, 'gameForm': gameForm, 'sizeForm': sizeForm})
 
+    # Redirect to the index if something is wrong. TODO: Add a message about this to the template?
     return HttpResponseSeeOtherRedirect(reverse('site-index'))
 
 def doMove(request):
     '''This is the initial view placeholder for the game itself.
     '''
 
-    if(request.method == 'POST' and gameforms.TicTacToeForm(request.POST).is_valid()):
-        game = TicTacToeModel(gameID=request.POST["gameID"], sessionID=request.session.session_key)
-        gameForm = gameforms.TicTacToeForm(initial={'gameID': game.gameID})
-        game.save()
-        return render(request, 'game/playGame.html', {'gameObj': game, 'gameForm': gameForm})
+    if(request.method == 'POST'):
+        gameForm = gameforms.TicTacToeForm(request.POST)
+        if gameForm.is_valid():
+            print request.POST['tictacBoxSelection']
+            game = TicTacToeModel.objects.get(gameID=request.POST["gameID"], sessionID=request.session.session_key)
+            sizeForm = gameforms.BoardSizeForm(initial={'boardSize': game.boardSize, 'playerCharacter':game.playerCharacter})
+
+            row, sep, col = request.POST['tictacBoxSelection'].partition(',')
+            game.putPlayerMove(int(row), int(col))
+
+            game.calculateCPUMove()
+
+            print game.gameID
+            print game.gameBoard
+            #TODO: Check that game was loaded.
+
+            game.save()
+
+            return render(request, 'game/playGame.html', {'gameObj': game, 'gameForm': gameForm, 'sizeForm': sizeForm})
+
+
+    # Redirect to the index if something is wrong. TODO: Add a message about this to the template?
+    #return HttpResponseSeeOtherRedirect(reverse('site-index'))
