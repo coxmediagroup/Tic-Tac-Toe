f290d723cab7864ac081841b841ff71ae9b34ba4
InternetFett <internetfett@gmail.com>
2012-09-30 01:03:24 -0500

    add gameboard form

--- tictactoe/game/views.py
+++ tictactoe/game/views.py
@@ -1,4 +1,13 @@
 from django.views.generic import TemplateView
 
 class GameView(TemplateView):
-    template_name = "base.html"
+    template_name = "game.html"
+    
+    def do_move(self, request, **kwargs):
+        # do game logic
+        context = super(GameView, self).get_context_data(**kwargs)
+        context['message'] = 'Invalid move!'
+        return context
+    
+    def post(self, request, **kwargs):
+        return self.render_to_response(self.do_move(request, **kwargs))
