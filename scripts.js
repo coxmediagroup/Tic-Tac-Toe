35112dacb00540d5be75502e23da35aa76c0f3e1
Mark Ransom <megamark16@gmail.com>
2012-10-24 13:38:24 +0000

    Fixing another logic loophole that can allow the player to win

--- scripts.js
+++ scripts.js
@@ -63,13 +63,37 @@ function take_turn(event) {
 // Handle taking a turn as the computer
 function auto_play() {
     var rows = get_game_state();
+
+    // Check each of the possible lines of cells and see if any of them
+    // have two in a row. It's possible to have more than one such line
+    var blocking_cells = [];
     for (index in rows) {
         var row = rows[index];
         if (Math.abs(row) > 1) {
            var blocking_cell = lines[index].filter('[value="0"]'); 
-           $(blocking_cell).click();
-           return;
+           blocking_cells.push({ 'cell': blocking_cell, 'row': row });
         }
+    }
+    if (blocking_cells.length == 1) {
+        $(blocking_cells[0].cell).click();
+        return;
+    }
+    // If there are two lines with two in a row, block or win 
+    // but win above block, otherwise you could end up losing
+    else if (blocking_cells.length > 1) {
+        // Loop through the lines with two in a row and find the one
+        // that would cause a win for the computer
+        for (index in blocking_cells) {
+            var row = blocking_cells[index].row;
+            if (row < 0) { // O's are -1, X's are 1
+                $(blocking_cells[index].cell).click();
+                return
+            }
+        }
+        // Do both winning possibilities belong to the player?
+        // Seems unlikely, but just in case, block the first one
+        $(blocking_cells[0].cell).click();
+        return;
     }
     // If the player starts with a corner, take center
     if ($('#5').attr('value') == "0") {
@@ -86,6 +110,12 @@ function auto_play() {
     // Get a random corner, unless the other player started in a corner,
     // in which case get the opposite corner, otherwise they will win
     var corners = {1:9, 3:7, 9:1, 7:3};
+    var adjacent_cells_map = {
+        "1": $("#2,#4"), 
+        "3": $("#2,#6"),
+        "7": $("#4,#8"),
+        "9": $("#8,#6")
+    };
     for (corner in corners) {
         var other_corner = corners[corner];
         if ($('#' + corner + '[value="1"], #' + other_corner + '[value="0"]').length == 2) {
@@ -102,7 +132,19 @@ function auto_play() {
         .sort(function() { 
                 return Math.round(Math.random())-.5;
             });
+    // So, generally taking the center when possible and from there 
+    // taking corners is a good strategy, if the player doesn't take 
+    // center and the computer does, and then the computer's next move
+    // is not adjacent to one of the player's moves, the player can win
     if (available_corners.length > 0) {
+        for (index in available_corners) {
+            var available_corner = available_corners[index];
+            var adjacent_cells = adjacent_cells_map[$(available_corner).attr('id')];
+            if (adjacent_cells.filter('[value="1"]').length > 0) {
+                $(available_corner).click();
+                return;
+            }
+        }
         $(available_corners[0]).click();
     }
     else {
