3d81a8f3bd9748c06f0782a50959167d8219837b
Andrew Schoen <andrew.schoen@gmail.com>
2012-11-11 12:52:23 -0600

    added a couple simple test cases for ComputerPlayerO.  Tests blocks and wins.

---
+++ tests.py
@@ -0,0 +1,45 @@
+import unittest
+
+from players import ComputerPlayerO, WINNING_MOVES
+
+
+def get_board(xes=[], oes=[]):
+    board = [{"has_x": False, "has_o": False} for x in range(9)]
+    for x in xes:
+        board[x]["has_x"] = True
+    for o in oes:
+        board[o]["has_o"] = True
+    return board
+
+
+class ComputerPlayerOTests(unittest.TestCase):
+
+    def setUp(self):
+        pass
+
+    def tearDown(self):
+        pass
+
+    def test_blocks_all_threatened_wins(self):
+        """tests if the player will give the correct move to block any
+        winning combination.
+        """
+        for win in WINNING_MOVES:
+            board = get_board(xes=win[0:2])
+            player = ComputerPlayerO(board, 2)
+            next_move = player.play(win[1])
+            self.assertEqual(next_move, win[2])
+
+    def test_takes_the_win(self):
+        """tests if the player will take any possible winning combination
+        if given the chance.
+        """
+        for win in WINNING_MOVES:
+            board = get_board(oes=win[0:2])
+            player = ComputerPlayerO(board, 3)
+            next_move = player.play(win[1])
+            self.assertEqual(next_move, win[2])
+
+
+if __name__ == "__main__":
+    unittest.main()
