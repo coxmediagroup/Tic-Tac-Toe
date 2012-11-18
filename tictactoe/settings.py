0fc350c4b31cecfbb00da78de0bcd2c466bd4f6e
Ben Spaulding <ben@benspaulding.us>
2012-11-18 10:47:05 -0600

    Add request and site context processors.

--- tictactoe/settings.py
+++ tictactoe/settings.py
@@ -57,9 +57,11 @@ TEMPLATE_CONTEXT_PROCESSORS = (
     'django.core.context_processors.debug',
     'django.core.context_processors.i18n',
     'django.core.context_processors.media',
+    'django.core.context_processors.request',
     'django.core.context_processors.static',
     'django.core.context_processors.tz',
     'django.contrib.messages.context_processors.messages',
+    'basic_extras.context_processors.site',
 )
 
 TEMPLATE_LOADERS = (
