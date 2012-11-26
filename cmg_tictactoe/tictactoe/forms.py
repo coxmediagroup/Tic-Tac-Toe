37bef4b9e735bef843f1464bf69ac7f72b18f6a8
Ben Spaulding <ben@benspaulding.us>
2012-11-25 19:01:15 -0600

    Upgrade version of basic-extras to get non-editable fields.

--- cmg_tictactoe/tictactoe/forms.py
+++ cmg_tictactoe/tictactoe/forms.py
@@ -1,4 +1,3 @@
 
-# FIXME: Create a form for the Game model that hides metadata fields.
 # FIXME: Create a form field for the grid that makes playing more intuitive
 # than the current counting characters and typing 'o'.
