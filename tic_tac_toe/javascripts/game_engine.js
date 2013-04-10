eb5934fe0de01a6c65db49dcc9f25eed48e1dcb9
surgemcgee <surgemcgee@Bust0ut13.04>
2013-04-09 20:43:07 -0400

    Game board built.

---
+++ tic_tac_toe/javascripts/game_engine.js
@@ -0,0 +1,52 @@
+/* Copyright 2013 Robert Edward Steckroth II <RobertSteckroth@gmail.com> Bust0ut, Surgemcgee
+
+* This program is free software; you can redistribute it and/or modify
+* it under the terms of the GNU Lesser General Public License as published by
+* the Free Software Foundation; version 3.
+*
+* This program is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU Lesser General Public License for more details.
+*
+* You should have received a copy of the GNU Lesser General Public License
+* along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
+
+var Board = function () {
+
+	this.ai_move = function (square) {
+	    this.player_moves[square] = true
+     };
+
+    this.player_moves = [false,false,false,false,false,false,false,false,false];
+
+};
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
