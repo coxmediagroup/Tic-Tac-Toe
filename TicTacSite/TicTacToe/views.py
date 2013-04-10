2d6767ba971ed6e1879ffbcf620a481fea541111
ZetaZeta <MatthewSCarson@gmail.com>
2013-04-09 23:57:41 -0400

    Setting up board to use GET to maintain board state.  If this were production, we'd probably use POST at least (and probably more security if we really cared about people cheating), but GET makes it easier to debug.

--- TicTacSite/TicTacToe/views.py
+++ TicTacSite/TicTacToe/views.py
@@ -1,5 +1,17 @@
 from django.shortcuts import render_to_response
 
 def index(request):
-    return render_to_response('tictactoe.html', {'board': [['x','x','o'],['o','o','x'],['x','x','o']]})
-    return HttpResponse("Tic Tac Toe enabled.")
+    # If we didn't get a board state, or got an invalid board state,
+    # then reset the board.
+    if "b" in request.GET and len(request.GET["b"]) == 9:
+        boardstring = request.GET["b"]
+    else:
+        boardstring = "---------"
+
+    # Divide the boardstring up into a proper 3x3 tic-tac-toe board.
+    board = []
+    board.append(boardstring[:3])
+    board.append(boardstring[3:6])
+    board.append(boardstring[6:9])
+    
+    return render_to_response('tictactoe.html', {'board': board})
