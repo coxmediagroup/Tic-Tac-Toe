6820437ea48d9b2ac3d9a4ecfc2fe1351dbc0382
charter <harterc1@live.com>
2013-04-18 10:34:29 -0500

    Added property to GameBoard to print a visual representation of the current state of the game; Refactored player input parsing

--- gameboard.py
+++ gameboard.py
@@ -2,24 +2,46 @@
 class GameBoard(object):
     def __init__(self, symbol):
         self._symbol = symbol
-        self._spaces = [[None] * 3] * 3
+        self._spaces = [[None] * 3 for i in xrange(3)]
     
     @property
     def moves_available(self):
         return True
+
+    @property
+    def winner(self):
+        return None
     
     def make_move(self, x, y, symbol):
-        pass
+        if self._spaces[x][y] is not None:
+            return False
+
+        self._spaces[x][y] = symbol
+        return True
     
     def move(self):
         pass
     
-    def display(self):
-        print 'TODO'
-    
+    @property
+    def display_str(self):
+        display =  '-' * 25 +'\n'
+        for y in xrange(3):
+            coordinate_row = '|'
+            symbol_row = '|'
+            for x in xrange(3):
+                coordinate_row += ' ({0},{1}) |'.format(x, y)
+                symbol = self._spaces[x][y]
+                symbol_row += '   {0}   |'.format(' ' if symbol is None else symbol)
+
+            display += '|       ' * 3 + '|\n'
+            display += '{0}\n{1}\n'.format(coordinate_row, symbol_row)
+            display += '|       ' * 3 + '|\n'
+            display += '-' * 25 + '\n'
+
+        return display
+
     def __str__(self):
         return '{0}(symbol={1}, spaces={2})'.format(self.__class__.__name__,
                                                     self._symbol,
                                                     self._spaces)
-
     __repr__ = __str__
--- tictactoe.py
+++ tictactoe.py
@@ -1,11 +1,13 @@
 import re
+import sys
 
 from gameboard import GameBoard
 
 MOVE_PATTERN = "^(q|\(?(?P<x>[0-2]),?(?P<y>[0-2])\)?)$"
 
-def collect_initial_game_data():
+def collect_player_symbol():
     print "Before we start, Would you like to be X's or O's? (x/o)"
+
     player_symbol = None
     while player_symbol is None:
         player_symbol = raw_input().upper().strip()
@@ -13,20 +15,36 @@ def collect_initial_game_data():
             print "'{0}' is not a valid decision.  Please enter 'x' or 'o'.".format(player_symbol)
             player_symbol = None
 
-    print 'Great! Would you like to have the first turn? (y/n)'
+    print 'Great!'
+    return player_symbol
 
-    player_turn = None
-    while player_turn is None:
+def collect_player_move():
+    print "It's your turn! To make a move, enter the x,y coordinates of space you want (ex. 0,2), or enter 'q' to quit at any time."
+
+    move = None
+    while not move:
+        move_input = raw_input().lower().strip()
+        move = re.match(MOVE_PATTERN, move_input)
+        if not move:
+            print "'{0}' is not a valid move. Please use x,y coordinates. For example: 0,2".format(move_input)
+
+    return move
+
+def collect_yes_or_no(question):
+    print '{0} (y/n)'.format(question)
+
+    answer = None
+    while answer is None:
         player_input = raw_input().lower().strip()
         if player_input in ('y', 'ye', 'yes'):
-            player_turn = True
+            answer = True
         elif player_input in ('n', 'no'):
-            player_turn = False
+            answer = False
         else:
             print "'{0}' is not a valid decision. Please enter 'y' or 'n'.".format(player_input)
-    
-    print "OK! Let's begin!"
-    return player_symbol, player_turn
+
+    print 'OK!'
+    return answer
 
 def start_game():
     print 'Welcome to Tic-Tac-Toe!'
@@ -34,26 +52,37 @@ def start_game():
     board = None
     while True:
         if not board or not board.moves_available:
-            player_symbol, player_turn = collect_initial_game_data()
+            player_symbol = collect_player_symbol()
+            player_turn = collect_yes_or_no('Would you like to have the first turn?')
             board = GameBoard('X' if player_symbol == 'O' else 'O')
+            print ("Let's begin!\n"
+                   "The image below is a view of the initial gameboard.  "
+                   "As you can see, there are no X's or O's yet.")
+            print board.display_str
 
         if player_turn:
-            print "It's your turn! To make a move, enter the x,y coordinates of space you want (ex. 0,2), or enter 'q' to quit at any time."
-            move = None
-            while not move:
-                move_input = raw_input().lower().strip()
-                move = re.match(MOVE_PATTERN, move_input)
-                if not move:
-                    print "'{0}' is not a valid move. Please use x,y coordinates. For example: 0,2".format(move_input)
+            move = collect_player_move()
             if move.string == 'q':
                 print "Thanks for playing!"
                 sys.exit(0)
 
-            board.make_move(move.group('x'), 
-                            move.group('y'), 
+            board.make_move(int(move.group('x')), 
+                            int(move.group('y')), 
                             player_symbol)
         else:
             board.move()
+        
+        print board.display_str
+        
+        if not board.moves_available:
+            if board.winner:
+                print 'Aww looks like you lost!'
+            else:
+                print 'Wow the game is a draw!'
+
+            if not collect_yes_or_no('Would you like to play again?'):
+                print "Thanks for playing!"
+                sys.exit(0)
 
         player_turn = not player_turn
 
