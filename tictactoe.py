c8101610b0e7e001f10bf21b8d524eb7545cd432
ronbraly <ronbraly@gmail.com>
2013-04-22 02:40:35 -0400

    Keep track of player moves

--- tictactoe.py
+++ tictactoe.py
@@ -1,7 +1,9 @@
 #Authored by: Ronald Braly
 
+PLAYER = ['X', 'O']
+
 def init_board():
-  board = {(0,0):'X',
+  board = {(0,0):'-',
            (0,1):'-',
            (0,2):'-',
            (1,0):'-',
@@ -19,16 +21,16 @@ def show_board(board):
       row_str = row_str + board[(i,j)]
     print row_str
 
-def get_input(board):
+def get_input(board, player):
   print "line 22"
   try:
-    row_coord = input("Enter Row (1,2, or 3)")
+    row_coord = input("Player %s: Enter Row (1,2, or 3)" %player)
     row_coord = int(row_coord) - 1
     if row_coord not in range(3):
       print "invalid input"
       return None
 
-    column_coord = input("Enter Column (1,2, or 3)")
+    column_coord = input("Player %s: Enter Column (1,2, or 3)" %player)
     column_coord = int(column_coord) - 1
     if column_coord not in range(3):
       print "invalid input"
@@ -45,17 +47,25 @@ def get_input(board):
   except:
     print "invalid input"
     return None
-  
+
+def update_board(board, player, move_coord):
+  board[move_coord] = player  
 
 def run():
 
+  move_coord = True
   board = init_board()
   show_board(board)
-  valid_input = True
-  while valid_input:
-    valid_input = get_input(board)
-    print valid_input
+  curr_turn = 0
+  curr_player = PLAYER[curr_turn%2]
 
+  while move_coord:
+    move_coord = get_input(board, curr_player)
+    print ''
+    update_board(board, curr_player, move_coord)
+    show_board(board)
+    curr_turn += 1
+    curr_player = PLAYER[curr_turn%2]
 
 if __name__=="__main__":
   run()
