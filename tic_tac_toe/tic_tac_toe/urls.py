86d91c437bfd54070acb1c564f17ee7624dda685
eikonomega <mike@eikonomega.com>
2013-04-23 19:59:11 -0400

    Create homepage app and modify default urls.py to point to homepage app on "site.com/" url.

--- tic_tac_toe/tic_tac_toe/urls.py
+++ tic_tac_toe/tic_tac_toe/urls.py
@@ -4,7 +4,7 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    #url(r'^$', 'user_management.views.log'),
+    url(r'^$', 'homepage.views.index'),
     url(r'^usermanagement/', include('user_management.urls')),
     #url(r'^userdirectory/', include('user_directory.urls')),
     #url(r'^analytics/', include('analytics.urls')),
