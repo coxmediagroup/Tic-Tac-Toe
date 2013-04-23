5b542c84689815628bf1ecab0ef9c4896d3c2bff
eikonomega <mike@eikonomega.com>
2013-04-23 13:40:59 -0400

    Add settings/development.py to .gitignore

--- tic_tac_toe/tic_tac_toe/settings/development.py
+++ tic_tac_toe/tic_tac_toe/settings/development.py
@@ -3,11 +3,11 @@ TEMPLATE_DEBUG = DEBUG
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'eikonomega',                      # Or path to database file if using sqlite3.
-        'USER': 'eikonomega',                      # Not used with sqlite3.
-        'PASSWORD': '',                  # Not used with sqlite3.
-        'HOST': 'localhost',                      # Set to empty string for localhost. Not used with sqlite3.
-        'PORT': '5432',                      # Set to empty string for default. Not used with sqlite3.
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'tic_tac_toe',
+        'USER': 'tic_tac_toe',
+        'PASSWORD': 'tic_tac_toe',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
