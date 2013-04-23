5340b5c1d3a3b821cc088ac41b9d12780d9e103e
eikonomega <mike@eikonomega.com>
2013-04-23 17:36:00 -0400

    Add registration, login, and login controllers.

---
+++ tic_tac_toe/user_management/urls.py
@@ -0,0 +1,9 @@
+from django.conf.urls import patterns, include, url
+
+urlpatterns = patterns('',
+    url(r'^$', 'user_management.views.index'),
+    url(r'^registration/$', 'user_management.views.user_registration'),
+    url(r'^login/$', 'django.contrib.auth.views.login', {'template_name': 'login.html'}),
+    url(r'^logout/', 'django.contrib.auth.views.logout', {'next_page': '/'}),
+     
+)
