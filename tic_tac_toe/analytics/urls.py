46e879adfebaeee3035de4333982aa68d707d0ce
eikonomega <mike@eikonomega.com>
2013-04-24 17:29:23 -0400

    Add versioning to API.  Reach the api via /analyics/api/v1/resourcename/?format=json.

--- tic_tac_toe/analytics/urls.py
+++ tic_tac_toe/analytics/urls.py
@@ -1,10 +1,11 @@
 from django.conf.urls import patterns, include, url
-
+from tastypie.api import Api
 from .api import EventResource
 
-event_resource = EventResource()
+v1_api = Api(api_name='v1')
+v1_api.register(EventResource())
 
 urlpatterns = patterns(
     '',
-    url(r'^api/', include(event_resource.urls)),
+    url(r'^api/', include(v1_api.urls)),
 )
