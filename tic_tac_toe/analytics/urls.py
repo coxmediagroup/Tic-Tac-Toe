89c91ed15dd61c390119d42d38ae0b893f3245c7
eikonomega <mike@eikonomega.com>
2013-04-24 16:31:27 -0400

    Add api.py to analytics app.  Uses tastypie to make analytics data publicly available.

--- tic_tac_toe/analytics/urls.py
+++ tic_tac_toe/analytics/urls.py
@@ -1,5 +1,9 @@
 from django.conf.urls import patterns, include, url
 
+from .api import EventResource
+
+event_resource = EventResource()
+
 urlpatterns = patterns(
     '',
     url(r'^api/', include(event_resource.urls)),
