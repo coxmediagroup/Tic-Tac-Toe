7ffed690d7f24612855143b20d6fd1388b348d94
eikonomega <mike@eikonomega.com>
2013-04-26 22:24:37 -0400

    Create 'TIC_TAC_TOE_START' analytics event in game().

--- tic_tac_toe/tic_tac_toe_game/views.py
+++ tic_tac_toe/tic_tac_toe_game/views.py
@@ -1,11 +1,25 @@
 from django.template import loader
 from django.template.context import RequestContext
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
+
+from analytics.models import Event
 
 
 def game(request):
+
+    
+
     template = loader.get_template('game.html')
     context = RequestContext(request)
+
+    analytics_event = Event(
+        event_type='TIC_TAC_TOE_START',
+        event_url=request.path,
+        event_model=request.user.__class__.__name__,
+        event_model_id=request.user.id
+    )
+    analytics_event.save()
+
     return HttpResponse(template.render(context))
 
 
