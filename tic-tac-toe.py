30d91c3ed8193473e645f1d30d504ec0dbc169cb
Michael Orr <michael@orr.co>
2013-05-12 14:03:46 -0400

    Setting up basic structure and empty classes

---
+++ tic-tac-toe.py
@@ -0,0 +1,24 @@
+class Game(object):
+    """Represents the game, maintains game board and manages game loop"""
+
+    def __init__(self):
+        pass
+
+    def run(self):
+        print "Welcome to TIC TAC TOE!"
+
+
+class Board(object):
+    """Represents current state of game board and provides functional access"""
+    def __init__(self):
+        pass
+
+
+class Square(object):
+    """Represents one location on the game board"""
+    def __init__(self):
+        pass
+
+if __name__ == '__main__':
+    game = Game()
+    game.run()
