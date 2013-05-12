b4bf6f4c56f53fb0c25f1669b0720ca4ff5fc6db
Michael Orr <michael@orr.co>
2013-05-12 15:42:00 -0400

    setting up the default board and squares, need to finish setting up players before game sequencing can happen

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -1,23 +1,42 @@
 class Game(object):
     """Represents the game, maintains game board and manages game loop"""
-
     def __init__(self):
-        pass
+        print "Welcome to Tic Tac Toe!"
+        self.board = Board()
+        self.init_players()
+
+    def init_players(self):
+        selection = ''
+        prefix = ''
+        suffix = '--> Would you like to play first or second? (1 or 2): '
+        while selection not in ('1', '2'):
+            selection = raw_input('%s%s' % (prefix, suffix))
+            prefix = '(Invalid Input) '
 
     def run(self):
-        print "Welcome to TIC TAC TOE!"
+        print "Game is starting..."
 
 
 class Board(object):
     """Represents current state of game board and provides functional access"""
-    def __init__(self):
-        pass
+    def __init__(self, size=3):
+        self.size = size
+        self.squares = [Square(x, y) for x in range(size) for y in range(size)]
+
+    def __str__(self):
+        """
+        Print a human-friendly version of the board
+        to be shown between turns
+        """
+        return ''.join("(%d,%d)" % (i.x, i.y) for i in self.squares)
 
 
 class Square(object):
     """Represents one location on the game board"""
-    def __init__(self):
-        pass
+    def __init__(self, x, y, value=''):
+        self.x = x
+        self.y = y
+        self.value = value
 
 if __name__ == '__main__':
     game = Game()
