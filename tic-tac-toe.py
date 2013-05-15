9aaa9c6cc388c36e305a0a1e63d9686a6e91b591
Michael Orr <michael@orr.co>
2013-05-15 00:47:47 -0400

    adding player setup, turn sequencing init, needs turn logic to do anything, needs docstrings

--- tic-tac-toe.py
+++ tic-tac-toe.py
@@ -12,9 +12,43 @@ class Game(object):
         while selection not in ('1', '2'):
             selection = raw_input('%s%s' % (prefix, suffix))
             prefix = '(Invalid Input) '
+        if selection == '1':
+            self.human = Human(symbol='X')
+            self.computer = Computer(symbol='O')
+            self.next_player = self.human
+        else:
+            self.human = Human(symbol='O')
+            self.computer = Computer(symbol='X')
+            self.next_player = self.computer
+
+    def toggle_turn(self):
+        if self.next_player is self.human:
+            self.next_player = self.computer
+        else:
+            self.next_player = self.human
+
+    def over(self):
+        return len(self.board) == self.board.size ** 2
 
     def run(self):
         print "Game is starting..."
+        while not game.over():
+            self.next_player.go(self)
+
+
+class Player(object):
+    def __init__(self, symbol=''):
+        self.symbol = symbol
+
+
+class Human(Player):
+    def go(self, game):
+        print game.board
+
+
+class Computer(Player):
+    def go(self, game):
+        pass
 
 
 class Board(object):
@@ -22,6 +56,9 @@ class Board(object):
     def __init__(self, size=3):
         self.size = size
         self.squares = [Square(x, y) for x in range(size) for y in range(size)]
+
+    def __len__(self):
+        return len([s for s in self.squares if s.value in ('X', 'O')])
 
     def __str__(self):
         """
@@ -33,7 +70,7 @@ class Board(object):
 
 class Square(object):
     """Represents one location on the game board"""
-    def __init__(self, x, y, value=''):
+    def __init__(self, x, y, value=' '):
         self.x = x
         self.y = y
         self.value = value
