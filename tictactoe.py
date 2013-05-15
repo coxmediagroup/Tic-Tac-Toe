7e84b0653bdd61dfce512172cd3f08a7caff534b
Michael Orr <michael@orr.co>
2013-05-15 02:35:43 -0400

    added some pretty print stuff as well as logic to the board, added human turn logic and dummy computer logic (just picks first open space), next up is cpu logic and game end scenario detection

--- tictactoe.py
+++ tictactoe.py
@@ -43,12 +43,38 @@ class Player(object):
 
 class Human(Player):
     def go(self, game):
+        game.toggle_turn()
+        move_loc = ''
+
         print game.board
+        print('Where would you like to move?')
+
+        err = ''
+        while move_loc not in game.board.unused():
+            print err
+            move_loc = self.move_prompt(game)
+            err = '\nInvalid Selection. Try again.\n'
+        move_loc.mark = self.symbol
+
+    def move_prompt(self, game):
+        x = ''
+        y = ''
+        prefix = ''
+        while x not in ('0', '1', '2'):
+            x = raw_input('%s--> Select an X coordinate: ' % prefix)
+            prefix = '(Invalid Input) '
+        prefix = ''
+        while y not in ('0', '1', '2'):
+            y = raw_input('%s--> Select a Y coordinate: ' % prefix)
+            prefix = '(Invalid Input) '
+        x, y = int(x), int(y)
+        return game.board.square(x, y)
 
 
 class Computer(Player):
     def go(self, game):
-        pass
+        game.toggle_turn()
+        game.board.unused()[0].mark = self.symbol
 
 
 class Board(object):
@@ -57,23 +83,42 @@ class Board(object):
         self.size = size
         self.squares = [Square(x, y) for x in range(size) for y in range(size)]
 
+    def square(self, x, y):
+        return [i for i in self.squares if i.x == x and i.y == y].pop()
+
+    def unused(self):
+        return [i for i in self.squares if i.mark not in ('X', 'O')]
+
     def __len__(self):
-        return len([s for s in self.squares if s.value in ('X', 'O')])
+        return len([s for s in self.squares if s.mark in ('X', 'O')])
 
     def __str__(self):
         """
         Print a human-friendly version of the board
         to be shown between turns
         """
-        return ''.join("(%d,%d)" % (i.x, i.y) for i in self.squares)
+        res = ['\n    0   1   2\n']
+        res.append(' 0  %s | %s | %s\n' %
+                   (self.square(0, 0), self.square(1, 0), self.square(2, 0)))
+        res.append('   ---+---+---\n')
+        res.append(' 1  %s | %s | %s\n' %
+                   (self.square(0, 1), self.square(1, 1), self.square(2, 1)))
+        res.append('   ---+---+---\n')
+        res.append(' 2  %s | %s | %s\n' %
+                   (self.square(0, 2), self.square(1, 2), self.square(2, 2)))
+
+        return ''.join(res)
 
 
 class Square(object):
     """Represents one location on the game board"""
-    def __init__(self, x, y, value=' '):
+    def __init__(self, x, y, mark=' '):
         self.x = x
         self.y = y
-        self.value = value
+        self.mark = mark
+
+    def __str__(self):
+        return self.mark
 
 if __name__ == '__main__':
     game = Game()
