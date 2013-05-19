a4b5d0532bb353f06f20eaf10b6358524bba497d
Owen Nelson <onelson@gmail.com>
2013-05-19 15:22:14 -0400

    refactor/cleanup on game loop

--- tictactoe/management/commands/play_game.py
+++ tictactoe/management/commands/play_game.py
@@ -1,7 +1,6 @@
 import sys
-import itertools
 from django.core.management.base import NoArgsCommand
-from tictactoe import Board, naught_bot
+from tictactoe import Board, naught_bot, NAUGHT, CROSS
 from tictactoe import exceptions as ex
 
 
@@ -9,12 +8,17 @@ class Command(NoArgsCommand):
     args = None
     help = 'Start a new game of Tic Tac Toe'
 
-    def clear_stdout(self, stream):
+    def clear_stdout(self):
         # clears the terminal (I had no idea how to do this so I googled).
         self.stdout.write(chr(27) + "[2J")
 
+    def render_board(self):
+        self.clear_stdout()
+        self.stdout.write(str(self.board))
+
     def handle_noargs(self, **options):
-        board = Board()
+        self.board = Board()
+        self.render_board()
         try:
             while True:
                 while True:
@@ -22,20 +26,17 @@ class Command(NoArgsCommand):
                         user_selection = raw_input("\nEnter a valid open cell number:")
                         idx = int(user_selection)
                         assert 8 >= idx >= 0, "cell must be 0-8"
-                        board[int(user_selection)] = board.CROSS
-                        self.clear_stdout()
-                        self.stdout.write(str(board))
+                        self.board[int(user_selection)] = CROSS
                         break
-                    except (AssertionError, TypeError, ex.TicTacToeError) as exc:
-                        if isinstance(exc, ex.GameOver):
-                            self.stdout.write(str(exc))
-                            sys.exit(0)
-
+                    except (AssertionError, TypeError, ex.MoveError) as exc:
                         self.stderr.write(str(exc))
-                # after the player's mark is set, let the bot look at the board.
-                board[naught_bot(board)] = board.NAUGHT
-                # then re-render the board
-                self.clear_stdout()
-                self.stdout.write(str(board))
+
+                # After the player's mark is set, let the bot look at
+                # the board.
+                self.board[naught_bot(self.board)] = NAUGHT
+                self.render_board()
+        except ex.GameOver as exc:
+            self.render_board()
+            self.stderr.write(str(exc))
         except KeyboardInterrupt:
             self.stderr.write("\nExiting...")
