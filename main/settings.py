f64234b44d8281b66a65d93a3a07dc2fca8bd42e
Owen Nelson <onelson@gmail.com>
2013-05-21 00:08:26 -0400

    stubbing out the views for the front-end

--- main/settings.py
+++ main/settings.py
@@ -106,9 +106,7 @@ ROOT_URLCONF = 'main.urls'
 WSGI_APPLICATION = 'main.wsgi.application'
 
 TEMPLATE_DIRS = (
-    # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
-    # Always use forward slashes, even on Windows.
-    # Don't forget to use absolute paths, not relative paths.
+    proj_path('templates'),
 )
 
 INSTALLED_APPS = (
--- tictactoe/views.py
+++ tictactoe/views.py
@@ -1,1 +1,43 @@
-# Create your views here.
+from django import forms
+from django.core.validators import MaxValueValidator, MinValueValidator
+from django.views.generic import TemplateView, View
+from django.views.generic.edit import FormMixin, ProcessFormView
+from tictactoe import Board, NAUGHT, CROSS, EMPTY
+
+
+class Game(TemplateView):
+    """Front page allows the user to start a new game."""
+    template_name = 'game.html'
+
+    def get_board(self):
+        return Board()
+
+
+    def get_context_data(self, **kwargs):
+        context = super(Game, self).get_context_data(**kwargs)
+        context['cells'] = enumerate(self.get_board().cells)
+        context['NAUGHT'] = NAUGHT
+        context['CROSS'] = CROSS
+        context['EMPTY'] = EMPTY
+        return context
+
+
+class MarkForm(forms.Form):
+    cell = forms.IntegerField(required=True, validators=[
+        MinValueValidator(0),
+        MaxValueValidator(8)
+    ])
+
+
+class MakeMark(FormMixin, ProcessFormView):
+    """Update the active game board by placing the user's mark, then letting
+    the bot make its own."""
+
+    http_method_names = ('post', )
+    form_class = MarkForm
+
+    def form_invalid(self, form):
+        pass
+
+    def form_valid(self, form):
+        pass
