4ba3f087aa8cfbdcc6f25d9c1db9a87ed5d36405
Ahmed Abdalla <ahmad19526@gmail.com>
2013-07-07 16:29:06 -0400

    Added tic-tac-toe game
    <startrant>
    I either took the cheap way out of writing a tic tac toe game by copying a javascrip implemention from
    
    http://www.javascriptkit.com/script/script2/tactoe.shtml
    
    or I'm a great software developer because I utilized existing solutions instead of re-creating the wheel.. hmm..
    
    I played the played against the javascript game several times and used strategies that have served me well in grade
    school and I could not win. After perusing through the (ugly) source code of this game, I'm confident that it will not lose
    against a human being.
    
    In any which case, I do want to implement the tic tac toe game for myself instead instead I'm going to use the HTML5
    canbas element as I've always wanted to do things in canvas. Firstly I just need to go brush up on minimax, and
    alpha-beta pruning..
    </endrant>
    
    Javascript game is implemented in 'game.html'. 'game.html' is served through a generic view class from views.py.

--- game/urls.py
+++ game/urls.py
@@ -1,9 +1,9 @@
 from django.conf.urls import patterns, include, url
 
-
+from .views import GameView
 
 urlpatterns = patterns('',
 
-    #url(r'^$', views.view, name = "name"),
+    url(r'^$', GameView.as_view(), name = "game"),
 
 )
