f06f1a6ed9a531439188a350f1f035be80e7141d
Ahmed Abdalla <ahmad19526@gmail.com>
2013-07-07 19:15:12 -0400

    Changed home page verbage...

--- game/urls.py
+++ game/urls.py
@@ -1,10 +1,8 @@
 from django.conf.urls import patterns, include, url
 
-from .views import GameView, AlmostWonView
+from .views import GameView
 
 urlpatterns = patterns('',
 
     url(r'^(?P<version>.*)$', GameView.as_view(), name = "game"),
-    url(r'^loser/$', AlmostWonView.as_view(), name = "loser"),
-
 )
--- game/views.py
+++ game/views.py
@@ -4,5 +4,3 @@ class GameView(TemplateView):
 	def get_template_names(self):
 		return ["game/game" + self.kwargs['version'] + ".html"]
 
-class AlmostWonView(TemplateView):
-	template_name = "loser.html"
