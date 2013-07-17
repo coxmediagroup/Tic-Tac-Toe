be91ec92159ed6e02b8b143e25cbd1c46cfe106a
Adam Stein <adam.stein@pictometry.com>
2013-07-17 15:04:33 -0400

    Added more functionality to the board class to get information
    Added 'turn' code for Human (prompts for and verifies position to mark)
    
    Added 'turn' code for Computer (implemented basic win strategy, need to
    enhance normal strategy)

--- src/board.py
+++ src/board.py
@@ -10,7 +10,8 @@ class Board(object):
         #               ---|---|---
         #                6 | 7 | 8
 
-        self.spaces = [False for index in xrange(9)]
+        self.__spaces = [False for index in xrange(9)]
+        self.numturn = 1
 
     def draw(self):
         self.__drawrow(0, 3)
@@ -23,7 +24,7 @@ class Board(object):
         print "\t ",
 
         for index in xrange(start, end):
-            mark = [str(index + 1), self.spaces[index]][self.spaces[index]]
+            mark = [str(index + 1), self.__spaces[index]][self.__spaces[index] != False]
 
             print "%s" % mark,
 
@@ -42,10 +43,64 @@ class Board(object):
         else:
             return True
 
+    def is_empty(self, pos):
+        """Return True if the position is empty, False if filled"""
+
+        return self.__spaces[pos] == False
+
+    def match_nummarks(self, mark, nummarks):
+        """Return a list of empty positions given the number of marks to look for"""
+
+        empty_pos = []
+
+        numempties = 3 - nummarks
+
+        # Check rows
+        for start in xrange(0, 9, 3):
+            self._get_empty_pos(xrange(start, start + 3), mark, nummarks, numempties, empty_pos)
+
+        # Check columns
+        for index in xrange(3):
+            self._get_empty_pos(xrange(index, 8, 3), mark, nummarks, numempties, empty_pos)
+
+        # Check diagonals
+        self._get_empty_pos(xrange(0, 8, 4), mark, nummarks, numempties, empty_pos)
+        self._get_empty_pos(xrange(2, 8, 2), mark, nummarks, numempties, empty_pos)
+
+        return empty_pos
+
+    def _get_empty_pos(self, pos_range, mark, nummarks, numempties, empty_pos):
+        """Return positions of empty spaces given # marks criteria"""
+
+        filled = []
+        empty = []
+
+        for pos in pos_range:
+            if self.__spaces[pos] == mark:
+                filled.append(pos)
+            elif self.__spaces[pos] == False:
+                empty.append(pos)
+
+            if len(filled) == nummarks and len(empty) == numempties:
+                empty_pos.extend(empty)
+
+    def setmark(self, pos, name, mark):
+        """Set a player's mark in the given position.  Return True if
+        successful, False otherwise"""
+
+        if self.__spaces[pos] == False:
+            print "\n%s places an '%c' in position %d." % (name, mark, pos + 1)
+
+            self.__spaces[pos] = mark
+            self.numturn += 1
+            return True
+        else:
+            return False
+
     def tie(self):
         """Return True if there is a tie game, False if not"""
 
-        empty = filter(lambda value: value == False, self.spaces)
+        empty = filter(lambda value: value == False, self.__spaces)
 
         # I realize winner() is called twice in rapid succession,
         # once in endgame() and then again here since endgame()
@@ -66,7 +121,7 @@ class Board(object):
     def winner(self):
         """Return the mark of the winner, False if no winner"""
 
-        spaces = self.spaces
+        spaces = self.__spaces
 
         # Check rows
         for index in xrange(0, 9, 3):
--- src/player.py
+++ src/player.py
@@ -2,20 +2,151 @@ import sys
 
 class Player(object):
     def __init__(self, board, first):
-        self.__board = board
-        self.__first = first
+        self._board = board
+        self._first = first
 
         # Set the mark used by this player (player who goes first is
         # always 'X' -- official rules)
-        self.mark = ["O", "X"][first]
+        self._mark = ["O", "X"][first]
+
+    def _setmark(self, pos):
+        self._board.setmark(pos, self._name, self._mark)
 
     def turn(self):
         print >> sys.stderr, "need to override turn()"
         sys.exit(1)
 
 class Computer(Player):
-    pass
+    def __init__(self, board, first):
+        super(Computer, self).__init__(board, first)
+        self.__winning = [False, True][first]   # Set to True if first player, False otherwise
+        self._name = "Computer"
+
+    def turn(self):
+        # Given experience players, there is a sneaky way to win.  If we have
+        # the following scenario:
+        #
+        #   1) 1st player puts his mark in a corner on 1st turn
+        #   2) 2nd player puts his mark in center on 1st turn
+        #   3) 1st player puts his mark in opposite corner on 2nd turn
+        #   4) 2nd player puts his mark in any empty corner on 2nd turn
+        #
+        # then 1st player can win.
+        #
+        # The alternative strategy is to firt check if the human has a winning
+        # position and to block that.  If not, try to find the best position we
+        # can.
+
+        if self.__winning:
+            # We will try the ONLY chance of winning strategy
+
+            if self._board.numturn == 1:
+                # First turn, mark a corner
+                self._setmark(0)
+            elif self._board.numturn == 3:
+                # Second turn, if human put a mark in the center,
+                # continue with the winning strategy.  Otherwise
+                # follow through on the normal strategy
+
+                if not self._board.is_empty(4):
+                    self._setmark(8)
+                else:
+                    self.__winning = False
+            else:
+                # If the first 2 steps were done, we don't need to check
+                # anymore as the normal strategy will work to complete the
+                # winning strategy as part of it's normal course.  If the first
+                # two steps weren't done, then we need to switch strategy
+                # anyway.
+
+                self.__winning = False
+
+        # Can't do this as an 'else' to the IF statement above since the status
+        # could chance within that statement and we would need to do this
+        # anyway.
+        if self.__winning == False:
+            # Execute the alternative strategy
+
+            # If we can win, do it now
+            pos = self._board.match_nummarks(self._mark, 2)
+            if len(pos):
+                self._setmark(pos[0])
+
+            # Get list of winning positions for Human
+            win_pos = self._board.match_nummarks(["X", "O"][self._first], 2)
+
+            if len(win_pos):
+                # Human can win, must block
+                self._setmark(win_pos[0])
+            else:
+                self._setmark(self.__bestpos())
+
+    def __bestpos(self):
+        """Find the best position to mark"""
+
+        # The best position would be if there are already 2 computer marks in a
+        # row, column, or diagonal and the third space is empty.  Second best
+        # would be 1 computer mark and the other two spaces empty.  Otherwise,
+        # try for (in this order):
+        #   - a non-corner, non-center space
+        #   - a non-center space
+        #   - first available space
+
+        pos = self._board.match_nummarks(self._mark, 2)
+
+        if len(pos):
+            return pos[0]
+
+        # Go for center
+        if self._board.is_empty(4):
+            return 4
+
+        # Find the first available empty space
+        for pos in xrange(9):
+            if self._board.is_empty(pos):
+                print "First Avail"
+                return pos
 
 class Human(Player):
-    pass
+    def __init__(self, board, first):
+        super(Human, self).__init__(board, first)
+        self._name = "Lowly Human"
+
+    def turn(self):
+        print
+
+        # Show the board if going first
+        if self._first:
+            self._board.draw()
+            print
+
+        print "It's your turn.  Enter the value of the position to put an '%c'." % self._mark
+
+        # Keep asking for a space to mark until we get a valid answer
+        while True:
+            try:
+                choice = raw_input("Position? ")
+
+                try:
+                    # Make sure answer is an integer value
+                    choice = int(choice) - 1
+
+                    # Make sure answer is in the valid range of choices
+                    if choice < 0 or choice > 8:
+                        print "Not a valid choice, try again"
+                        continue
+
+                    # Make sure answer is an empty space
+                    if self._board.is_empty(choice):
+                        self._setmark(choice)
+                        break
+                    else:
+                        print "Position already has a mark, try again"
+                        continue
+                except ValueError:
+                    print "Not a valid choice, try again"
+                    continue
+            except (EOFError, KeyboardInterrupt):
+                print "\n\nI see you want to quit.  Computer wins by default."
+                sys.exit(0)
 
