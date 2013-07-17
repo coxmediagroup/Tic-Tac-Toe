833eced90da875bb27bdf7437e0ee2cbd92b7b80
Adam Stein <adam.stein@pictometry.com>
2013-07-16 20:31:22 -0400

    Cleaned up (left debugging print statement when parsing options)

--- src/tictactoe.py
+++ src/tictactoe.py
@@ -19,11 +19,10 @@ def parse_args():
     program = os.path.basename(sys.argv[0])
 
     parser = OptionParser(
-        usage = "%s [-h] [-v]" % program,
+        usage = "%s [-1] [-h] [--version]" % program,
         version = "%prog v" + __version__,
         description = """\
-Interactively play the game of Tic-Tac-Toe against a human player and never
-lose.
+Interactively play the game of Tic-Tac-Toe against a human player and never lose.
 """
     )
 
@@ -36,12 +35,6 @@ lose.
     )
 
     (options, args) = parser.parse_args()
-
-    print "Human Start = [" + str(options.humanstart) + "]"
-
-    # Error checking
-
-    # Any manipulations needed (convert strings to lists for example)
 
     return (options, args)
 
