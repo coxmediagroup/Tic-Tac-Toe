6ae5f2238c3723c9b9c49270c5b36297045bb93f
reubenchristie <reuben_social@hotmail.com>
2013-07-23 15:52:11 -0400

    unittest for checking winner

--- tests/test_board.py
+++ tests/test_board.py
@@ -24,6 +24,15 @@ class Test(unittest.TestCase):
         self.board.move('human', '01')
         self.board.move('human', '02')
         assert(self.board.board[0][0]=='O' and self.board.board[0][1] == 'O' and self.board.board[0][2] == 'O')
+    
+    def test_is_winner(self):
+        player = 'human'
+        self.board.move(player, '00')
+        self.board.move(player, '01')
+        self.board.move(player, '02')
+        self.assertTrue(self.board.is_winner(player))
+        self.board.board[0][2] = '-'
+        self.assertFalse(self.board.is_winner(player))
 
 
 if __name__ == "__main__":
