30a1a5b86c29bc26fdb63e0047e1199f951dfc32
reubenchristie <reuben_social@hotmail.com>
2013-07-22 23:50:03 -0400

    create board class

---
+++ com/cox/board.py
@@ -0,0 +1,19 @@
+'''
+Created on Jul 22, 2013
+
+@author: christie
+'''
+
+class board(object):
+    '''
+    class that represents board
+    '''
+
+
+    def __init__(self):
+        '''
+        initialize board as two-d list
+        '''
+        self.board = [['-','-','-'],
+                      ['-','-','-'],
+                      ['-','-','-']]
