08615725aa9e608af133a9bb9b8eaf9fb63738d1
reubenchristie <reuben_social@hotmail.com>
2013-07-23 12:57:38 -0400

    change how board prints

--- com/cox/board.py
+++ com/cox/board.py
@@ -16,11 +16,16 @@ class board(object):
         '''
         self.board = [['-','-','-'],
                       ['-','-','-'],
-                      ['-','-','-']]
+                      ['-','-','-']
+                      ]
     def print_board(self):
         """
         print current board state
-        """
-        print self.board[0]
-        print self.board[1]
-        print self.board[2]
+        """        
+        # This function prints out the board that it was passed.
+        # "board" is a list of 10 strings representing the board (ignore index 0)        
+        print(' ' + self.board[2][0] + ' | ' + self.board[2][1] + ' | ' + self.board[2][2])        
+        print('-----------')        
+        print(' ' + self.board[1][0] + ' | ' + self.board[1][1] + ' | ' + self.board[1][2])        
+        print('-----------')        
+        print(' ' + self.board[0][0] + ' | ' + self.board[0][1] + ' | ' + self.board[0][2])
