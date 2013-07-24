242ef1439565350c49d932832b609c649b4a8a95
reubenchristie <reuben_social@hotmail.com>
2013-07-24 10:35:35 -0400

    more adjustment to the play logic

--- com/cox/game.py
+++ com/cox/game.py
@@ -16,7 +16,7 @@ class game(object):
         choices = ['O','X']
         while(True):
         #while not (selection == 'X' or selection == 'O'):
-            selection = raw_input('Choose X OR O').upper()
+            selection = raw_input('Choose X OR O\n').upper()
             if selection in choices:
                 choices.remove(selection)
                 self.board = board.board(human=selection, computer= choices[0])
@@ -34,7 +34,7 @@ class game(object):
         """
         available moves
         """
-        print self.possible_moves
+        return self.possible_moves
     
     def choose_move(self):
         """
@@ -45,12 +45,11 @@ class game(object):
         while not valid_move:
             self.board.print_board()
             next_move = raw_input("your turn buddy!")
-            if next_move in self.possible_moves:
-                self.board.is_space_available(next_move)
+            if self.board.is_space_available(next_move):
                 valid_move = True
                 self.possible_moves.remove(next_move)
-            else:
-                self.available_moves()
+            #else:
+            #    self.available_moves()
         return next_move
         
     def get_computer_move(self):
@@ -60,22 +59,25 @@ class game(object):
         #check if computer can win in next move, pick that move        
         for move in self.possible_moves:
             secret_board = deepcopy(self.board)
-            secret_board.move("computer", move)
+            if secret_board.is_space_available(move):
+                secret_board.move("computer", move)
             if secret_board.is_winner("computer"):
                 return move
-        #check if human can win in next move, if pick that move so to block him        
+        #check if human can win in next move, if pick that move so to block him                
         for move2 in self.possible_moves:
             secret_board2 = deepcopy(self.board)
-            secret_board2.move("computer", move2)
+            if secret_board2.is_space_available(move2):
+                secret_board2.move("human", move2)
             if secret_board2.is_winner("human"):
                 return move2
-        #take center box if available
-        if self.board.is_space_available(self.center):
-            return self.center
         #take one of the corner box
         for crn in self.corners:
             if self.board.is_space_available(crn):
                 return crn
+        #take center box if available
+        if self.board.is_space_available(self.center):
+            return self.center
+        
         #just pick random available
         for poss_mv in self.possible_moves:
             if self.board.is_space_available(poss_mv):
@@ -104,7 +106,9 @@ class game(object):
                     print "congratulations You have won."
                     break
                 if self.board.is_board_full():
+                    self.board.print_board()
                     print "It's a tie."
+                    break
                 turn = 'computer'        
             else:
                 #move for computer
@@ -117,7 +121,9 @@ class game(object):
                     print "Computer has won."
                     break
                 if self.board.is_board_full():
-                    print "It's a tie."
+                    self.board.print_board()
+                    print "It's a tie."                    
+                    break
                 turn = 'human'
             
 if __name__== "__main__":
