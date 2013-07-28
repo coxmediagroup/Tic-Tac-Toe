6c41761372fa323ab099b33c18f58f61b73f5e67
Derick Eisenhardt <derick.eisenhardt@gmail.com>
2013-07-28 16:28:47 -0400

    Defining base URLs

--- ticTacToe/views.py
+++ ticTacToe/views.py
@@ -1,1 +1,18 @@
-# Create your views here.
+from django.http import HttpResponse
+
+def index(request):
+	'''Default page'''
+	return HttpResponse('Start a new game?')
+
+def newGame(request):
+	'''Creates and redirects to new game instance'''
+	return HttpResponse('New game created.')
+
+def game(request, gameId):
+	'''Loads a game and displays to user'''
+	return HttpResponse('not yet defined')
+
+def move(request, gameId, positionX, positionY):
+	'''Applies player move to game'''
+	return HttpResponse('not yet defined')
+
