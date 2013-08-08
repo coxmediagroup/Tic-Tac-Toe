43faee45ec121faf3f42ecbc8d919d75aa3f5b90
Liam Curry <liam@curry.name>
2013-08-08 17:43:23 -0400

    put players in an array; added better indicators for current players

--- main.js
+++ main.js
@@ -1,33 +1,38 @@
 (function () {
 
-  // for simple native jquery-style code
   var $ = document.querySelectorAll.bind(document);
-  Element.prototype.on = Element.prototype.addEventListener;
-
+  var elContainer = $('#container')[0];
   var elBoard = $('#board')[0];
-  var player1 = { symbol: '×', w: 0, l: 0, d: 0, el: $('#player1')[0], isHuman: true },
-      player2 = { symbol: '○', w: 0, l: 0, d: 0, el: $('#player2')[0], isHuman: true };
-  var currentPlayer = player1;
+  var players = [
+    { symbol: '×', w: 0, l: 0, d: 0, el: $('#player1')[0], isHuman: true },
+    { symbol: '○', w: 0, l: 0, d: 0, el: $('#player2')[0], isHuman: true }
+  ];
+  var player = 0;
   var board = [[], [], []];
 
-  elBoard.on('click', function (e) {
-    if (!currentPlayer.isHuman || e.target.className === 'marked') return;
+  elBoard.addEventListener('click', function (e) {
+    if (!players[player].isHuman || e.target.className.indexOf('marked') !== -1)
+      return;
     mark(e.target);
   });
 
   function mark(elSquare) {
     var index = Array.prototype.indexOf.call(elBoard.children, elSquare);
     var coords = locate(index);
-    elSquare.innerHTML = board[coords.x][coords.y] = currentPlayer.symbol;
-    elSquare.className = 'marked';
-    currentPlayer.el.className = '';
-    currentPlayer = currentPlayer === player1 ? player2 : player1;
-    currentPlayer.el.className = 'current';
+    elSquare.innerHTML = board[coords.x][coords.y] = players[player].symbol;
+    elSquare.className = 'marked player' + (player + 1);
+    players[player].el.className = '';
+    player = player === 0 ? 1 : 0;
+    elContainer.className = 'turn-player' + (player + 1);
   }
 
   // Gets coordinates from the index
   function locate(index) {
     return { x: Math.floor(index / board.length), y: index % board.length };
+  }
+
+  function findWins(lastMark) {
+
   }
 
 }());
