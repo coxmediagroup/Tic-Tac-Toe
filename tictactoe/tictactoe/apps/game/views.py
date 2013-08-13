1b7df59f424d28651de3861e4656d2704a2f57bf
Dustin Davis <dustin.davis@gmail.com>
2013-08-13 01:29:03 -0600

    Remove magic strings

--- tictactoe/tictactoe/apps/game/views.py
+++ tictactoe/tictactoe/apps/game/views.py
@@ -4,6 +4,9 @@ import random
 from django.http import HttpResponse
 from django.shortcuts import render
 
+X = 'X'
+O = 'O'
+BLANK = ''
 
 WINNING_COMBOS = (
     (1, 2, 3),
@@ -28,15 +31,15 @@ NEIGHBORS = {
 }
 
 EMPTY_GAME = {
-    1: '',
-    2: '',
-    3: '',
-    4: '',
-    5: '',
-    6: '',
-    7: '',
-    8: '',
-    9: '',
+    1: BLANK,
+    2: BLANK,
+    3: BLANK,
+    4: BLANK,
+    5: BLANK,
+    6: BLANK,
+    7: BLANK,
+    8: BLANK,
+    9: BLANK,
 }
 
 
@@ -52,7 +55,7 @@ def JsonResponse(request, game, winning_combo=None):
 def moves(game):
     counter = 0
     for move in game:
-        if game[move] != '':
+        if game[move] != BLANK:
             counter += 1
     return counter
 
@@ -73,15 +76,15 @@ def next_move(request):
     if game[clicked] != '':
         return HttpResponse('Cell already occupied')
 
-    game[clicked] = 'x'
+    game[clicked] = X
 
     if moves(game) == 1:
         # on the first move I'll take the middle
-        if game[5] == '':
-            game[5] = 'o'
+        if game[5] == BLANK:
+            game[5] = O
         # unless the middle is taken, then I'll take a random corner
         else:
-            game[random.choice((1, 3, 7, 9))] = 'o'
+            game[random.choice((1, 3, 7, 9))] = O
         return JsonResponse(request, game)
     else:
         # Look for a potential winning combo to block or win
@@ -92,9 +95,9 @@ def next_move(request):
             o_count = 0
             empty = 0
             for num in combo:
-                if game[num] == 'x':
+                if game[num] == X:
                     x_count += 1
-                elif game[num] == 'o':
+                elif game[num] == O:
                     o_count += 1
                 else:
                     empty = num
@@ -107,7 +110,7 @@ def next_move(request):
                 if x_count == 2:
                     play = empty
         if play:
-            game[play] = 'o'
+            game[play] = O
             return JsonResponse(request, game, winning)
 
         # Or pick a neighboring cell
@@ -115,13 +118,13 @@ def next_move(request):
         random.shuffle(neighbors)
         for n in neighbors:
             if game[n] == '':
-                game[n] = 'o'
+                game[n] = O
                 return JsonResponse(request, game)
 
         # Or pick any remaining spots available
         for i, cell_no in enumerate(game):
-            if game[cell_no] == '':
-                game[cell_no] = 'o'
+            if game[cell_no] == BLANK:
+                game[cell_no] = O
         return JsonResponse(request, game)
 
 
