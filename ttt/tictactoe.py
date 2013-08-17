eb7a58f5ba4f671ae16bc808a4be0995c348dfe9
Nick Loadholtes <nick@ironboundsoftware.com>
2013-08-17 12:19:54 -0400

    Rough sketch of the main loop of the game

---
+++ ttt/tictactoe.py
@@ -0,0 +1,25 @@
+"""Play a game of tic-tac-toe
+Nick Loadholtes <nick@ironboundsoftware.com
+"""
+
+import sys
+
+BOARD = []
+
+
+def showBoard():
+    pass
+
+
+def getUserInput():
+    pass
+
+
+def playGame():
+    pass
+
+if __name__ == '__main__':
+    print("Starting")
+    args = sys.argv
+    print("args seen:" + str(args))
+    playGame()
