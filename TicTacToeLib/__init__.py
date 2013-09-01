430bbca0436b09c672802a1734b0236cb1144cc0
Mike Olensky <mike@aretoo.com>
2013-09-01 13:04:52 -0400

    Added first real test and basis for a Board class

---
+++ TicTacToeLib/__init__.py
@@ -0,0 +1,30 @@
+#TODO Is TicTacToeEngine a better name?
+
+'''        
+    Going to prematurely optimize here by turning a 2D structure into 1D.
+    Our board will map 1D positions to 2D like this:
+     0 | 1 | 2
+     __+___+__
+     3 | 4 | 5
+     __+___+__
+     6 | 7 | 8
+     
+     For AI: 
+     corners = [0,2,6,8]
+     edges = [1,3,5,7]
+     center = [4]
+     winning lines horizontal = [0,1,2], [3,4,5], [6,7,8]
+     winning lines vertical = [0,3,6], [1,4,7], [2,5,8]
+     winning lines diagonal = [0,4,8], [2,4,6] 
+'''
+GAME_BOARD_SIZE = 3*3
+BLANK = ''
+
+# TODO Is naming clear?
+class Board(object):
+    def __init__(self):
+        # Create an empty board        
+        self.__gameboard = [BLANK] * (GAME_BOARD_SIZE)
+        
+    def getGameBoard(self):
+        return self.__gameboard
--- test/TicTacToeLibTests.py
+++ test/TicTacToeLibTests.py
@@ -4,9 +4,12 @@ import TicTacToeLib
 
 class TicTacToeLibTests(unittest.TestCase):
 
+    def setUp(self):
+        self.testboard=[TicTacToeLib.BLANK]*TicTacToeLib.GAME_BOARD_SIZE
 
-    def testBoard(self):
-        pass
+    def testGetBoard(self):
+        board = TicTacToeLib.Board()
+        self.assertListEqual(self.testboard, board.getGameBoard())
 
 
 if __name__ == "__main__":
