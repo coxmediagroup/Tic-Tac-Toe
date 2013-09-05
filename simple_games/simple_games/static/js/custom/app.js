d6a66eefe7df7d75fd4452a11af0989b25f5aebd
Rodolfo <rodolfo2488@gmail.com>
2013-09-05 14:54:53 -0500

    Cleaned the app main JavaScript App.

--- simple_games/simple_games/static/js/custom/app.js
+++ simple_games/simple_games/static/js/custom/app.js
@@ -12,24 +12,4 @@ angular.module('simple-games',
   function($httpProvider) {
       $httpProvider.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
   };
-}).factory('simple-games', ['$http', function($http){
-
-    var SimpleGames = function(data) {
-        angular.extend(this, data);
-    };
-
-    SimpleGames.get = function(id) {
-        return $http.get('/tictactoe/' + id).then(function(response) {
-            return new Product(response.data);
-        });
-    };
-
-    SimpleGames.prototype.update = function() {
-        var product = this;
-        return $http.put('/tictactoe/move/' + product.id + '/', product).then(function(response) {
-            return response.data;
-        });
-    };
-
-    return SimpleGames;
-}]);
+});
