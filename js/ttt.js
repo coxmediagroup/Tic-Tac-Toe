7c48c12b4a42646eb8df8aba849d00d005237999
Adrian Thiele <at@fu-labs.com>
2013-09-05 07:12:32 -0400

    Hulk was beaten repeatedly by Celine yesterday. Obviously, Hulk needed a more aggressive strategy. Hulk Smash Celine!!!!

--- js/ttt.js
+++ js/ttt.js
@@ -10,8 +10,7 @@ var hulk={
 	name:'hulk',
 	squares:[],
 	//order in which Hulk makes moves
-	moves:["four","six","two","eight","zero","seven","one","three","five","four"],
-			
+	moves:["four","two","seven","one","three","five","zero","eight","six"],		
 	//if no block is needed, Hulk takes the first available move
 	findmove: function(){
 		var i=0
@@ -27,37 +26,36 @@ var hulk={
 	},			
 			
 	picksquare: function(){
-				
-		var blk=hulk.chkwingroups()
-		if (blk){
-			var sq=blk
-		}else{
-			if (hulk.findmove()){
-				var sq=hulk.findmove()
-			}	
-		}
-		if (sq){
-			tictac.setsquare(sq,hulk)
-		}
-		hulk.didhulkwin()			
+	//After Celine smashed Hulk yesterday, Hulk is more aggressive at trying to win.
+		if (hulk.canhulkwin()==="win"){
+			return
+		}else{		
+			var blk=hulk.chkwingroups()
+			if (blk){
+				var sq=blk
+			}else{
+				if (hulk.findmove()){
+					var sq=hulk.findmove()
+				}	
+			}
+			if (sq){
+				tictac.setsquare(sq,hulk)
+			}
+		}	
 	},		
 	
 					
 	//Hulk checks wingroups to see if a block is needed
 	chkwingroups: function(){
 		var wgl=tictac.wingroups.length 
-		
 		while(wgl--){
 			var blk= hulk.chkforblock(tictac.wingroups[wgl])
 			if (blk !="noblock"){
 				var used=tictac.usedsquares.join()
-				if (used.match(blk)){
-				;			
-				} else {
+				if (!used.match(blk)){
 					return blk
 				}
-			}
-					
+			}			
 		}
 	},
 			
@@ -66,9 +64,7 @@ var hulk={
 		var wl=wingroup.length
 		var fs=foozie.squares.join()
 		while (wl--){			
-			if (fs.match(wingroup[wl])) {
-					;
-			}else{		
+			if (!fs.match(wingroup[wl])) {	
 				opensquares.unshift(wingroup[wl])
 			}
 		}		
@@ -77,49 +73,54 @@ var hulk={
 		}
 		return opensquares[0]	
 	},
+	
 			
 	chkforwin:function(wg){
 		var results=[]
+		var unresults=[]
 		var wl=wg.length
 		var hs=hulk.squares.join()
 		while(wl--){
 			if (hs.match(wg[wl])){
 				results.unshift(wg[wl])
 			}else{
-			}	
-			if (results.length===3){
-				var wrap=document.getElementById("wrap")
-				var squares=Array.prototype.slice.call(wrap.children)	
-				
-				var sl=squares.length
-				while(sl--){
-					tictac.rmclick(squares[sl])
-				}	
-				//highlight hulk winning squares
-				var rl=results.length
-				while(rl--){
-					var el=document.getElementById(results[rl])
-					el.className +=" win"
-				}
+				var us=tictac.usedsquares.join()
+				if (!us.match(wg[wl])){
+					unresults.unshift(wg[wl])
+				}			
+			}
+			if (results.length===2 && unresults.length===1){
+				tictac.setsquare(unresults[0],hulk)
+				results.unshift(unresults[0])
+				tictac.rmclicks()
+				tictac.highlight(results)
+				return "win"
 			}		
 		}
 	},
 			
 	// Calls hulk.chkwin on each wingroup to see if Hulk won.  
-	didhulkwin: function(){
+	canhulkwin: function(){
+		var won="no"
 		var wgl=tictac.wingroups.length 
 		while(wgl--){
-		var wg=tictac.wingroups[wgl]	
-			hulk.chkforwin(wg)					
+			var wg=tictac.wingroups[wgl]	
+			won=hulk.chkforwin(wg)
+			if( won==="win"){
+				return won		
+			}	
 		}			
 	}	
-
+	
 			
 } //end hulk
 		
 var tictac={
+
 		 		
 	squareclass:'tictac',
+
+	squares:[],
 		
 	usedsquares:[],
 	
@@ -139,12 +140,10 @@ var tictac={
 			["two","four","six"]  
 	],	
 		 	
-	mksquares: function(){
-		var wrap=document.getElementById("wrap")
-		this.squares=Array.prototype.slice.call(wrap.children)	
-		var sl=this.squares.length
+	mkclicks: function(){
+		var sl=tictac.squares.length
 				while(sl--){
-					tictac.addclick(this.squares[sl])
+					tictac.addclick(tictac.squares[sl])
 				}	
 		
 	},
@@ -157,8 +156,12 @@ var tictac={
 		
 	},		
 			
-	rmclick: function(el){
-		el.onclick=""
+	rmclicks: function(el){
+		var sl=tictac.squares.length
+		while(sl--){
+			tictac.squares[sl].onclick=null
+		}	
+		
 	},	
 	
 
@@ -167,35 +170,42 @@ var tictac={
 		el.className=player.name
 		tictac.usedsquares.unshift(square)
 		player.squares.unshift(square)
-		el.onclick=""
+		el.onclick=null
 					
 	}, 
+	
+	highlight: function(results){
+		var rl=results.length
+		while(rl--){
+			var el=document.getElementById(results[rl])
+			el.className +=" win"
+		}
+	},	
 			
 	resetsquare: function(square){ 
 		square.className=tictac.squareclass
 	},		
 			
 	resetboard: function(){
-		var wrap=document.getElementById("wrap")
-		var squares=Array.prototype.slice.call(wrap.children)
-		var sl=squares.length
+		var sl=tictac.squares.length
 		while (sl--){		
-			tictac.resetsquare(squares[sl])
+			tictac.resetsquare(tictac.squares[sl])
 		}
 		tictac.usedsquares=[]
 		foozie.squares=[]
 		hulk.squares=[]
 		document.title="Tic-Tac-Toe with Hulk "
-		tictac.mksquares()
- 		tictac.setsquare("four",hulk)
+		tictac.mkclicks()
 	},
 	
 	init: function(){
+		var wrap=document.getElementById("wrap")
+		tictac.squares=Array.prototype.slice.call(wrap.children)
 		var ngd=document.getElementById("newgame")
 		ngd.onclick=tictac.resetboard	
 		tictac.resetboard()
- tictac.setsquare("four",hulk)
 	}
+	
 }// end tictac 
 
 tictac.init()
