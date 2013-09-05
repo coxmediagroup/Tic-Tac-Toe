9a82aefd6206c8cd195bc2555b8b7f3b0e4b2e62
Rodolfo <rodolfo2488@gmail.com>
2013-09-05 14:55:47 -0500

    Changed the controller of the board to use the angular scope and not jQuery.

--- simple_games/simple_games/static/js/custom/controllers.js
+++ simple_games/simple_games/static/js/custom/controllers.js
@@ -5,46 +5,30 @@
 angular.module('simple-games.controllers', []).
   controller('TicTacToeController', function ($scope, $http, $location) {
     
-    function drawBoard( squares, message ) {
-      for (var i = 0; i < squares.length; i++) {
-        if (!$(".game .board li:eq("+i+")").hasClass(squares[i])) {
-          $(".game .board li:eq("+i+")").addClass(squares[i]);
-        }
-      };
-      $scope.message = message;
-    };
-    $('.board').hide();
     $scope.message = "Pick your sign";
     
-    $scope.makeMove = function( move ){
-      $http.get('/tictactoe/move/' + move + "/").then(function(response) {
-
-            var squares = response.data[0].board;
-            var message = response.data[0].message;
-            drawBoard(squares, message);
-
+    $scope.makeMove = function ( index ) {
+      $http.get('/tictactoe/move/' + index + "/").then(function(response) {
+            $scope.squares = response.data[0].board;
+            $scope.message = response.data[0].message;
             if( !response.data[0].status ) { 
               $scope.makeMove = null;
-              
               $scope.message = null;
               $scope.winner = response.data[0].winner;
             }
         });
     };
-    
-    $scope.setGame = function( sign ) {
+
+    $scope.setGame = function ( sign ) {
       $http.get('/tictactoe/new/' + sign + "/").then(function(response) {
             $('.sign-block').unbind().remove();
-            $('.board').show();
-            var squares = response.data[0].board;
-            var message = response.data[0].message;
-            drawBoard(squares, message);
+            $scope.squares = response.data[0].board;
+            $scope.message = response.data[0].message; 
         });
     };
   }).
-  controller('loginController', function ($scope, $location) {
+  controller('loginController', function ( $scope, $location ) {    
     $scope.games = ['tictactoe'];
-
     $scope.goGame = function(game) {
       $("#goGameForm").attr("action", game + "/").submit();
     };
