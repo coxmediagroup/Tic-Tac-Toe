b9549013f4b01a33bbedf3df51eb051e4ccdcec0
Art McGee <kingartjr@gmail.com>
2013-09-10 12:51:55 -0700

    Views Not Working

--- mysite/tictactoe/views.py
+++ mysite/tictactoe/views.py
@@ -1,1 +1,8 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def startgame(request):
+    return HttpResponse("Start Hello, world. You're at the games index.")
+
+def index(request):
+    return render(request, 'tictactoe/index.html')
