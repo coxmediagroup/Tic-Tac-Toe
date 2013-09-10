0d401a38a96504df35877d3ab8a9c6d9c52b5980
Art McGee <kingartjr@gmail.com>
2013-09-10 11:08:03 -0700

    Game Data Model
    Created Game data model and Adminstration

--- mysite/tictactoe/models.py
+++ mysite/tictactoe/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+    player = models.CharField(max_length=1)
+    computer = models.CharField(max_length=1)
+    board = models.CharField(max_length=9)
