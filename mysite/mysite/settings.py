8e3c7409750409f442068824828e452b7eca816d
Art McGee <kingartjr@gmail.com>
2013-09-10 10:18:15 -0700

    Updated Paths

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -12,7 +12,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'C:/Users/kingartjr/Documents/Projects/mysite/mysite.db',                      # Or path to database file if using sqlite3.
+        'NAME': 'C:/Users/kingartjr/Documents/GitHub/Tic-Tac-Toe/mysite/mysite.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
@@ -128,7 +128,7 @@ INSTALLED_APPS = (
 )
 
 TEMPLATE_DIRS = (
-    'C:\\Users\\kingartjr\\Documents\\Projects\\mysite\\templates\\', # Change this to your own directory.
+    'C:\\Users\\kingartjr\\Documents\\github\\tic-tac-toe\\mysite\\templates\\', # Change this to your own directory.
 )
 
 # A sample logging configuration. The only tangible logging
