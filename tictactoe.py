0137984430b90c02edaa0abc077aa0aa096c0182
Art McGee <kingartjr@gmail.com>
2013-09-11 13:16:16 -0700

    Alpha TicTacToe Game complete
    One Player TicTacToe with the computer is complete.

--- tictactoe.py
+++ tictactoe.py
@@ -1,40 +1,108 @@
-#from Tkinter import Tk, BOTH
+"""tictactoe.py: A one player tic tac toe game with the computer.  The computer never loses."""
+
+__author__      = "Art McGee"
+__copyright__   = "Copyright 2013, USA"
+
 from Tkinter import *
 from ttk import Frame, Button, Style
 
+class TicTacToeComputer:
+    def __init__(self):
+        self.x=0
+        self.y=0
+
+    def location(self, x, y):
+        self.x = x
+        self.y = y
+        print 'location x={0:1d} y={1:1d}\n'.format(self.x,self.y)
+
+    def play(self,game,player):
+        if player=='X':
+            aponent = 'O'
+        else:
+            aponent = 'X'
+        x = 0
+        while x<3:
+            y = 0
+            while y <3:
+                if game.getxy(x,y)==' ':
+                    if game.check(player,x,y)==player:
+                        self.location(x,y)
+                        return
+                    elif game.check(aponent,x,y)==aponent:
+                        self.location(x,y)
+                        return
+                y = y + 1
+            x = x + 1
+        if game.board[1][1]==' ':
+            self.location(1,1)
+        elif game.board[0][0]==' ':
+            self.location(0,0)
+        elif game.board[2][2]==' ':
+            self.location(2,2)
+        elif game.board[0][2]==' ':
+            self.location(0,2)
+        elif game.board[2][0]==' ':
+            self.location(2,0)
+        elif game.board[0][1]==' ':
+            self.location(0,1)
+        elif game.board[1][0]==' ':
+            self.location(1,0)
+        elif game.board[2][1]==' ':
+            self.location(2,1)
+        elif game.board[1][2]==' ':
+            self.location(1,2)
+        
 class TicTacToe:
     def __init__(self):
         self.start()
+
     def play(self, player, x, y):
+        if self.board[x][y]==' ':
+            self.board[x][y] = player;
+        else:
+            print "Play Error Space Taken {0:s} {1:d} {2:d}".format(player,x,y)
+        
+    def check(self, player, x, y):
+        before = self.board[x][y]
         self.board[x][y] = player;
+        result = self.win()
+        self.board[x][y] = before;
+        if result==player:
+            print 'check:player {0:s} at {1:d} {2:d}\n'.format(player,x,y)
+        return result;
+
     def getxy(self,x,y):
         return self.board[x][y]
+
     def boardfull(self):
         for ox in self.board:
             for oy in ox:
                 if oy==' ':
                     return False
         return True
+
     def start(self):
         self.board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+
     def win(self):
         result = ' '
         if ((self.board[0][0]!=' ') and (self.board[0][0]==self.board[0][1]) and (self.board[0][0]==self.board[0][2])):
-            return self.board[0][0] + "A"
+            return self.board[0][0]
         if ((self.board[1][0]!=' ') and (self.board[1][0]==self.board[1][1]) and (self.board[1][0]==self.board[1][2])):
-            return self.board[1][0] + "B"
+            return self.board[1][0]
         if ((self.board[2][0]!=' ') and (self.board[2][0]==self.board[2][1]) and (self.board[2][0]==self.board[2][2])):
-            return self.board[2][0] + "C"
+            return self.board[2][0]
         if ((self.board[0][0]!=' ') and (self.board[0][0]==self.board[1][1]) and (self.board[0][0]==self.board[2][2])):
-            return self.board[0][0] + "D"
+            return self.board[0][0]
         if ((self.board[0][2]!=' ') and (self.board[0][2]==self.board[1][1]) and (self.board[2][0]==self.board[0][2])):
-            return self.board[0][2] + "E"
+            return self.board[0][2]
         if ((self.board[0][1]!=' ') and (self.board[0][1]==self.board[1][1]) and (self.board[0][1]==self.board[2][1])):
-            return self.board[0][1] + "F"
+            return self.board[0][1]
         if ((self.board[0][2]!=' ') and (self.board[0][2]==self.board[1][2]) and (self.board[0][2]==self.board[2][2])):
-            return self.board[0][2] + "G"
+            return self.board[0][2]
         if ((self.board[0][0]!=' ') and (self.board[0][0]==self.board[1][0]) and (self.board[0][0]==self.board[2][0])):
-            return self.board[0][0] + "H"
+            return self.board[0][0]
         return result
 
     def printx(self):
@@ -51,39 +119,61 @@ class Example(Frame):
         
         self.initUI()
         self.game = TicTacToe();
+        self.computer = TicTacToeComputer()
         self.player = 'X'
         self.NewX()
+
     def NewX(self):
         self.player = 'X'
         self.game.start()
+        print self.game.printx()
         for ox in self.Bboard:
             for oy in ox:
                 oy.config(text=' ')
         print "New X\n"
         self.status["text"] = "X Pick a Square"
+
     def NewO(self):
         self.player = 'O'
         self.game.start()
+        print self.game.printx()
         for ox in self.Bboard:
             for oy in ox:
                 oy.config(text=' ')
         print "New O\n"
         self.status["text"] = "O Pick a Square"
+        self.computer.play(self.game,'X')
+        print 'Computer X Plays x={0:1d} y={1:1d}\n'.format(self.computer.x,self.computer.y)
+        self.game.play('X',self.computer.y,self.computer.x)
+        self.Bboard[self.computer.y][self.computer.x].config(text=self.game.getxy(self.computer.y,self.computer.x))
+        print self.game.printx()
+
     def Play(self,x,y):
-        print 'Play x={0:1d} y={1:1d}\n'.format(x,y)
-        self.game.play(self.player,y,x)
-        self.Bboard[y][x].config(text=self.game.getxy(y,x))
+        print 'Play {2:s} x={0:1d} y={1:1d}\n'.format(x,y,self.player)
+        self.game.play(self.player,x,y)
+        self.Bboard[x][y].config(text=self.game.getxy(x,y))
         print self.game.printx()
         if self.game.win()!=' ':
             self.status["text"] = self.game.win()+" Won the game"
         elif self.game.boardfull():
             self.status["text"] = "Cats Game"
         elif self.player=='X':
-            self.player='O'
-            self.status["text"] = "O Pick a Square"
+            self.computer.play(self.game,'O')
+            print 'Computer O Plays x={0:1d} y={1:1d}\n'.format(self.computer.x,self.computer.y)
+            self.game.play('O',self.computer.x,self.computer.y)
+            self.Bboard[self.computer.x][self.computer.y].config(text=self.game.getxy(self.computer.x,self.computer.y))
+            print self.game.printx()
         else:
-            self.player='X'
-            self.status["text"] = "X Pick a Square"
+            self.computer.play(self.game,'X')
+            print 'Computer X Plays x={0:1d} y={1:1d}\n'.format(self.computer.x,self.computer.y)
+            self.game.play('X',self.computer.x,self.computer.y)
+            self.Bboard[self.computer.x][self.computer.y].config(text=self.game.getxy(self.computer.x,self.computer.y))
+            print self.game.printx()
+        if self.game.win()!=' ':
+            self.status["text"] = self.game.win()+" Won the game"
+        elif self.game.boardfull():
+            self.status["text"] = "Cats Game"
+
     def initUI(self):
         self.status = Label(self, text = "Status")
         self.status.place(x=0,y=30)
@@ -102,19 +192,19 @@ class Example(Frame):
             command=self.quit)
         quitButton.place(x=160, y=0)
         self.Bboard = [[Button(self,command=lambda: self.Play(0,0),width=1,text=" "),
-                        Button(self,command=lambda: self.Play(1,0),width=1,text=" "),
-                        Button(self,command=lambda: self.Play(2,0),width=1,text=" ")],
-                       [Button(self,command=lambda: self.Play(0,1),width=1,text=" "),
+                        Button(self,command=lambda: self.Play(0,1),width=1,text=" "),
+                        Button(self,command=lambda: self.Play(0,2),width=1,text=" ")],
+                       [Button(self,command=lambda: self.Play(1,0),width=1,text=" "),
                         Button(self,command=lambda: self.Play(1,1),width=1,text=" "),
-                        Button(self,command=lambda: self.Play(2,1),width=1,text=" ")],
-                       [Button(self,command=lambda: self.Play(0,2),width=1,text=" "),
-                        Button(self,command=lambda: self.Play(1,2),width=1,text=" "),
+                        Button(self,command=lambda: self.Play(1,2),width=1,text=" ")],
+                       [Button(self,command=lambda: self.Play(2,0),width=1,text=" "),
+                        Button(self,command=lambda: self.Play(2,1),width=1,text=" "),
                         Button(self,command=lambda: self.Play(2,2),width=1,text=" ")]]
         x = 0
-        for ox in self.Bboard:
-            y=0
-            for oy in ox:
-                oy.place(y=30*x+50,x=30*y)
+        while x<3:
+            y = 0
+            while y<3:
+                self.Bboard[x][y].place(y=30*y+50,x=30*x)
                 y = y + 1
             x = x + 1
 
