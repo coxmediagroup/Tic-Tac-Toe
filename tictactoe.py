8e63962a91885847191602e08d1d1213ec9ffab7
Art McGee <kingartjr@gmail.com>
2013-09-11 10:11:50 -0700

    Working two player

--- tictactoe.py
+++ tictactoe.py
@@ -4,29 +4,37 @@ from ttk import Frame, Button, Style
 
 class TicTacToe:
     def __init__(self):
-        self.board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
+        self.start()
     def play(self, player, x, y):
-        self.board[x-1][y-1] = player;
+        self.board[x][y] = player;
     def getxy(self,x,y):
-        return self.board[x-1][y-1]
+        return self.board[x][y]
+    def boardfull(self):
+        for ox in self.board:
+            for oy in ox:
+                if oy==' ':
+                    return False
+        return True
+    def start(self):
+        self.board = [[' ',' ',' '],[' ',' ',' '],[' ',' ',' ']]
     def win(self):
         result = ' '
-        if ((self.board[0][0]==self.board[0][1]) and (self.board[0][0]==self.board[0][2])):
-            return self.board[0][0]
-        if ((self.board[1][0]==self.board[1][1]) and (self.board[1][0]==self.board[1][2])):
-            return self.board[1][0]
-        if ((self.board[2][0]==self.board[0][1]) and (self.board[0][0]==self.board[0][2])):
-            return self.board[0][0]
-        if ((self.board[0][0]==self.board[1][1]) and (self.board[0][0]==self.board[2][2])):
-            return self.board[0][0]
-        if ((self.board[0][2]==self.board[1][1]) and (self.board[2][0]==self.board[0][2])):
-            return self.board[0][0]
-        if ((self.board[0][1]==self.board[1][1]) and (self.board[0][1]==self.board[2][1])):
-            return self.board[0][1]
-        if ((self.board[0][2]==self.board[1][2]) and (self.board[0][2]==self.board[2][2])):
-            return self.board[0][2]
-        if ((self.board[0][0]==self.board[1][0]) and (self.board[0][0]==self.board[2][0])):
-            return self.board[0][0]
+        if ((self.board[0][0]!=' ') and (self.board[0][0]==self.board[0][1]) and (self.board[0][0]==self.board[0][2])):
+            return self.board[0][0] + "A"
+        if ((self.board[1][0]!=' ') and (self.board[1][0]==self.board[1][1]) and (self.board[1][0]==self.board[1][2])):
+            return self.board[1][0] + "B"
+        if ((self.board[2][0]!=' ') and (self.board[2][0]==self.board[2][1]) and (self.board[2][0]==self.board[2][2])):
+            return self.board[2][0] + "C"
+        if ((self.board[0][0]!=' ') and (self.board[0][0]==self.board[1][1]) and (self.board[0][0]==self.board[2][2])):
+            return self.board[0][0] + "D"
+        if ((self.board[0][2]!=' ') and (self.board[0][2]==self.board[1][1]) and (self.board[2][0]==self.board[0][2])):
+            return self.board[0][2] + "E"
+        if ((self.board[0][1]!=' ') and (self.board[0][1]==self.board[1][1]) and (self.board[0][1]==self.board[2][1])):
+            return self.board[0][1] + "F"
+        if ((self.board[0][2]!=' ') and (self.board[0][2]==self.board[1][2]) and (self.board[0][2]==self.board[2][2])):
+            return self.board[0][2] + "G"
+        if ((self.board[0][0]!=' ') and (self.board[0][0]==self.board[1][0]) and (self.board[0][0]==self.board[2][0])):
+            return self.board[0][0] + "H"
         return result
 
     def printx(self):
@@ -43,18 +51,39 @@ class Example(Frame):
         
         self.initUI()
         self.game = TicTacToe();
+        self.player = 'X'
+        self.NewX()
     def NewX(self):
         self.player = 'X'
+        self.game.start()
+        for ox in self.Bboard:
+            for oy in ox:
+                oy.config(text=' ')
         print "New X\n"
+        self.status["text"] = "X Pick a Square"
     def NewO(self):
         self.player = 'O'
+        self.game.start()
+        for ox in self.Bboard:
+            for oy in ox:
+                oy.config(text=' ')
         print "New O\n"
+        self.status["text"] = "O Pick a Square"
     def Play(self,x,y):
         print 'Play x={0:1d} y={1:1d}\n'.format(x,y)
-        self.game.play(self.player,x,y)
-        self.Bboard[y][x].config(text=self.game.getxy(x,y))
+        self.game.play(self.player,y,x)
+        self.Bboard[y][x].config(text=self.game.getxy(y,x))
+        print self.game.printx()
         if self.game.win()!=' ':
-            self.status["text"] = "Winner"
+            self.status["text"] = self.game.win()+" Won the game"
+        elif self.game.boardfull():
+            self.status["text"] = "Cats Game"
+        elif self.player=='X':
+            self.player='O'
+            self.status["text"] = "O Pick a Square"
+        else:
+            self.player='X'
+            self.status["text"] = "X Pick a Square"
     def initUI(self):
         self.status = Label(self, text = "Status")
         self.status.place(x=0,y=30)
@@ -71,11 +100,7 @@ class Example(Frame):
 
         quitButton = Button(self, text="Quit",
             command=self.quit)
-        quitButton.place(x=150, y=0)
-        #fred = Button(self)
-        #fred["text"] = "hello"
-        #fred.place(x=0,y=0)
-        #print fred
+        quitButton.place(x=160, y=0)
         self.Bboard = [[Button(self,command=lambda: self.Play(0,0),width=1,text=" "),
                         Button(self,command=lambda: self.Play(1,0),width=1,text=" "),
                         Button(self,command=lambda: self.Play(2,0),width=1,text=" ")],
@@ -96,9 +121,9 @@ class Example(Frame):
 
 def main():
     game = TicTacToe()
-    game.play('X',1,1);
+    game.play('X',0,0);
     game.play('X',2,2);
-    game.play('X',3,3);
+    game.play('X',1,1);
     if game.win()!=' ':
         print game.win()+"Won the game"
     print game.printx();
