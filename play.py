41b8552461685370088c028f4a69cb2d803f1b54
Mike Olensky <mike@aretoo.com>
2013-09-13 15:38:14 -0400

    Made a single playable game of Tic Tac Toe - first move chosen randomly - Winner/Draw reported at the top of the screen

--- play.py
+++ play.py
@@ -1,9 +1,12 @@
 import pygame, sys
 import TicTacToeLib as TTTL
+from random import choice
 BG = "res/paper.png"
 CD = "res/choosedialog.png"
 XB = "res/xboxbutton.png"
 OB = "res/oboxbutton.png"
+XPC = "res/X96.png"
+OPC = "res/O96.png"
 BOARD = "res/board.png"
 
 STATE_CHOOSE = 0
@@ -19,6 +22,17 @@ class BoxButton(pygame.sprite.Sprite):
         self.size = (self.width, self.height) = (66,66)
         self.image = pygame.transform.smoothscale(temp_image, self.size)
         self.rect = pygame.Rect(x, y, self.width, self.height)
+
+        
+class Piece(pygame.sprite.Sprite):
+    def __init__(self, piece, x, y):
+        super(Piece, self).__init__()
+        img = XPC if piece == TTTL.PIECE_X else OPC
+        temp_image = pygame.image.load(img)
+        self.size = (self.width, self.height) = (96,96)
+        self.image = pygame.transform.smoothscale(temp_image, self.size)
+        self.rect = pygame.Rect(x, y, self.width, self.height)
+
         
 class GameBoard(pygame.sprite.Sprite):
     def __init__(self, x, y):
@@ -30,6 +44,8 @@ class GameBoard(pygame.sprite.Sprite):
         self.rect = pygame.Rect(x, y, self.width, self.height)
         self.cell_list = self.createCellList()
         self.board = None
+        
+        self.pieces = pygame.sprite.Group()
         
     def createCellList(self):
         cells = []
@@ -57,10 +73,36 @@ class GameBoard(pygame.sprite.Sprite):
             if cell.collidepoint(relative_pos):
                 return idx
             
-    def update(self):
-        # TODO will process game board and draw X's and O's
-        pass
+    def moveAI(self, aiplayer):
+        idx = aiplayer.move(self.board)
+        self.movePlayer(aiplayer, idx)
+        
+    def movePlayer(self, player, idx):
+        if idx != TTTL.NO_MOVE:
+            if self.board.move(player, idx):
+                self.move(idx)
+ 
+    def move(self, idx):
+        if idx in xrange(9):
+            piece = self.board.getGameBoard()[idx]
+            if piece != TTTL.BLANK:
+                x = self.cell_list[idx].left + self.rect.left
+                y = self.cell_list[idx].top + self.rect.top
+                self.pieces.add(Piece(piece,x,y))
                 
+    def isGameOver(self, player1, player2):
+        return ( self.board.isBoardFull() or 
+                self.board.isWinner(player1) or 
+                self.board.isWinner(player2) )
+        
+    def getWinner(self, player1, player2):
+        piece = TTTL.BLANK
+        if self.board.isWinner(player1):
+            piece = player1.piece
+        elif self.board.isWinner(player2):
+            piece = player2.piece
+        return piece
+                                
 
 class Main(object):
     def __init__(self):
@@ -68,19 +110,23 @@ class Main(object):
         
     def setup(self):
         pygame.init()
+        self.game_over = False
         size = (self.width, self.height) = (640,480)
+        self.screen = pygame.display.set_mode(size)
         self.black = (0, 0, 0)
         self.background_image = pygame.image.load(BG)
         self.choose_dialog = pygame.image.load(CD)
-        self.game_board = GameBoard(200,100)
+        self.game_board = GameBoard(self.width/2-144,self.height/2-144)
         self.boardGroup = pygame.sprite.GroupSingle(self.game_board)
-        self.screen = pygame.display.set_mode(size)
         self.game_state = STATE_CHOOSE
         self.clicked = False
         self.click_pos = (0,0)
         self.player = None
         self.aiplayer = None
         self.aipiece = None
+        self.turn = None
+        self.moved = False
+        self.winner = None
         self.box_buttons = pygame.sprite.Group()
         self.box_buttons.add(BoxButton(XB, TTTL.PIECE_X, 255, 179))
         self.box_buttons.add(BoxButton(OB, TTTL.PIECE_O, 339,179))
@@ -98,33 +144,62 @@ class Main(object):
             self.screen.blit(self.choose_dialog, (250, 150))
             self.box_buttons.update()
             self.box_buttons.draw(self.screen)
-        if self.game_state == STATE_PLAY:
+        if self.game_state == STATE_PLAY or self.game_state == STATE_GAME_OVER:
             self.boardGroup.update()
             self.boardGroup.draw(self.screen)
+            self.game_board.pieces.update()
+            self.game_board.pieces.draw(self.screen)
+            
+            if self.game_state == STATE_GAME_OVER:
+                winner_text = ""
+                if self.winner == TTTL.BLANK:
+                    winner_text =  "Draw"
+                elif self.winner == self.aipiece:
+                    winner_text = "CPU Wins!"
+                else:
+                    winner_text = "Human Wins! Inconceivable!!!!"
+                
+                # print winner_text
+                if pygame.font:
+                    font = pygame.font.Font(None, 36)
+                    text = font.render(winner_text, 1, (10, 10, 10))
+                    textpos = text.get_rect(centerx=self.screen.get_width()/2)
+                    self.screen.blit(text, textpos)
+            
             
         pygame.display.flip()
+    
+    def nextTurn(self):
+        return TTTL.PIECE_X if self.turn == TTTL.PIECE_O else TTTL.PIECE_O
+        
+    def moveAI(self):
+        if self.turn == self.aipiece and not self.moved:
+            self.game_board.moveAI(self.aiplayer)
+            self.moved = True
+            self.turn = self.nextTurn()
         
     def processClick(self, state):
         if state == STATE_CHOOSE:
             for button in self.box_buttons.sprites():
                 if button.rect.collidepoint(self.click_pos):
                     self.player = TTTL.Player(button.name)
-                    self.aipiece = TTTL.PIECE_X if button.name == TTTL.PIECE_X else TTTL.PIECE_O
+                    self.aipiece = TTTL.PIECE_X if button.name == TTTL.PIECE_O else TTTL.PIECE_O
                     self.aiplayer = TTTL.AIPlayer(self.aipiece)
                     self.game_board.board = TTTL.Board()
-                    # TODO randomly determine who goes first
+                    # randomly determine who goes first
+                    self.turn = choice([TTTL.PIECE_X,TTTL.PIECE_O])
                     self.game_state = STATE_PLAY
                     return
-        if state == STATE_PLAY:
+
+        if state == STATE_PLAY and self.turn != self.aipiece:
             if self.game_board.rect.collidepoint(self.click_pos):
-                print self.game_board.getRelativePos(self.click_pos)
-                print self.game_board.whichCell(self.click_pos)
-              
-            
-        
+                idx = self.game_board.whichCell(self.click_pos)
+                self.game_board.movePlayer(self.player, idx)
+                self.turn = self.nextTurn()
+                self.moved = True
 
     def event_loop(self):
-        while True:
+        while not self.game_over:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
                     pygame.quit()
@@ -135,8 +210,19 @@ class Main(object):
                 if event.type == pygame.MOUSEBUTTONUP and self.clicked:
                     self.clicked = False
                     self.processClick(self.game_state)
+                       
+            if self.game_state == STATE_PLAY:
+                self.moveAI()
+            
+            if self.moved:
+                #check for end of game
+                if self.game_board.isGameOver(self.player, self.aiplayer):
+                    self.winner = self.game_board.getWinner(self.player,self.aiplayer)
+                    self.game_state = STATE_GAME_OVER
                     
-#                elif event.type == pygame.KEYDOWN:
+                self.moved = False
+
+#                
             self.draw()
             pygame.time.delay(30)
 
