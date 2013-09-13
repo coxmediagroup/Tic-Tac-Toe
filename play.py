47a2dfd8f67a219cedfa57889c55384082c7af50
Mike Olensky <mike@aretoo.com>
2013-09-13 11:11:02 -0400

    Added a game board sprite class which can map clicks to Board() cell numbers

--- play.py
+++ play.py
@@ -19,6 +19,48 @@ class BoxButton(pygame.sprite.Sprite):
         self.size = (self.width, self.height) = (66,66)
         self.image = pygame.transform.smoothscale(temp_image, self.size)
         self.rect = pygame.Rect(x, y, self.width, self.height)
+        
+class GameBoard(pygame.sprite.Sprite):
+    def __init__(self, x, y):
+        super(GameBoard, self).__init__()
+        temp_image = pygame.image.load(BOARD)
+        self.size = (self.width, self.height) = (288,288)
+        self.cell_size = (self.cell_width, self.cell_height) = (96,96)
+        self.image = pygame.transform.smoothscale(temp_image, self.size)
+        self.rect = pygame.Rect(x, y, self.width, self.height)
+        self.cell_list = self.createCellList()
+        self.board = None
+        
+    def createCellList(self):
+        cells = []
+        x,y = 0,0
+        for i in xrange(9):
+            cells.append(pygame.Rect(x,y,96,96))
+            # new row
+            if (i+1) % 3 == 0:
+                x = 0
+                y += self.cell_height
+            else:
+                x += self.cell_width
+                
+        return cells            
+        
+    def getRelativePos(self, pos):
+        absolute_x, absolute_y = pos
+        relative_x = absolute_x - self.rect.left
+        relative_y = absolute_y - self.rect.top
+        return (relative_x, relative_y)
+    
+    def whichCell(self, pos):
+        relative_pos = self.getRelativePos(pos)
+        for idx, cell in enumerate(self.cell_list):
+            if cell.collidepoint(relative_pos):
+                return idx
+            
+    def update(self):
+        # TODO will process game board and draw X's and O's
+        pass
+                
 
 class Main(object):
     def __init__(self):
@@ -30,13 +72,15 @@ class Main(object):
         self.black = (0, 0, 0)
         self.background_image = pygame.image.load(BG)
         self.choose_dialog = pygame.image.load(CD)
-        self.game_board = pygame.image.load(BOARD)
+        self.game_board = GameBoard(200,100)
+        self.boardGroup = pygame.sprite.GroupSingle(self.game_board)
         self.screen = pygame.display.set_mode(size)
         self.game_state = STATE_CHOOSE
         self.clicked = False
         self.click_pos = (0,0)
         self.player = None
         self.aiplayer = None
+        self.aipiece = None
         self.box_buttons = pygame.sprite.Group()
         self.box_buttons.add(BoxButton(XB, TTTL.PIECE_X, 255, 179))
         self.box_buttons.add(BoxButton(OB, TTTL.PIECE_O, 339,179))
@@ -55,7 +99,8 @@ class Main(object):
             self.box_buttons.update()
             self.box_buttons.draw(self.screen)
         if self.game_state == STATE_PLAY:
-            self.screen.blit(self.game_board, (200,100))
+            self.boardGroup.update()
+            self.boardGroup.draw(self.screen)
             
         pygame.display.flip()
         
@@ -64,15 +109,21 @@ class Main(object):
             for button in self.box_buttons.sprites():
                 if button.rect.collidepoint(self.click_pos):
                     self.player = TTTL.Player(button.name)
-                    aipiece = TTTL.PIECE_X if button.name == TTTL.PIECE_X else TTTL.PIECE_O
-                    self.aiplayer = TTTL.AIPlayer(aipiece)
+                    self.aipiece = TTTL.PIECE_X if button.name == TTTL.PIECE_X else TTTL.PIECE_O
+                    self.aiplayer = TTTL.AIPlayer(self.aipiece)
+                    self.game_board.board = TTTL.Board()
+                    # TODO randomly determine who goes first
                     self.game_state = STATE_PLAY
-                    return  
+                    return
+        if state == STATE_PLAY:
+            if self.game_board.rect.collidepoint(self.click_pos):
+                print self.game_board.getRelativePos(self.click_pos)
+                print self.game_board.whichCell(self.click_pos)
+              
             
         
 
     def event_loop(self):
-        #toon = self.toon
         while True:
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
