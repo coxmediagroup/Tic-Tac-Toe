a817aab9c10c9e57e3414d8052b3290a71c6979e
Kelly Elton <kelly.elton@skylabsonline.com>
2013-09-18 16:35:59 -0500

    Started with some potential classes that might be needed.

---
+++ src/TicTacToe.Core/GameState.cs
@@ -0,0 +1,76 @@
+﻿namespace TicTacToe.Core
+{
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class GameState
+    {
+        public Player Player1 { get; internal set; }
+        public Player Player2 { get; internal set; }
+        public GameBoard Board { get; internal set; }
+
+        /// <summary>
+        /// Gets the <see cref="Player"/> who's turn it is.
+        /// </summary>
+        public Player PlayerTurn { get; internal set; }
+
+        /// <summary>
+        /// Create a new <see cref="GameState"/> with 2 players
+        /// </summary>
+        /// <param name="player1">Player 1</param>
+        /// <param name="player2">Player 2</param>
+        /// <param name="gameBoard">Game board to use</param>
+        public GameState(Player player1, Player player2, GameBoard gameBoard)
+        {
+            Player1 = player1;
+            Player2 = player2;
+            Board = gameBoard;
+        }
+    }
+
+    public class GameBoard
+    {
+        /// <summary>
+        /// Positions and state of the board
+        /// </summary>
+        public List<List<Player>> BoardPositions { get; internal set; }
+
+        public GameBoard()
+        {
+            //Create three rows of three board positions
+            BoardPositions = Enumerable.Repeat(Enumerable.Repeat<Player>(null, 3).ToList(),3).ToList();
+        }
+    }
+
+    public class Player
+    {
+        public IAi Ai { get; internal set; }
+        public string Name { get; internal set; }
+
+        /// <summary>
+        /// Create a human <see cref="Player"/>
+        /// </summary>
+        /// <param name="name">Name of the human <see cref="Player"/></param>
+        public Player(string name)
+        {
+            Name = name;
+        }
+
+        /// <summary>
+        /// Create an non human <see cref="IAi"/> <see cref="Player"/>
+        /// </summary>
+        /// <param name="ai">An <see cref="IAi"/> instance</param>
+        public Player(IAi ai)
+        {
+            Name = ai.Name;
+            Ai = ai;
+        }
+    }
+
+    public interface IAi
+    {
+        string Name { get; set; }
+
+
+    }
+}
