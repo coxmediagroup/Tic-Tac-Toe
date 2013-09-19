792744c44b80155286058c1aec0672182682e6d1
Kelly Elton <kelly.elton@skylabsonline.com>
2013-09-18 21:41:10 -0500

    Added AiPlayer.cs

---
+++ src/TicTacToe.Core/AiPlayer.cs
@@ -0,0 +1,26 @@
+﻿namespace TicTacToe.Core
+{
+    public class AiPlayer : IPlayer
+    {
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Create a <see cref="AiPlayer"/>
+        /// </summary>
+        /// <param name="name">Name of the <see cref="AiPlayer"/></param>
+        public AiPlayer(string name)
+        {
+            this.Name = name;
+        }
+
+        /// <summary>
+        /// Gets invoked when it's the <cref see="AiPlayer"/>'s turn.
+        /// </summary>
+        /// <param name="state">State of the game</param>
+        /// <returns>True to end the turn, False otherwise. Human players would return False, while AI would return True.</returns>
+        public bool OnTurn(Game state)
+        {
+            return true;
+        }
+    }
+}
