33debc72803626e41b252c1a0b77d1e00c21ac7c
Kelly Elton <kelly.elton@skylabsonline.com>
2013-09-18 21:09:03 -0500

    Created and finished ResetGameActionTests.cs

---
+++ src/TicTacToe.Core/Tests/Actions/ResetGameActionTests.cs
@@ -0,0 +1,48 @@
+﻿namespace TicTacToe.Core.Tests.Actions
+{
+	using NUnit.Framework;
+
+	using TicTacToe.Core.Actions;
+
+    public class ResetGameActionTests
+    {
+        [Test]
+        public void Consturctor_SetsClass()
+        {
+            var p1 = new HumanPlayer("jim");
+            var p2 = new HumanPlayer("tim");
+            var game = new Game(p1, p2);
+            var action = new ResetGameAction(game,p1);
+
+            Assert.AreEqual(p1, action.Player);
+            Assert.AreEqual(game, action.Game);
+        }
+
+        [Test]
+        public void Do_DoesReset()
+        {
+            var p1 = new HumanPlayer("jim");
+            var p2 = new HumanPlayer("tim");
+            var game = new Game(p1, p2);
+            var action = new ResetGameAction(game,p1);
+            
+			game.Status = GameStatus.Finished;
+			action.Do();
+            Assert.AreEqual(GameStatus.Running, game.Status);
+
+        }
+
+        [Test]
+        public void Do_DoesLog()
+        {
+            var p1 = new HumanPlayer("jim");
+            var p2 = new HumanPlayer("tim");
+            var game = new Game(p1, p2);
+            var action = new ResetGameAction(game,p1);
+
+			Assert.AreEqual(0,game.GameLog.Count);
+            action.Do();
+            Assert.AreEqual(1, game.GameLog.Count);
+        }
+    }
+}
