a82ff36f12611dc0e4233b2f7b74ea75f28f9934
Kelly Elton <kelly.elton@skylabsonline.com>
2013-09-18 21:04:36 -0500

    GameBoardTests.cs Tests for Winner()

--- src/TicTacToe.Core/Tests/GameBoardTests.cs
+++ src/TicTacToe.Core/Tests/GameBoardTests.cs
@@ -79,5 +79,149 @@
 
             Assert.True(board.IsFull());
         }
+
+        [Test]
+        public void IndexToCoords_IsCorrect()
+        {
+            var board = new GameBoard();
+            var x = 0;
+            var y = 0;
+			board.IndexToCoords(0,out x,out y);
+            Assert.AreEqual(0, x);
+            Assert.AreEqual(0, y);
+			board.IndexToCoords(1,out x,out y);
+            Assert.AreEqual(1, x);
+            Assert.AreEqual(0, y);
+			board.IndexToCoords(2,out x,out y);
+            Assert.AreEqual(2, x);
+            Assert.AreEqual(0, y);
+			board.IndexToCoords(3,out x,out y);
+            Assert.AreEqual(0, x);
+            Assert.AreEqual(1, y);
+			board.IndexToCoords(4,out x,out y);
+            Assert.AreEqual(1, x);
+            Assert.AreEqual(1, y);
+			board.IndexToCoords(5,out x,out y);
+            Assert.AreEqual(2, x);
+            Assert.AreEqual(1, y);
+			board.IndexToCoords(6,out x,out y);
+            Assert.AreEqual(0, x);
+            Assert.AreEqual(2, y);
+			board.IndexToCoords(7,out x,out y);
+            Assert.AreEqual(1, x);
+            Assert.AreEqual(2, y);
+			board.IndexToCoords(8,out x,out y);
+            Assert.AreEqual(2, x);
+            Assert.AreEqual(2, y);
+        }
+
+        [Test]
+        public void Winner_IsCorrect()
+        {
+            GameBoard board = null;
+            var p1 = new HumanPlayer("jim");
+            
+            board = new GameBoard();
+			Assert.Null(board.Winner());
+            board.BoardPositions[0][0] = p1;
+            board.BoardPositions[0][1] = p1;
+            board.BoardPositions[0][2] = p1;
+            Assert.AreEqual(p1, board.Winner());
+
+            board = new GameBoard();
+			Assert.Null(board.Winner());
+            board.BoardPositions[1][0] = p1;
+            board.BoardPositions[1][1] = p1;
+            board.BoardPositions[1][2] = p1;
+            Assert.AreEqual(p1, board.Winner());
+
+            board = new GameBoard();
+			Assert.Null(board.Winner());
+            board.BoardPositions[2][0] = p1;
+            board.BoardPositions[2][1] = p1;
+            board.BoardPositions[2][2] = p1;
+            Assert.AreEqual(p1, board.Winner());
+
+            board = new GameBoard();
+			Assert.Null(board.Winner());
+            board.BoardPositions[0][0] = p1;
+            board.BoardPositions[1][0] = p1;
+            board.BoardPositions[2][0] = p1;
+            Assert.AreEqual(p1, board.Winner());
+
+            board = new GameBoard();
+			Assert.Null(board.Winner());
+            board.BoardPositions[0][1] = p1;
+            board.BoardPositions[1][1] = p1;
+            board.BoardPositions[2][1] = p1;
+            Assert.AreEqual(p1, board.Winner());
+
+            board = new GameBoard();
+			Assert.Null(board.Winner());
+            board.BoardPositions[0][2] = p1;
+            board.BoardPositions[1][2] = p1;
+            board.BoardPositions[2][2] = p1;
+            Assert.AreEqual(p1, board.Winner());
+
+            board = new GameBoard();
+			Assert.Null(board.Winner());
+            board.BoardPositions[0][0] = p1;
+            board.BoardPositions[1][1] = p1;
+            board.BoardPositions[2][2] = p1;
+            Assert.AreEqual(p1, board.Winner());
+
+            board = new GameBoard();
+			Assert.Null(board.Winner());
+            board.BoardPositions[0][2] = p1;
+            board.BoardPositions[1][1] = p1;
+            board.BoardPositions[2][0] = p1;
+            Assert.AreEqual(p1, board.Winner());
+
+			// Make sure doesn't work if one of the spots is occupied by someone else
+            var p2 = new HumanPlayer("tim");
+
+            board = new GameBoard();
+			Assert.Null(board.Winner());
+            board.BoardPositions[0][0] = p2;
+            board.BoardPositions[0][1] = p1;
+            board.BoardPositions[0][2] = p1;
+            Assert.Null(board.Winner());
+
+            board = new GameBoard();
+			Assert.Null(board.Winner());
+            board.BoardPositions[0][0] = p1;
+            board.BoardPositions[0][1] = p2;
+            board.BoardPositions[0][2] = p1;
+            Assert.Null(board.Winner());
+
+            board = new GameBoard();
+			Assert.Null(board.Winner());
+            board.BoardPositions[0][0] = p1;
+            board.BoardPositions[0][1] = p1;
+            board.BoardPositions[0][2] = p2;
+            Assert.Null(board.Winner());
+
+			// Make sure doesn't work if one of the spots is not occupied
+            board = new GameBoard();
+			Assert.Null(board.Winner());
+            board.BoardPositions[0][0] = null;
+            board.BoardPositions[0][1] = p1;
+            board.BoardPositions[0][2] = p1;
+            Assert.Null(board.Winner());
+
+            board = new GameBoard();
+			Assert.Null(board.Winner());
+            board.BoardPositions[0][0] = p1;
+            board.BoardPositions[0][1] = null;
+            board.BoardPositions[0][2] = p1;
+            Assert.Null(board.Winner());
+
+            board = new GameBoard();
+			Assert.Null(board.Winner());
+            board.BoardPositions[0][0] = p1;
+            board.BoardPositions[0][1] = p1;
+            board.BoardPositions[0][2] = null;
+            Assert.Null(board.Winner());
+        }
     }
 }
