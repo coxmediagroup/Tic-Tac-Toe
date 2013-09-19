ea5337f7b16abd9cfbefe59586b75f7f28ece4bb
Kelly Elton <kelly.elton@skylabsonline.com>
2013-09-18 22:49:30 -0500

    Untested start of bruteforce ai move system.

--- src/TicTacToe.Core/AiPlayer.cs
+++ src/TicTacToe.Core/AiPlayer.cs
@@ -1,20 +1,23 @@
 ﻿namespace TicTacToe.Core
 {
+    using System.Collections.Generic;
     using System.Linq;
 
     using TicTacToe.Core.Actions;
 
     public class AiPlayer : IPlayer
     {
-        public string Name { get; private set; }
+        public string Name { get; internal set; }
+		public Game Game { get; internal set; }
 
         /// <summary>
         /// Create a <see cref="AiPlayer"/>
         /// </summary>
         /// <param name="name">Name of the <see cref="AiPlayer"/></param>
-        public AiPlayer(string name)
+        public AiPlayer(string name, Game game)
         {
             this.Name = name;
+            this.Game = game;
         }
 
         /// <summary>
@@ -34,6 +37,81 @@
 			// Otherwise, things get a bit sticky here.
 			// I suppose we can brute force this sucker and see
 			//    how long it takes first.
+
+            var result = new PlayItOutResults(this, state.Board, Game);
+        }
+
+        internal class PlayItOutResults
+        {
+			public int Index { get; internal set; }
+            public List<PlayItOutResults> Moves { get; internal set; }
+			public GameWinStatus Status { get; internal set; }
+			public IPlayer WinPlayer { get; internal set; }
+			public Game Game { get; set; }
+
+            public PlayItOutResults(IPlayer movePlayer, GameBoard board, Game game)
+            {
+                Game = game;
+                Index = -1;
+                Moves = new List<PlayItOutResults>();
+                for (var i = 0; i < 9; i++)
+                {
+                    if (!board.IsPositionOccupied(i))
+                    {
+                        var res = new PlayItOutResults(i, movePlayer, board, game);
+						Moves.Add(res);
+                    }
+                }
+            }
+
+            public PlayItOutResults(int idx, IPlayer movePlayer, GameBoard board, Game game)
+            {
+                Game = game;
+                Moves = new List<PlayItOutResults>();
+                Index = idx;
+                var bclone = CloneBoard(board);
+                bclone.Occupy(movePlayer, idx);
+
+				//Check if it's a win
+                WinPlayer = bclone.Winner();
+                if (WinPlayer != null)
+                {
+                    Status = GameWinStatus.Win;
+                    return;
+                }
+
+				// Check if it's a tie
+                if (bclone.IsFull())
+                {
+                    Status = GameWinStatus.Tie;
+                    return;
+                }
+
+                // invert move player
+                var newMovePlayer = movePlayer == Game.Player1 ? Game.Player2 : Game.Player1;
+
+                for (var i = 0; i < 9; i++)
+                {
+                    if (!bclone.IsPositionOccupied(i))
+                    {
+                        var res = new PlayItOutResults(i, newMovePlayer, bclone, Game);
+                        Moves.Add(res);
+                    }
+                }
+            }
+
+            internal GameBoard CloneBoard(GameBoard board)
+            {
+                var bclone = new GameBoard();
+                for (var row = 0; row < board.BoardPositions.Length; row++)
+                {
+                    for (var p = 0; p < board.BoardPositions[row].Length; p++)
+                    {
+                        bclone.BoardPositions[row][p] = board.BoardPositions[row][p];
+                    }
+                }
+                return bclone;
+            }
         }
     }
 }
