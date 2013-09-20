edc405e79b9465b07c9c73fe75288696f14f91d1
Kelly Elton <kelly.elton@skylabsonline.com>
2013-09-19 21:05:45 -0500

    Added converter to invert the standard BooleanToVisibilityConverter

---
+++ src/TicTacToe/Converters.cs
@@ -0,0 +1,22 @@
+﻿namespace TicTacToe
+{
+    using System;
+    using System.Globalization;
+    using System.Windows;
+    using System.Windows.Data;
+
+    [ValueConversion(typeof(bool), typeof(Visibility))]
+    public class InvertedBooleanToVisibilityConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var val = (bool)value;
+            return val ? Visibility.Collapsed : Visibility.Visible;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
