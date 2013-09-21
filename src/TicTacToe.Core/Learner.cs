f82c811a91b84305cd92b1ae00c4c7be51ce0db3
Kelly Elton <kelly.elton@skylabsonline.com>
2013-09-21 14:33:22 -0500

    Stubbed out some more of the learner.cs

--- src/TicTacToe.Core/Learner.cs
+++ src/TicTacToe.Core/Learner.cs
@@ -1,13 +1,32 @@
 ﻿namespace TicTacToe.Core
 {
     using System;
+    using System.Collections.Generic;
+    using System.Globalization;
+    using System.IO;
     using System.Linq;
+
+    using Common.Logging;
 
     using TicTacToe.Core.Actions;
 
     public class LearnProcessor
     {
-        public void Process(string learnFile, Game game)
+        internal static ILog Log = LogManager.GetCurrentClassLogger();
+
+		internal string LearnFile { get; set; }
+
+		internal List<long> CacheList { get; set; }
+
+        public LearnProcessor(string learnFile)
+        {
+            LearnFile = LearnFile;
+            if (!File.Exists(learnFile))
+                File.Create(LearnFile).Close();
+            CacheList = File.ReadAllLines(LearnFile).Select(long.Parse).ToList();
+        }
+
+        public void ProcessEndGame(Game game)
         {
             if (game.Status != GameStatus.Finished) 
                 throw new InvalidOperationException("Game must be finished before processing");
@@ -16,6 +35,13 @@
 				throw new InvalidOperationException("Game win status must be set before processing");
 
             var state = BoardToLong(game);
+            if (CacheList.Contains(state))
+            {
+                Log.Debug("State already exists");
+                return;
+            }
+            File.AppendAllLines(LearnFile, new[] { state.ToString(CultureInfo.InvariantCulture) });
+			CacheList.Add(state);
         }
 
         internal long BoardToLong(Game game)
@@ -33,10 +59,12 @@
                 if (m.Player == startPlayer) pbyte = 2;
                 else if (m.Player != null) pbyte = 3;
 
+				// 4 bits
                 boardState = (boardState << skip) | idx;
 
                 skip = 2;
 
+				// 2 bits
                 boardState = (boardState << skip) | (pbyte & 0x03);
 
                 skip = 4;
@@ -52,6 +80,25 @@
             boardState = (boardState << 2) | (pbyte & 0x03);
 
             return boardState;
+        }
+
+        /// <summary>
+        /// Get the next move
+        /// </summary>
+        /// <param name="player">The player requesting a move</param>
+        /// <param name="game">Game being played</param>
+        /// <param name="learning">Are we learning moves</param>
+        /// <returns></returns>
+        public int GetNextMove(IPlayer player, Game game, bool learning)
+        {
+			// If we're learning, then don't use any states that we have saved
+            if (learning)
+            {
+
+                return 0;
+            }
+
+            return 0;
         }
     }
 }
