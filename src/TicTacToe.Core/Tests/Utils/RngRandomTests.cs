340f245e3385f9cb37ec107572cd264cefd79bd2
Kelly Elton <kelly.elton@skylabsonline.com>
2013-09-22 17:00:08 -0500

    Added RngRandomTests.cs

---
+++ src/TicTacToe.Core/Tests/Utils/RngRandomTests.cs
@@ -0,0 +1,22 @@
+﻿namespace TicTacToe.Core.Tests.Utils
+{
+	using NUnit.Framework;
+
+	using TicTacToe.Core.Utils;
+
+    public class RngRandomTests
+    {
+        [Test]
+        public void Next_min_max__PicksCorrectRange()
+        {
+            for (var i = 0; i < 10000; i++)
+            {
+                var num = RngRandom.Instance.Next(0, 2);
+				Assert.GreaterOrEqual(num,0);
+				Assert.LessOrEqual(num,1);
+				Assert.AreNotEqual(2,num);
+                Assert.AreNotEqual(-1, num);
+            }
+        }
+    }
+}
