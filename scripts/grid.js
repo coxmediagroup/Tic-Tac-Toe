721229b81f5be30884f8be765f33adeeed1e4b3d
Rachel J. Morris <racheljmorris@gmail.com>
2013-09-22 15:33:53 -0500

    WIP computer AI. Also fixed bug in win checking

--- scripts/grid.js
+++ scripts/grid.js
@@ -43,25 +43,51 @@ objGrid = {
 		
 	},
 	
+	GetIndexAtPosition: function( x, y )
+	{
+		return parseInt( x / 100 ) + parseInt( y / 100 ) * 3;
+	},
+	
 	ClickedGrid: function( mouseX, mouseY, player ) 
 	{
 		// Figure out which grid this is
-		var index = parseInt( mouseX / 100 ) + parseInt( mouseY / 100 ) * 3;
+		var index = this.GetIndexAtPosition( mouseX, mouseY );
 		
 		return this.RandomGrid( index, player );
 	},
 	
 	RandomGrid: function( index, player ) 
 	{
+		stateGame.DebugMessage( player + " trying to move to " + index );
 		if ( this.gridData[index].state == "empty" )
 		{
 			stateGame.DebugMessage( "Set block " + index + " to " + player );
 			this.gridData[index].state = player;
 			
+			stateGame.DebugMessage( "Move success" );
+			
 			return true;
 		}
 		
+		stateGame.DebugMessage( "Move failure" );
+		
 		return false;
+	},
+	
+	EdgeOrCorner: function( index ) 
+	{
+		if ( index % 2 != 0 )
+		{
+			return "edge";
+		}
+		else if ( index % 2 == 0 && index != 4 )
+		{
+			return "corner";
+		}
+		else
+		{
+			return "center";
+		}
 	},
 	
 	EmptyBlocksLeft: function()
@@ -87,9 +113,9 @@ objGrid = {
 		 * i, i+4, i+8 for i == 0 (Diagonal Top-Left to Bottom-Right)
 		 * i, i+2, i+4 for i == 2 (Diagonal Top-Right to Bottom-Left)
 		 * */
-		
+		 
 		// Horizontal check
-		for ( var y = 0; y < 3; y++ )
+		for ( var y = 0; y <= 6; y += 3 )
 		{
 			if ( 	this.gridData[y].state == this.gridData[y+1].state &&
 					this.gridData[y].state == this.gridData[y+2].state &&
@@ -99,7 +125,7 @@ objGrid = {
 				return this.gridData[y].state;
 			}
 		}
-		
+
 		// Vertical check
 		for ( var x = 0; x < 3; x++ )
 		{
@@ -111,7 +137,7 @@ objGrid = {
 				return this.gridData[x].state;
 			}
 		}
-		
+
 		// Diagonal 1
 		if ( 	this.gridData[0].state == this.gridData[4].state &&
 				this.gridData[0].state == this.gridData[8].state &&
@@ -120,8 +146,8 @@ objGrid = {
 			stateGame.DebugMessage( "** Diagonal 1 win " + this.gridData[0].state );
 			return this.gridData[0].state;
 		}
-		
-		
+
+
 		// Diagonal 2
 		if ( 	this.gridData[2].state == this.gridData[4].state && 
 				this.gridData[2].state == this.gridData[6].state &&
@@ -132,5 +158,132 @@ objGrid = {
 		}
 		
 		return "none";
-	}	
+	},
+	
+	FindWinIndex: function( player )
+	{		
+		stateGame.DebugMessage( "Try to find win position" );
+		
+		// Horizontal check
+		for ( var y = 0; y < 3; y++ )
+		{
+			var consecutiveMarks = 0;
+			
+			for ( var x = 0; x < 3; x++ )
+			{
+				if ( this.gridData[y+x].state == player ) 
+				{
+					consecutiveMarks++;
+				}
+			}	
+			
+			if ( consecutiveMarks == 2 )
+			{
+				// which element is empty?
+				for ( var x = 0; x < 3; x++ )
+				{
+					if ( this.gridData[y+x].state == "empty" ) 
+					{
+						return y+x;
+					}
+				}	
+			} 
+		}
+		
+		// Vertical check
+		for ( var x = 0; x < 3; x++ )
+		{
+			var consecutiveMarks = 0;
+			
+			for ( var y = 0; y <= 6; y += 3 )
+			{
+				if ( this.gridData[y+x].state == player ) 
+				{
+					consecutiveMarks++;
+				}
+			}
+			
+			if ( consecutiveMarks == 2 )
+			{
+				// which element is empty?
+				for ( var y = 0; y <= 6; y += 3 )
+				{
+					if ( this.gridData[y+x].state == "empty" ) 
+					{
+						return y+x;
+					}
+				}	
+			} 
+		}
+		
+		// Diagonal Check 1
+		var consecutiveMarks = 0;
+		for ( var i = 0; i < 9; i += 4 )
+		{
+			if ( this.gridData[i].state == player ) 
+			{
+				consecutiveMarks++;
+			}
+		}
+		
+		if ( consecutiveMarks == 2 ) 
+		{
+			for ( var i = 0; i < 9; i += 4 )
+			{
+				if ( this.gridData[i].state == "empty" )
+				{
+					return i;
+				}
+			}
+		}
+		
+		// Diagonal Check 2	
+		var consecutiveMarks = 0;
+		for ( var i = 2; i <= 6; i += 2 )
+		{
+			if ( this.gridData[i].state == player ) 
+			{
+				consecutiveMarks++;
+			}
+		}
+		
+		if ( consecutiveMarks == 2 ) 
+		{
+			for ( var i = 2; i <= 6; i += 2 )
+			{
+				if ( this.gridData[i].state == "empty" )
+				{
+					return i;
+				}
+			}
+		}	
+		
+		stateGame.DebugMessage( "Could not find win position" );
+		return -1;
+	},
+	
+	LogBoard: function() 
+	{
+		stateGame.TrackMove( "\n" );
+		for ( var i = 0; i < 9; i++ )
+		{			
+			if ( this.gridData[i].state == "player" )
+			{
+				stateGame.TrackMove( "P" );
+			}
+			else if ( this.gridData[i].state == "computer" )
+			{
+				stateGame.TrackMove( "C" );
+			}
+			else
+			{
+				stateGame.TrackMove( "-" );
+			}
+			
+			if ( i == 2 || i == 5 || i == 8 ) 
+			{
+				stateGame.TrackMove( "\n" );
+			}
+		}
+	}
 }
--- scripts/main.js
+++ scripts/main.js
@@ -13,6 +13,23 @@ stateGame = {
 	{
 		$( "#debug-out" ).append( stateGame.GetTime() + message + "\n" );
 	},
+	
+	TrackMove: function( message )
+	{
+		$( "#move-tracker" ).append( message );
+	},
+	
+	TrackWin: function( player )
+	{
+		if ( player != "none" )
+		{
+			$( "#move-tracker" ).append( player + " won round " + stateGame.turnInfo.timesPlayed + "\n" );
+		}
+		else
+		{
+			$( "#move-tracker" ).append( "There was a tie for round " + stateGame.turnInfo.timesPlayed + "\n" );
+		}
+	},
 
 	Setup: function( settings, images )
 	{
@@ -27,7 +44,10 @@ stateGame = {
 		images.o = new Image();
 		images.o.src = images.pathbase + "o.png";
 		
+		stateGame.turnInfo.timesPlayed 	= 1;
 		stateGame.turnInfo.turn 		= "player";		
+		stateGame.turnInfo.turnCount 	= 0;
+		stateGame.turnInfo.lastMove 	= -1;
 		stateGame.turnInfo.computer 	= $( "input:radio[name='computer-movement']" ).val();
 		stateGame.turnInfo.player 		= $( "input:radio[name='player-movement']" ).val();
 		stateGame.turnInfo.gameOver = "false";
@@ -64,9 +84,11 @@ stateGame = {
 			if ( mouseX >= 0 && mouseX <= settings.width && mouseY >= 0 && mouseY <= settings.height &&
 				objGrid.ClickedGrid( mouseX, mouseY, "player" ) )
 			{
-				stateGame.turnInfo.turn = "computer";
-				stateGame.DebugMessage( "Turn: " + stateGame.turnInfo.turn );
+				stateGame.turnInfo.lastMove = objGrid.GetIndexAtPosition( mouseX, mouseY );
+				stateGame.ToggleTurns();
 			}
+			
+			stateGame.CheckForWinner();
 		}
 	},
 
@@ -76,30 +98,292 @@ stateGame = {
 		{
 			if ( stateGame.turnInfo.computer == "strategy" )
 			{
+				this.ComputerStrategy();
 			}
 			else if ( stateGame.turnInfo.computer == "random" )
 			{
-				var randBlock = parseInt( Math.random() * 9 );
-				
-				if ( objGrid.RandomGrid( randBlock, "computer" ) ) 
-				{
-					stateGame.turnInfo.turn = "player";
-					stateGame.DebugMessage( "Turn: " + stateGame.turnInfo.turn );
-				}
+				this.RandomStrategy( "computer" );
 			}
 		}
 		else if ( stateGame.turnInfo.turn == "player" && stateGame.turnInfo.player == "random" )
 		{
-			var randBlock = parseInt( Math.random() * 9 );
-			
-			if ( objGrid.RandomGrid( randBlock, "player" ) ) 
-			{
-				stateGame.turnInfo.turn = "computer";
-				stateGame.DebugMessage( "Turn: " + stateGame.turnInfo.turn );
-			}
+			this.RandomStrategy( "player" );
 		}
 		
 		// Check to see if anyone won this round, or if there are no blocks left.
+		this.CheckForWinner();
+	},
+
+	Draw: function( canvasWindow, settings, images )
+	{
+		// Draw background
+		canvasWindow.fillStyle = "#654f1b";
+		canvasWindow.fillRect( 0, 0, settings.width, settings.height );
+		
+		// Draw board grid & X's & O's
+		objGrid.Draw( canvasWindow, settings, images );
+	},
+	
+	ResetGame: function( settings )
+	{
+		stateGame.turnInfo.turnCount = 0;
+		objGrid.Setup( settings );
+		stateGame.DebugMessage( "Reset Game" );	
+	},
+	
+	ComputerStrategy: function()
+	{
+		stateGame.DebugMessage( "Computer strategy... turn #" + stateGame.turnInfo.turnCount );
+		
+		// First Move logic
+		if ( stateGame.turnInfo.turnCount == 0 )
+		{
+			// Computer is moving first, mark center block
+			if ( objGrid.RandomGrid( 4, "computer" ) ) 
+			{
+				stateGame.turnInfo.lastMove = 4;
+				this.ToggleTurns();
+				return;
+			}
+		}
+		
+		
+		else if ( stateGame.turnInfo.turnCount == 1 )
+		{
+			// Computer is responding to first movement
+			if ( stateGame.turnInfo.lastMove == 4 )
+			{
+				// Player marked center, mark a corner
+				var rand = parseInt( Math.random() * 4 );
+				if ( rand == 0 ) 
+				{
+					if ( objGrid.RandomGrid( 0, "computer" ) ) 
+					{
+						stateGame.turnInfo.lastMove = 0;
+						this.ToggleTurns();
+						return;
+					}
+				}
+				else if ( rand == 1 )
+				{
+					if ( objGrid.RandomGrid( 2, "computer" ) ) 
+					{
+						stateGame.turnInfo.lastMove = 2;
+						this.ToggleTurns();
+						return;
+					}
+
+				}
+				else if ( rand == 2 )
+				{
+					if ( objGrid.RandomGrid( 6, "computer" ) ) 
+					{
+						stateGame.turnInfo.lastMove = 6;
+						this.ToggleTurns();
+						return;
+					}
+
+				}
+				else if ( rand == 3 )
+				{
+					if ( objGrid.RandomGrid( 8, "computer" ) ) 
+					{
+						stateGame.turnInfo.lastMove = 8;
+						this.ToggleTurns();
+						return;
+					}
+
+				}
+			}
+			else 
+			{
+				// Player didn't mark center, mark the center for self.
+				if ( objGrid.RandomGrid( 4, "computer" ) ) 
+				{
+					stateGame.turnInfo.lastMove = 4;
+					this.ToggleTurns();
+					return;
+				}
+			}
+		}
+		
+		// Logic for other moves - At this point, the center is definitely taken.
+		else 
+		{
+			// 1. Try to find a win position for computer
+			var winPos = objGrid.FindWinIndex( "computer" );
+			if ( winPos != -1 )
+			{
+				if ( objGrid.RandomGrid( winPos, "computer" ) ) 
+				{
+					stateGame.turnInfo.lastMove = winPos;
+					this.ToggleTurns();
+					return;
+				}
+			}
+			
+			// 2. Try to block a win position from player
+			var winPos = objGrid.FindWinIndex( "player" );
+			if ( winPos != -1 )
+			{
+				if ( objGrid.RandomGrid( winPos, "computer" ) ) 
+				{
+					stateGame.turnInfo.lastMove = winPos;
+					this.ToggleTurns();
+					return;
+				}
+			}
+			
+			// 3. Try to make good move
+			stateGame.DebugMessage( "No winning positions, try to make good move" );
+			var lastMoveType = objGrid.EdgeOrCorner( stateGame.turnInfo.lastMove );
+			if ( lastMoveType == "edge" )
+			{
+				// Mark a corner that isn't in the same row/column
+				var moved = false;
+				var rand = parseInt( Math.random() * 2 );
+				
+				if ( stateGame.turnInfo.lastMove == 3 )
+				{
+					if ( rand == 0 && objGrid.RandomGrid( 2, "computer" ) ) 
+					{
+						stateGame.turnInfo.lastMove = 2;
+						this.ToggleTurns();
+						moved = true;
+						return;
+					}
+					else if ( rand == 1 && objGrid.RandomGrid( 8, "computer" ) ) 
+					{
+						stateGame.turnInfo.lastMove = 8;
+						this.ToggleTurns();
+						moved = true;
+						return;
+					}
+				}
+				else if ( stateGame.turnInfo.lastMove == 5 )
+				{
+					if ( rand == 0 && objGrid.RandomGrid( 0, "computer" ) ) 
+					{
+						stateGame.turnInfo.lastMove = 0;
+						this.ToggleTurns();
+						moved = true;
+						return;
+					}
+					else if ( rand == 1 && objGrid.RandomGrid( 6, "computer" ) ) 
+					{
+						stateGame.turnInfo.lastMove = 6;
+						this.ToggleTurns();
+						moved = true;
+						return;
+					}
+				}
+				else if ( stateGame.turnInfo.lastMove == 1 )
+				{
+					if ( rand == 0 && objGrid.RandomGrid( 6, "computer" ) ) 
+					{
+						stateGame.turnInfo.lastMove = 6;
+						this.ToggleTurns();
+						moved = true;
+						return;
+					}
+					else if ( rand == 1 && objGrid.RandomGrid( 8, "computer" ) ) 
+					{
+						stateGame.turnInfo.lastMove = 8;
+						this.ToggleTurns();
+						moved = true;
+						return;
+					}
+				}
+				else if ( stateGame.turnInfo.lastMove == 7 )
+				{
+					if ( rand == 0 && objGrid.RandomGrid( 0, "computer" ) ) 
+					{
+						stateGame.turnInfo.lastMove = 0;
+						this.ToggleTurns();
+						moved = true;
+						return;
+					}
+					else if ( rand == 1 && objGrid.RandomGrid( 2, "computer" ) ) 
+					{
+						stateGame.turnInfo.lastMove = 2;
+						this.ToggleTurns();
+						moved = true;
+						return;
+					}
+				}
+				
+				if ( moved == false )
+				{
+					// Couldn't move to an ideal position - random?
+					this.RandomStrategy( "computer" );
+					return;
+				}
+			}
+			else if ( lastMoveType == "corner" )
+			{
+				// Try to mark opposite corner
+				if ( stateGame.turnInfo.lastMove == 0 && objGrid.RandomGrid( 8, "computer" ) )
+				{
+					stateGame.turnInfo.lastMove = 8;
+					this.ToggleTurns();
+					return;
+				}
+				else if ( stateGame.turnInfo.lastMove == 2 && objGrid.RandomGrid( 6, "computer" ) )
+				{
+					stateGame.turnInfo.lastMove = 6;
+					this.ToggleTurns();
+					return;
+				}
+				else if ( stateGame.turnInfo.lastMove == 6 && objGrid.RandomGrid( 2, "computer" ) )
+				{
+					stateGame.turnInfo.lastMove = 2;
+					this.ToggleTurns();
+					return;
+				}
+				else if ( stateGame.turnInfo.lastMove == 8 && objGrid.RandomGrid( 0, "computer" ) )
+				{
+					stateGame.turnInfo.lastMove = 0;
+					this.ToggleTurns();
+					return;
+				}
+				else
+				{
+					// Unable to mark opposite corner
+					this.RandomStrategy( "computer" );
+					return;
+				}
+			}
+		}
+	},
+	
+	RandomStrategy: function( player )
+	{
+		var randBlock = parseInt( Math.random() * 9 );
+		
+		if ( objGrid.RandomGrid( randBlock, player ) ) 
+		{
+			stateGame.turnInfo.lastMove = randBlock;
+			this.ToggleTurns();
+		}
+	},
+	
+	ToggleTurns: function() 
+	{		
+		if ( stateGame.turnInfo.turn == "player" ) 
+		{
+			stateGame.turnInfo.turn = "computer";
+		}
+		else
+		{
+			stateGame.turnInfo.turn = "player";
+		}
+		
+		stateGame.turnInfo.turnCount++;
+		stateGame.DebugMessage( "Turn " + stateGame.turnInfo.turnCount + ": " + stateGame.turnInfo.turn );
+	},
+	
+	CheckForWinner: function() 
+	{
 		var whoWon = objGrid.WhoWon();
 		if ( objGrid.EmptyBlocksLeft() == 0 || whoWon != "none" ) 
 		{						
@@ -116,27 +400,14 @@ stateGame = {
 			{ 
 				$( "#wins-ties" ).html( parseInt( $( "#wins-ties" ).html() ) + 1 );
 				stateGame.DebugMessage( "Tie" );
-			 }
+			}
 			
+			objGrid.LogBoard();
+			stateGame.TrackWin( whoWon );
+			stateGame.turnInfo.timesPlayed++;			
 			
 			// TODO: Make reset prettier / have user click before resetting game
 			stateGame.ResetGame( settings );
 		}
-	},
-
-	Draw: function( canvasWindow, settings, images )
-	{
-		// Draw background
-		canvasWindow.fillStyle = "#654f1b";
-		canvasWindow.fillRect( 0, 0, settings.width, settings.height );
-		
-		// Draw board grid & X's & O's
-		objGrid.Draw( canvasWindow, settings, images );
-	},
-	
-	ResetGame: function( settings )
-	{
-		objGrid.Setup( settings );
-		stateGame.DebugMessage( "Reset Game" );	
 	}
 }
