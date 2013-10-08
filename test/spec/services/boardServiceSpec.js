da48c9c797ebf91887496ebb660467f9817cd9d5
William Scott <wds@williamscott.me>
2013-10-07 22:44:10 -0400

    Test Board get/set move helpers.

--- test/spec/services/boardServiceSpec.js
+++ test/spec/services/boardServiceSpec.js
@@ -14,5 +14,14 @@ describe('Service: boardService', function () {
     var board = service.newBoard();
     expect(Array.isArray(board.tiles)).toEqual(true);
     expect(board.tiles.length).toEqual(9);
-  })
+  });
+
+  describe('A Board', function() {
+    it('should be able to get and set moves.', function() {
+      var board = service.newBoard();
+      expect(board.getMove(1, 1)).toEqual('');
+      board.setMove(1, 1, 'X');
+      expect(board.getMove(1, 1)).toEqual('X');
+    });
+  });
 });
