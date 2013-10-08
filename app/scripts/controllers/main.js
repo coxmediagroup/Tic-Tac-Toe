891fa9e7e6efc60d9749a31fa1327a5576e0b6d9
William Scott <wds@williamscott.me>
2013-10-07 23:17:34 -0400

    Draw a simple tic-tac-toe board.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -2,5 +2,14 @@
 
 angular.module('TicTacToeApp')
   .controller('MainCtrl', ['$scope', 'boardService', function ($scope, boardService) {
+    $scope.Math = Math;
+    $scope.displaySymbols = {
+      O: '○',
+      X: '×'
+    };
+
     $scope.board = boardService.newBoard();
+    $scope.board.setMove(0, 0, 'X');
+    $scope.board.setMove(1, 1, 'O');
+    $scope.board.setMove(2, 2, 'X');
   }]);
