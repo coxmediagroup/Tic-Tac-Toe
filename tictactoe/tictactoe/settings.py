ef6e00f3cea798a7a6ddbe36d098f78a503f4348
Kurtis Mullins <kurtis.mullins@gmail.com>
2013-11-05 13:30:10 -0500

    Re-introduced white-space for consistency.

--- tictactoe/tictactoe/settings.py
+++ tictactoe/tictactoe/settings.py
@@ -64,10 +64,15 @@ DATABASES = {
 
 # Internationalization
 # https://docs.djangoproject.com/en/dev/topics/i18n/
+
 LANGUAGE_CODE = 'en-us'
+
 TIME_ZONE = 'America/New_York' # Eastern Time
+
 USE_I18N = True
+
 USE_L10N = True
+
 USE_TZ = True
 
 
