72bf488b49360eba3e4350f937fc63e52c51d997
Kurtis Mullins <kurtis.mullins@gmail.com>
2013-11-05 21:42:25 -0500

    Implemented a disabled state for Game Board Cells.
    When a User adds a piece to the board, that cell (or box) is no longer
    available to add new pieces. I have labeled this a 'disabled state'.
    
    During a disabled state, the game board cell will show a red background
    if the User hovers over it. I may decide later to remove the 'pointer'
    cursor in this state as well.

--- static/js/app.js
+++ static/js/app.js
@@ -2,19 +2,26 @@ var userFontClass = "fa-linux";
 
 /* Inserts the User's Selected piece into the box specified. */
 var insertPiece = function(element) {
+
+	// Create the Game Piece Element
 	var gamePiece = $(document.createElement("i"))
 						.addClass("fa fa-5x")
 						.addClass(userFontClass);
 
-	$(element).append(gamePiece);
-}
+	// Insert and Disable the box.
+	$(element)
+		.append(gamePiece)
+		.addClass('disabled');
+
+};
+
 
 $(document).ready(function() {
 
 	// Insert a Game Piece when a User selects a position on the Game Board. 
 	$('#game-table tbody tr td').click(function() {
 		$(this).has('i').length 
-			? notifyUser("Please select an empty box.")
+			? console.log("This cell already contains a game piece.")
 			: insertPiece(this);
 	});
 
