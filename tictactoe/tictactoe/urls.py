d36a3d916efebb4ed070120e2dc654c299dcd9c8
Kurtis Mullins <kurtis.mullins@gmail.com>
2013-11-05 20:19:43 -0500

    Created a URL to render the application template.
    This is accessible at the root path. (e.g. http://localhost:8000)

--- tictactoe/tictactoe/urls.py
+++ tictactoe/tictactoe/urls.py
@@ -1,4 +1,5 @@
 from django.conf.urls import patterns, include, url
+from django.views.generic import TemplateView
 
 from django.contrib import admin
 admin.autodiscover()
@@ -8,5 +9,9 @@ urlpatterns = patterns('',
     # url(r'^$', 'tictactoe.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    # Sample 'Home' Page
+    url(r'^$', TemplateView.as_view(template_name='app.html'), name='home'),
+
+    # Django Admin
     url(r'^admin/', include(admin.site.urls)),
 )
