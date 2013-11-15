ef5ed0e674584c87ab0bc615383faedd4de62c8a
Kurtis Mullins <kurtis.mullins@gmail.com>
2013-11-14 19:04:33 -0500

    Tidied up a JQuery selector.

--- static/js/app.js
+++ static/js/app.js
@@ -29,7 +29,7 @@ var setStatusMessage = function(message, messageClass) {
 $(document).ready(function() {
 
 	// Insert a Game Piece when a User selects a position on the Game Board. 
-	$('#game-table tbody tr td').click(function() {
+	$('.piece-box').click(function() {
 		$(this).has('i').length 
 			? console.log("This cell already contains a game piece.")
 			: insertPiece(this);
