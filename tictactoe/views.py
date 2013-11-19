14e12d0f02966afe03081299b446134d737a18d7
Kurtis Mullins <kurtis.mullins@gmail.com>
2013-11-19 16:45:50 -0500

    Code clean-up according to pylint warnings.
    * My IDE decided to use Tabs instead of Spaces. This is pretty big
    no-no in Python.
    * I had used a single variable reference when it was un-needed.
    * Removed a single trailing white-space.

--- tictactoe/views.py
+++ tictactoe/views.py
@@ -6,71 +6,71 @@ from tictactoe.ai import get_best_move
 from tictactoe.models import Board
 
 def game_view(request):
-	""" Renders the Game Board """
+    """ Renders the Game Board """
 
-	# Get or Create a new Game Board
-	board_id = request.session.get('board_id', None)
-	if board_id:
-		board = Board.objects.get(id=board_id)
-	else:
-		board = Board.objects.create()
-		request.session['board_id'] = board.id
-		messages.success(request, 'Started a New Game. Your turn!')
+    # Get or Create a new Game Board
+    board_id = request.session.get('board_id', None)
+    if board_id:
+        board = Board.objects.get(id=board_id)
+    else:
+        board = Board.objects.create()
+        request.session['board_id'] = board.id
+        messages.success(request, 'Started a New Game. Your turn!')
 
-	# Check for Victory
-	if board.victory_status() == -1:
-		messages.warning(request, 'The Computer has won the Game')
-	elif board.victory_status() == 1:
-		messages.success(request, 'You have miraculously won the Game!')
-	elif board.victory_status() == 0:
-		messages.info(request, 'The Game has ended in a Draw')
+    # Check for Victory
+    if board.victory_status() == -1:
+        messages.warning(request, 'The Computer has won the Game')
+    elif board.victory_status() == 1:
+        messages.success(request, 'You have miraculously won the Game!')
+    elif board.victory_status() == 0:
+        messages.info(request, 'The Game has ended in a Draw')
 
-	return render(request, 'app.html', { 'board': board })
+    return render(request, 'app.html', { 'board': board })
 
 def select_piece(request):
-	"""
-	Accepts a Piece Selection from a User, executes the Computer Player's next 
-	move, and redirects the User back to the `game_view`.
-	"""
+    """
+    Accepts a Piece Selection from a User, executes the Computer Player's next
+    move, and redirects the User back to the `game_view`.
+    """
 
-	# Get or Create a new Game Board
-	board_id = request.session.get('board_id', None)
-	if board_id:
-		board = Board.objects.get(id=board_id)
-	else:
-		board = Board.objects.create()
-		request.session['board_id'] = board.id
+    # Get or Create a new Game Board
+    board_id = request.session.get('board_id', None)
+    if board_id:
+        board = Board.objects.get(id=board_id)
+    else:
+        board = Board.objects.create()
+        request.session['board_id'] = board.id
 
-	# Check for Victory
-	if board.victory_status() == -1:
-		return redirect('home')
-	elif board.victory_status() == 1:
-		return redirect('home')
+    # Check for Victory
+    if board.victory_status() == -1:
+        return redirect('home')
+    elif board.victory_status() == 1:
+        return redirect('home')
 
-	# Set the User's and Computer's Selection
-	if request.method == 'POST':
-		if 'selection' in request.POST:
-			selection = request.POST['selection']
-			if getattr(board, selection) == 0:
-				piece = setattr(board, selection, 1)
-				computer_selection = get_best_move(board, -1)
-				if computer_selection:
-					setattr(board, computer_selection, -1)
-				board.save()
-			else:
-				messages.warning(request, "Cannot place a Game Piece here")
+    # Set the User's and Computer's Selection
+    if request.method == 'POST':
+        if 'selection' in request.POST:
+            selection = request.POST['selection']
+            if getattr(board, selection) == 0:
+                setattr(board, selection, 1)
+                computer_selection = get_best_move(board, -1)
+                if computer_selection:
+                    setattr(board, computer_selection, -1)
+                board.save()
+            else:
+                messages.warning(request, "Cannot place a Game Piece here")
 
-	return redirect('home')
+    return redirect('home')
 
 def end_game(request):
-	""" Ends the current game and redirects the User to play a new Game. """
+    """ Ends the current game and redirects the User to play a new Game. """
 
-	# Get and delete the current Game Board if it exists.
-	board_id = request.session.get('board_id', None)
-	if board_id:
-		request.session.pop('board_id')
-		board = Board.objects.get(id=board_id)
-		board.delete()
+    # Get and delete the current Game Board if it exists.
+    board_id = request.session.get('board_id', None)
+    if board_id:
+        request.session.pop('board_id')
+        board = Board.objects.get(id=board_id)
+        board.delete()
 
-	# Redirect the User to play the Game.
-	return redirect('home')
+    # Redirect the User to play the Game.
+    return redirect('home')
