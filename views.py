4a0f1876f1e087dbd91c4d2a587ea7c8cd34a55e
Christa Roberts <cgbroberts@yahoo.com>
2013-12-09 10:13:47 -0600

    Set up board using template and css.

--- views.py
+++ views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render_to_response
 
 def index(request):
-    return HttpResponse("Would you like to play a game?")
+
+    return render_to_response('tic_tac_toe/tic_tac_toe.html');
