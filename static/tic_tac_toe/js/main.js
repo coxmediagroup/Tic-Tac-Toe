01784d25550a9a569eeec989051d5384307d1227
Christa Roberts <cgbroberts@yahoo.com>
2013-12-09 12:03:41 -0600

    Allowed user to change their mind before submission.

--- static/tic_tac_toe/js/main.js
+++ static/tic_tac_toe/js/main.js
@@ -1,3 +1,10 @@
+var current_move = '';
+
 function addToken(cell) {
+    if ( current_move ) { 
+        var current_move_cell = document.getElementById(current_move);
+        current_move_cell.innerHTML = "";
+    }
     cell.innerHTML = "O";
+    current_move = cell.id
 }
