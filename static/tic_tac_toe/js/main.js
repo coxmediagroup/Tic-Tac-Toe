34a65937c499efd123db01d24f05c0d699bea51c
Christa Roberts <cgbroberts@yahoo.com>
2013-12-09 19:52:58 -0600

    Changed the input buttons to hidden input fields as the buttons wouldn't come through in the POST data.  Implemented the initial functionality for a user move submission.

--- static/tic_tac_toe/js/main.js
+++ static/tic_tac_toe/js/main.js
@@ -4,8 +4,11 @@ function addToken(cell) {
 
     if ( current_move ) { 
         var current_move_cell = document.getElementById(current_move);
-        current_move_cell.value = "";
+        current_move_cell.innerHTML = "";
     }
-    cell.value = "O";
-    current_move = cell.id
+    cell.innerHTML = "O";
+    current_move = cell.id;
+
+    var move_input = document.getElementsByName(current_move)[0];
+    move_input.value = "O";
 }
--- views.py
+++ views.py
@@ -35,8 +35,32 @@ def index(request):
     }, RequestContext(request));
 
 def user_move(request):
+
+    upper_row = None
+
+    if request.method == "POST":
+        a1 = request.POST.get('A1', '')
+        a2 = request.POST.get('A2', '')
+        a3 = request.POST.get('A3', '')
+        b1 = request.POST.get('B1', '')
+        b2 = request.POST.get('B2', '')
+        b3 = request.POST.get('B3', '')
+        c1 = request.POST.get('C1', '')
+        c2 = request.POST.get('C2', '')
+        c3 = request.POST.get('C3', '')
+    
+        upper_row = [ { 'index': 'A1', 'value': a1, 'position': 'left' }, 
+            { 'index': 'A2', 'value': a2, 'position': 'center' }, 
+            { 'index': 'A3', 'value': a3, 'position': 'right' } ]
+        middle_row = [ { 'index': 'B1', 'value': b1, 'position': 'left' }, 
+            { 'index': 'B2', 'value': b2, 'position': 'center' }, 
+            { 'index': 'B3', 'value': b3, 'position': 'right' } ]
+        bottom_row = [ { 'index': 'C1', 'value': c1, 'position': 'left' }, 
+            { 'index': 'C2', 'value': c2, 'position': 'center' }, 
+            { 'index': 'C3', 'value': c3, 'position': 'right' } ]
+
     return render_to_response('tic_tac_toe/tic_tac_toe.html', {
-        'upper_row': None,
-        'middle_row': None,
-        'bottom_row': None
+        'upper_row': upper_row,
+        'middle_row': middle_row,
+        'bottom_row': bottom_row
     }, RequestContext(request));
