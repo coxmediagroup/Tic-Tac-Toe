0fa446e141e73f9eb71ee725baa23112c97f28b1
Jeff Bradberry <jeff.bradberry@gmail.com>
2013-12-19 18:43:41 -0500

    Cleaned up and improved the populate_nextmove management command.

--- tictactoe/management/commands/populate_nextmove.py
+++ tictactoe/management/commands/populate_nextmove.py
@@ -11,55 +11,83 @@ class Command(BaseCommand):
     def handle(self, *args, **options):
         self.table = {}
 
+        # recursively find the reachable positions, collapsing down
+        # the positions that are effectively the same due to symmetry
         self._visit(' '*9)
 
+        self.stdout.write(
+            "{0} essentially different positions found.".format(
+                len(self.table)))
+
+        # clear out any old ones that were in the db
+        NextMove.objects.all().delete()
+
         for state, dct in self.table.iteritems():
-            if ' ' not in state: # ditch the draw positions,
-                continue         # we want playable ones
+            if dct['move'] is None: # ditch the endgame positions,
+                continue            # we want playable ones
             r, c, player = dct['move']
             NextMove.objects.create(state=state, row=r, column=c)
 
     def _visit(self, position):
-        self.table[position] = {'result': None, 'move': None, 'ratio': None}
-        player = 'ox'[position.count(' ') % 2]
+        # result: 'x', 'o', or 'draw'
+        # move: (r, c, player) where player is 'x' or 'o'
+        # losses: the number of losing moves for this position for this player
+        self.table[position] = {'result': None, 'move': None, 'losses': None}
+
+        player, other = ('x', 'o') if position.count(' ') % 2 else ('o', 'x')
+
+        if Position._is_won(position):
+            # a position that is won will always be due to the move of
+            # the previous player, so this position is maximally bad
+            # for the current player
+            self.table[position].update(result=other, losses=9)
+            return
 
         plays = [(r, c) for r in xrange(3)
                  for c in xrange(3)
                  if position[3*r + c] == ' ']
 
         if not plays:
-            self.table[position].update(result='draw', ratio=0.0)
+            # this position is ok for the current player, since it
+            # isn't an outright loss, and there is no more trouble we
+            # can get into
+            self.table[position].update(result='draw', losses=0)
             return
 
         play_mapping, child_mapping = {}, {}
-        wins = 0
+        losses = 0
         for r, c in plays:
             found = False
             result = Position._play(position, (r, c, player))
+
+            # for a raw result position, which move was used
             play_mapping[result] = (r, c, player)
+
+            # for a given raw position, which symmetric equivalent can
+            # be found in the table
+            child_mapping[result] = result
+
             for child in Position.expand_symmetry(result):
                 if child in self.table:
                     child_mapping[result] = child
-                    result = child
-                    wins += 1 if self.table[child]['result'] == player else 0
+                    if self.table[child]['result'] == other:
+                        losses += 1
                     found = True
                     break
-
-            if Position._is_won(result):
-                self.table[position].update(result=player, move=(r, c, player),
-                                            ratio=1.0)
-                return
 
             if found:
                 continue
 
-            child_mapping[result] = result
             self._visit(result)
-            wins += 1 if self.table[result]['result'] == player else 0
+            if self.table[result]['result'] == other:
+                losses += 1
 
-        self.table[position]['ratio'] = wins / len(play_mapping)
+        # We want to choose the move that maximizes the number of losing
+        # moves available to the other player.
+        L, move, pos = max((self.table[child_mapping[p]]['losses'], m, p)
+                           for p, m in play_mapping.iteritems())
 
-        ratio, move, pos = min((self.table[child_mapping[p]]['ratio'], m, p)
-                               for p, m in play_mapping.iteritems())
         self.table[position].update(
-            result=self.table[child_mapping[pos]]['result'], move=move)
+            result=self.table[child_mapping[pos]]['result'],
+            losses=losses, move=move
+        )
