da3e5be51680d40d872af9af74d01d8ef42c6960
Jeff Bradberry <jeff.bradberry@gmail.com>
2013-12-19 19:47:15 -0500

    Added ajax view SubmitMoveView.

--- tictactoe/urls.py
+++ tictactoe/urls.py
@@ -10,4 +10,6 @@ urlpatterns = patterns('',
     url(r'^create/$', views.CreateGameView.as_view(), name='create_game'),
     url(r'^game/(?P<pk>\d+)/$', views.GameDetailView.as_view(),
         name='game_detail'),
+    url(r'^game/(?P<pk>\d+)/move/$', views.SubmitMoveView.as_view(),
+        name='submit_move'),
 )
--- tictactoe/views.py
+++ tictactoe/views.py
@@ -1,8 +1,12 @@
 from django.views.generic import CreateView, DetailView
+from django.views.generic.detail import SingleObjectMixin
+from django.views.generic.edit import BaseFormView
 from django.contrib.auth.decorators import login_required
+from django.http import HttpResponse, Http404, HttpResponseForbidden
 from django.utils.decorators import method_decorator
 
 from . import models, forms
+import json
 
 
 class CreateGameView(CreateView):
@@ -28,3 +32,57 @@ class CreateGameView(CreateView):
 class GameDetailView(DetailView):
     model = models.Game
     context_object_name = 'game'
+
+
+class SubmitMoveView(SingleObjectMixin, BaseFormView):
+    model = models.Game
+    form_class = forms.MoveForm
+
+    def render_to_json_response(self, context, **response_kwargs):
+        data = json.dumps(context)
+        response_kwargs['content_type'] = 'application/json'
+        return HttpResponse(data, **response_kwargs)
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if self.object.user != self.request.user:
+            return self.render_to_json_response(
+                {'error': "You are not authorized to submit"
+                 " turns for this game.",
+                 'state': self.object.positions.latest().state},
+                status=400
+            )
+        return super(SubmitMoveView, self).get(request, *args, **kwargs)
+
+    def post(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if self.object.user != self.request.user:
+            return self.render_to_json_response(
+                {'error': "You are not authorized to submit"
+                 " turns for this game.",
+                 'state': self.object.positions.latest().state},
+                status=400
+            )
+        return super(SubmitMoveView, self).post(request, *args, **kwargs)
+
+    def get_form_kwargs(self):
+        kwargs = super(SubmitMoveView, self).get_form_kwargs()
+        kwargs.update(game=self.object)
+        return kwargs
+
+    def form_valid(self, form):
+        row = form.cleaned_data.get('row')
+        column = form.cleaned_data.get('column')
+        new_position = self.object.positions.latest().new((row, column))
+
+        if ' ' in new_position.state and not new_position.is_won():
+            next_move = models.NextMove.objects.lookup(new_position.state)
+            new_position = new_position.new(next_move)
+
+        data = {'state': new_position.state, 'done': True}
+        return self.render_to_json_response(data)
+
+    def form_invalid(self, form):
+        data = {'state': self.object.positions.latest().state}
+        data.update(**form.errors)
+        return self.render_to_json_response(data, status=400)
