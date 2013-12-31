5998ec6f06a893ad16265fa5c9bf7e247b126553
logofhog <thomasscoggins@gmail.com>
2013-12-30 22:56:12 -0500

    added algorithm for computer move

--- tictactoe/game/models.py
+++ tictactoe/game/models.py
@@ -10,11 +10,18 @@ class SingleGame(models.Model):
     state = models.CharField(max_length = 50)
     player_piece = models.CharField(max_length = 1)
     
+    WINNING_COMBINATIONS = [(0,1,2), (3,4,5), (6,7,8), #horizontal
+                           (0,3,6), (1,4,7), (2,5,8), #vert
+                           (2,4,6), (0,4,8)]          #diag
+
+
     def move_added(self, spot):
+        if self.state.find(str(spot)) > -1:
+            return -1
         self.make_move(spot, 'P')
-        computer_move = self.make_computer_move()
-        self.make_move(computer_move, 'C')
-        return computer_move
+        computer_move = self.make_computer_move(self.state, 'C')
+        self.make_move(computer_move[1], 'C')
+        return computer_move[1]
 
     def make_move(self, spot, piece):
         if len(self.state) > 0:
@@ -24,8 +31,56 @@ class SingleGame(models.Model):
         self.save()
         return
 
-    def make_computer_move(self):
-        return choice(self.get_unused_squares(self.state))
+    def make_computer_move(self, state, player = 'C'):
+        next_player = self.player_toggle(player)
+        unused_squares = self.get_unused_squares(state)
+        #
+        # end points for recursion
+        #
+        if self.is_won(state, next_player): 
+            if player == 'P':
+                return (1, -1)
+            else:
+                return (-1, -1)
+        if len(unused_squares) == 0: #tie
+            return (0, -1)
+
+        if player == 'C': #choose best square for computer
+            best_score = -1
+            best_square = -1
+            for square in unused_squares:
+                temp_state = state + ',' + str(square) +player
+                score = self.make_computer_move(temp_state, next_player)
+                if best_score < score[0]:
+                    best_score = score[0]
+                    best_square = square
+            return (best_score, best_square)
+        else:
+            best_score = 1
+            best_square = -1
+            for square in unused_squares:
+                temp_state = state + ',' + str(square) + player
+                score = self.make_computer_move(temp_state, next_player)
+                if best_score > score[0]:
+                    best_score = score[0]
+                    best_square = square
+            return (best_score, best_square)
+
+    def is_won(self, state, player):
+        # 
+        # tests to see if a player is occupying all 3 spots
+        # in WINNING_COMBINATIONS
+        #
+        for combo in self.WINNING_COMBINATIONS:
+            is_won = True
+            for i in combo:
+                if state.find(str(i) + player) == -1:
+                    is_won = False
+            if is_won:
+                return True
+        return False
+
+
     
     def get_unused_squares(self, state):
         unused_squares = []
@@ -33,6 +88,12 @@ class SingleGame(models.Model):
             if state.find(str(num)) == -1:
                 unused_squares.append(num)
         return unused_squares
+
+    def player_toggle(self, player):
+        return 'P' if player == 'C' else 'C'
+
+
+
 
 
         
--- tictactoe/game/tests.py
+++ tictactoe/game/tests.py
@@ -21,3 +21,19 @@ class SimpleTest(TestCase):
         state = '0P,1C,2C,3P,8P,5C,6P,7C'
         self.assertEqual(self.new_game.get_unused_squares(state), [4])
         
+    def test_player_toggle(self):
+        self.assertEqual(self.new_game.player_toggle('P'), 'C')
+        self.assertEqual(self.new_game.player_toggle('C'), 'P')
+
+    def test_is_won(self):
+        state = '0P,1P,2P'
+        self.assertTrue(self.new_game.is_won(state, 'P'))
+        state = '0P,1C,2P'
+        self.assertFalse(self.new_game.is_won(state, 'P'))
+        state = '0P,1P,2C,3P,4C,5P,6P,7C,8C'
+        self.assertTrue(self.new_game.is_won(state, 'P'))
+        state = '4P,0C,2P,6C,8P,3C'
+        self.assertTrue(self.new_game.is_won(state, 'C'))
+        state = '1P,2C'
+        self.assertFalse(self.new_game.is_won(state, 'P'))
+        self.assertFalse(self.new_game.is_won(state, 'C'))
