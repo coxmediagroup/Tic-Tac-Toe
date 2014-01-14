e8ed01f7e9f57567a81d9eeba3fac27d8382eac8
Jeff Bradberry <jeff.bradberry@gmail.com>
2014-01-14 16:32:01 -0500

    Added setup.py.
    \facepalm

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='Tic-Tac-Toe',
+    version='0.1dev',
+    author='Jeff Bradberry',
+    author_email='jeff.bradberry@gmail.com',
+    packages=['tictactoe'],
+    description="A play-by-web game of Tic-Tac-Toe.",
+)
