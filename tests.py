f6027c578049f5751419c89b78551d229de889f4
Shawn Frame <shawn.frame@indepthstudios.com>
2014-01-30 22:21:11 -0500

    Add more tests
    Signed-off-by: Shawn Frame <shawn.frame@indepthstudios.com>

--- tests.py
+++ tests.py
@@ -12,11 +12,33 @@ def new_board():
     assert all(len(x) == b.COLS for x in b.board)
     return 'new board: "{0}"'.format(b.board)
 
+def board_positions():
+    """
+     1 | 2 | 3 
+    --- --- ---
+     4 | 5 | 6 
+    --- --- ---
+     7 | 8 | 9
+     
+    """
+    b = Board(rows=16, cols=16)
+    last_space_on_board = b.board[-1][-1].board_index
+    
+    for x in range(1, last_space_on_board+1):
+        row, col = b.board_position_by_index(x)
+        board_space_index = b.board[row][col].board_index
+        msg = "b.board[{}][{}].board_index == {} (It is {}!)"
+        msg = msg.format(row, col, x, board_space_index)
+        assert b.board[row][col].board_index == x, msg
+    
+    return True
+
 def tests():
     """Tests for the Board class"""
     # test that a new board is rational
     new_space()
     new_board()
+    board_positions()
 
     print "All Board tests have passed!"
 
