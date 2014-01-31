321a6bd7de4a650a17d4ca0bfe372dbad6df608a
Shawn Frame <shawn.frame@indepthstudios.com>
2014-01-30 22:25:08 -0500

    Change board index display to be 1-based index (instead of 0-based)
    Signed-off-by: Shawn Frame <shawn.frame@indepthstudios.com>

--- __main__.py
+++ __main__.py
@@ -8,37 +8,12 @@
 import curses
 from Board import Board
 
-def display(screen, line_number, msg, x_pos=2, **kwargs):
-    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
-    is_error = kwargs.get('error', False)
-    if is_error:
-        screen.addstr(line_number, x_pos, msg, curses.color_pair(1))
-    else:
-        screen.addstr(line_number, x_pos, msg)
-    screen.refresh()
-    line_number += 1
-    return line_number
-
-def show_board(screen, board, line_number, number_view):
-
-    row_separator = (u'--- ' * board.COLS).strip()
-
-    i = 0
-    for row in board.board:
-        msg =  ''
-        for space in row:
-            if number_view:
-                value = space.board_index
-            else:
-                value = space.player
-            msg += '{}|'.format(str(value).center(3, ' '))
-        msg = msg.strip('|')
-        line_number = display(screen, line_number, msg)
-        if i < (board.ROWS -1):
-            line_number = display(screen, line_number, row_separator)
-        i += 1
-
-    return line_number
+def set_style(screen, styles, unstyle=False):
+    for style in styles:
+        if unstyle:
+            screen.attroff(style)
+        else:
+            screen.attron(style)
 
 def main(screen):
 
@@ -55,6 +30,70 @@ def main(screen):
 
     # Collect input errors
     errors = []
+
+    # ==========
+    # = Styles =
+    # ==========
+    
+    # Error msgs
+    curses.init_pair(1, curses.COLOR_RED, curses.COLOR_WHITE)
+    ERROR_COLORS = curses.color_pair(1)
+
+    # player winning spaces
+    curses.init_pair(2, curses.COLOR_GREEN, curses.COLOR_BLACK)
+    WINNER_COLORS = curses.color_pair(2)
+
+    # computer winning spaces
+    curses.init_pair(3, curses.COLOR_MAGENTA, curses.COLOR_BLACK)
+    LOSING_COLORS = curses.color_pair(3)
+
+    # visual indicator of a previous move
+    curses.init_pair(4, curses.COLOR_CYAN, curses.COLOR_BLACK)
+    LAST_MOVE_COLORS = curses.color_pair(4)
+
+    def display(screen, line_number, msg, letter_position=2, **kwargs):
+        is_error = kwargs.get('error', False)
+        if is_error:
+            set_style(screen, [ERROR_COLORS])
+        screen.addstr(line_number, letter_position, msg)
+        if is_error:
+            set_style(screen, [ERROR_COLORS], unstyle=True)
+        screen.refresh()
+        line_number += 1
+        return line_number
+
+    def show_board(screen, board, line_number, number_view):
+
+        row_separator = (u'--- ' * board.COLS).strip()
+
+        i = 0
+        for row in board.board:
+            msg =  ''
+            letter_position = 2
+            for x, space in enumerate(row):
+                styles = []
+                if number_view:
+                    value = space.board_index
+                else:
+                    value = space.player
+                    if space.last_move:
+                        styles.append(LAST_MOVE_COLORS)
+                set_style(screen, styles)
+                msg = str(value).center(3, ' ')
+                screen.addstr(line_number, letter_position, msg)
+                set_style(screen, styles, unstyle=True)
+                letter_position += len(msg)
+                if x < len(row)-1:
+                    msg = '|'
+                    screen.addstr(line_number, letter_position, msg)
+                    letter_position += len(msg)
+            line_number += 1
+
+            if i < (board.ROWS -1):
+                line_number = display(screen, line_number, row_separator)
+            i += 1
+
+        return line_number
 
     while True:
 
@@ -125,23 +164,22 @@ def main(screen):
                 curses.echo()
                 curses.nocbreak()
                 curses.curs_set(2)
-                msg = 'Enter an integer (0-{}): '.format(b.last_space_index())
+                msg = 'Enter an integer (1-{}): '.format(b.last_space_index())
                 line_number = display(screen, line_number, msg)
                 board_index = screen.getstr(line_number-1, 2+len(msg), 3)
                 board_index = board_index.strip()
+                msg = '"{}" is not valid, please try again!'
                 try:
                     board_index = int(board_index)
-                    if b.player_to_space(this_player, board_index):
+                    if b.move_player_to_space(this_player, board_index):
                         next_player, this_player = this_player, next_player
                     else:
-                        errors.append('"{}" not a valid move!'.format(
-                            board_index))
+                        errors.append(msg.format(board_index))
                 except:
-                    errors.append('"{}" not valid, please try again!'.format(
-                        board_index))
+                    errors.append(msg.format(board_index))
                 finally:
                     curses.noecho()
-                    curses.cbreak()
+                    # curses.cbreak()
                     curses.curs_set(0)
                     toggle_move_text = False
                     number_view = False
