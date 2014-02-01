9516fec62808f70d4d83d4c42ee9ff58a06efa0d
Ben Rose <brose@logarythmics.com>
2014-02-01 14:54:21 -0500

    First simple test case added

--- tictactoe/board/tests.py
+++ tictactoe/board/tests.py
@@ -1,3 +1,22 @@
 from django.test import TestCase
+from models import Player, Game, Move
+from django.db import IntegrityError
+import unittest
 
-# Create your tests here.
+class SavingModelsTestCase(TestCase):
+  def setUp(self):
+    player_1 = Player.objects.create(name="Person1", is_human=True)
+    player_2 = Player.objects.create(name="Person2", is_human=False)
+    Game.objects.create(player_1=player_1, player_2=player_2)
+
+  def test_save_move_normal(self):
+    ''' Tests a simple move to valid location.
+        Expected: no exceptions.
+    '''
+    game = Game.objects.get(id=1)
+    player1 = Player.objects.get(id=1)
+    move = Move(game=game, player=player1, position_x=1, position_y=1)
+    move.save()
+    from_db = Move.objects.get(id=1)
+    self.assertEqual(from_db.position_x, 1)
+
