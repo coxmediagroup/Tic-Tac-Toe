1cd75fd301d14ff865d8fc495f1c006f6a84e1c5
Ben Rose <brose@logarythmics.com>
2014-02-01 15:21:54 -0500

    Finished tests for saving move model

--- tictactoe/board/tests.py
+++ tictactoe/board/tests.py
@@ -3,7 +3,11 @@ from models import Player, Game, Move
 from django.db import IntegrityError
 import unittest
 
-class SavingModelsTestCase(TestCase):
+class SavingMoveModelTestCase(TestCase):
+  '''
+     Testing custom integrity checks for the Move model.
+  '''
+
   def setUp(self):
     player_1 = Player.objects.create(name="Person1", is_human=True)
     player_2 = Player.objects.create(name="Person2", is_human=False)
@@ -19,4 +23,53 @@ class SavingModelsTestCase(TestCase):
     move.save()
     from_db = Move.objects.get(id=1)
     self.assertEqual(from_db.position_x, 1)
+
+  def test_save_move_duplicate_positions(self):
+    ''' Tests second move in same x/y position from different player.
+        Expected: An exception is raised with message for unique together (game, x, y).
+    '''
+    game = Game.objects.get(id=1)
+    player1 = Player.objects.get(id=1)
+    player2 = Player.objects.get(id=2)
+    move = Move(game=game, player=player1, position_x=1, position_y=1)
+    move2 = Move(game=game, player=player2, position_x=1, position_y=1)
+    move.save()
+    from_db = Move.objects.get(id=1)
+    self.assertEqual(from_db.position_x, 1)
+    with self.assertRaises(IntegrityError) as context:
+      move2.save()
+    self.assertEqual(context.exception.message, 'columns game_id, position_x, position_y are not unique')
+
+  def test_save_same_player_immediate_succession(self):
+    ''' Tests same player making otherwise valid moves back to back.
+        Expected: An exception is raised with message for consecutive moves.
+    '''
+    game = Game.objects.get(id=1)
+    player1 = Player.objects.get(id=1)
+    player2 = Player.objects.get(id=2)
+    move = Move(game=game, player=player1, position_x=1, position_y=1)
+    move2 = Move(game=game, player=player1, position_x=1, position_y=2)
+    move.save()
+    from_db = Move.objects.get(id=1)
+    self.assertEqual(from_db.position_x, 1)
+    with self.assertRaises(IntegrityError) as context:
+      move2.save()
+    self.assertEqual(context.exception.message, 'Same player cannot make consecutive moves in the same game')
+
+  def test_invalid_positions(self):
+    ''' Tests for positions outside of possible values.
+        Expected: Exception raised
+    '''
+    game = Game.objects.get(id=1)
+    player1 = Player.objects.get(id=1)
+    invalid_position = 7
+    move = Move(game=game, player=player1, position_x=invalid_position, position_y=1)
+    with self.assertRaises(IntegrityError) as context:
+      move.save()
+    self.assertEqual(context.exception.message, 'position_x, %s is outside of valid range,0-2' % invalid_position)
+    move.position_x = 0
+    move.position_y = invalid_position
+    with self.assertRaises(IntegrityError) as context:
+      move.save()
+    self.assertEqual(context.exception.message, 'position_y, %s is outside of valid range,0-2' % invalid_position)
 
