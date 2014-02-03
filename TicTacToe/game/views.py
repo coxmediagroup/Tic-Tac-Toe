b321fb392473698231084f3fab48cce10fdf8f14
Lehel Kovach <lehelkovach@gmail.com>
2014-02-03 13:30:51 -0800

    added necessary views, templates, images

--- TicTacToe/game/views.py
+++ TicTacToe/game/views.py
@@ -1,3 +1,22 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
-# Create your views here.
+#handles the "start game" view
+def start_game(request):
+    context = {}
+    return render(request, 'game/startgame.html', context)
+
+#handles when player presses "Start Game" button
+def launch(request):
+    context = {}
+    return render(request, 'game/game.html', context)
+
+#loads the current game based on settings in memory
+def game_page(request):
+    context = {}
+    return render(request, 'game/game.html', context)
+
+def ajax_make_move(request):
+    context = {}
+    return render(request, 'game/game.html', context)
+
