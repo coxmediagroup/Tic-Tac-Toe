f0fcbeba4d38c72fc3eb925e826d3df5ec95f7f8
Lehel Kovach <lehelkovach@gmail.com>
2014-02-03 17:28:51 -0800

    added code for handling ajax requests of the game state

--- TicTacToe/game/views.py
+++ TicTacToe/game/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+import simplejson
+from django.core.cache import cache
 
 #handles the "start game" view
 def start_game(request):
@@ -17,6 +19,25 @@ def game_page(request):
     return render(request, 'game/game.html', context)
 
 def ajax_make_move(request):
-    context = {}
-    return render(request, 'game/game.html', context)
+    if request.is_ajax():
+        callback = request.GET.get('callback', '')
+        req = {}
+        req ['box1'] = cache.get('tictactoe_box1')
+        req ['box2'] = cache.get('tictactoe_box2')
+        req ['box3'] = cache.get('tictactoe_box3')
+        req ['box4'] = cache.get('tictactoe_box4')
+        req ['box5'] = cache.get('tictactoe_box5')
+        req ['box6'] = cache.get('tictactoe_box6')
+        req ['box7'] = cache.get('tictactoe_box7')
+        req ['box8'] = cache.get('tictactoe_box8')
+        req ['box9'] = cache.get('tictactoe_box9')
+        req ['game_state'] = cache.get('tictactoe_game_state')
+
+        response = simplejson.dumps(req)
+        response = callback + '(' + response + ');'
+    else:
+        response = 'fail'
+
+    return HttpResponse(response, mimetype="application/json")
+
 
