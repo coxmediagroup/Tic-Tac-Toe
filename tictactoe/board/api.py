19938d389e1c1ff7d4b46f80bb0ef1c05d4552ff
Ben Rose <brose@logarythmics.com>
2014-02-04 04:31:06 -0500

    new computer move selection functionality

--- tictactoe/board/api.py
+++ tictactoe/board/api.py
@@ -5,6 +5,7 @@ from tastypie.resources import ModelResource
 from tastypie.utils import trailing_slash
 from django.http import Http404
 from board.models import Player, Move, Game
+from collections import defaultdict
 import numpy as np
 
 def generate_matrix(game, p1, p2):
@@ -19,8 +20,6 @@ def generate_matrix(game, p1, p2):
       matrix[item.position_y, item.position_x] = p1
     else:
       matrix[item.position_y, item.position_x] = p2
-  print(matrix)
-  print(matrix.ravel())
   return matrix
 
 def determine_winner(game):
@@ -70,16 +69,62 @@ def update_game_status(game):
     game.save(update_fields=['is_over'])
     return True
 
+def select_move_for_computer(matrix, user, comp):
+  '''
+     Selects move for computer using the following criteria in order of precedence (where p2 is computer):
+     1) If middle not selected, select it.
+     2) Win if possible by selecting empty coordinate where line has sum == p2_token * 2.
+     3) Prevent win for other player (line w/ sum == p1_token * 2).
+     4) Select first empty coordinate going right to left, top to bottom.
+  '''
+  def find_empty_in_coordinate_list(coords):
+    '''
+       First tuple from list comprehension to gather all empty coordinates is returned.
+       Will error if there are no empty coordinates in the list!
+    '''
+    #matrix is in row major order, so y comes first when looking up values
+    return [coord for coord in coords if matrix[coord[1], coord[0]] == 0][0]
+    
+  #1
+  if matrix[1][1] == 0:
+    return (1,1)
+  #2-4
+  else:
+    corners = [(0,0),(2,0),(0,2),(2,2)]
+    sums = defaultdict(list)
+    #diagonals
+    sums[np.trace(matrix)].append([(0,0),(1,1),(2,2)])
+    sums[np.trace(np.rot90(matrix))].append([(2,0),(1,1),(0,2)])
+    #columns
+    column_sums = np.sum(matrix, axis=0).ravel().tolist()
+    for index, column_sum in enumerate(column_sums):
+      sums[column_sum].append([(index, 0),(index,1),(index,2)])
+    #rows
+    row_sums = np.sum(matrix, axis=1).ravel().tolist()
+    for index, row_sum in enumerate(row_sums):
+      sums[row_sum].append([(0,index),(1,index),(2,index)])
+    #2
+    if comp*2 in sums:
+      return find_empty_in_coordinate_list(sums[comp*2][0])
+    #3
+    if user*2 in sums:
+      return find_empty_in_coordinate_list(sums[user*2][0])
+    #4
+    first_zed = matrix.ravel().tolist().index(0)
+    return (first_zed % 3, first_zed / 3)
+    
+
 def make_computer_move(game):
   '''
      Accepts Game model object, pulls the moves, if next player is non-human, creates and saves new move for computer player.
      Does nothing if game is None or is_over.
-     Currently, it simply takes the first available coordinate (row major order)
   '''
   if not game or game.is_over:
     return
   player1 = game.player_1
   player2 = game.player_2
+  p1_token = 1
+  p2_token = 9
   #If no moves and player1 is computer, make move.
   if len(game.move_set.all()) < 1:
     current_player = player1
@@ -92,11 +137,10 @@ def make_computer_move(game):
   if current_player.is_human:
     pass
   else:
-    matrix = generate_matrix(game, 1, 9)
-    #default for ravel is row major order, so y is the first order of the array
-    first_zed = matrix.ravel().tolist().index(0)
-    x = first_zed % 3
-    y = first_zed / 3
+    matrix = generate_matrix(game, p1_token, p2_token)
+    (x,y) = select_move_for_computer(matrix, p1_token, p2_token)
+
+
     Move(game=game, player=current_player, position_x=x, position_y=y).save()
    
     
@@ -143,9 +187,7 @@ class MoveResource(ModelResource):
     bundle = super(MoveResource, self).obj_create(bundle, **kwargs)
     game = Game.objects.get(id=bundle.data['game']['id'])
     if not update_game_status(game):
-      #TODO: call to method for computer's turn if next turn is for computer
       make_computer_move(game)
-      #also call update_game_status
       game = Game.objects.get(id=game.id)
       update_game_status(game)
 
