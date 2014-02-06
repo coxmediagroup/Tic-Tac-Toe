d8e7cfccca861c520f319e90c5af1b2adc2d1a2b
Shawn Frame <shawn.frame@indepthstudios.com>
2014-02-06 02:03:19 -0500

    Add KeyboardInterrupt exception handling
    Add KeyboardInterrupt exception handling to prevent rude game exits
    
    Signed-off-by: Shawn Frame <shawn.frame@indepthstudios.com>

--- __main__.py
+++ __main__.py
@@ -1,3 +1,4 @@
+import sys
 import curses
 from curses_game_screen import GameScreen
 
@@ -5,6 +6,16 @@ def main(screen):
     
     # Make new Game
     s = GameScreen(screen)
+    
+    # s.board.board[0][0].player = s.board.P1 #1
+    # s.board.board[0][1].player = s.board.P1 #2
+    # s.board.board[0][2].player = s.board.P0 #3
+    # s.board.board[1][0].player = s.board.P0 #4
+    # s.board.board[1][1].player = s.board.P2 #5
+    # s.board.board[1][2].player = s.board.P2 #6
+    # s.board.board[2][0].player = s.board.P0 #7
+    # s.board.board[2][1].player = s.board.P0 #8
+    # s.board.board[2][2].player = s.board.P0 #9
     
     # Game Loop
     while s.game_is_running:
@@ -45,5 +56,9 @@ def main(screen):
         s.game_is_running = s.key_events()
 
 if __name__ == "__main__":
-    curses.wrapper(main)
-    print "Thanks for playing!"
+    try:
+        curses.wrapper(main)
+    except KeyboardInterrupt:
+        sys.exit(1)
+    finally:
+        print "Thanks for playing!"
