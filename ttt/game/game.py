8eb29cb5a6b2ce4906fe46d173b7d30fd3540c25
Christian Sauer <sauerc@gmail.com>
2014-02-11 18:12:02 -0500

    Added Game
    Holds the nuts and bolts of playing a game of TTT:
    1) calls each player to pick a square
    2) validates their choice
    3) checks for win or draw
    4) repeat
    
    Tests pass
    PEP8 pass

---
+++ ttt/game/game.py
@@ -0,0 +1,77 @@
+
+from ttt.board import board
+
+
+class AbstractGame:
+    """
+    The nuts and bolts of TTT itself.  Shouldn't be used directly, rather
+    used as a base class for <UIType>Game (i.e. TextGame(AbstractGame)
+    or GUIGame(AbstractGame)
+    """
+    def __init__(self, player1, player2):
+        self.player1 = player1
+        self.player2 = player2
+
+        self.current_player = player1
+        self.winner = None
+        self.board = board.Board(9)
+        self.win_paths = [
+            [0, 1, 2],  # row 1
+            [3, 4, 5],  # row 2
+            [6, 7, 8],  # row 3
+            [0, 3, 6],  # col 1
+            [1, 4, 7],  # col 2
+            [2, 5, 8],  # col 3
+            [0, 4, 8],  # diag 1
+            [2, 4, 6]   # diag 2
+        ]
+
+    def check_for_winner(self):
+        """
+        Checks the in-play board for a winner based on the rules of TTT.
+        returns the winning person or None if no one has won yet.
+        """
+        if self.winner:
+            return self.winner
+
+        for a, b, c in self.win_paths:
+            if self.board.squares[a] \
+                    and self.board.squares[a] == self.board.squares[b] \
+                    and self.board.squares[a] == self.board.squares[c]:
+                self.winner = self.board.squares[a]
+                return self.winner
+
+        return None
+
+    def play(self):
+        """
+        Main game loop, only terminates when play results in a lose or a draw
+        """
+        while True:
+            self.display_board()
+
+            message = ""
+            square = None
+            while True:
+                square = self.current_player.get_square(message)
+                if self.board.square_free(square):
+                    break
+                message = "Invalid Square"
+
+            self.board.place(square, self.current_player.marker)
+            if self.check_for_winner() or self.board.is_full():
+                break
+
+            if self.current_player == self.player1:
+                self.current_player = self.player2
+            else:
+                self.current_player = self.player1
+
+        self.display_board()
+        self.display_finale()
+
+    def display_board(self):
+        raise NotImplemented
+
+    def display_finale(self):
+        raise NotImplemented
---
+++ ttt/game/tests.py
@@ -0,0 +1,55 @@
+
+import unittest
+import itertools
+from ttt.board import board
+import game
+
+
+class GameTests(unittest.TestCase):
+
+    @staticmethod
+    def create_board(o_squares, x_squares):
+        b = board.Board(9)
+        [b.place(i, "O") for i in o_squares]
+        [b.place(i, "X") for i in x_squares]
+        return b
+
+    @staticmethod
+    def combos(size):
+        items = list(itertools.combinations(range(9), size))
+        lists = [list(i) for i in items]
+        return lists
+
+    def test_check_for_winner_singles(self):
+        g = game.AbstractGame(None, None)
+        for i in GameTests.combos(1):
+            g.board = GameTests.create_board(i, [])
+            self.assertIsNone(g.check_for_winner(), i)
+
+    def test_check_for_winner_doubles(self):
+        g = game.AbstractGame(None, None)
+        for i in GameTests.combos(2):
+            g.board = GameTests.create_board(i, [])
+            self.assertIsNone(g.check_for_winner(), i)
+
+    def test_check_for_winner_triples(self):
+        g = game.AbstractGame(None, None)
+        winners = [
+            [0, 1, 2],
+            [3, 4, 5],
+            [6, 7, 8],
+            [0, 3, 6],
+            [1, 4, 7],
+            [2, 5, 8],
+            [0, 4, 8],
+            [2, 4, 6]
+        ]
+
+        for test in GameTests.combos(3):
+            g.winner = None
+            g.board = GameTests.create_board(test, [])
+
+            if test in winners:
+                self.assertEqual("O", g.check_for_winner(), test)
+            else:
+                self.assertIsNone(g.check_for_winner(), test)
