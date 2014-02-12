c5d6a15846eeeda94621541f2e1ebac695a11aeb
Spencer Chastain <sechastain@gmail.com>
2014-02-11 23:39:45 -0500

    Basic site up and running

--- ttt/tttui/tests.py
+++ ttt/tttui/tests.py
@@ -25,7 +25,7 @@ class ViewTest(TestCase):
         assert response.context['games'][2].id == 3
 
     def test_get_all(self):
-        response = self.client.get('/game/')
+        response = self.client.get('/')
 
         assert response.status_code == 200
         assert len(response.context['games']) == 3
@@ -34,33 +34,33 @@ class ViewTest(TestCase):
         assert response.context['games'][2].id == 3
 
     def test_create_new(self):
-        response = self.client.post('/game/new')
-        self.assertRedirects(response, '/game/%d/' % (self.max_id+1))
+        response = self.client.post('/new')
+        self.assertRedirects(response, '/%d/' % (self.max_id+1))
 
     def test_get_game(self):
-        response = self.client.get('/game/1/')
+        response = self.client.get('/1/')
 
         assert response.status_code == 200
         assert type(response.context['game']) == Game
 
     def test_post_move(self):
-        response = self.client.post('/game/1/move',
+        response = self.client.post('/1/move',
                                     {'player': 'x', 'col': 0, 'row': 0})
-        self.assertRedirects(response, '/game/1/')
+        self.assertRedirects(response, '/1/')
 
-        response = self.client.get('/game/1/')
+        response = self.client.get('/1/')
 
         assert response.status_code == 200
         assert type(response.context['game']) == Game
         assert response.context['game'][0][0] == PLAYER_X
 
     def test_post_move_by_ajax(self):
-        response = self.client.post('/game/2/move',
+        response = self.client.post('/2/move',
                                     {'player': 'x', 'col': 0, 'row': 0},
                                     HTTP_X_REQUESTED_WITH='XMLHttpRequest',
                                     )
         assert response.status_code == 200
-        content = simplejson.loads(response.content)
+        content = json.loads(response.content)
         assert content['player'] == 'o'
         assert 'col' in content
         assert 'row' in content
