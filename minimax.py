8caaa94491f08cf0971e9ac72f02a52e1db6b2c5
william <william@williamlyles.com>
2014-02-14 16:02:10 -0600

    passing the stupidly trivial test, now to add actual logic

--- minimax.py
+++ minimax.py
@@ -1,3 +1,11 @@
 """
 implementation of the minimax algorithm (http://en.wikipedia.org/wiki/Minimax) for tic-tac-toe purposes
 """
+
+class MinimaxCalculator:
+
+    def __init__(self):
+        pass
+
+    def bestMove(self, player, board):
+        return (2,0)
