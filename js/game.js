33b21e76b962b9e51abbb6098071ca9aecce43d2
rogangriffin <rogangriffin@gmail.com>
2014-02-15 01:30:37 +0000

    using css float instead of table. using ng-repeat for game grid

--- js/game.js
+++ js/game.js
@@ -1,12 +1,18 @@
 var mainModule = angular.module('tictactoeApp', []);
 
 var gameCtrl = mainModule.controller('GameCtrl', function ($scope) {
-  $scope.test = 'X';
+  $scope.grid = ["0","1","2","3","4","5","6","7","8"];
+  
+  $scope.getButtonText = function(index){
+      return index;
+  }
+  
+  $scope.getButtonClass = function(index){
+      if((index%3)==0){
+          return "clear-left";
+      } else{
+          return "";
+      }
+  }
 });
 
-gameCtrl.directive('gameButton', function() {
-    return {
-      restrict: 'E',
-      templateUrl: 'gamebtn.html'
-    };
- });
