a106d3901347ec259c1da516aa7224c808b35ff8
rogangriffin <rogangriffin@gmail.com>
2014-02-15 09:46:57 +0000

    implemented game ai

--- js/game.js
+++ js/game.js
@@ -1,30 +1,35 @@
 var mainModule = angular.module('tictactoeApp', []);
 
-var gameCtrl = mainModule.controller('GameCtrl', function ($scope) {
-  $scope.grid = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+var gameCtrl = mainModule.controller('GameCtrl', function ($scope, aiFactory) {
+    $scope.grid = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-  $scope.getButtonText = function(index){
-      switch($scope.grid[index]){
-          case 0:
-              return "";
-          case 1:
-              return "O";
-          case 2:
-              return "X";
-      }
-  }
+    $scope.getTurnText = function () {
+        var turnText = { 0: "Computer's turn. Please wait...",
+                         1: "Your turn. Click any open squre." }
+        return turnText[$scope.turn];
+    }
+
+    $scope.getButtonText = function ( index ) {
+        var playerSymbol = { 0 : "", 1 : "O", 2 : "X" };
+        return playerSymbol [ $scope.grid [ index ] ];
+    }
   
-  $scope.getButtonClass = function(index){
-      if((index%3)==0){
-          return "clear-left";
-      } else{
-          return "";
-      }
-  }
+    $scope.getButtonClass = function ( index ) {
+        if (( index % 3 ) == 0 ) {
+            return "clear-left";
+        } else{
+            return "";
+        }
+    }
   
-  $scope.clickButton = function(index){
-      $scope.grid[index] = 1;
-  }
+    $scope.clickButton = function ( index ) {
+        $scope.grid [ index ] = 2;
+        var nextMove = aiFactory.calculateMove( $scope.grid.slice( 0 ) );
+        console.log( "Next move: " + nextMove );
+        if ( nextMove != undefined ) {
+            $scope.grid [ nextMove ] = 1;
+        }
+    }
   
 });
 
