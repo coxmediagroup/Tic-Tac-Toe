5faa0a9af088da1404ae39e86b15ee9cf0e686ac
william <william@williamlyles.com>
2014-02-17 13:55:02 -0600

    handles bad requests

--- server.py
+++ server.py
@@ -2,23 +2,26 @@ import board
 import minimax
 import movecache
 import json
-from bottle import route, run, static_file, request
+from bottle import route, run, static_file, request, abort
 
 @route('/api/omove', method='GET')
 def omove():
-    boardState = json.loads(request.query.get('board', None))
-    gameBoard = board.Board(boardState)
-    result = gameBoard.finished()
-    move = None
-    if not result[0]:
-        moveCache = movecache.MoveCache()
-        calc = minimax.MinimaxCalculator(moveCache) 
-        move = calc.bestMove('O', gameBoard)
-        gameBoard.move('O', move[0], move[1])
+    try:
+        boardState = json.loads(request.query.get('board', None))
+        gameBoard = board.Board(boardState)
         result = gameBoard.finished()
-    return {
+        move = None
+        if not result[0]:
+            moveCache = movecache.MoveCache()
+            calc = minimax.MinimaxCalculator(moveCache) 
+            move = calc.bestMove('O', gameBoard)
+            gameBoard.move('O', move[0], move[1])
+            result = gameBoard.finished()
+        return {
                 'result': {'gameOver':result[0], 'winner':result[1]},
                 'move': move}
+    except:#board not specified correctly
+        abort(400)
 
 @route('/<filename>')
 def staticFile(filename):
