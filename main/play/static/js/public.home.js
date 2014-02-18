d14914ebab52059686c9990e2376593baadef4e0
Henry Adam <henryradam@gmail.com>
2014-02-17 21:02:06 -0500

    adding java script functionality to sent the django view the board via the url, the view then parses the board and does it's thing

--- main/play/static/js/public.home.js
+++ main/play/static/js/public.home.js
@@ -15,11 +15,21 @@ $(document).ready(function () {
 
     var the_board = ['','','','','','','','',''];
 
+    $("[id^='cell']").each(function(index,value){
+        if ($(value).children().html() == "None") $(this).children().html('');
+    });
+
     $("[id^='cell']").on("click",function(e){
         //here we build the board from the cell
         $(this).children().html('O')
         var x = $("[id^='cell']").map(function(){return $(this).children().html()}).get().slice();
+        $.each(x,function(index, value){
+            if (!value.length || value == "None") x[index] = null
+        });
         console.log(x);
+        s = '/?board=' + JSON.stringify(x);
+        window.location = s;
+
     });
 
 });
