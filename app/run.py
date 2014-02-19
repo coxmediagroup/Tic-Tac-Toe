0e4f009232c6285e353bbdb6ffe21e3307ff99a6
J Leadbetter <jleadbet@gmail.com>
2014-02-18 22:52:25 -0700

    Working on 'Start New Game' button

--- app/run.py
+++ app/run.py
@@ -6,8 +6,9 @@ kivy.require('1.8.0')
 
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.button import Button
 
-from ttt import TicTacToeBoard
+from ttt import TicTacToeBoard 
 
 
 class TicTacToeFrame(BoxLayout):
@@ -20,10 +21,10 @@ class TicTacToeFrame(BoxLayout):
         return game_over, winner
     
     def get_btn_parent(self):
-        return self.wrapper.board_wrapper.game_board.game_board_buttons
+        return self.board_wrapper.game_board.game_board_buttons
     
     def player_move(self, square, btn):
-        self.wrapper.board_wrapper.board_text.go_first_label.text = ""
+        self.board_wrapper.board_text.go_first_label.text = ""
         move, game_over, winner = self.board.human_move(square)
         btn.text = self.square_label(square)
         
@@ -32,10 +33,10 @@ class TicTacToeFrame(BoxLayout):
 
         if game_over:
             self.update_scores()
-    
-    def update_scores(self):
-        self.wrapper.player1_score.text = self.player_text(1)
-        self.wrapper.player2_score.text = self.player_text(2)
+            self.new_game_btn = Button(text='Start Another Game', 
+                                       on_press=self.reset_game,
+                                       size_hint=(1, .15))
+            self.board_wrapper.add_widget(self.new_game_btn)
     
     def player_text(self, player_number):
         if player_number == 1:
@@ -44,15 +45,21 @@ class TicTacToeFrame(BoxLayout):
             name_text = "[color=2ba6cb]Hal[/color] " 
         return name_text + self.board.player_stats(player_number)
     
-    def reset_game(self):
+    def reset_game(self, btn):
+        self.board_wrapper.remove_widget(btn)
         self.board.reset_board()
         if self.board.is_computer_turn():
             self.computer_move()
+            self.board_wrapper.board_text.go_first_label.text = "Your turn..."
         else:
-            self.wrapper.board_wrapper.board_text.go_first_label.text = "You go first..."
+            self.board_wrapper.board_text.go_first_label.text = "You go first..."
     
     def square_label(self, square):
         return self.board.get_square_label(square)
+    
+    def update_scores(self):
+        self.player1_score.text = self.player_text(1)
+        self.player2_score.text = self.player_text(2)
 
 
 class TicTacToeApp(App):
