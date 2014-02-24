059f51b1997feed1ceb4317b139da581809cf07d
Eduardo Perez <edrdo.prz@gmail.com>
2014-02-24 14:54:52 -0500

    Initial commit. HTML basic layout, CSS, and basic JavaScript.Next up is Tic Tac Toe logic.

---
+++ js/main.js
@@ -0,0 +1,13 @@
+// initiate some variables
+$(function() {  
+    var turn        = true;
+        gamestarted = true;
+
+});
+
+// wait for everything to load
+$(document).ready(function() {
+    $( "#top-left" ).click(function() {
+        $( "#top-left" ).addClass("x");
+    });
+});
