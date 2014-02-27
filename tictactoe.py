438e278688b2f771e253fbd11bcf34d715f8761f
Tim Sleeper <mrt.sleeper@gmail.com>
2014-02-27 03:00:57 -0500

    Completed AI logic and finished game. After testing everything checks out!

--- tictactoe.py
+++ tictactoe.py
@@ -1,18 +1,19 @@
 from math import pow
-from random import randint
 
-board = [[" "," "," "],[" "," "," "],[" "," "," "],]
+board = [[" ", " ", " "], [" ", " ", " "], [" ", " ", " "], ]
 plr = "x"
 com = "o"
 winner = None
+
 
 def get_open_spaces(board):
     spaces = []
     for i in range(0, len(board)):
         for j in range(0, len(board[i])):
             if (board[i][j] == " "):
-                spaces.append([i,j])
+                spaces.append([i, j])
     return spaces
+
 
 print "Let's play Tic Tac Toe"
 
@@ -29,12 +30,16 @@ def setup_players():
             com = "x"
             valid_setup = True
 
+
 def print_board(b):
     print " ", " ", "1", " ", "2", " ", "3"
     print "1 |", b[0][0], "|", b[0][1], "|", b[0][2], "|"
+    print " ", "-" * 13
     print "2 |", b[1][0], "|", b[1][1], "|", b[1][2], "|"
+    print " ", "-" * 13
     print "3 |", b[2][0], "|", b[2][1], "|", b[2][2], "|"
     print " ", "-" * 13
+
 
 def check_win(b):
     """
@@ -71,6 +76,58 @@ def check_win(b):
     # Catch All for tie
     return "-"
 
+def plr_turn(p):
+    if p == "x":
+        return "o"
+    else:
+        return "x"
+
+def imminent_win(b, plr):
+    spaces = get_open_spaces(b)
+    move = None
+    for space in spaces:
+        b[space[0]][space[1]] = plr
+        if check_win(b) == plr:
+            move = [space[0],space[1]]
+            b[space[0]][space[1]] = " "
+            break
+        b[space[0]][space[1]] = " "
+    return move
+
+def min_max(row, col, b, curr_player, eval_player, move_count):
+    orig = b[row][col]
+    b[row][col] = curr_player
+    curr_player = plr_turn(curr_player)
+    was_win = check_win(b)
+    score = 0
+    if was_win:
+        if was_win == eval_player:
+            score = 1 / pow(move_count, move_count)
+        elif was_win == "-":
+            score = 0
+        else:
+            score = -1 / pow(move_count, move_count)
+    else:
+        spaces = get_open_spaces(board)
+        for space in spaces:
+            score += min_max(space[0], space[1], list(b[:]) ,curr_player, eval_player, move_count + 1)
+    b[row][col] = orig
+    return score
+
+def com_move(b, plr):
+    spaces = get_open_spaces(board)
+    move = []
+    score = -100000
+    i_move = imminent_win(b, plr_turn(plr))
+    if i_move:
+        return i_move
+    for space in spaces:
+        e_score = min_max(space[0], space[1], list(b[:]), plr, plr, 1)
+        if e_score > score:
+            score = e_score
+            move = [space[0], space[1]]
+    return move
+
 def request_move(b):
     row = None
     col = None
@@ -98,35 +155,12 @@ def request_move(b):
 def move(row, col, p, b):
     b[row][col] = p
 
-def plr_turn(p):
-    if p == "x":
-        return "o"
-    else:
-        return "x"
-
 setup_players()
 while not winner:
     print_board(board)
-    turn_no = 0
     if plr in com:
         print "Computer is contemplating..."
-        if turn_no == 0:
-            com_row = 2
-            com_col = 1
-            plr_move = (com_row, com_col)
-            turn_no += 1
-        def rand_row(board):
-            return randint(0, len(board) - 1)
-
-        def rand_col(board):
-            return randint(0, len(board) - 1)
-
-        com_row = rand_row(board)
-        com_col = rand_col(board)
-        plr_move = (com_row, com_col)
-        turn_no += 1
-        print plr_move
-        print turn_no
+        plr_move = com_move(board, plr)
     else:
         plr_move = request_move(board)
     move(plr_move[0], plr_move[1], plr, board)
@@ -134,3 +168,7 @@ while not winner:
     plr = plr_turn(plr)
 
 print print_board(board)
+if winner == "-":
+    print "Players tied!"
+else:
+    print "Plaser %s wins!" %winner
