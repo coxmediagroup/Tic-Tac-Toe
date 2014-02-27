ea63de8125255718c417afa4beb08b0bfc84d30a
Andrew Sledge <andrew.j.sledge@gmail.com>
2014-02-26 19:31:23 -0500

    added setup shell

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(name='Tic-Tac-Toe',
+      version='1.0',
+      description='Tic-Tac-Toe in pure Python',
+      author='Andrew Sledge',
+      author_email='andrew.j.sledge@gmail.com',
+      url='http://www.linkedin.com/in/andrewjsledge/',
+      py_modules=['tictactoe'],
+      )
