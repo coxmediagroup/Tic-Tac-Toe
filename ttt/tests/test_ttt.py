719bbdaba4cb824cccfa21889228710a70310ba4
Ed Rahn <edsrahn@gmail.com>
2014-03-03 16:46:34 -0500

    implement has_win_move and tests

---
+++ ttt/tests/test_ttt.py
@@ -0,0 +1,52 @@
+from django.test import TestCase
+from tic_tac_toe import TicTacToe
+
+class TestTicTacToe(TestCase):
+    def test_has_win_move_along_x(self):
+        """
+        The player has a winning move along the x axis.
+        """
+        ttt = TicTacToe(first=False)
+        ttt.board = [['O', 'O', None], [None]*3, [None]*3]
+        where = ttt.has_win_move()
+        assert where == (0, 2)
+
+        ttt = TicTacToe(first=False)
+        ttt.board = [[None, 'O', 'O'], [None]*3, [None]*3]
+        where = ttt.has_win_move()
+        assert where == (0, 0)
+
+    def test_has_win_movie_along_y(self):
+        """
+        The player has a winning move along the y axis.
+        """
+        ttt = TicTacToe(first=False)
+        ttt.board = [['O', None,  None], ['O', None, None], [None]*3]
+        where = ttt.has_win_move()
+        assert where == (2, 0)
+
+        ttt = TicTacToe(first=False)
+        ttt.board = [[None]*3, ['O', None, None], ['O', None, None]]
+        where = ttt.has_win_move()
+        assert where == (0, 0)
+
+    def test_has_win_along_diagonals(self):
+        ttt = TicTacToe(first=False)
+        ttt.board = [['O', None, None], [None, 'O', None], [None]*3]
+        where = ttt.has_win_move()
+        assert where == (2, 2)
+
+        ttt = TicTacToe(first=False)
+        ttt.board = [[None, None, 'O'], [None, 'O', None], [None]*3]
+        where = ttt.has_win_move()
+        assert where == (2, 0)
+
+        ttt = TicTacToe(first=False)
+        ttt.board = [[None]*3, [None, 'O', None], [None, None, 'O']]
+        where = ttt.has_win_move()
+        assert where == (0, 0)
+
+        ttt = TicTacToe(first=False)
+        ttt.board = [[None]*3, [None, 'O', None], ['O', None, None]]
+        where = ttt.has_win_move()
+        assert where == (0, 2)
---
+++ ttt/tic_tac_toe.py
@@ -0,0 +1,65 @@
+class TicTacToe:
+    def __init__(self, xo='X', first=False):
+        self.board = [[None]*3, [None]*3, [None]*3]
+        self.xo = xo
+        if first:
+            self.ai_move()
+
+    def output(self):
+        """
+        Print the board.
+        """
+        for i in range(3):
+            print self.board[i]
+
+    def has_win_move(self, mark='O'):
+        """
+        mark has a move where they can win
+        
+        Returns a where to place the opposite mark to bloack the win.
+        """
+
+        # TODO - I should propbably test that the open value is actualy None
+
+        # First along the x axis
+        for i in range(3):
+            if self.board[i][0] == mark and self.board[i][1] == mark:
+                return (i, 2)
+            if self.board[i][1] == mark and self.board[i][2] == mark:
+                return (i, 0)
+
+        # Then along the y axis
+        for i in range(3):
+            if self.board[0][i] == mark and self.board[1][i] == mark:
+                return (2, i)
+            if self.board[1][i] == mark and self.board[2][i] == mark:
+                return (0, i)
+
+        # Now diagonal
+        # (1,1) is always going to be set for diagonals, all we need to do is
+        # match the corners
+        diagonals = {(0, 0): (2, 2),
+                     (2, 2): (0, 0),
+                     (0, 2): (2, 0),
+                     (2, 0): (0, 2)}
+
+        for diags, ret in diagonals.items():
+            if (self.board[diags[0]][diags[1]] == mark and
+                self.board[1][1] == mark):
+                return ret
+
+        return False
+
+    
+
+
+    def ai_move(self):
+        if self.xo == 'X':
+            opponent = 'O'
+        else:
+            opponent = 'X'
+
+        # win move for me
+        where = self.has_win_move(mark=self.xo)
+        if where:
+            self.board[where[0]][where[1]] = self.xo
