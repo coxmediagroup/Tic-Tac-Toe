dba4efd578b6758ea6a187dc60c9e136affd61bb
Michale Hendren <mhendren@streetsmarttechnology.com>
2014-03-06 02:47:45 -0600

    AI features added
    Added the first and second move options based on best practices (avoid the
    middle edges, they can only win in 2 ways, center and corner spots can win in
    3 ways).
    
    Added a findWinner() which will determine if there is a next move that will win.
    
    Added a mustBlock() which will determine if not taking a spot could result  in
    a loss.
    
    Started the __selectBestMove() which will determine the state of the game. While
    not finished it will sepect a spot based on
    1) First or second moves are in a corner or center spot
    2) If a win can be had, take it
    3) If a must block situation is found, block the spot
    4) (not implemented yet) check to see if two moves ahead will require
       multiple blocks, if so, don't choose an option that leads to this.
    5) Pick random from the available spots
    
    Adedd the makeMove() which will make a move on the active board based on the
    best move available

--- AI.py
+++ AI.py
@@ -1,4 +1,6 @@
 
+from random import choice
+
 class AI:
     def __init__(self, player, board):
         self.player = player
@@ -7,3 +9,36 @@ class AI:
     def generateBoards(self):
         return {option:Board(self.board.fetch(), self.board.turn).move(option) for option in self.board.validPositions()}
 
+    def isFirstMove(self):
+        if self.board.isEmpty():
+            return True
+        return False
+
+    # A corner or the center is the best starting move
+    def selectFirstMove(self):
+        return choice([1, 3, 5, 7, 9])
+    
+    def isSecondMove(self):
+        if len(self.board.validPositions()) == 8:
+            return True
+        return False
+
+    # If the opponent has taken the center, pick a corner,
+    # If the opponent has taken a corner, take the center
+    # otherwise pick a corner or the center
+    def selectSecondMove(self):
+        validMoves = self.board.validPositions()
+        if 5 not in validMoves:
+            return choice([1, 3, 7, 9])
+        if len([position for position in [1, 3, 7, 9] if position not in validMoves()]) != 0:
+            return 5
+        return choice([1, 3, 5, 7, 9])
+        
+    def __selectBestMove(self):
+        if self.__isFirstMove():
+            return self.__selectFirstMove()
+        if self.__isSecondMove():
+            return self.__selectSecondMove()
+        
+    def makeMove(self):
+        self.board.move(self.__selectBestMove())
