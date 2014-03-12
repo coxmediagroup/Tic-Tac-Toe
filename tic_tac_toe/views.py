ec44fa933acaf5af38b18d5c75589ce0ef6fbae7
Christopher Adams <adams.christopher.m@gmail.com>
2014-03-12 19:36:54 -0400

    make the index view build the board as a list of lists so that the (soon to come) template can traverse it easily without much logic

--- tic_tac_toe/views.py
+++ tic_tac_toe/views.py
@@ -31,10 +31,36 @@ def index(request):
         # if so, he should go on and take his turn
 
     # it's our turn, so:
+
     # display the board
+
+    board = game.board_set.first() # get the board
+    rows = board.row_set.all() # get the board's rows
+
+    locations = [] # this will hold each cell and its contents
+    for row_i, row in enumerate(rows):
+        row_locations_set = row.location_set.all()
+        row_locations = [] # this will be our translated row
+        for loc_i, location in enumerate(row_locations_set):
+            # figure out who owns the cell
+            symbol = '' # default to unowned
+            if(location.occupier != None):
+                symbol = location.occupier.symbol
+
+            location_tpl = (row_i, loc_i, symbol)
+            row_locations.append(location_tpl) # add the location to the row
+        locations.append(row_locations) # add the row to the board
+
+    # invert the grid vertically so we can build from the top down
+    locations = locations[::-1]
+
+    # DEBUG: show locations, so it can be verified
+    import pprint
+    rendered_html = pprint.pformat(locations)
+
     # and let's get some input
 
-    return HttpResponse('Index')
+    return HttpResponse(rendered_html)
 
 def make_move(request, row_id, column_id):
     return HttpResponse('Move')
