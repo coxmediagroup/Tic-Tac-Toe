84066b7f874da08a106d06e76715013384d17cc1
Christopher Adams <adams.christopher.m@gmail.com>
2014-03-12 02:29:33 -0400

    interim checkin, move Game model definition above Entity, add comments to models, add game backreference to Entity model, add method setup_new_game() to Game model

--- tic_tac_toe/models.py
+++ tic_tac_toe/models.py
@@ -2,8 +2,42 @@ from django.db import models
 
 # Create your models here.
 
+class Game(models.Model):
+    current_player = models.ForeignKey('Entity', null=True, related_name='+')
+
+    def get_location(self, x, y):
+        # get the location object of the given location
+        pass
+
+    def setup_new_game(self):
+        self.save()
+
+        # 1 entity for the player, 1 for the computer
+        human = Entity(is_ai=False, game=self)
+        ai = Entity(is_ai=True, game=self)
+
+        # save what we have so far
+        human.save()
+        ai.save()
+
+        # ai starts
+        self.current_player = ai #TODO: randomize starting order
+        self.save()
+
+        board = Board(game=self)
+        board.save()
+
+        # create a 3x3 grid, saving each object along the way
+        for i in range(3):
+            row = Row(row=i, board=board)
+            row.save()
+            for i in range(3):
+                loc = Location(column=i, row=row)
+                loc.save()
+
 class Entity(models.Model):
     is_ai = models.BooleanField(default=False)
+    game = models.ForeignKey(Game)
 
     def get_type(self):
         # get a human-readable string of this entity's type
@@ -22,12 +56,7 @@ class Entity(models.Model):
             pass
         pass
 
-class Game(models.Model):
-    current_player = models.ForeignKey(Entity)
 
-    def get_location(self, x, y):
-        # get the location object of the given location
-        pass
 class Board(models.Model):
     game = models.ForeignKey(Game)
 
@@ -42,5 +71,5 @@ class Location(models.Model):
 
     def claim(self, entity_id):
         # set the occupier of this field to the given entity id
+        #TODO: how do I get an entity by its index?
         pass
-
