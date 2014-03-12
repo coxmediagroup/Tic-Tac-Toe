db78a4f2a60fbdeba54f0d8b7730b6d693f678da
Christopher Adams <adams.christopher.m@gmail.com>
2014-03-12 01:40:14 -0400

    stub out model methods mentioned in design.md

--- tic_tac_toe/models.py
+++ tic_tac_toe/models.py
@@ -5,10 +5,31 @@ from django.db import models
 class Entity(models.Model):
     is_ai = models.BooleanField(default=False)
 
+    def get_type(self):
+        # get a human-readable string of this entity's type
+        #    this should be either "Human" or "Computer"
+        #    this could possibly be expanded to be a player's name
+        if self.is_ai:
+            return 'Computer'
+        return 'Human'
+
+    def get_decision(self):
+        # get a decision from either the human or the ai
+        #    this may need to change as I figure out django
+        if(self.get_type() == 'Human'):
+            pass
+        else:
+            pass
+        pass
+
 class Location(models.Model):
     column = models.IntegerField()
     row = models.ForeignKey(Row)
     occupier = models.ForeignKey(Entity)
+
+    def claim(self, entity_id):
+        # set the occupier of this field to the given entity id
+        pass
 
 class Row(models.Model):
     board = models.ForeignKey(Board)
@@ -20,3 +41,7 @@ class Board(model.Model):
 class Game(model.Model):
     board = models.ForeignKey(Board)
     current_player = models.ForeignKey(Entity)
+
+    def get_location(self, x, y):
+        # get the location object of the given location
+        pass
