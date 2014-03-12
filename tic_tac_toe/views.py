021d8170bbc865fe83cc35fab7868529d0d5ef81
Christopher Adams <adams.christopher.m@gmail.com>
2014-03-12 18:44:25 -0400

    add simple views with matching url's

--- tic_tac_toe/views.py
+++ tic_tac_toe/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+def index(request):
+    return HttpResponse('Index')
+
+def make_move(request, row_id, column_id):
+    return HttpResponse('Move')
