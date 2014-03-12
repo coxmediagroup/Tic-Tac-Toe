f63b93da275fc8605e5a9c943784dffd89b813b6
Christopher Adams <adams.christopher.m@gmail.com>
2014-03-12 01:32:42 -0400

    add models

--- tic_tac_toe/models.py
+++ tic_tac_toe/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+
+class Entity(models.Model):
+    is_ai = models.BooleanField(default=False)
+
+class Location(models.Model):
+    column = models.IntegerField()
+    row = models.ForeignKey(Row)
+    occupier = models.ForeignKey(Entity)
+
+class Row(models.Model):
+    board = models.ForeignKey(Board)
+    row = models.IntegerField()
+
+class Board(model.Model):
+    game = models.ForeignKey(Game)
+
+class Game(model.Model):
+    board = models.ForeignKey(Board)
+    current_player = models.ForeignKey(Entity)
