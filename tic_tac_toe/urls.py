2dd9144989d61d835d314f857172219842c6dcb0
Christopher Adams <adams.christopher.m@gmail.com>
2014-03-12 19:50:17 -0400

    fix make_move url name

--- tic_tac_toe/urls.py
+++ tic_tac_toe/urls.py
@@ -3,5 +3,5 @@ from tic_tac_toe import views
 
 urlpatterns = patterns('',
     url(r'^$', views.index, name='index'),
-    url(r'^move/(?P<row_id>\d+)/(?P<column_id>\d+)/?$', views.make_move, name='index'),
+    url(r'^move/(?P<row_id>\d+)/(?P<column_id>\d+)/?$', views.make_move),
 )
