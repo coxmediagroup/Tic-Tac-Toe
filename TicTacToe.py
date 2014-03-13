3392ac73dda91c76bf22ef706616bb41f27e9538
Staikov <kiril.staikov@kboland-wl.corp.weather.com>
2014-03-13 14:10:43 -0400

    Created TicTacToe class that will basically be a whole game I think.
    Layed out some basic categories of info we may want to keep in mind.
    Currently thinking of the board as a set of boxes addressed 1-9.
    They are categorized into edges, corners, and center, and contained in rows, columns, and/or diagonals that form sets of winnign combinations.

---
+++ TicTacToe.py
@@ -0,0 +1,44 @@
+import re
+
+class TicTacToe():
+    def __init__(self):
+        #what stage of the game we are in. 
+        self.numTurn = 1 
+
+        #layout of basic info we may need to keep in mind. 
+        self.squares = {1, 2, 3, 4, 5, 6, 7, 8, 9}
+        self.availSquares = self.squares
+        self.xSquares = set()
+        self.oSquares = set()
+        self.corners = {1, 3, 7, 9}
+        self.center = {5}
+        self.edges = {2, 4, 6, 8}
+
+        #Winning combos I am calling RDCs(Rows, Diagonals, Columns)
+        self.RDCs = {'R1': [1, 2, 3],
+                'R2': [4, 5, 6],
+                'R3': [7, 8, 9],
+                'C1': [1, 4, 7],
+                'C2': [2, 5, 8],
+                'C3': [3, 6, 9],
+                'D1': [1, 5, 9],
+                'D2': [3, 5, 7]}
+
+game=TicTacToe()
+players=['X', 'O']
+
+if __name__ == '__main__':
+    while True:
+        move = raw_input("Pick a spot: ")
+        game.availSquares.discard(int(move))
+        
+        
+        
+    
+
+
+
+
+
+
+
