9ad752956e272f24ed3bda157ba3c67fbce17b6d
Staikov <kiril.staikov@kstaikov-ml.local>
2014-03-14 02:52:46 -0400

    rearranged the order of bestMove evaluations to make more sense. and avoid any surprises from situations I haven't thought of.  Any time other than the first turn, we will check for danger and forkability.

--- TicTacToe.py
+++ TicTacToe.py
@@ -139,14 +139,6 @@ class TicTacToe():
             if self.aiMark == 'O':
                 if self.center & self.availSquares:
                     return list(self.center).pop()
-        if self.numTurn == 2:
-#            if self.aiMark == 'X':
-#                if self.center & self.availSquares:
-            if self.aiMark == 'O':
-#
-                #If both X's are in corners, we know O is in middle.  Take an edge
-                if len(self.corners & self.xSquares) == 2:
-                    return random.choice(list(self.edges & self.availSquares))
                     
 
         dangerousSquares = self.evalDanger()
@@ -162,6 +154,11 @@ class TicTacToe():
         elif forkOpps[self.aiMark]:
             return forkOpps[self.aiMark][0]
         
+        elif self.numTurn == 2:
+            if self.aiMark == 'O':
+                #If both X's are in corners, we know O is in middle.  Take an edge
+                if len(self.corners & self.xSquares) == 2:
+                    return random.choice(list(self.edges & self.availSquares))
         #Can I create a dangerous situation?
 
         #Can my opponent create a fork on his next turn? If so, take one of his forks?
