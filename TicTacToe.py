88227b2a6034ebfbd85ba56cf91bb9d42ad26980
Staikov <kiril.staikov@kstaikov-ml.local>
2014-03-13 23:59:43 -0400

    more elaborate instructions to players for picking next move

--- TicTacToe.py
+++ TicTacToe.py
@@ -36,7 +36,7 @@ class TicTacToe():
     # Collect player's next move and make sure his response is valid.
     def getMove(self):
         try:
-            move = int(raw_input('Pick a spot: '))
+            move = int(raw_input('Pick a spot [1-9, left to right and top to bottom.  1 is top left, 9 is bottom right]: '))
         except ValueError:
             print 'Try Again'
             move = self.getMove() 
