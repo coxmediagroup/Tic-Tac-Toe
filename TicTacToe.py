bda21a33779c329f0366be1fea1c9167c737d6fc
Staikov <kiril.staikov@kstaikov-ml.local>
2014-03-15 15:20:44 -0400

    removed bestMove() from TicTacToe.py.  Game now has to explicitly load a strategy file's bestMove() using 'setStrategy('fileName')' Default strategy is aiStrategy1.  However, player may try game in which AI is programmed to use a different strategy by passing a different strategy name in as an argument (i.e. `python TicTacToe.py aiStrategy2`).  The new strategy has to be a file in the strategies folder.

--- TicTacToe.py
+++ TicTacToe.py
@@ -1,12 +1,19 @@
 import sys
 import re
 import itertools
-import random
+import types
+import os.path
+
+#Default Strategy, but let user set a diff one in params.
+try:
+    STRATEGY = os.path.splitext(sys.argv[1])[0]
+except:
+    STRATEGY = 'aiStrategy1'
 
 class TicTacToe():
     def __init__(self, humanMark=None):
+        #self.setStrategy(self, STRATEGY)
         self.chooseSides(humanMark)
-
         #what stage of the game we are in. 
         self.numTurn = 0 
 
@@ -130,54 +137,6 @@ class TicTacToe():
             if square in self.RDCs[RDC]:
                 self.RDCs[RDC][self.RDCs[RDC].index(square)] = player
 
-    # Strategize and make best move.
-    # If it's my first turn, don't bother with rest, what's the best first
-    # move for O and X?
-    # 1) Can I win? [Danger in my benefit]
-    # 2) Can human win? [Danger for human benefit, must block if so]
-    # 3) Can I fork?
-    # 4) What?
-    def bestMove(self):
-        if self.numTurn == 1:
-#            if self.aiMark == 'X':
-#                return random.choice(list(self.corners))
-            if self.aiMark == 'O':
-                if self.center & self.availSquares:
-                    return list(self.center).pop()
-                    
-        dangerousSquares = self.evalDanger()
-        forkOpps = self.evalForkability()
-
-        #If I can win, take the first available winning box!
-        if dangerousSquares[self.aiMark]:
-            return dangerousSquares[self.aiMark][0]
-        #If I can't win but human can on his next turn, block the first danger!!
-        elif dangerousSquares[self.humanMark]:
-            return dangerousSquares[self.humanMark][0]
-        #Can I create a fork?
-        elif forkOpps[self.aiMark]:
-            return forkOpps[self.aiMark][0]
-        
-        elif self.numTurn == 2:
-            if self.aiMark == 'O':
-                #If both X's are in corners, we know O is in middle.  Take an edge
-                if len(self.corners & self.xSquares) == 2:
-                    return random.choice(list(self.edges & self.availSquares))
-            else:
-                #ai is X
-                if list(self.oSquares)[0] not in self.center :
-                    return 5
-        #Can I create a dangerous situation?
-
-        #Can my opponent create a fork on his next turn? If so, take one of his forks?
-        if forkOpps[self.humanMark]:
-            return forkOpps[self.humanMark][0]
-
-        #Do something else if none of the above
-        else:
-            return self.availSquares.pop()
-
-
     def printBoard(self):
         print re.sub('[0-9]', '-', self.board)
         #print self.board
@@ -204,13 +163,18 @@ class TicTacToe():
         self.board = self.board.replace(str(move), self.whosTurn)
         self.history.append(move)
 
+    def setStrategy(self, stratFile):
+        #Pull in strategy and it's corresponding bestMove() function
+        exec "from strategies import %s" % stratFile
+        newBestMove = eval("%s.bestMove" % stratFile)
+        #set its bestMove function as TicTacToe's bestMove() function.
+        return setattr(self, newBestMove.__name__, types.MethodType(newBestMove, self))
+
 if __name__ == '__main__':
     game=TicTacToe()
+    game.setStrategy(STRATEGY)
     while not game.gameOver():
         game.printBoard()
         game.advanceMove()
 
         
-        
-
-
