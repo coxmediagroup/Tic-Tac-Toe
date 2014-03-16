f8052f17d69d3ec5fcd35261fe8ad97a79184ea4
Evan Stern <suriansg@gmail.com>
2014-03-16 23:08:35 +0000

    Basic JS Application is now working

---
+++ django/t3/static/t3/js/application.js
@@ -0,0 +1,38 @@
+/* global define */
+define([
+  'underscore',
+  'backbone'
+], function(_, Backbone) {
+  'use strict';
+
+  var Application = function() {
+    // The layout manager controls the `Layout` view for this application.
+    this.layoutManager = null;
+
+    // Monitors the state of the application
+    this.state = new (Backbone.Model.extend({
+      defaults: {name: 'stopped'}
+    }))();
+  };
+
+  // Extend Application with inheritable properties.
+  _.extend(Application.prototype, Backbone.Events, {
+
+    // Run the application
+    run: function() {
+      this.trigger('application:run', this);
+    },
+
+    // Close the application
+    close: function() {
+      this.trigger('application:close', this);
+    },
+
+    // Handle application state changes
+    onChangeState: function() {
+    }
+  });
+
+  return Application;
+});
+
---
+++ django/t3/static/t3/js/main.js
@@ -0,0 +1,44 @@
+require.config({
+  paths: {
+    'jquery': '../components/jquery/dist/jquery',
+    'backbone': '../components/backbone/backbone',
+    'underscore': '../components/underscore/underscore',
+    'backbone-layout': '../components/backbone-layout/backbone-layout',
+    'bootstrap': '../components/bootstrap/dist/js/bootstrap'
+  },
+  shim: {
+    'jquery': {
+      exports: '$'
+    },
+    'underscore': {
+      exports: '_'
+    },
+    'backbone': {
+      deps: ['jquery', 'underscore'],
+      exports: 'Backbone'
+    },
+    'bootstrap': {
+      deps: ['jquery']
+    },
+    'backbone-layout': {
+      deps: ['backbone']
+    }
+  }
+});
+
+/* global define */
+define(['application', 'bootstrap'], function(Application) {
+  'use strict';
+
+  var app = new Application();
+
+  // Monitor the status of the application
+  app.on('application:run', function() {
+    console && (console.log('running', arguments));
+  });
+  app.on('application:close', function() {
+    console && (console.log('closed', arguments));
+  });
+
+  app.run();
+});
