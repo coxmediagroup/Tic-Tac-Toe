f40710b651516320baf09910a176ee92ee8fa82e
Kiril Staikov <kstaikov@gmail.com>
2014-03-15 23:05:20 -0400

    made tallying output of undefeatable_test more informative

--- undefeatable_test.py
+++ undefeatable_test.py
@@ -1,3 +1,13 @@
+'''
+Unit test that simultaneously plays out every game possibility.
+
+Idea is:
+    1) Initiatie a game
+    2) progress through it until human/user is to make a choice about where to move
+    3) see how many available squares they are, and for each available move, make a clone of the game and mark that available square in the clone. The list of clones replace the original game they branched from. Until a clone wins. 
+    4) Repeat 2) and 3) until each game possibility/branch/universe is played out
+'''
+
 import sys
 import os
 import copy
@@ -12,22 +22,13 @@ if not os.path.exists('./strategies/%s.py' % STRATEGY):
 
 from TicTacToe import *
 
-'''
-Unit test that simultaneously plays out every game possibility.
-
-Idea is:
-    1) Initiatie a game
-    2) progress through it until human/user is to make a choice about where to move
-    3) see how many available squares they are, and for each available move, make a clone of the game and mark that available square in the clone. The list of clones replace the original game they branched from. Until a clone wins. 
-    4) Repeat 2) and 3) until each game possibility/branch/universe is played out
-'''
 
 class TTTTest:
     def __init__(self):
         self.games = [] #Main list of all currently active games
         self.games.append(TicTacToe('X')) #Initiate a game.  No moves yet. X is human player.
         self.games.append(TicTacToe('O')) #Initiate a game.  No moves yet. O is human player.
-        self.tally = {'X': 0, 'O': 0, 'tie': 0, 'badGames': []}
+        self.tally = {'X-wins': 0, 'O-wins': 0, 'badGames': [], 'X-ties': 0, 'O-ties': 0, 'X-loss': 0, 'O-loss': 0}
 
         for game in self.games:
             game.setStrategy(STRATEGY)
@@ -72,10 +73,13 @@ class TTTTest:
             return False
         else:
             #Game is over.  Tally results
-            self.tally[status] += 1
-            #If human/user won, this is bad.
             if gameInst.humanMark == status:
                 self.tally['badGames'].append(gameInst)
+                self.tally['%s-loss' % gameInst.aiMark] += 1
+            elif gameInst.aiMark == status:
+                self.tally['%s-wins' % status] += 1
+            else: #Tie
+                self.tally['%s-ties' % gameInst.aiMark] += 1
             return True
 
     #Traverse through all possible games trajectories.
@@ -84,22 +88,39 @@ class TTTTest:
         while self.games:
             self.branchAllGames()
 
+        self.printResults()
+
+    def printResults(self):
+        print "===================================================================================================="
+        print 'Games AI won as X: %d' % self.tally['X-wins']
+        print 'Games AI won as O: %d' % self.tally['O-wins']
+        print 'Games AI lost as X: %d' % self.tally['X-loss']
+        print 'Games AI lost as O: %d' % self.tally['O-loss']
+        print 'Games tied when AI was X: %d' % self.tally['X-ties']
+        print 'Games tied when AI was O: %d' % self.tally['O-ties']
+        print 'Total AI wins: %d' % (self.tally['X-wins'] + self.tally['O-wins'])
+        print 'Total ties: %d' % (self.tally['X-ties'] + self.tally['O-ties'])
+        print "===================================================================================================="
+
+        failures = len(self.tally['badGames'])
+        if failures > 0:
+            print 'FAIL: AI lost in %d scenarios!' % failures 
+        else:
+            print 'PASS: AI never lost!' 
     
 
 if __name__ == '__main__':
+    #Redirect output to log
     stdout = sys.stdout
     log = open('log.txt', 'w')
     sys.stdout = log
 
     test = TTTTest()
     test.runTest()
+    #Output back to stdout
     sys.stdout = stdout
-    print test.tally
-    failures = len(test.tally['badGames'])
-    if failures > 0:
-        print 'FAIL: AI lost in %d scenarios!' % failures 
-    else:
-        print 'PASS: AI never lost!' 
+    log.close()
+    test.printResults()
 
 
 
