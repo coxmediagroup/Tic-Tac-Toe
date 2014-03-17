2ff4e0daf4a86173220f052312990354cada32f3
Ben Vinson <bvinson@vtionline.com>
2014-03-17 02:18:34 -0400

    CMG Code Test: Django version of the game is finished.  I added the option to allow O to go first since the requirements is that the computer must be able to never lose.  Statistically, the best way to guarantee 100% winning is to start first.

--- django_ttt/board/views.py
+++ django_ttt/board/views.py
@@ -1,6 +1,116 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
 # Create your views here.
+import re
+
+noDigits = re.compile('\D')
+onlyDigits = re.compile('\d')
+lines = [
+                [ 0, 1, 2 ],
+                [ 3, 4, 5 ],
+                [ 6, 7, 8 ],
+                [ 0, 3, 6 ],
+                [ 1, 4, 7 ],
+                [ 2, 5, 8 ],
+                [ 0, 4, 8 ],
+                [ 2, 4, 6 ]
+            ]
+
+bestLocs = [4, 8, 6, 2, 0, 7, 5, 3, 1]
 
 def index(request):
-    return HttpResponse("heello")
+    gameBoard = []
+    information = ""
+    gameOn = 1;
+    firstPlay = 1;
+    if request.method == 'POST':
+        firstPlay = 0;
+        for element in range(0,9):
+            if int(request.POST.get("chosenLoc", "")) == element:
+                gameBoard.append("X")
+            else:
+                gameBoard.append(request.POST.get("loc" + str(element), ""))
+        result = checkBoard(gameBoard)
+        if(result == 0):
+            gameBoard = getNextMove(gameBoard)
+            result = checkBoard(gameBoard)
+            if(result != 0):
+                information = result
+                gameOn = 0
+        else:
+            information = result
+            gameOn = 0
+    else:
+        for element in range(0,9):
+            gameBoard.append(" ")
+    t = loader.get_template('board/index.html')
+    c = RequestContext(request,{'gameBoard': gameBoard, "information":information, "gameOn":gameOn, 'firstPlay':firstPlay});
+    return HttpResponse(t.render(c))
+
+def checkBoard(gameBoard):
+    for line in lines:
+        sum = 0
+        openLoc = 0
+        for loc in line:
+            if gameBoard[loc] == 'X':
+                sum += 1
+            elif gameBoard[loc] == 'O':
+                sum += -1
+            else:
+                openLoc = loc
+        print sum
+        if sum==3:
+            return "X wins"
+        elif sum == -3:
+            return "O wins"
+    for loc in gameBoard:
+        if loc==" ":
+            return 0
+    return "Tie!"
+
+def getNextMove(gameBoard):
+    for line in (lines):
+        sum = 0
+        openLoc = -1
+        for loc in (line):
+            if(gameBoard[loc] == 'X'):
+                sum += 1
+            elif(gameBoard[loc] == 'O'):
+                sum += -1
+            else:
+                openLoc = loc
+
+        if(sum == -2 and openLoc >= 0):
+            gameBoard[openLoc]='O'
+            return gameBoard
+    for line in (lines):
+        sum = 0
+        openLoc = -1
+        for loc in (line):
+            if(gameBoard[loc] == 'X'):
+                sum += 1
+            elif(gameBoard[loc] == 'O'):
+                sum += -1
+            else:
+                openLoc = loc
+        if(sum == 2 and openLoc >= 0):
+            gameBoard[openLoc]='O'
+            return gameBoard
+    for line in ((lines[6], lines[7])):
+        sum = 0
+        openLoc = -1
+        for loc in (line):
+            if(gameBoard[loc] == 'X'):
+                sum += 1
+            elif(gameBoard[loc] == 'O'):
+                sum += -1
+            else:
+                openLoc = loc
+        if((sum == 0 or sum == 1) and openLoc >= 0):
+            gameBoard[openLoc]='O'
+            return gameBoard
+    for bestLoc in (bestLocs):
+        if gameBoard[bestLoc]==" ":
+            gameBoard[bestLoc]='O'
+            return gameBoard
--- django_ttt/settings.py
+++ django_ttt/settings.py
@@ -1,4 +1,8 @@
 # Django settings for django_ttt project.
+import os
+
+PROJECT_PATH = os.path.realpath(os.path.dirname(__file__))
+
 
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
@@ -76,6 +80,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'board', 'static'),
 )
 
 # List of finder classes that know how to find static files in
@@ -110,6 +115,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    os.path.join(PROJECT_PATH, 'board', 'templates'),
 )
 
 INSTALLED_APPS = (
