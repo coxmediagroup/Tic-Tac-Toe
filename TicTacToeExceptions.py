8dadf7dd67be80b3dc68ec0c34398e39af03d9a0
Daniel Avila (sparky) <sparky@theunixman.com>
2014-03-20 22:26:43 -0400

    Little bit of code cleanup

--- TicTacToeExceptions.py
+++ TicTacToeExceptions.py
@@ -1,10 +1,13 @@
+"""
+A custom exception for misplacing a token
+"""
 
 class TokenPlacementException(Exception):
 
     def __init__(self, value):
 
-        self.parameter = value 
+        self.parameter = value
 
-    def __str__(self): 
+    def __str__(self):
 
         return repr(self.parameter)
