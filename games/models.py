6d144d3e26217c096748cc24929df2c97c5aa986
Eric Honkanen <dump@epicowl.com>
2014-03-23 16:30:31 -0700

    Updated and refactored more logic including computer AI.

--- games/models.py
+++ games/models.py
@@ -3,11 +3,11 @@ import json
 from django.db import models
 
 
-DEFAULT_BOARD = '000000000'
-EMPTY_MARK = '0'
-COMPUTER_MARK = '1'
-PLAYER_MARK = '2'
+EMPTY_MARK = '-'
+COMPUTER_MARK = 'O'
+PLAYER_MARK = 'X'
 SIZE = 3
+DEFAULT_BOARD = EMPTY_MARK * SIZE**2
 
 class Game(models.Model):
     start_date = models.DateTimeField(null=True, blank=True)
@@ -43,7 +43,7 @@ class TicTacToe(Game):
         determines it's next move based on the following logic:
 
         1) Check if player has more than 1 mark, if not, place at center.
-        2) If the player has more than one mark, block any attemp at win.
+        2) If the player has more than one mark, block any attempt at win.
         3) If no attempt at win, place mark in one of the corners.
 
         """
@@ -58,22 +58,50 @@ class TicTacToe(Game):
             if int(position) <= len(self.board) and self._is_valid_move(position):
                 self._place_mark(position, PLAYER_MARK)
 
-                # Check if the game is complete, if not then computers turn.
-                if self._is_complete:
-                    print "GAME OVER"
-                else:
-                    self.move()
+                # Now it is the computers turn.    
+                self.move()
             else:
                 print 'OH NOEZ NOT VALID MOVE'
         else:
-            # Check if player has more than 1 mark, if not, move to center
+            # Check if player has more than 1 mark, if not, move to center.
             marks = self.board.count(PLAYER_MARK)
             
             if marks < 2 and self._center_empty:
                 self._place_mark(self._center_position, COMPUTER_MARK)
 
+            else:
+                # Make winning move or block the player from his winning move, then check
+                # to see if the game was won and if so set is_complete = True.
+                if not self.winning_move(COMPUTER_MARK) or not self.winning_move(PLAYER_MARK):
+                    # Otherwise just pick an empty space and move there.
+                    # TODO: Choose corners or random space.
+                    position = self.board.find(EMPTY_MARK)
+                    self._place_mark(position, COMPUTER_MARK)
 
-        return
+            if self._game_won:
+                self.is_complete = True
+                self.save()
+                print "GAME OVER - COMPUTER WINS!"
+
+        return self.board
+
+    def winning_move(self, player_mark):
+        next_move = []
+        for position, mark in enumerate(self.board):
+            if mark == EMPTY_MARK:
+                old = self.board
+                self._place_mark(position, player_mark)
+                if self._game_won:
+                    next_move.append(position)
+                    break
+                self.board = old
+        
+        if len(next_move) > 0:
+            print next_move
+            self._place_mark(next_move[0], COMPUTER_MARK)
+            return True
+
+        return False
 
     def _place_mark(self, position, mark):
         self.board = list(self.board)
@@ -105,27 +133,29 @@ class TicTacToe(Game):
         return winners
 
     @property
-    def _is_complete(self):
+    def _game_won(self):
         # Iterate through winning paths to check if game is won.
-        for item in self._winning_paths:
-            if all(item[0] == x and x != EMPTY_MARK for x in item):
-                print "WINNER"
-                self.is_complete = True
-                self.save()
-                return True
+        won = False
+
+        for path in self._winning_paths:
+            if all(path[0] == x and x != EMPTY_MARK for x in path):
+                won = True
+                break
 
         # Check if there is a stalemate.
-        if not EMPTY_MARK in self.board:
-            print "STALEMATE!!!"
-            self.is_complete = True
-            self.save()
-            return True
+        if not EMPTY_MARK in self.board and not won:
+            won = True
 
         # Game not over, return False.
-        return False
+        return won
 
     def _is_valid_move(self, position):
         return list(self.board).pop(position) == EMPTY_MARK
+
+    @property
+    def _get_corners(self):
+        # Could get this programatically but simple enough to just return them.
+        return [self.board[i] for i in (0, 2, 6, 8)]
 
     @property
     def _center_empty(self):
@@ -133,9 +163,9 @@ class TicTacToe(Game):
 
     @property
     def _center_position(self):
-        return self.center(SIZE)
+        return self._center(SIZE)
 
     @staticmethod
-    def center(size):
+    def _center(size):
         return ((SIZE**2) - 1) / 2
 
