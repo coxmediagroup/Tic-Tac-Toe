9bfcab42bb8bcc98affe83fd196db99e07b05bd8
Clay <mirtt67@yahoo.com>
2014-03-24 12:12:12 -0400

    Moved logic from the main file to the GameBoard class
    Corrected some logic on drawing the board and keeping track of what
    valid moves are left. Finished player input validation.

--- GameBoard.py
+++ GameBoard.py
@@ -4,11 +4,15 @@ class GameBoard(object):
 	
 	def __init__(self):
 		self.board_data = ["1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9"]
+		self.board_moves_left = ["1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9"] 
 		
 	##board_data contains all the data that goes into the gameboard, it will contain numbers for spaces player can move to,
 	##or spaces that contain a move (X or O).
 	board_data = []	
+	##moves_left contains the board numbers that are left to play
+	board_moves_left = []
 	def draw_board(self):
+
 		print "  |   | "
 		print "%c | %c | %c " % (self.board_data[0] , self.board_data[1] , self.board_data[2])
 		print "----------"
@@ -17,10 +21,16 @@ class GameBoard(object):
 		print "%c | %c | %c " % (self.board_data[6] , self.board_data[7] , self.board_data[8])
 		print "  |   | "
 		
-	##this function updates the board's data space is the selected space and player_token is either an X or O depening if 
+	##this function updates the board's data space is the selected space and player_token is either an X or O depending if 
 	##the player or the computer made the move
-	def update_board_data(self, space, player_token):
-		self.board_data[int(space) - 1 ] = player_token
+	def update_board_data(self , space , player_token):
+		##need to correct the index since the user enters an index that is +1
+		correctedIndex = int(space) - 1
+		self.board_data[correctedIndex] = player_token
+		self.board_moves_left.pop(correctedIndex)
+	
 	
 	def get_board_data(self):
 		return self.board_data
+	def get_board_moves_left(self):
+		return self.board_moves_left
--- tic-tac-toe-main.py
+++ tic-tac-toe-main.py
@@ -3,28 +3,27 @@ import GameBoard
 from GameBoard import *
 
 def getFirstMove():
-	if (random.randint(1,2) == 1):
+	if (random.randint(1 , 2) == 1):
 		return "you"
 	else:
 		return "the computer"
 		
 ##gets the players input and handles validation of the input
-def getPlayersMove(game_board):
-	##while the input is not the numbers 1-9 we let the user know their input is not correct
-	validMoves = getValidMoves(game_board.get_board_data())
+def getPlayersMove(gameBoard):
+	validMoves = gameBoard.get_board_moves_left()
+	gameBoard.draw_board()
 	playersMove = raw_input("Player, please enter your move:")
 	validMove = checkPlayerInput(playersMove , validMoves)
 	while validMove == False:
 		print "Invalid move, the following are valid entries: ", validMoves
 		playersMove = raw_input("Player, please enter your move:")
 		validMove = checkPlayerInput(playersMove , validMoves)
+		
+	##if we make it to here, the move is valid, update the gameboard.
+	gameBoard.update_board_data(playersMove , "X" )
+	gameBoard.draw_board()
+	
 
-def getValidMoves(board_data):
-	valid_spaces = []
-	for entry in board_data:
-		if entry != "X" or entry !="O":
-			valid_spaces.append(entry)
-	return valid_spaces
 	
 def checkPlayerInput(playersMove, validMoves):
 	valid = False
@@ -39,6 +38,7 @@ gameBoard= GameBoard()
 print "Let's play a game of tic-tac-toe"
 firstMove = getFirstMove()
 print "We randomly chose who gets to go first, and %s goes first" % firstMove
+
 
 ##if the first move is the player get their move then we can start regular play
 ##if the first move is the computer determine the best move then start regular play
