437fb13affa1d14473c10b83b8d7967c2d849d37
Jassen Michaels <jmichaels@officite.com>
2014-03-24 08:23:29 -0500

    Adding user play handlers

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -6,7 +6,7 @@ function AppCtrl($scope, $rootScope, $location, $http, $timeout) {
     var matrixSize = 3, empty = 0, player = 1, robot = 2, displayMsgFor = 4000;
 
     /**
-     * Start new game
+     * Start a new game
      */
     $scope.startGame = function () {
         $scope.hasWinner = false;
@@ -16,7 +16,55 @@ function AppCtrl($scope, $rootScope, $location, $http, $timeout) {
             var cols = [0,0,0];
             $scope.rows.push(cols);
         }
+
+        // Let computer start first
+        $scope.robotPlay();
     }
 
+    /**
+     * Display an alert
+     *
+     * @param msg
+     */
+    $scope.showMessage = function(msg){
+        $scope.message = msg;
+        $scope.displayMessage = true;
+        $timeout(function(){
+            $scope.displayMessage = false;
+        }, displayMsgFor)
+    }
+
+    /**
+     * User play handler
+     *
+     * @param event
+     * @param index
+     * @param parentIndex
+     */
+    $scope.userPlay = function(event, index, parentIndex) {
+        if($scope.hasWinner) {
+            $scope.showMessage ('Game has a winner.');
+            return;
+        }
+        $scope.moves.push('You Played ' + index.toString() + "," + parentIndex.toString());
+        if($scope.rows[parentIndex][index] == empty) {
+            $scope.rows[parentIndex][index] = player;
+            $scope.checkWinner(player); //check if the player won
+            if($scope.hasWinner) {
+                var msg = 'Congrats you won';
+                $scope.moves.push(msg); $scope.showMessage(msg);
+                return;
+            }
+            $scope.robotPlay();
+            $scope.checkWinner(robot);  //check if robot won
+            if($scope.hasWinner) {
+                var msg = 'Sorry I won';
+                $scope.moves.push(msg); $scope.showMessage(msg);
+                return;
+            }
+        } else {
+            $scope.showMessage('Oops! that cell is already taken');
+        }
+    }
 
 }
