ed191d010d60452aa53833355ce4049185da3131
Rich Vazquez <rich.vazquez@gmail.com>
2014-03-24 11:43:18 -0500

    Accept options for beginning to play the game and very options are set and selected

--- js/game.js
+++ js/game.js
@@ -3,6 +3,21 @@
 	Set the board to be ready for play.
 */
 function setBoard(){
+
+	//Determine if the game options are set to begin play
+	var optionsAreEmpty = false;
+	$('.control > option:selected').each(function( index, value ) {
+  		if($(value).val()===''){
+  			optionsAreEmpty=true;
+  		};
+
+	});
+
+	if(optionsAreEmpty){
+		alert('Select game options first.');
+		return;
+	};
+
 	//Set the visual cues that the board is ready
 	$('#game-board td').css('cursor','pointer');
 	$('#game-board td').css('background-color','#fff');
@@ -17,3 +32,4 @@ function closeBoard(){
 	$('#game-board td').css('background-color','#e7e7e7');
 	$('#game-board td').css('border-color','#999');
 }
+
