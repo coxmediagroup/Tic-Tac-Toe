203f9658acf762adabc06c9f2a01b07b2fbf8bb7
Clay <mirtt67@yahoo.com>
2014-03-24 15:10:54 -0400

    Refactored how I get valid moves left, the old method could cause an array out of bounds error

--- GameBoard.py
+++ GameBoard.py
@@ -4,14 +4,10 @@ class GameBoard(object):
 	
 	def __init__(self):
 		self.board_data = ["1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9"]
-		self.board_moves_left = ["1" , "2" , "3" , "4" , "5" , "6" , "7" , "8" , "9"] 
 		
 	##board_data contains all the data that goes into the gameboard, it will contain numbers for spaces player can move to,
 	##or spaces that contain a move (X or O).
 	board_data = []	
-	
-	##moves_left contains the board numbers that are left to play
-	board_moves_left = []
 	
 	##This dictionary contains all 8 possible way to win the game
 	##1 and 2 are the horizontal wins
@@ -35,18 +31,18 @@ class GameBoard(object):
 	##this function updates the board's data space is the selected space and player_token is either an X or O depending if 
 	##the player or the computer made the move
 	def update_board_data(self , space , player_token):
-		##need to correct the index since the user enters an index that is +1
-		correctedIndex = int(space) - 1
-		self.board_data[correctedIndex] = player_token
-		self.board_moves_left.pop(correctedIndex)
+		self.board_data[int(space) - 1] = player_token
 	
 	def get_board_data(self):
 		return self.board_data
 	def get_board_moves_left(self):
-		return self.board_moves_left
-		
+		moves_left = []
+		for items in self.board_data:
+			if items != "X" or items != "O":
+				moves_left.append(items)
+		return moves_left
 	def count_board_moves_left(self):
-		return len(self.board_moves_left)
+		return len(self.get_board_moves_left())
 		
 	##check all paths to see if the game has a winner
 	def check_for_winner(self):
