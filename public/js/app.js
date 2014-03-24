4e90c81dce06150da95e1083358e412876416f44
Jassen Michaels <jmichaels@officite.com>
2014-03-24 08:45:02 -0500

    Setup basic angular app controller with single route for game

--- public/js/app.js
+++ public/js/app.js
@@ -1,26 +1,14 @@
 'use strict';
 
+
 // Declare app level module which depends on filters, and services
+var myApp = angular.module('myApp', [ 'myApp.directives'])
+    .config(['$routeProvider', '$locationProvider', function($routeProvider, $locationProvider) {
 
-angular.module('myApp', [
-  'myApp.controllers',
-  'myApp.filters',
-  'myApp.services',
-  'myApp.directives'
-]).
-config(function ($routeProvider, $locationProvider) {
-  $routeProvider.
-    when('/view1', {
-      templateUrl: 'partials/partial1',
-      controller: 'MyCtrl1'
-    }).
-    when('/view2', {
-      templateUrl: 'partials/partial2',
-      controller: 'MyCtrl2'
-    }).
-    otherwise({
-      redirectTo: '/view1'
-    });
+        // Set primary route
+        $routeProvider.when('/tic-tac-toe', {templateUrl: 'partials/_board'});
 
-  $locationProvider.html5Mode(true);
-});
+        // Fallback
+        $routeProvider.otherwise({redirectTo: '/tic-tac-toe'});
+    }]
+    )
