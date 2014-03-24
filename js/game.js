e6830fd4f5a73cdb2fdb3eaa44088e06a5f14708
Rich Vazquez <rich.vazquez@gmail.com>
2014-03-24 11:05:59 -0500

    Initial game board design and javascript to control visual queues for active and inactive game board

---
+++ js/game.js
@@ -0,0 +1,19 @@
+
+/*
+	Set the board to be ready for play.
+*/
+function setBoard(){
+	//Set the visual cues that the board is ready
+	$('#game-board td').css('cursor','pointer');
+	$('#game-board td').css('background-color','#fff');
+	$('#game-board td').css('border-color','#e7e7e7');
+}
+
+/*
+	Close the board for play
+*/
+function closeBoard(){
+	$('#game-board td').css('cursor','not-allowed');
+	$('#game-board td').css('background-color','#fff');
+	$('#game-board td').css('border-color','#e7e7e7');
+}
