54dfd3ef88f2853258a3f79090aaa7dbe643b200
Jassen Michaels <jmichaels@officite.com>
2014-03-24 08:15:01 -0500

    Building methods within game controller

--- public/js/controllers.js
+++ public/js/controllers.js
@@ -2,3 +2,21 @@
 
 /* Controllers */
 
+function AppCtrl($scope, $rootScope, $location, $http, $timeout) {
+    var matrixSize = 3, empty = 0, player = 1, robot = 2, displayMsgFor = 4000;
+
+    /**
+     * Start new game
+     */
+    $scope.startGame = function () {
+        $scope.hasWinner = false;
+        $scope.moves = new Array();
+        $scope.rows = new Array();
+        for(var i = 0 ; i < matrixSize; i++) {
+            var cols = [0,0,0];
+            $scope.rows.push(cols);
+        }
+    }
+
+
+}
