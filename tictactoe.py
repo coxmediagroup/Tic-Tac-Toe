af0f66aba919fa0dc1e7fedd44e0e7e31f6bbff0
Ali Nabavi <github@hourback.info>
2014-03-25 17:15:57 -0400

    - Stubbing out functions  - start_game, draw_board, who_goes_first - Draw board

--- tictactoe.py
+++ tictactoe.py
@@ -1,2 +1,25 @@
 __author__ = 'ali'
 
+
+def start_game():
+    draw_board()
+    pass
+
+
+def draw_board():
+    """Function for drawing the game board"""
+    print "-------------"
+    print "|   |   |   |"
+    print "-------------"
+    print "|   |   |   |"
+    print "-------------"
+    print "|   |   |   |"
+    print "-------------"
+    pass
+
+
+def who_goes_first():
+    pass
+
+if __name__ == "__main__":
+    start_game()
