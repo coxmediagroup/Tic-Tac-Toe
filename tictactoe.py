a95d0af69eb9cf407670a60c82d4798eddf3921a
Ali Nabavi <github@hourback.info>
2014-03-25 17:34:02 -0400

    - Reformatted game board - Figured out who goes first

--- tictactoe.py
+++ tictactoe.py
@@ -2,24 +2,40 @@ __author__ = 'ali'
 
 
 def start_game():
+    player_symbol = who_goes_first()
+    print 'The player is ' + player_symbol + '.'
+    if player_symbol == 'X':
+        print 'Great!  You go first.'
+    elif player_symbol == 'O':
+        print 'No problem.  I\'ll go first.'
     draw_board()
     pass
 
 
 def draw_board():
     """Function for drawing the game board"""
-    print "-------------"
-    print "|   |   |   |"
-    print "-------------"
-    print "|   |   |   |"
-    print "-------------"
-    print "|   |   |   |"
-    print "-------------"
+    print "----------------"
+    print "|    |    |    |"
+    print "----------------"
+    print "|    |    |    |"
+    print "----------------"
+    print "|    |    |    |"
+    print "----------------"
     pass
 
 
 def who_goes_first():
-    pass
+    valid_player_symbols = ['X', 'O']
+    player_symbol = ''
+    while player_symbol not in valid_player_symbols:
+        player_goes_first = raw_input('Want to go first? [y/n]')
+        if player_goes_first == 'y':
+            player_symbol = 'X'
+        elif player_goes_first == 'n':
+            player_symbol = 'O'
+        else:
+            print "Sorry.  I didn't catch that."
+    return player_symbol
 
 if __name__ == "__main__":
     start_game()
