84e5ca1cd539a1ef11b12f17ac6f0a16a64641e1
protein <tolmsted@Traviss-MacBook-Pro.local>
2014-03-26 10:55:04 -0400

    Added simple layout of tic-tac-toe grid board with working buttons.

--- tic_tac_toe.py
+++ tic_tac_toe.py
@@ -10,20 +10,32 @@ class tic_tac_toe_board(Tkinter.Tk):
         self.parent = parent
         self.player = "X"
         self.computer = "O"
-        self.empty = "."
-        self.size = 3
-        self.fields = {}
-        for y in range(self.size):
-            for x in range(self.size):
-                self.fields[x,y] = self.empty
         
         self.initialize()
 
-    #Layout of the actual tic-tac-toe board
+    #Layout of the actual tic-tac-toe board in grid format
     def initialize(self):
         self.grid()
+        board_grid = [[None] *3 for x in range(3)]
+        self.labelVariable = Tkinter.StringVar()
+
+        for x in range(3):
+            for y in range(3):
+                board_grid[x][y] = Tkinter.Button(self, height=5, width=5, text=u"Click me!", command=self.OnButtonClick)
+                board_grid[x][y].grid(column=x, row=y, sticky='NSEW')
+        self.update()
+
+        self.labelVariable.set(u"Test!")
+#        self.labelVariable = Tkinter.StringVar()
+#        label = Tkinter.Label(self, textvariable=self.labelVariable, anchor="w", fg="white", bg="blue")
+#        self.labelVariable.set(u"Hello!")
+#        label.grid(column=0, row=1, columnspan=2, sticky='EW')
 
 #Create a player object to handle the human player
+
+    def OnButtonClick(self):
+        pass
+
 class player:
 
     def __init__(self):
