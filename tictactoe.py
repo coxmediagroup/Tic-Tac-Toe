8557bd0a93805a1b2d1623f02a550469c5106609
Ali Nabavi <github@hourback.info>
2014-03-26 04:25:23 -0400

    - Moved valid_player_symbols[] to class object - Made board_values{} an instance object - Added finish_move() to our taking the center square - Moved our regular moves out of the block that checks for near defeat - Added method any_moves_left()

--- tictactoe.py
+++ tictactoe.py
@@ -1,16 +1,18 @@
 class TicTacToe:
 
     wins = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 7], [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]]
+    valid_player_symbols = ['X', 'O']
 
     def __init__(self):
         self.our_squares = []
         self.player_squares = []
         self.player_symbol = ''
         self.our_symbol = ''
-        self.board_values = self.initialize_board()
+        self.board_values = {}
+        self.initialize_board()
 
     def start_game(self):
-        self.player_symbol = self.who_goes_first()
+        self.who_goes_first()
         print 'The player is ' + self.player_symbol + '.'
         if self.player_symbol == 'X':
             self.our_symbol = 'O'
@@ -31,9 +33,10 @@ class TicTacToe:
             print "This is not the first move."
             """See where we should move next"""
             """Take square 5 if it's open"""
-            if 5 not in self.our_squares:
+            if self.is_square_free(5):
                 print "Taking square 5."
                 self.record_move(self.our_squares, self.our_symbol, 5)
+                self.finish_move(self.our_symbol, self.our_squares)
             else:
                 """See if the player is about to win"""
                 print "Square 5 is gone.  Picking another."
@@ -44,31 +47,44 @@ class TicTacToe:
                     win_misses = []
                     for i in win:
                         if i in self.player_squares:
-                            print "square is in win"
+                            print "square %d is in win" % i
                             win_count += 1
                             win_matches.append(i)
-                        else:
+                        elif i not in self.our_squares:
                             win_misses.append(i)
                     print "win_count is %s" % win_count
-                    if win_count == 2:
+                    if win_count == 2 and win_misses.__len__() > 0:
                         print "Uh-oh!  Looks like the player might win soon."
                         print "win is %s" % win
                         print "win_matches is %s" % win_matches
                         print "win_misses is %s" % win_misses[0]
                         self.record_move(self.our_squares, self.our_symbol, win_misses[0])
                         self.finish_move(self.our_symbol, self.our_squares)
-                    elif self.is_square_free(1):
-                        self.record_move(self.our_squares, self.our_symbol, 3)
-                        self.finish_move(self.our_symbol, self.our_squares)
-                    elif self.is_square_free(3):
-                        self.record_move(self.our_squares, self.our_symbol, 3)
-                        self.finish_move(self.our_symbol, self.our_squares)
-                    elif self.is_square_free(9):
-                        self.record_move(self.our_squares, self.our_symbol, 3)
-                        self.finish_move(self.our_symbol, self.our_squares)
-                    elif self.is_square_free(7):
-                        self.record_move(self.our_squares, self.our_symbol, 3)
-                        self.finish_move(self.our_symbol, self.our_squares)
+                        return
+                if self.is_square_free(1):
+                    self.record_move(self.our_squares, self.our_symbol, 1)
+                    self.finish_move(self.our_symbol, self.our_squares)
+                elif self.is_square_free(3):
+                    self.record_move(self.our_squares, self.our_symbol, 3)
+                    self.finish_move(self.our_symbol, self.our_squares)
+                elif self.is_square_free(9):
+                    self.record_move(self.our_squares, self.our_symbol, 9)
+                    self.finish_move(self.our_symbol, self.our_squares)
+                elif self.is_square_free(7):
+                    self.record_move(self.our_squares, self.our_symbol, 7)
+                    self.finish_move(self.our_symbol, self.our_squares)
+                elif self.is_square_free(2):
+                    self.record_move(self.our_squares, self.our_symbol, 2)
+                    self.finish_move(self.our_symbol, self.our_squares)
+                elif self.is_square_free(6):
+                    self.record_move(self.our_squares, self.our_symbol, 6)
+                    self.finish_move(self.our_symbol, self.our_squares)
+                elif self.is_square_free(8):
+                    self.record_move(self.our_squares, self.our_symbol, 8)
+                    self.finish_move(self.our_symbol, self.our_squares)
+                elif self.is_square_free(4):
+                    self.record_move(self.our_squares, self.our_symbol, 4)
+                    self.finish_move(self.our_symbol, self.our_squares)
 
     def is_square_free(self, square):
         if self.board_values[square] in range(9):
@@ -84,20 +100,33 @@ class TicTacToe:
 
     def check_for_winner(self, symbol, squares):
         """Check to see if someone won"""
+        print 'Checking for winner: %s, %s' % (symbol, squares)
         for win in self.wins:
             """Check winning combination for matches"""
             if all(x in squares for x in win):
                 print "%s wins!!!" % symbol
                 print self.draw_board()
                 exit()
+        if not self.any_moves_left():
+            print 'Game over!  Looks like a draw.'
+            print self.draw_board()
+            exit()
+        else:
+            print "%s didn't win." % symbol
+            if symbol == self.our_symbol:
+                self.prompt_player()
+            elif symbol == self.player_symbol:
+                self.we_move()
             else:
-                print "%s didn't win." % symbol
-                if symbol == self.our_symbol:
-                    self.prompt_player()
-                elif symbol == self.player_symbol:
-                    self.we_move()
-                else:
-                    "Oops."
+                "Oops."
+
+    def any_moves_left(self):
+        squares_free = 0
+        for key in self.board_values:
+            if not self.is_player_symbol(self.board_values[key]):
+                squares_free += 1
+        if squares_free > 0:
+            return True
 
     def prompt_player(self):
         """Prompt the player for a move"""
@@ -106,10 +135,9 @@ class TicTacToe:
         self.player_moves(self.board_values)
 
     def is_player_symbol(self, symbol):
-        valid_player_symbols = ['X', 'O']
-        if symbol in valid_player_symbols:
+        if symbol in TicTacToe.valid_player_symbols:
             return True
-        elif symbol not in valid_player_symbols:
+        elif symbol not in TicTacToe.valid_player_symbols:
             return False
         else:
             print "Oops."
@@ -129,8 +157,7 @@ class TicTacToe:
 
     def initialize_board(self):
         """Create the board_values dictionary with no X's and O's"""
-        board_values = {x:x for x in(range(1,10))}
-        return board_values
+        self.board_values = {x:x for x in(range(1,10))}
 
     def draw_board(self):
         """Function for drawing the game board"""
@@ -144,17 +171,14 @@ class TicTacToe:
         return board
 
     def who_goes_first(self):
-        player_symbol = ''
-        while self.is_player_symbol(player_symbol) is False:
+        while self.player_symbol not in TicTacToe.valid_player_symbols:
             player_goes_first = raw_input('Want to go first? [y/n]')
             if player_goes_first == 'y':
-                player_symbol = 'X'
+                self.player_symbol = 'X'
             elif player_goes_first == 'n':
-                player_symbol = 'O'
+                self.player_symbol = 'O'
             else:
                 print "Sorry.  I didn't catch that."
-        return player_symbol
-
 
 if __name__ == "__main__":
     t = TicTacToe()
