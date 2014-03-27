d1bba07b7f5c6eb4351a099f44e52a79d07a2cd4
Clay <mirtt67@yahoo.com>
2014-03-26 21:42:47 -0400

    Final commit, fixed first move logic is the player was first
    If the player was first and choose a corner, we must choose the middle
    to prevent the player from getting to a state that would let him win.

--- GameBoard.py
+++ GameBoard.py
@@ -12,16 +12,22 @@ class GameBoard(object):
 	board_data = []	
 	
 	##The following data is used to help the computer know whats open and make the right decision
-	corners = [1 , 3 , 7 , 9]
-	sides = [2 , 3 , 5 , 7]
+	corners = [ 1 , 3 , 7 , 9 ]
+	sides = [ 2 , 3 , 5 , 7 ]
 	
 	##This dictionary contains all 8 possible way to win the game
 	##1 and 2 are the horizontal wins
 	##3-6 are vertical wins
 	##6-8 are horizontal wins
 	board_winning_paths = {
-		1:[1 , 5 , 9] , 2:[3 , 5 , 7] , 3:[1 , 4 , 7] , 4:[2 , 5 , 8] ,
-		5:[3 , 6 , 9] , 6:[1 , 2 , 3] , 7:[4 , 5 , 6] , 8:[7 , 8 , 9]
+		1:[ 1 , 5 , 9 ] , 2:[ 3 , 5 , 7 ] , 3:[ 1 , 4 , 7 ] , 4:[ 2 , 5 , 8 ] ,
+		5:[ 3 , 6 , 9 ] , 6:[ 1 , 2 , 3 ] , 7:[ 4 , 5 , 6 ] , 8:[ 7 , 8 , 9 ]
+	}
+	
+	##This dictionary contains opposite corners we need to choose from, if the user is first and selects a corner spot
+	opposite_corner_choices = {
+		1:[ 3 , 7 ] , 3:[ 1 , 9 ] , 
+		7:[ 1 , 3] , 9:[ 3 , 7 ]
 	}
 		
 	def draw_board(self):
@@ -37,21 +43,28 @@ class GameBoard(object):
 		
 	##this function updates the board's data space is the selected space and player_token is either an X or O depending if 
 	##the player or the computer made the move
-	def update_board_data(self , space , player_token):
+	def update_board_data( self , space , player_token ):
 		self.board_data[int(space) - 1] = player_token
 	
-	def get_board_data(self):
+	def get_board_data( self ):
 		return self.board_data
 		
-	def get_board_moves_left(self):
+	def get_board_moves_left( self ):
 		moves_left = []
 		for items in self.board_data:
 			if items != 'X' and items != 'O':
 				moves_left.append(items)
 		return moves_left
-
+	##Checks and sees if a corner was taken as the first move
+	##If we have 2 X's return false as its no longer the first turn
+	def check_players_first_move( self ):
+		for corner in self.corners:
+			if self.board_data[ corner - 1 ] == "X":
+				return True
+		return False
+	
 	##check all paths to see if the game has a winner
-	def check_for_winner(self):
+	def check_for_winner( self ):
 		##first lets make sure there isnt a tie
 		if ( len( self.get_board_moves_left() ) == 0):
 			return "tie"
@@ -64,10 +77,8 @@ class GameBoard(object):
 			cpu_token_count = 0
 			##nested loop for the board rows that result in a win
 			for board_positions in value:
-				print board_positions
-				print self.board_data
+
 				if ( self.board_data[board_positions - 1] == "X" ):
-					print self.board_data[board_positions -1 ]
 					player_token_count += 1
 				elif ( self.board_data[board_positions - 1] == "O" ):
 					cpu_token_count += 1
@@ -82,7 +93,7 @@ class GameBoard(object):
 		
 	##Check and see if someone can win, the X or O given as the second parameter determines who we are looking for
 	##if so return the index that would win the game so we know where to place our O
-	def is_game_winnable(self, token):
+	def is_game_winnable( self, token ):
 	##This starts off a lot like checking for winners, but the logic is a little different
 		##loop through the dictionary of winnable paths
 		for key, value in self.board_winning_paths.iteritems():
@@ -101,23 +112,23 @@ class GameBoard(object):
 			if winnable_space != "-1" and len(taken_spaces) == 2:
 				return winnable_space
 		return False
-	def get_open_corners(self):
+	def get_open_corners( self ):
 		open_corners = []
 		for corner in self.corners:
 			if self.board_data[corner - 1] != "X" and self.board_data[corner - 1] != "O":
 				open_corners.append(corner)
 		return open_corners
 		
-	def get_open_sides(self):
+	def get_open_sides( self ):
 		open_sides = []
 		for side in self.sides:
-			if self.board_data[side - 1] != "X" and self.board_data[corner - 1] != "O":
+			if self.board_data[side - 1] != "X" and self.board_data[side - 1] != "O":
 				open_sides.append(side)
-		return open_corners
+		return open_sides
 	
 	##returns available the best place to move to
 	## spaceToCheck is either corner or side, which tells us how to proceed
-	def get_move(self, space_to_check):
+	def get_move( self , space_to_check ):
 		open_spaces = []
 		open_spaces_size = []
 		
@@ -137,7 +148,7 @@ class GameBoard(object):
 			##return the number since that is the only open corner
 			return open_spaces[0]
 		else:
-			##randomly chose what corner we want
+			##randomly chose what space we want
 			return random.choice(open_spaces)
 			
 		
