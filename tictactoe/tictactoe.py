75e9b049968766103211c1887f5e9338b276a04f
Gail Edwards <gailhedw@gmail.com>
2014-03-29 16:55:51 -0400

    Modified to move the second player to the server.

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -3,6 +3,12 @@
 import webapp2
 from google.appengine.ext.webapp import template
 
+import json
+import random
+
+#
+# main page: start the first game and wait for the human to move
+#
 class MainPage (webapp2.RequestHandler):
     def get (self):
         self.response.out.write(template.render('views/tictactoe.html',{}))
@@ -10,6 +16,26 @@ class MainPage (webapp2.RequestHandler):
     def post (self):
         self.get()
 
+#
+# pick server's next move
+#
+class Move (webapp2.RequestHandler):
+    def move(self, emptySquares):
+        # just pick a random empty square
+        return(random.choice(emptySquares))
+
+    def get (self):
+        emptySquares= json.loads(self.request.get('empty_squares'));
+        resp = self.move(emptySquares);
+        self.response.out.write(resp)
+        
+    def post (self):
+        self.get()
+
+#
+# route url to handler class
+#
 app = webapp2.WSGIApplication([
     webapp2.Route('/tictactoe', MainPage),
+    webapp2.Route('/tictactoe/move', Move),
     ], debug=True)
