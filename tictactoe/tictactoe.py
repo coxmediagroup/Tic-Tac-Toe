5a5bb40444f3e0a36e4e2d2c3a0e512c47549c4b
Gail Edwards <gailhedw@gmail.com>
2014-03-29 20:18:20 -0400

    Block the human from winning.

--- tictactoe/tictactoe.py
+++ tictactoe/tictactoe.py
@@ -6,6 +6,9 @@ from google.appengine.ext.webapp import template
 import json
 import random
 
+import pprint
+pp = pprint.PrettyPrinter(indent=4)
+
 #
 # main page: start the first game and wait for the human to move
 #
@@ -20,13 +23,24 @@ class MainPage (webapp2.RequestHandler):
 # pick server's next move
 #
 class Move (webapp2.RequestHandler):
-    def move(self, emptySquares):
+    def move(self, emptySquares, winningScores, humanScore):
+        # block human from any next-move win
+        for i in emptySquares:
+            # use the same algorithm applied by javascript to check for a win 
+            for score in winningScores:
+                if ( int(emptySquares[i]) + int(humanScore) & int(score) == int(score)):
+                    return(i)
+        # todo: thwart human for setting up a win
         # just pick a random empty square
-        return(random.choice(emptySquares))
+        return(random.choice([i for i in emptySquares]))
+            
 
     def get (self):
-        emptySquares= json.loads(self.request.get('empty_squares'));
-        resp = self.move(emptySquares);
+        parms = json.loads(self.request.get('parms'))
+        emptySquares = parms['empty_squares']
+        winningScores = parms['winning_scores']
+        humanScore = parms['human_score']
+        resp = self.move(emptySquares,winningScores,humanScore);
         self.response.out.write(resp)
         
     def post (self):
