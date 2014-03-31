d614ffc9ee10db0c2f817e7070f2a71d54cf4d40
Danny Burrows <burrows.danny@gmail.com>
2014-03-31 13:47:45 -0500

    Initial psuedo code, installation and test of Tkinter

---
+++ tictactoe.py
@@ -0,0 +1,16 @@
+#	Tic Tac Toe
+#	Danny Burrows
+#
+#	Implement a version of tic-tac-toe where a player plays against an AI bot where the AI never loses.
+
+#	draw board
+
+#	get user move
+#	-test valid user move
+#	-mark move
+
+#	computer move
+#	-find best move
+#	-mark move
+
+#	again?
