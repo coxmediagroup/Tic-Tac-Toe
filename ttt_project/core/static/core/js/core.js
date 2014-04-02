9152ae0c1a683a00c043ece777b96754b4fe1c65
Doug Waite <deegeedubb@gmail.com>
2014-04-02 14:27:13 -0600

    Prevent user from clicking on a space that's already played

--- ttt_project/core/static/core/js/core.js
+++ ttt_project/core/static/core/js/core.js
@@ -1,21 +1,21 @@
 $(function() {
-    $('input[name="username"]').focus();
-
     $('#game_board td').click(function(el) {
-        space_id = this.id;
-        game_id = $('#header span').text();
-        // alert('clicked on space ' + game_id);
+        if (!$(this).text()) {
+            space_id = this.id;
+            game_id = $('#header span').text();
+            // alert('clicked on space ' + game_id);
 
-        $.ajax({
-            type: "GET",
-            url: "/player_move/"+game_id+"/"+space_id+"/",
-            dataType: "html",
-            success: function(data) {
-                if (data == "True") {
-                    // alert("Move Saved for space: " + space_id);
+            $.ajax({
+                type: "GET",
+                url: "/player_move/"+game_id+"/"+space_id+"/",
+                dataType: "html",
+                success: function(data) {
+                    if (data == "True") {
+                        // alert("Move Saved for space: " + space_id);
+                    }
+                    $("#game_board").replaceWith(data);
                 }
-                $("#game_board").replaceWith(data);
-            }
-        });
+            });
+        };
     });
 });
