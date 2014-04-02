c3a24ff4bdef7dbb1d8d887f6fba3fc2594a450c
Doug Waite <deegeedubb@gmail.com>
2014-04-02 13:19:41 -0600

    Adding initial ajax functionality

---
+++ ttt_project/core/static/core/js/core.js
@@ -0,0 +1,20 @@
+$(function() {
+    $('input[name="username"]').focus();
+
+    $('#game_board td').click(function(el) {
+        space_id = this.id;
+        game_id = $('#header span').text();
+        // alert('clicked on space ' + game_id);
+
+        $.ajax({
+            type: "GET",
+            url: "/player_move/"+game_id+"/"+space_id+"/",
+            dataType: "html",
+            success: function(data) {
+                if (data == "True") {
+                    // alert("Move Saved for space: " + space_id);
+                }
+            }
+        });
+    });
+});
--- ttt_project/core/urls.py
+++ ttt_project/core/urls.py
@@ -2,4 +2,5 @@ from django.conf.urls import patterns, url
 
 urlpatterns = patterns('core.views',
     url(r'^$', 'main'),
+    url(r'^player_move/(?P<game_id>\d+)/(?P<space_id>\d)/', 'player_move', name='player_move'),
 )
