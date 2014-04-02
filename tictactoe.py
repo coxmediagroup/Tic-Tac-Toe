d3015fad87bffb305485a7681a563f6bc8337816
Danny Burrows <burrows.danny@gmail.com>
2014-04-02 15:02:25 -0500

    added basic turns to test, turns out my assumptions on where the ai chooses for testing were incorrect. that's problematic...

--- tictactoe.py
+++ tictactoe.py
@@ -3,146 +3,7 @@
 #	Danny Burrows
 #
 #	Implement a version of tic-tac-toe where a player plays against an AI bot where the AI never loses.
-import copy
-# possible wins:
-#	x, x + 1, x + 2 | y = i (0 to 2)
-#	y, y + 1, y + 2 | x = i (0 to 2)
-#	z, z+1, z+2 | z = (x, y)
-#	z, z-1, z-2 | z = (x, y) from x=2,y=0
 
-#
-def checkWin(board):
-	# check wins on board	
-	for i in range(3):
-		# check rows
-		if (board[i][0] == board[i][1] and board[i][1] == board[i][2] and board[i][0] != 0):
-			return board[i][0]
-		# check columns
-		elif (board[0][i] == board[1][i] and board[1][i] == board[2][i] and board[0][i] != 0):
-			return board[0][i]
-		# check both cross conditions
-		elif (board[0][0] == board[1][1] and board[1][1] == board[2][2] and board[0][0] != 0):
-			return board[0][0]
-		elif (board[2][0] == board[1][1] and board[1][1] == board[0][2] and board[2][0] != 0):
-			return board[2][0]
-	return False
-
-def checkTie(board):
-	for x in board:
-		for y in x:
-			if not y:
-				return False
-	return True
-
-#	find the best move for the AI to ensure no lose situation
-def generateMoves(board):
-	# check all possible open moves
-	possMoves = []
-	for x in range(3):
-		for y in range(3):
-			if possibleMove(board, (x,y)):
-				possMoves.append((x,y))
-	
-	return possMoves
-
-def miniMax(board, player):
-	# pseudo code for minimax
-	# miniMax(node)
-	# 	max = -inf, min = +inf
-	#	if node.isBlack
-	#		foreach(child of node)
-	#			max = max(max, minimax(child))
-	#		return max
-	#	else
-	#		foreach(child of node)
-	#			min = min(min, minimax(child))
-	#		return min
-	#printBoard(board)
-	#print "\n"
-	moves = generateMoves(board)
-	if (checkWin(board) == 1):
-		return (1, None)
-	elif (checkWin(board) == 2):
-		return (2, None)
-	elif (checkTie(board)):
-		return (0, None)
-
-	if player == 1:
-		nextPlayer = 2
-	elif player == 2:
-		nextPlayer = 1
-	
-	if (player == 1):
-		best = (-100, None);
-		for move in moves:
-			markMove(board, move, player)
-			value = miniMax(board, nextPlayer)[0]
-			if value > best[0]:
-				best = (value,move)
-		return best
-	elif (player == 2):
-		best = (+100, None);
-		for move in moves:
-			markMove(board, move, player)
-			value = miniMax(board, nextPlayer)[0]
-			if value < best[0]:
-				best = (value,move)
-		return best
-
-def markMove(board, position, player):
-	x = position[0]
-	y = position[1]
-	board[x][y] = player
-
-#	check if move is possible
-def possibleMove(board, possible):
-	x = possible[0]
-	y = possible[1]
-	if board[x][y] == 0:
-		return True
-	return False
-
-#	draw board
-def printBoard(board):
-	print "%s | %s | %s" % (board[0][0], board[0][1], board[0][2])
-	print "--+---+--"
-	print "%s | %s | %s" % (board[1][0], board[1][1], board[1][2])
-	print "--+---+--"
-	print "%s | %s | %s" % (board[2][0], board[2][1], board[2][2])
-
-#	some simple test cases
-"""
-def runTests():
-	board = [ [1,0,0],[1,2,0],[0,0,0] ]
-	testCase(board, (0,2))
-
-	board = [ [1,1,0],[2,2,0],[0,0,0] ]
-	testCase(board, (1,2))
-
-	board = [ [1,2,1],[2,1,2],[2,1,2] ]
-	testCase(board, None)
-
-	board = [ [0,0,1],[0,2,1],[0,0,0] ]
-	testCase(board, (2,2))
-
-	board = [ [2,2,0],[0,1,1],[0,1,0] ]
-	testCase(board, (0,2))
-	
-	board = [ [1,0,2],[0,1,0],[0,0,0] ]
-	testCase(board, (2,2))
-
-	board = [ [0,0,0],[0,2,0],[1,0,1] ]
-	testCase(board, (2,1))
-#	run test, determine outcome
-
-def testCase(board, expected):
-	#printBoard(board)
-	bestMove = miniMax(board, 2)
-	if (bestMove[1] != expected):
-		print "FAILED \t Expected " + str(expected) + " Calculated " + str(bestMove[1])
-	else:
-		print "PASSED \t Expected " + str(expected)
-"""
 # game class
 # need the following
 # -actual board array
@@ -158,7 +19,12 @@ class gameState:
 		self.winner = None
 
 	def printBoard(self):
-		print self.board
+		print "%s | %s | %s" % (self.board[0], self.board[1], self.board[2])
+		print "--+---+--"
+		print "%s | %s | %s" % (self.board[3], self.board[4], self.board[5])
+		print "--+---+--"
+		print "%s | %s | %s" % (self.board[6], self.board[7], self.board[8])
+		#print self.board
 
 	def availMoves(self):
 		moves = []
@@ -176,6 +42,10 @@ class gameState:
 			if self.board[i] == self.board[j] == self.board[k] and self.board[i] != 0:
 				self.winner = self.board[i]
 				return True
+
+		if 0 not in self.board:
+			self.winner = 3
+			return True
 
 		return False
 
@@ -206,7 +76,8 @@ class botAI:
 		self.opponent = opponent
 
 	def miniMax(self, gameInstance):
-		return self.maxMove(gameInstance)
+		move = self.maxMove(gameInstance)[1]
+		gameInstance.makeMove(move, self.marker)
 
 	# bestMove location and bestMove value
 	# value will be used to determine if the move is a good one or not, from eval game
@@ -224,7 +95,7 @@ class botAI:
 				value = self.evalGame(ply)
 			# recusive case
 			else:
-				value,move = self.minMove(ply)
+				value,testMove = self.minMove(ply)
 			
 			ply.clearMove(move)
 
@@ -243,13 +114,13 @@ class botAI:
 		for move in ply.availMoves():
 			# for each move, make the move and test the result
 			# if the move is not the best move, based on evaluation of each ply, then undo move
-			ply.makeMove(move, self.marker)
+			ply.makeMove(move, self.opponent)
 			# base case
 			if ply.gameOver():
 				value = self.evalGame(ply)
 			# recusive case
 			else:
-				value,move = self.minMove(ply)
+				value,testMove = self.maxMove(ply)
 			
 			ply.clearMove(move)
 
@@ -272,13 +143,18 @@ class botAI:
 
 def testCase(gameState, expectedMove):
 	bot = botAI()
-	nextMove = bot.miniMax(gameState)[1]
+	nextMove = bot.miniMax(gameState)
+	#print nextMove
 	if nextMove == expectedMove:
 		print 'PASSED'
 	else:
 		print 'FAILED Expected ' + str(expectedMove) + ' Calculated ' + str(nextMove)
+	
 
 def runTests():
+	# 0 1 2
+	# 3 4 5
+	# 6 7 8
 	game = gameState()
 	game.setBoard("0 0 1 0 2 1 0 0 0")
 	testCase(game, 8)
@@ -286,6 +162,32 @@ def runTests():
 	testCase(game, 5)
 	game.setBoard("1 1 2 2 2 1 0 0 0")
 	testCase(game, 6)
+	game.setBoard("1 0 0 1 2 0 0 0 0")
+	testCase(game, 6)
+	game.setBoard("2 1 1 0 2 0 0 0 0")
+	testCase(game, 8)
+
+def main():
+	game = gameState()
+	bot = botAI()
+	game.printBoard()
+	while True:
+		
+		playermove = raw_input("Move ? ")
+		game.makeMove(int(playermove), 1)
+		print "\n"
+		game.printBoard()
+		if game.gameOver():
+			print game.winner
+			exit()
+		
+		bot.miniMax(game)
+		print "\n"
+		game.printBoard()
+		if game.gameOver():
+			print game.winner
+			exit()
 
 if __name__ == "__main__":
-	runTests()
+	#runTests()
+	main()
