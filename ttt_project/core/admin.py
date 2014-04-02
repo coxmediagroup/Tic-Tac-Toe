f3b21bc63d5cc603fe3dec47145db4d4140169ce
Doug Waite <deegeedubb@gmail.com>
2014-04-02 11:05:32 -0600

    Initial models & migration

--- ttt_project/core/admin.py
+++ ttt_project/core/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
+from core.models import Game, Move
 
-# Register your models here.
+
+class GameAdmin(admin.ModelAdmin):
+    list_display = ('id', 'completed')
+
+
+class MoveAdmin(admin.ModelAdmin):
+    list_display = ('id', 'game', 'player', 'computer', 'space')
+
+
+admin.site.register(Move, MoveAdmin)
+admin.site.register(Game, GameAdmin)
--- ttt_project/core/models.py
+++ ttt_project/core/models.py
@@ -1,20 +1,20 @@
 from django.db import models
-
-
-class Board(models.Model):
-
-    def __unicode__(self):
-        pass
+from django.contrib.auth.models import User
 
 
 class Game(models.Model):
+    completed = models.BooleanField(default=False)
 
     def __unicode__(self):
-        pass
+        return self.id
 
 
 class Move(models.Model):
+    game = models.ForeignKey(Game)
+    player = models.ForeignKey(User)
+    computer = models.BooleanField(default=False)
+    space = models.IntegerField()
 
     def __unicode__(self):
-        pass
+        return "Move for game %s" % self.game
 
