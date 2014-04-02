91d1c7f9379bf35de6c78553d2c2f8755a8e11e3
Reverend Scandal <a@debian>
2014-04-02 14:49:18 -0400

    clean up

--- tic/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from tic.models import TicBoard
-
-
-admin.site.register(TicBoard)
--- tic/urls.py
+++
@@ -1,8 +0,0 @@
-from django.conf.urls import patterns, include, url
-from django.conf import settings
-
-urlpatterns = patterns('', 
-	url(r'^$', 'tic.views.play'),  
-
-)
-
