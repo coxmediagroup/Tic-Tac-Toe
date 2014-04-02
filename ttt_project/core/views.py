da5172eced83bed28962d6423d74da5de4e17859
Doug Waite <deegeedubb@gmail.com>
2014-04-02 13:25:14 -0600

    Initial views & working with ajax

--- ttt_project/core/views.py
+++ ttt_project/core/views.py
@@ -1,6 +1,21 @@
-from django.shortcuts import render
+from django.http import HttpResponse
 from annoying.decorators import render_to
+from core.models import Game
 
 @render_to('core/core.html')
 def main(request):
-    return {}
+    new_game = Game.objects.create()
+    return {'game_num': new_game.id}
+
+
+@render_to('core/board.html')
+def player_move(request, game_id, space_id):
+    return HttpResponse("True")
+
+
+def calculate_computer_move(request, current_game):
+    pass
+
+
+def computer_move(request):
+    pass
