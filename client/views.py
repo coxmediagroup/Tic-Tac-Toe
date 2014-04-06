840ef499cbc3f5a4b7e9c4efc97783c0f1273032
Jack Slingerland <jack.slingerland@gmail.com>
2014-04-06 08:42:47 -0400

    Adding endpoint for creating a new game.

--- client/views.py
+++ client/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from annoying.decorators import ajax_request
+from django.views.decorators.csrf import csrf_exempt
+from .models import Game
 
-# Create your views here.
+
+@csrf_exempt
+@ajax_request
+def new_game(request):
+    name = request.POST.get('name', None)
+    if name:
+        game = Game(name=name)
+        game.save()
+        result = {
+            'id' : game.id
+        }
+    else:
+        result = {
+            'error' : "'name' field is required."
+        }
+
+    return result
--- tictactoe/urls.py
+++ tictactoe/urls.py
@@ -1,12 +1,17 @@
 from django.conf.urls import patterns, include, url
 
+from client.views import new_game
+
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'tictactoe.views.home', name='home'),
+    #url(r'^$', 'tictactoe.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
+
+    # API
+    url(r'^api/new-game/$', new_game, name='new-game'),
 
     url(r'^admin/', include(admin.site.urls)),
 )
