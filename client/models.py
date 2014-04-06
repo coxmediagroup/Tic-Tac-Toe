6bbba409455a4679bb940e17fb788d8a3384a242
Jack Slingerland <jack.slingerland@gmail.com>
2014-04-06 08:06:19 -0400

    Adding Game model and admin

--- client/models.py
+++ client/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
-# Create your models here.
+class Game(models.Model):
+
+    name = models.CharField(max_length=100)
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return "%d:%s" % (self.pk, self.name)
