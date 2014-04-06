173a1388d10e0da130f826737107471a28961f0e
Jack Slingerland <jack.slingerland@gmail.com>
2014-04-06 08:16:41 -0400

    Adding Move model and admin.

--- client/admin.py
+++ client/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Game
+from .models import Game, Move
 
 class GameAdmin(admin.ModelAdmin):
     pass
 
+class MoveAdmin(admin.ModelAdmin):
+    list_display = ('id', 'game', 'x_position', 'y_position', 'created')
+    list_filter = ('game__name',)
+
 admin.site.register(Game, GameAdmin)
+admin.site.register(Move, MoveAdmin)
--- client/models.py
+++ client/models.py
@@ -6,4 +6,16 @@ class Game(models.Model):
     created = models.DateTimeField(auto_now_add=True)
 
     def __unicode__(self):
-        return "%d:%s" % (self.pk, self.name)
+        return "%s" % (self.name)
+
+class Move(models.Model):
+
+    board = models.CharField(max_length=25)
+    game = models.ForeignKey(Game)
+    x_position = models.IntegerField()
+    y_position = models.IntegerField()
+
+    created = models.DateTimeField(auto_now_add=True)
+
+    def __unicode__(self):
+        return "%d: %s (%d,%d)" % (self.pk, self.game.name, self.x_position, self.y_position)
