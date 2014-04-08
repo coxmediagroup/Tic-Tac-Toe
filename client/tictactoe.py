aae75f14e615950c934e609a9b3abaedc25107de
Jack Slingerland <jack.slingerland@gmail.com>
2014-04-08 19:15:43 -0400

    Adding some documentation.

--- client/tictactoe.py
+++ client/tictactoe.py
@@ -1,7 +1,16 @@
 from copy import copy
 
+"""
+AI for Playing TicTacToe.  This is an imperfect algorithm, which I find
+to be nicer, but isn't exactley part of the original spec.  I simply ran
+out of time to finish debugging this, and decided to focus on other parts.
+"""
 class TicTacToe:
 
+    """
+    You can either start with a blank board or start from an existing
+    board.  Coordinates are the move you are making.
+    """
     def __init__(self, board=None, coordinates=None):
         self.board = {}
         self.opponent = 'O'
@@ -18,6 +27,9 @@ class TicTacToe:
             self.board[coordinates] = self.player
             (self.player, self.opponent) = (self.opponent, self.player)
 
+    """
+    Pretty printed output of the board for debugging.
+    """
     def __str__(self):
         output = ""
         for y in range(3):
@@ -26,10 +38,19 @@ class TicTacToe:
             output += '\n'
         return output
 
+    """
+    Given a set of coordinates, make your play.  Then switch who the
+    player and opponent are.
+    """
     def move(self, coordinates):
         self.board[coordinates] = self.player
         (self.player, self.opponent) = (self.opponent, self.player)
 
+    """
+    Recursive MinMax algorithm for determining next best move.  Essentially
+    brute-forces its way through the game board, searching possible move
+    combinations for the best possible outcome.
+    """
     def minmax(self, player):
         if self._won():
             return {'value' : -1, 'coordinates' : None} if player else {'value': 1, 'coordinates' : None}
@@ -56,12 +77,18 @@ class TicTacToe:
                         best_move['coordinates'] = (x,y)
             return best_move
 
+    """
+    Checks to see if the game board is tied.
+    """
     def _tied(self):
         for x,y in self.board:
             if self.board[x,y] == None:
                 return False
         return True
 
+    """
+    Checks if the game is won horizonally, vertically, and diagonally both ways.
+    """
     def _won(self):
         # Horizontal wins
         for y in range(3):
