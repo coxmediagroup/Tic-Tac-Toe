757ae4cd8ebe9e187725127a08738b08bd1ce804
Reverend Scandal <a@debian>
2014-04-08 08:34:50 -0400

    removed debug output from tic.js template.

--- asite/templates/tic/tic.js
+++ asite/templates/tic/tic.js
@@ -2,11 +2,6 @@ var xpick="{{xpick}}";
 
 tic.pk={{pk}};
 
-var all={
-	{% for key, value in mtd.items %}
-    {{ key }}: {{ value }},
-	{% endfor %}
-	};
 {% if xpick %}
 	tic.setsquare("{{xpick}}","x");
 	tic.addclicks();
