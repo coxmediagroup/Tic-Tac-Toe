e4a31ea7de21a42a0ee0e8b24d53ecd4353b0866
Reverend Scandal <a@debian>
2014-04-08 06:33:25 -0400

    updated tic.urls.py because opick was set for just a single char in the url <snip> url(r'^(?P<opick>\w) </snip> now <snip> url(r'^(?P<opick>\w+) </snip>

--- asite/tic/urls.py
+++ asite/tic/urls.py
@@ -3,7 +3,7 @@ from django.conf import settings
 
 urlpatterns = patterns('', 
 	url(r'^tic.js$', 'tic.views.createBoard'),  
-	url(r'^(?P<opick>\w)/(?P<pk>\d+)/tic.js$', 'tic.views.updateBoard',name='update'),
+	url(r'^(?P<opick>\w+)/(?P<pk>\d+)/tic.js$', 'tic.views.updateBoard',name='update'),
 	url(r'^$','tic.views.index'),
 )
 
