aaad6a59fe441ddaf9f7c16fe0acef8cf4ed01e1
Percy Perez <percyp3@gmail.com>
2014-04-17 18:49:56 -0700

    Serving static files via static tag

--- game/urls.py
+++ game/urls.py
@@ -1,5 +1,7 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 
 admin.autodiscover()
@@ -10,3 +12,6 @@ urlpatterns = patterns(
     url(r'', include('game.tictactoe.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+if settings.DEBUG:
+    urlpatterns += staticfiles_urlpatterns()
