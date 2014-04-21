0f47df27c17ffad78f8bdb58e4eb78e0e5d0abae
Percy Perez <percyp3@gmail.com>
2014-04-20 20:27:37 -0700

    Bringing bootstrap into templates and updating makeMove method to send POST data

--- static/js/controllers/tictactoe.js
+++ static/js/controllers/tictactoe.js
@@ -7,17 +7,31 @@ tictactoe.config([
         $interpolateProvider.startSymbol('[[');
         $interpolateProvider.endSymbol(']]');
         $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+        $httpProvider.defaults.transformRequest = function(data) {
+            if (data === undefined) {
+                return data;
+            }
+            return $.param(data);
+        };
     }
 ]);
 
-tictactoe.controller('TicTacToeCtrl', ['$scope', '$http', function($scope, $http, _) {
+tictactoe.controller('TicTacToeCtrl', ['$scope', '$http', '$element', function($scope, $http, $element) {
 
     $scope.makeMove = function(row, col) {
-        var data = {};
+        var data = {
+                'row': row,
+                'col': col
+            },
+            el = '#move_' + row + col;
 
         $http.post('/makemove/', data)
             .success(function(data, status, headers, config) {
+                $element.find(el).prop('disabled', true).html('X');
                 console.log('success');
+                // TODO: Check the following in data:
+                // if player won
+                // else if ai won
             })
             .error(function(data, status, headers, config) {
                 console.log('error');
