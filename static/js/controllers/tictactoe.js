8954b544c35d52b25fc3aa5296db406ef9604f42
Percy Perez <percyp3@gmail.com>
2014-04-26 16:59:46 -0700

    Updating TicTacToe controller to be more Angular-like and displaying alert message

--- static/js/controllers/tictactoe.js
+++ static/js/controllers/tictactoe.js
@@ -6,7 +6,7 @@ tictactoe.config([
     function($httpProvider, $interpolateProvider) {
         $interpolateProvider.startSymbol('[[');
         $interpolateProvider.endSymbol(']]');
-        $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+        $httpProvider.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
         $httpProvider.defaults.transformRequest = function(data) {
             if (data === undefined) {
                 return data;
@@ -16,26 +16,43 @@ tictactoe.config([
     }
 ]);
 
-tictactoe.controller('TicTacToeCtrl', ['$scope', '$http', '$element', function($scope, $http, $element) {
+tictactoe.controller('TicTacToeCtrl', ['$scope', '$http', '$timeout', function($scope, $http, $timeout) {
+    $scope.makeMove = function() {
+        var move = {'move': $scope.move};
+        $scope.letter = 'O';
 
-    $scope.makeMove = function(mv) {
-        var move = {'move': mv};
-
-        // Player move
-        markMove(mv, 'O');
         $http.post('/makemove/', move)
             .success(function(data, status, headers, config) {
                 if (data.success) {
-                    setTimeout(function() {
-                        // AI move
-                        markMove(data.move, 'X');
-                    }, 4000);
-                    console.log(data);
-                    // TODO: Check the following in data:
-                    // if player won
-                    // else if ai won
+                    if (data.move !== undefined) {
+                        $timeout(function() {
+                            // Mark AI move
+                            markMove(data.move, 'X');
+                        }, 300);
+                    }
+
+                    if (data.win_status) {
+                        var alert = angular.element('.alert-box');
+                        alert.addClass('alert alert-success');
+
+                        if (data.win_status === 'tie') {
+                            alert.append("It's a tie. You almost got me that time!");
+                        } else {
+                            data.board.map(function(letter, i) {
+                                if (letter === '') {
+                                    markMove(i, '');
+                                }
+                            });
+                            if (data.win_status === 'computer') {
+                                alert.append('I win. Better luck next time!');
+                            } else {
+                                // This should never happen
+                                alert.append("You won! Oops I don't know how that happened!");
+                            }
+                        }
+                        alert.append(' Click on the links above to play again.');
+                    }
                 }
-                // else Something went wrong
             })
             .error(function(data, status, headers, config) {
                 console.log('error');
@@ -43,7 +60,10 @@ tictactoe.controller('TicTacToeCtrl', ['$scope', '$http', '$element', function($
     };
 
     function markMove(move, letter) {
-        var el = '#move_' + move;
-        $element.find(el).prop('disabled', true).html(letter);
+        var el = '#move_' + move,
+            model = angular.element(el).scope();
+
+        model.letter = letter;
+        model.btn_state = true;
     }
 }]);
