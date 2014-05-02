679b56bb5cff88e034f30f3c951de49ee04bc2ad
= <sampedro@colorado.edu>
2014-05-02 17:11:39 -0600

    Revised project structure, added base template.

--- tictactoe/apps/tictactoe/urls.py
+++ tictactoe/apps/tictactoe/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'tictactoe.views.home', name='home'),
+    url(r'^$', 'tictactoe.views.index', name='index'),
     # url(r'^tictactoe/', include('tictactoe.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
