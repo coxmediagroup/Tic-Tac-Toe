b3df30665226561745945541093b49d82bdf5125
= <sampedro@colorado.edu>
2014-05-06 14:57:05 -0600

    Added high-score board to index page.

--- tictactoe/apps/tictactoe/views.py
+++ tictactoe/apps/tictactoe/views.py
@@ -6,6 +6,27 @@ from tictactoe.models import Player
 
 def index(request):
     ctx = {}
+    un = request.session.get('username',None)
+    if un:
+    	player_qs = Player.objects.filter(
+    		username=un
+    		)
+    	if player_qs.count() > 0:
+    		player = player_qs[0]
+    		rank = Player.objects.all(
+    			).filter(
+    			plays__gte=player.plays
+    			).exclude(
+    			username=player.username
+    			).order_by(
+    			'-plays'
+    			).count() + 1
+    		ctx.update({'player':player,
+    			'player_rank':rank})
+
+    players = Player.objects.all().order_by('-plays')[:10]
+    ctx.update({'players':players})
+
     return render(request, 'index.html', ctx)
 
 def play_game(request):
