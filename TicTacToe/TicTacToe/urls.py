21994e143a99a8eee50d0e6699f9f31c977b6506
reza <esfandani@gmail.com>
2014-05-09 20:47:55 -0700

    added first view

--- TicTacToe/TicTacToe/urls.py
+++ TicTacToe/TicTacToe/urls.py
@@ -9,4 +9,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^$', 'tic_app.views.index', name='index'),
 )
--- TicTacToe/tic_app/views.py
+++ TicTacToe/tic_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+
+# index view 
+def index(request):
+    return render(request, 'tic_app/index.html')
