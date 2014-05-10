b1003edc9e0ee9b777787786731d88b8bdb71e89
reza <esfandani@gmail.com>
2014-05-09 21:21:20 -0700

    completed the structure of index.html file, also added some filters(get_char, get_range and modulo in filter_utils(templatetags directory) to use them in template file(s). added css,images and js folders. added necessary images like  o.jpg and x.jpg for playing the game. added an empty tictactoe.js in js folder. added css file.

--- TicTacToe/tic_app/views.py
+++ TicTacToe/tic_app/views.py
@@ -1,8 +1,11 @@
 from django.shortcuts import render
+from django.views.decorators.csrf import csrf_exempt
 
 # Create your views here.
 
 
 # index view 
+
+@csrf_exempt
 def index(request):
     return render(request, 'tic_app/index.html')
