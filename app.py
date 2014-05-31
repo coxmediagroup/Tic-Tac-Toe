89f5cbad572f2385bc44b78ab03f2e1a6d1376de
Heather Billings <heatherjaybillings@gmail.com>
2014-05-31 14:38:24 -0500

    bones of cleaner structure

--- app.py
+++ app.py
@@ -13,12 +13,28 @@ def calculate_move():
         request.form.getlist('board[bottom][]')
     ]
 
+    return jsonify(board=BoardWizard(board))
 
-    return jsonify(board=board)
+class BoardWizard:
+    def __init__(self, board):
+        self.orig_board = board
+        self.new_board = []
+        self.player_can_win = False
+        self.computer_can_win = False
 
+    def check_for_victory(self):
+        pass
 
+    def find_possible_moves(self):
+        pass
 
+    def check_next_move_winner(self):
+        pass
 
+    def prioritize_moves(self):
+        pass
+
+    return self.new_board
 
 if __name__ == "__main__":
     app.run(debug=True)
