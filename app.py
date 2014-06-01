5b03476d79d9e522cfabd17857e79aeaf2cd348c
Heather Billings <heatherjaybillings@gmail.com>
2014-06-01 14:17:58 -0500

    represent board as array of tuples and tally potential wins

--- app.py
+++ app.py
@@ -17,43 +17,39 @@ def calculate_move():
 
 class Board:
     def __init__(self, board):
-        self.orig_board = board
-        self.possible_moves = []
-        self.new_board = [[],[],[]]
-        self.player_can_win = False
-        self.computer_can_win = False
+        # Tuples of every possible legit combo
+        # Represented by XY pos, then square's value
+        # Rows first top-bottom, then columns L-R, then diagonals
+        self.orig_board = [
+            [(0, 0, board[0][0]), (0, 1, board[0][1]), (0, 2, board[0][2])],
+            [(1, 0, board[1][0]), (1, 1, board[1][1]), (1, 2, board[1][2])],
+            [(2, 0, board[2][0]), (2, 1, board[2][1]), (2, 2, board[2][2])],
+            [(0, 0, board[0][0]), (1, 0, board[1][0]), (2, 0, board[2][0])],
+            [(1, 0, board[1][0]), (1, 1, board[1][1]), (1, 2, board[1][2])],
+            [(0, 2, board[0][2]), (1, 2, board[1][2]), (2, 2, board[2][2])],
+            [(0, 0, board[0][0]), (1, 1, board[1][1]), (2, 2, board[2][2])],
+            [(0, 2, board[0][2]), (1, 1, board[1][1]), (0, 2, board[0][2])]
+        ]
+        self.ranked_triplets = []
+        self.new_board = [[], [], []]
         self.player_letter = 'O'
         self.computer_letter = 'X'
 
-    def check_for_victory(self):
-        pass
-
-    def find_possible_moves(self):
-        cols = []
-        for rowindex, row in enumerate(self.orig_board):
-            self.predict_winner_rows(rowindex)
-            for colindex, square in enumerate(row):
-                self.predict_winner_cols(cols, colindex)
-                cols.append(self.orig_board[rowindex][colindex])
-                if self.orig_board[rowindex][colindex] == '':
-                    self.possible_moves.append([row,col])
-
-    def predict_winner_rows(self, rowindex, letter=player_letter):
-        check_row = sum([1 for square in self.orig_board[rowindex] if square == letter])
-        if check_row == 2:
-            for index, square in enumerate(self.orig_board[rowindex]:
-                    if square == '':
-                        self.possible_moves[rowindex][index] == 'X1'
-
-    def predict_winner_cols(self, cols, colindex, letter=player_letter):
-        check_col = sum([1 for square in cols if square == letter])
-        if check_col == 2:
-            for row, square in enumerate(cols):
-                if square == '':
-                    self.possible_moves[row][colindex]
 
     def prioritize_moves(self):
-        pass
+        for triplet in orig_board:
+            player_can_win = tally_values(triplet)
+            computer_can_win = talley_values(triplet, computer_letter)
+
+        def tally_values(triplet, letter=player_letter):
+            count = 0
+            for square in triplet:
+                if square[2] == letter:
+                    count += 1
+            if count == 2:
+                return True
+            else:
+                return False
 
     return self.new_board
 
