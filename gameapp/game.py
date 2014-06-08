780ae13ad77b4d10394d28267761e6a47d07e8a1
veri745 <github.veritas@xoxy.net>
2014-06-08 11:54:02 -0500

    Verify board integrity. Initial unit tests

--- gameapp/game.py
+++ gameapp/game.py
@@ -7,6 +7,8 @@ class GameBoard:
 
       self.board = self.create_board(game_state)
       
+      self.validate_board()
+      
    def create_board(self, board_string):
       "Convert a 9-character string to a 2-D grid of rows and columns"
       board = []
@@ -16,16 +18,29 @@ class GameBoard:
       return board
       
    def move(self, row, column):
-      player = 'X' if self.get_state().count('_') % 2 else 'O'
-      self.board[row][column] = player
+      self.board[row][column] = self.get_player()
+      
+   def get_player(self):
+      return 'X' if self.get_state().count('_') % 2 else 'O'
       
    def get_state(self):
       state = ''
       for row in self.board:
-         for col in row:
-            state += col
+         state += ''.join(row)
             
       return state
       
+   def validate_board(self):
+      state = self.get_state()
+      
+      x_count = state.count('X')
+      o_count = state.count('O')
+      if x_count < o_count or x_count - o_count > 1:
+         raise ValueError("Invalid play sequence in board: {}".format(state))
+      
+      for eachSquare in state:
+         if eachSquare not in ['X', 'O', '_']:
+            raise ValueError("Invalid character {} in state {}".format(eachSquare, state))
+      
    def __str__(self):
-      return self.state
+      return self.get_state()
--- gameapp/tests.py
+++ gameapp/tests.py
@@ -1,3 +1,24 @@
 from django.test import TestCase
 
-# Create your tests here.
+from gameapp.game import GameBoard
+
+class GameBoardTest(TestCase):
+   def test_game_init(self):
+      self.assertEqual("_________", GameBoard().get_state())
+      self.assertEqual("X________", GameBoard("X________").get_state())
+      self.assertEqual("_X_O_____", GameBoard("_X_O_____").get_state())
+      
+      #test board length
+      self.assertRaises(ValueError, GameBoard, ''           )
+      self.assertRaises(ValueError, GameBoard, 'XO______'   )
+      self.assertRaises(ValueError, GameBoard, '__________' )
+      
+      #test board integrity
+      self.assertRaises(ValueError, GameBoard, 'XX_______'  )
+      self.assertRaises(ValueError, GameBoard, 'O________'  )
+      self.assertRaises(ValueError, GameBoard, 'A________'  )
+      
+   def test_get_player(self):
+      self.assertEqual("X", GameBoard().get_player())
+      self.assertEqual("O", GameBoard('X________').get_player())
+      
