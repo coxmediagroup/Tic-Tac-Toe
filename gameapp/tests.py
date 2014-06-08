e30b80ad945295ef5db70416f874b911debd9a95
veri745 <github.veritas@xoxy.net>
2014-06-08 13:01:32 -0500

    Added tests for basic game rules

--- gameapp/tests.py
+++ gameapp/tests.py
@@ -4,9 +4,9 @@ from gameapp.game import GameBoard
 
 class GameBoardTest(TestCase):
    def test_game_init(self):
-      self.assertEqual("_________", GameBoard().get_state())
-      self.assertEqual("X________", GameBoard("X________").get_state())
-      self.assertEqual("_X_O_____", GameBoard("_X_O_____").get_state())
+      self.assertEqual('_________', GameBoard().get_state())
+      self.assertEqual('X________', GameBoard('X________').get_state())
+      self.assertEqual('_X_O_____', GameBoard('_X_O_____').get_state())
       
       #test board length
       self.assertRaises(ValueError, GameBoard, ''           )
@@ -21,4 +21,43 @@ class GameBoardTest(TestCase):
    def test_get_player(self):
       self.assertEqual("X", GameBoard().get_player())
       self.assertEqual("O", GameBoard('X________').get_player())
+      
+   def test_move(self):
+      board = GameBoard()
+      
+      #test out-of-bounds
+      self.assertRaises(IndexError, board.move, *(0, 3))
+      self.assertRaises(IndexError, board.move, *(0, -1))
+      self.assertRaises(IndexError, board.move, *(-1, 0))
+      self.assertRaises(IndexError, board.move, *(3, 2))
+      
+      #test valid moves
+      board.move(1, 0)
+      self.assertEqual('___X_____', board.get_state())
+      self.assertRaises(IndexError, board.move, *(1, 0))
+      
+      board.move(2, 2)
+      self.assertEqual('___X____O', board.get_state())
+      self.assertRaises(IndexError, board.move, *(2, 2))
+      
+   def test_game_over(self):
+      #game start
+      self.assertEqual(False, GameBoard().is_game_over())
+      
+      #game in progress
+      self.assertEqual(False, GameBoard('X___O____').is_game_over())
+      
+      #game ended
+      self.assertEqual(True, GameBoard('X__OOOX_X').is_game_over())
+      self.assertEqual(True, GameBoard('XOO_X___X').is_game_over())
+      self.assertEqual(True, GameBoard('XXOOOXXOX').is_game_over())
+      
+   def test_get_winner(self):
+      self.assertEqual('O', GameBoard('X__OOOX_X').get_winner())
+      self.assertEqual('X', GameBoard('XOO_X___X').get_winner())
+      self.assertEqual(None, GameBoard('XXOOOXXOX').get_winner())
+      
+      #game not finished
+      self.assertEqual(None, GameBoard('X___O____').get_winner())
+      
       
