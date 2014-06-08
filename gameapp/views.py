f5af1dad42d3049a3d331dde4b768bc82217b8b6
veri745 <github.veritas@xoxy.net>
2014-06-07 21:34:43 -0500

    Added initial view as a placeholder

--- gameapp/views.py
+++ gameapp/views.py
@@ -1,3 +1,15 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+   board = getboard("XOXOXOXOX")
+
+   return render(request, 'board.html', {'board': board})
+
+  
+def getboard(board_string):
+   "Super basic placeholder for getting a board to display to the player"
+   board = []
+   board.append([list(board_string)[i] for i in range(3)])
+   board.append([list(board_string)[i] for i in range(3, 6)])
+   board.append([list(board_string)[i] for i in range(6, 9)])
+   return board
--- tictactoe/urls.py
+++ tictactoe/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'tictactoe.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^tictactoe/', 'gameapp.views.index', name='index'),
     url(r'^admin/', include(admin.site.urls)),
 )
