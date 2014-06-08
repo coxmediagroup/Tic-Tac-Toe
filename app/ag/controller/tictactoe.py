251cf2ed892197424bb988b2dabef47cf404ae63
pokeyjoey <jkeauctions@gmail.com>
2014-06-07 21:36:56 -0400

    Update tictactoe.py
    Added evaluate_board method.
    
    Created and instance of the TicTacToeGame class and evaluates a board.
    
    Returns a JSON string.

--- app/ag/controller/tictactoe.py
+++ app/ag/controller/tictactoe.py
@@ -1,4 +1,7 @@
+import json
+
 from app.ag.controller import BaseEndecaController
+from app.ag.model.tictactoegame import TicTacToeGame
 from core.http import JsonResponse
 from core.http import TemplateResponse
 
@@ -14,3 +17,16 @@ class TicTacToeController(BaseEndecaController):
         template = self.templates[path]
 
         return TemplateResponse(template, env)
+
+    def evaluate_board(self, request):
+        """ Evaluate a tic tac toe board.
+        """
+        board = request.params.get('board')
+
+        # create a game and evaluate the board
+        game = TicTacToeGame()
+        turn_result = game.take_a_turn(json.loads(board))
+
+        # return turn data to client
+        return JsonResponse(turn_result)
+
