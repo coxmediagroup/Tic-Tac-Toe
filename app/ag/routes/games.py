2b8369146eb11d175fcd9357c52324c1d9df8ae9
pokeyjoey <jkeauctions@gmail.com>
2014-06-07 21:40:01 -0400

    Update games.py
    Added a RESTFul route to process a tic tac toe turn.

--- app/ag/routes/games.py
+++ app/ag/routes/games.py
@@ -1,3 +1,5 @@
+from app.ag.controller.tictactoe import TicTacToeController
+
 GET = {"method": ["GET"]}
 POST = {"method": ["POST"]}
 
@@ -8,3 +10,8 @@ def configure(router):
                    controller=TicTacToeController,
                    action='index',
                    path='/games/tictactoe.pd')
+
+    router.connect('/games/tictactoe/turn',
+                   conditions=POST,
+                   controller=TicTacToeController,
+                   action='evaluate_board')
