dabbb56a9e991471c644480b13ac233730454e7d
Tim Willis <tim@willishq.com>
2014-06-23 17:35:57 -0400

    Cleanup urls

--- tictactoe/urls.py
+++ tictactoe/urls.py
@@ -1,15 +1,8 @@
-from django.conf.urls import patterns, include, url
-from django.contrib import admin
+from django.conf.urls import patterns, url
 
 from tictactoe.api.views import RecommendedPlay
-admin.autodiscover()
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'tictactoe.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
-    url(r'^admin/', include(admin.site.urls)),
     url(r'^api/recommended_play/', RecommendedPlay.as_view(),
         name='recommended_play'),
 )
