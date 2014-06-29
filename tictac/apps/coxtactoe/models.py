8519c6928b04bd1af8a911942c9976ebc6135cd5
Brett Anderson <brett@bzdzb.com>
2014-06-29 15:05:41 -0700

    Created model for saving moves for board states

--- tictac/apps/coxtactoe/models.py
+++ tictac/apps/coxtactoe/models.py
@@ -1,3 +1,20 @@
+# -*- coding: utf-8 -*-
 from django.db import models
+from django.db.models import fields
 
-# Create your models here.
+from uuidfield import UUIDField
+
+
+__docformat__ = 'restructuredtext en'
+
+
+class TicTacToeMoveModel(models.Model):
+    """
+    Provides move node model for building a move tree and looking up moves
+    """
+    player = fields.CharField(verbose_name="Player",
+        null=False, blank=False, db_index=True, max_length=1)
+    prev_board = fields.IntegerField(verbose_name="Previous Board State",
+        null=False, blank=False, db_index=True)
+    move = fields.SmallIntegerField(verbose_name="Move",
+         null=False, blank=False, db_index=True)
