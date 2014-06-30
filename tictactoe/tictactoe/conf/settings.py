c45e78de64ca122cf371f49fe416ca30330a91dc
Eric Ressler <ericr@smashingideas.com>
2014-06-30 10:08:35 -0700

    added the requirements and updated settings

--- tictactoe/tictactoe/conf/settings.py
+++ tictactoe/tictactoe/conf/settings.py
@@ -11,6 +11,26 @@ https://docs.djangoproject.com/en/1.6/ref/settings/
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
+MODULE_DIR = os.path.realpath(os.path.dirname(BASE_DIR))
+PROJECT_DIR = os.path.dirname(MODULE_DIR)
+
+WEB_ROOT = os.path.join(PROJECT_DIR, 'webroot')
+if not os.path.exists(WEB_ROOT):
+    os.mkdir(WEB_ROOT)
+
+MEDIA_ROOT = os.path.join(WEB_ROOT, 'media')
+MEDIA_URL = '/media/'
+
+STATIC_ROOT = os.path.join(WEB_ROOT, 'static')
+STATIC_URL = '/static/'
+
+STATICFILES_DIRS = (
+    os.path.join(MODULE_DIR, 'static'),
+)
+
+TEMPLATE_DIRS = (
+    os.path.join(MODULE_DIR, 'templates'),
+)
 
 
 # Quick-start development settings - unsuitable for production
@@ -21,10 +41,9 @@ SECRET_KEY = 'ki!s*-_ql8q)ika!i*p$9a=9e13&$whaa=(!9(3po=gvykk1r*'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
+TEMPLATE_DEBUG = DEBUG
 
-TEMPLATE_DEBUG = True
-
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['localhost']
 
 
 # Application definition
@@ -45,6 +64,9 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+
+    # custom modules
+    'tictactoe.apps.game',
 )
 
 ROOT_URLCONF = 'tictactoe.conf.urls'
@@ -66,17 +88,8 @@ DATABASES = {
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
 LANGUAGE_CODE = 'en-us'
-
 TIME_ZONE = 'UTC'
-
 USE_I18N = True
-
 USE_L10N = True
-
 USE_TZ = True
 
-
-# Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/1.6/howto/static-files/
-
-STATIC_URL = '/static/'
