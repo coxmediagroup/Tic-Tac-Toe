835dfad5bfba1376a94cffd81fe582c56d92be6e
Eric Ressler <ericr@smashingideas.com>
2014-06-30 11:41:28 -0700

    initial game tile and board classes

--- tictactoe/tictactoe/apps/game/game.py
+++ tictactoe/tictactoe/apps/game/game.py
@@ -1,1 +1,17 @@
 
+class GameTile ():
+    """
+    Manages the current game tile at the specified x, y position on the board.
+    """
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+        self.value = None
+
+
+class GameBoard ():
+    """
+    Manages the current game state
+    """
+    def __init__(self):
+        pass
