1af5d41be507ea318bd545e2031dd335412850c6
Eric Ressler <ericr@smashingideas.com>
2014-06-30 12:05:49 -0700

    generate the games tile set and determine who goes first

--- tictactoe/tictactoe/apps/game/game.py
+++ tictactoe/tictactoe/apps/game/game.py
@@ -1,3 +1,5 @@
+import random
+
 
 class GameTile ():
     """
@@ -13,5 +15,15 @@ class GameBoard ():
     """
     Manages the current game state
     """
+    _BOARD_SIZE = 3
+    _COMPUTER = 'x'
+    _PLAYER = 'o'
+
     def __init__(self):
-        pass
+        # generate the tileset
+        self.tiles = [[GameTile(x, y) for y in range(GameBoard._BOARD_SIZE)] for x in range(GameBoard._BOARD_SIZE)]
+
+        # determine starting player and play if the computer starts
+        if random.choice([GameBoard._PLAYER, GameBoard._COMPUTER]) == GameBoard._COMPUTER:
+            # TODO: call the function to trigger computer move
+            pass
