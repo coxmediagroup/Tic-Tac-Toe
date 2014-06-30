8fef2323b75d9b74342009b782b104b960f332b7
Julio Perez <perezbjc@gmail.com>
2014-06-30 02:11:27 +0000

    Added a welcome page, create player object with profile using name from welcome page; game board is rendered by player now

--- tictactoe/game/urls.py
+++ tictactoe/game/urls.py
@@ -3,5 +3,8 @@ from django.conf.urls import patterns, url
 from game import views
 
 urlpatterns = patterns('',
-    url(r'^$', views.game, name='titctactoe_game')
+
+	url(r'^$', views.welcome, name='welcome'),
+	url(r'^player/(?P<xplayer>[-A-Za-z0-9_]+)/$', views.game, name='titctactoe_game')
+
 )
--- tictactoe/game/views.py
+++ tictactoe/game/views.py
@@ -1,6 +1,35 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
+from django.core.urlresolvers import reverse
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth.models import User as DjangoUser
 
-def game(request):
+from game.models import Game, PlayerProfile
+
+
+def welcome(request):
 	
-	context = {}
-	return render(request, 'game/board.html', context)
+	if request.POST:
+		xplayer = request.POST.get('xplayer')
+		xplayer_email = ''.join([xplayer,'@tictactoe_game.us'])
+		xplayer_password = ''.join([xplayer,'password'])
+		player, created = DjangoUser.objects.get_or_create(username=xplayer,
+														   email=xplayer_email,
+														   password=xplayer_password)
+		player_profile, created = PlayerProfile.objects.get_or_create(player=player,player_type=1)
+		return redirect(reverse('titctactoe_game',
+								args=(xplayer,)))
+
+	return render(request,
+				  'game/welcome.html')
+
+
+def game(request, xplayer):
+	
+	playerProfile = get_object_or_404(PlayerProfile, player__username=xplayer)
+	context = {'xplayer': xplayer,
+			   'oplayer': 'sion'}
+	return render(request,
+				  'game/board.html',
+				  context)
+
+
