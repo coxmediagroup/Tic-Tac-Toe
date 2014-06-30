20bdca6e9648f737cdd19c5c0ea462d2706025c1
Brett Anderson <brett@bzdzb.com>
2014-06-29 18:34:14 -0700

    Increased game test coverage to 100%

--- tictac/apps/coxtactoe/tests/test_game.py
+++ tictac/apps/coxtactoe/tests/test_game.py
@@ -12,8 +12,43 @@ log.basicConfig(level=log.INFO)
 class GameTests(unittest.TestCase):
     def setUp(self):
         self.game = ttt.Game()
+        self._ = ttt.Marker('_')
+        self.x = ttt.Marker('X')
+        self.o = ttt.Marker('O')
 
     def test_game_initializes(self):
-        log.debug(self.game.id)
-        repr(self.game)
+        self.assertIsInstance(self.game, ttt.Game)
+
+    def test_game_save(self):
+        self.game.board.place(self.x, 0)
+        self.game.save()
+
+    def test_get_game_by_gid(self):
+        self.game.board.place(self.x, 0)
+        self.game.save()
+        saved_game_id = self.game.id
+        game = ttt.Game(id=saved_game_id)
+        self.assertEquals(self.game.id, game.id)
+        self.assertEquals(self.game.board.key, game.board.key)
+        self.assertEquals(self.game.board.turn, game.board.turn)
+
+    def _win_game(self, player):
+        self.game.board.place(player, 0)
+        self.game.board.place(player.opponent, 2)
+        self.game.board.place(player, 4)
+        self.game.board.place(player.opponent, 3)
+        self.game.board.place(player, 8)
+
+    def test_game_over(self):
+        self._win_game(self.x)
+        self.assertTrue(self.game.over)
+
+    def test_game_winner(self):
+        self._win_game(self.x)
+        self.assertEquals(self.game.winner, self.x)
+
+    def test_game_reset(self):
+        self.game.board.place(self.x, 0)
+        self.game.reset()
+        self.assertEquals(self.game.board.key, self.game.board.initial_board)
 
