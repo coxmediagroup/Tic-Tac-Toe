2b7fd6426639242f56380cb82f0af61130ef9450
Eric Ressler <ericr@smashingideas.com>
2014-07-01 14:40:59 -0700

    Revamped the game board.  I've been overthinking this.  I've decided to simplify the gamedown to its most basic architecture.  There's no need to have a matrix of class instances to represent a single value.  The board can be represented most simply by just a list of values with a predefined list of winnable combinations.

--- tictactoe/tictactoe/apps/game/game.py
+++ tictactoe/tictactoe/apps/game/game.py
@@ -1,75 +1,59 @@
+import pdb
 import random
 
 
-class GameTile (object):
-    """
-    Manages the current game tile at the specified x, y position on the board.
-    """
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.value = None
-
-    def __unicode__(self):
-        return "[GameTile %s, %s: %s]" % (self.x, self.y, self.value)
-
-    def __str__(self):
-        return unicode(self).encode('utf-8')
-
-
-class GameBoard (object):
+class TicTacToe (object):
     """
     Manages the current game state
     """
     _board_size = 3
     _computer = 'x'
     _player = 'o'
-    _cats_game = 'cat'
+
+    # winning tile combinations never change, define them on the class level
+    _winning_combos = (
+        (0, 1, 2), (3, 4, 5), (6, 7, 8),    # horizontal
+        (0, 3, 6), (1, 4, 7), (2, 5, 8),    # vertical
+        (0, 4, 8), (2, 4, 6)                # diagonal
+    )
 
     def __init__(self):
-        # generate the tile set
-        self.tiles = [[GameTile(x, y) for y in range(GameBoard._board_size)] for x in range(GameBoard._board_size)]
         self.winner = None
 
-        # winning combos only need to be defined once at the beginning of the game after the tiles are generated
-        self.winning_combos = [col for col in self.tiles] + \
-                              [[col[i] for col in self.tiles] for i in range(GameBoard._board_size)] + \
-                              [[self.tiles[i][i] for i in range(GameBoard._board_size)]] + \
-                              [[self.tiles[i][GameBoard._board_size - i - 1] for i in range(GameBoard._board_size)]]
+        # generate the board
+        self.board = [None for x in range(self._board_size**2)]
 
-        # determine starting player and play if the computer starts
-        if random.choice([GameBoard._player, GameBoard._computer]) == GameBoard._computer:
-            #TODO: call the function to trigger computer move
-            pass
+        # randomly select starting player
+        if random.choice([self._player, self._computer]) == self._computer:
+            self.computer_move()
 
     @property
     def available_tiles(self):
-        """ Return a list of all tiles that have not been used """
-        return [row for col in self.tiles for row in col if not row.value]
+        """ Return a list of all tiles that are set to None """
+        return [x for x in self.board if not x]
 
     def check_winner(self):
         """
         Determine if there is currently a winner. Should only be called after a valid move is made.
         """
-        winner = reduce(lambda x, y: x or y, [combo[0].value for combo in self.winning_combos
-                                              if all([combo[0].value == tile.value for tile in combo[1:]])])
+        winner = reduce(lambda x, y: x or y, [self.board[combo[0]] for combo in self._winning_combos
+                                              if all(self.board[combo[0]] == self.board[y] for y in combo[1:])])
 
         if not winner and not self.available_tiles:
             # no winner and no moves left?  it's a cat's game
-            return GameBoard._cats_game
+            pass
         return winner
 
-    def player_move(self, x, y):
+    def player_move(self, tile):
         """
         Update the the game board when the user plays a tile and check for a winner
         """
-        tile = self.tiles[x][y]
-        if tile.value:
+        if self.board[tile]:
             # that tile has already been taken
             # TODO: notify the user that they made an illegal move
             return
 
-        tile.value = GameBoard._player
+        self.board[tile] = self._player
         self.winner = self.check_winner()
         return self.winner
 
