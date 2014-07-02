a5fab031985a4985e99a4062319610932ecbbb3c
Eric Ressler <ericr@smashingideas.com>
2014-07-02 16:59:41 -0700

    Updated the game board view to instantiate a new game and pass it to the template

--- tictactoe/tictactoe/apps/game/views.py
+++ tictactoe/tictactoe/apps/game/views.py
@@ -1,5 +1,8 @@
+import json
+import random
 from django.http import Http404
 from django.views.generic import TemplateView, View
+from tictactoe.apps.game import game
 
 
 class GameBoardView (TemplateView):
@@ -7,6 +10,20 @@ class GameBoardView (TemplateView):
     Displays the gameboard to the user.
     """
     template_name = 'apps/game/gameboard.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(GameBoardView, self).get_context_data(**kwargs)
+
+        # retrieve the game from the session data, or create a new one if needed
+        self.request.session.flush()  # temporary so that I can test new games with a browser refresh to start
+        session_data = self.request.session.get('tictactoe')
+        tictactoe = game.TicTacToe(user_starts=random.randint(0, 1), session_data=session_data)
+        self.request.session['tictactoe'] = tictactoe.__dict__
+
+        context.update({
+            'game': tictactoe
+        })
+        return context
 
 
 class SubmitMoveView (View):
