ed1d3373cb73620057862734dad5e9398acd8a59
Eric Ressler <ericr@smashingideas.com>
2014-07-05 12:40:57 -0700

    Implemented the ajax logic for keeping the game board in sync with the server

--- tictactoe/tictactoe/static/js/jquery.tictactoe.js
+++ tictactoe/tictactoe/static/js/jquery.tictactoe.js
@@ -8,10 +8,63 @@
         $.each($.find("input[type=submit]"), function() {
             $(this).on('click', function(e) {
                 e.preventDefault();
-                console.log($(this).attr('name'));
+                setStatus();
+
+                var $this = $(this),
+                    $form = $(this.form),
+                    data = $form.serialize();
+
+                $.ajax({
+                    url: $form.attr('action'),
+                    type: $form.attr('method'),
+                    data: data + "&move=" + $this.attr('name').split('-')[1],
+                    success: function (data) {
+                        if(!data.success) {
+                            setStatus(data.message);
+                        } else {
+                            // check for a winner
+                            switch (data.game.winner) {
+                                case 'x':
+                                    setStatus("Doh! Looks like the computer won this time.");
+                                    break;
+                                case "o":
+                                    setStatus("Wow! Well done! You figured out how to win the unwinnable!");
+                                    break;
+                                case "-":
+                                    setStatus("All right, we'll call it a draw.");
+                                    break;
+                                default:
+                            }
+
+                            // update the tile displays
+                            var board = data.game.board,
+                                boardLength = board.length;
+                            console.log("board: " + board);
+
+                            for (var i = 0; i < boardLength; i++) {
+                                var $tile = $("input[name=tile-" + i + "]");
+                                if(board[i] == null) {
+                                    $tile.removeClass().addClass("btn btn-info").prop('disabled', false);
+                                } else {
+                                    $tile.removeClass().addClass("btn btn-" + board[i]).val(board[i].toUpperCase()).prop('disabled', true);
+                                }
+                            }
+                        }
+                    },
+                    error: function (xhr, err) {
+                        setStatus('There was a problem communicating with the server. Please try again.')
+                    }
+                });
             });
         });
 
+        function setStatus(message) {
+            if(message == null) {
+                $('#status').html("&nbsp;");
+            } else {
+                $('#status').html(message);
+            }
+        }
 
         return this;
     }
