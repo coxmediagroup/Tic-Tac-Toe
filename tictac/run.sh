4ae35571d13d76f052f66e2450faa35c2881dfd6
Brett Anderson <brett@bzdzb.com>
2014-07-12 22:56:46 -0700

    Added graceful subprocess termination to run scripts

--- tictac/run.sh
+++ tictac/run.sh
@@ -3,6 +3,17 @@
 export PYTHONUNBUFFERED=1
 export DJANGO_SETTINGS_MODULE=tictac.settings.dev
 
-./apps/flask/websocktoe/websocktoe.py &
-./manage.py runserver 
+function _cleanup() {
+    # Kill all child processes of current process
+    pids=$(pgrep -f `which python`)
+    echo $pids | xargs kill -TERM
+}
 
+# Run flask websocket server
+python ./apps/flask/websocktoe/websocktoe.py &
+
+# Run Django server
+python ./manage.py runserver
+
+# Cleanup once Django server exits
+_cleanup
--- tictac/run_tests.sh
+++ tictac/run_tests.sh
@@ -1,3 +1,23 @@
 #!/usr/bin/env bash
 
+export PYTHONUNBUFFERED=1
+export DJANGO_SETTINGS_MODULE=tictac.settings.dev
+
+function _cleanup() {
+    pids=$(pgrep -f `which python`)
+    echo $pids | xargs kill -TERM
+}
+
+# Run flask websocket server in background
+./apps/flask/websocktoe/websocktoe.py &
+
+# Run Django server in background
+./manage.py runserver &
+
+sleep 3
+
+# Run tests
 ./manage.py test -v1 --settings=tictac.settings.test
+
+# Cleanup once tests finish
+_cleanup
