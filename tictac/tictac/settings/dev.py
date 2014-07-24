af737640a9a901718a10a25d1b5b34a64e191d0e
Brett Anderson <brett@bzdzb.com>
2014-07-24 09:39:22 -0700

    Disabled Django debug_toolbar.

--- tictac/tictac/settings/dev.py
+++ tictac/tictac/settings/dev.py
@@ -26,6 +26,6 @@ DATABASES = {
     # }
 }
 
-INSTALLED_APPS += ("debug_toolbar", )
-
-MIDDLEWARE_CLASSES += ("debug_toolbar.middleware.DebugToolbarMiddleware", )
+# INSTALLED_APPS += ("debug_toolbar", )
+#
+# MIDDLEWARE_CLASSES += ("debug_toolbar.middleware.DebugToolbarMiddleware", )
