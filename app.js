b17256e7851ff910a045bc489099d54f6774a40d
Paul Wade <paul.wade@psibitsoftware.com>
2014-08-05 01:59:17 +0000

    node skeleton

---
+++ app.js
@@ -0,0 +1,9 @@
+var express = require('express');
+var env = process.env.NODE_ENV = process.env.NODE_ENV || 'development';
+var app = express();
+var config = require('./server/config/config')[env];
+
+require('./server/config/express')(app, config);
+require('./server/routes/default')(app);
+
+module.exports = app;
---
+++ server/routes/default.js
@@ -0,0 +1,13 @@
+module.exports = function (app) {
+
+    app.get('/partials/*', function (req, res) {
+        res.render('../../public/app/' + req.params[0]);
+    });
+
+    // general route handler
+    app.get('*', function (req, res) {
+        res.render('index', {
+            bootstrappedUser: req.user
+        });
+    });
+};
