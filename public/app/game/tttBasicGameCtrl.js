ed4d52d4868ba009f1b0cefda297be3330b0ea7f
Paul Wade <paul.wade@psibitsoftware.com>
2014-08-10 02:13:08 +0000

    adding player cards fixing some layout

--- public/app/game/tttBasicGameCtrl.js
+++ public/app/game/tttBasicGameCtrl.js
@@ -3,7 +3,7 @@ angular.module('tttApp').controller('tttBasicGameCtrl', function ($scope) {
     var game = new Game(3, 3);
     $scope.game = game;
     $scope.currentPlayer = 1;
-    
+
     //need the controller to abstract out the UI needing to know whos turn it is
     $scope.move = function (col, row) {
         game.makeMove(col, row, $scope.currentPlayer);
@@ -22,7 +22,11 @@ function Game(cols, rows) {
     this.columnsCount = cols;
     this.rowCount = rows;
     this.pos1 = 0;
-    this.gameBoard = [[0,0,2],[0,0,0],[0,0,0]];
+    this.gameBoard = [
+        [0, 0, 0],
+        [0, 0, 0],
+        [0, 0, 0]
+    ];
     this.makeMove = function (col, row, player) {
         this.gameBoard[col][row] = player;
 
