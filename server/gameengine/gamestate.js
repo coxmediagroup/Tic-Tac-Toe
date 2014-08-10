0310aeaa663934aa4598c8fb8d0ba7c809590ce3
Paul Wade <paul.wade@psibitsoftware.com>
2014-08-10 21:43:16 +0000

    adding vertical win testing

--- server/gameengine/gamestate.js
+++ server/gameengine/gamestate.js
@@ -3,6 +3,25 @@ var OpenSpaces = require("./openspace");
 exports.IsWinner = function (gameBoard) {
 
     //search row wins
+    var rowresult = RowWinTester(gameBoard);
+    if (rowresult !== 0) return rowresult;
+
+    var colresult = ColWinTester(gameBoard);
+    if (colresult !== 0) return colresult;
+
+    var diagresult = DiagWinTester(gameBoard);
+    if (diagresult !== 0) return diagresult;
+
+    if (OpenSpaces.GetOpenSpacesCount(gameBoard) < 1) return 3;
+
+
+    return 0;
+};
+
+
+
+
+var RowWinTester = function (gameBoard) {
     for (var r = 0; r < gameBoard.length; r++) {
         //foreach row
         var row = gameBoard[r];
@@ -10,23 +29,68 @@ exports.IsWinner = function (gameBoard) {
         var consecutiveCount = 0;
         for (var c = 0; c < row.length; c++) {
             var currentmove = row[c];
-            if(currentmove == 0 && lastMove == 0)
-            {
+            if (currentmove === 0 && lastMove === 0) {
                 consecutiveCount = 0;
                 continue;
             }
             if (currentmove != lastMove) {
-               consecutiveCount = 0;
+                consecutiveCount = 0;
                 continue;
             }
             else consecutiveCount++;
         }
         if (consecutiveCount === 3) return lastMove;
     }
-    
-    if(OpenSpaces.GetOpenSpacesCount(gameBoard) < 1)
-    return 3;
-    
-    
     return 0;
 };
+
+var ColWinTester = function (gameBoard) {
+
+    //TODO hardcoded height to 3 make dynamic for bigger boards
+    for (var c = 0; c < gameBoard.length; c++) {
+        //iterate down columns rather than rows
+        var lastMove = gameBoard[0][c]; //always start at row 0 and move down
+        var consecutiveCount = 0;
+        for (var r = 0; r < gameBoard.length; r++) {
+            var currentmove = gameBoard[r][c];
+
+            if (currentmove === 0 && lastMove === 0) {
+                consecutiveCount = 0;
+                continue;
+            }
+
+            if (currentmove != lastMove) {
+                consecutiveCount = 0;
+                continue;
+            }
+            else consecutiveCount++;
+
+
+        }
+        if (consecutiveCount === 3) return lastMove;
+    }
+
+    return 0;
+};
+
+var DiagWinTester = function (gameBoard) {
+    //1,0,0
+    //0,1,0
+    //0,0,1
+    //each row change starts the search col out last c + 1
+    for (var r = 0; r < gameBoard.length; r++) {
+        var row = gameBoard[r];
+        var lastcol = 0;
+        for (var c = 0; c < row.length; r++) {
+ 
+        }
+
+    }
+
+
+
+    return 0;
+};
+
+exports.ColWinTester;
+exports.RowWinTester;
--- test/gamestate.spec.js
+++ test/gamestate.spec.js
@@ -1,6 +1,6 @@
 var assert = require("assert");
 var GameState = require("../server/gameengine/gamestate")
-describe('gamestate', function () {
+describe('gamestate Row win', function () {
     it('Should show a win along top row for player 1', function () {
         var gameboard = [
             [1, 1, 1],
@@ -40,5 +40,42 @@ describe('gamestate', function () {
             [2, 0, 1]
         ];
         assert.equal(0, GameState.IsWinner(gameboard));
+    });
+});
+describe('gamestate column win', function () {
+    it('Should return a win for player 1 on first column', function () {
+        var gameboard = [
+            [1, 0, 0],
+            [1, 0, 0],
+            [1, 0, 0]
+        ];
+        assert.equal(1, GameState.IsWinner(gameboard));
+    });
+    it('Should return a win for player 2 on second column', function () {
+        var gameboard = [
+            [0, 2, 0],
+            [0, 2, 0],
+            [0, 2, 0]
+        ];
+        assert.equal(2, GameState.IsWinner(gameboard));
+    });
+    it('Should return a win for player 1 on third column', function () {
+        var gameboard = [
+            [0, 0, 1],
+            [0, 0, 1],
+            [0, 0, 1]
+        ];
+        assert.equal(1, GameState.IsWinner(gameboard));
+    });
+
+    describe('gamestate column win', function () {
+        it('should return a win for player 1 on top left to bottom right', function () {
+            var gameboard = [
+                [1, 0, 0],
+                [0, 1, 0],
+                [0, 0, 1]
+            ];
+            assert.equal(1,GameState.IsWinner(gameboard));
+        });
     });
 });
