0dfda019909f9926aa0a02f69d09e68d38ba8049
Paul Wade <paul.wade@psibitsoftware.com>
2014-08-14 00:34:29 +0000

    ok I lied I added a game win modal with reset button

--- public/app/game/tttBasicGameCtrl.js
+++ public/app/game/tttBasicGameCtrl.js
@@ -3,12 +3,23 @@ angular.module('tttApp').controller('tttBasicGameCtrl', function ($scope, $http)
     var game = new Game(3, 3);
     $scope.game = game;
     $scope.currentPlayer = 1;
-
+    
+    $scope.showModal = false;
+    $scope.gameResult = '';
+    
+    $scope.playAgain = function()
+    {
+        game = new Game(3,3);
+        $scope.game = game;
+        $scope.currentPlayer = 1;
+        $scope.showModal = false;
+    }
+    
     //need the controller to abstract out the UI needing to know whos turn it is
-    $scope.move = function (col, row) {
+    $scope.move = function (row, col) {
 
-        if ($scope.currentPlayer === 1) {
-            var result = game.makeMove(col, row, $scope.currentPlayer);
+        if ($scope.currentPlayer === 1 && game.gameBoard[row][col] === 0) {
+            var result = game.makeMove(row, col, $scope.currentPlayer);
             if (result) $scope.currentPlayer = SetNextPlayer($scope.currentPlayer);
             //ai turn
             var request = $http({
@@ -20,13 +31,13 @@ angular.module('tttApp').controller('tttBasicGameCtrl', function ($scope, $http)
 
                 if (data.winner !== 0) {
                     if (data.winner === 1) {
-                        alert('player 1 wins');
+                         GameWon("player 1 wins")
                     }
                     else if (data.winner === 2) {
-                        alert('computer wins!');
+                       GameWon("player 2 wins")
                     }
                     else {
-                        alert('draw');
+                         GameWon("DRAW!")
                     }
                 }
                 $scope.currentPlayer = 1;
@@ -39,6 +50,12 @@ angular.module('tttApp').controller('tttBasicGameCtrl', function ($scope, $http)
     function SetNextPlayer(currentPlayer) {
         if (currentPlayer === 1) return 2;
         else return 1;
+    }
+    
+    function GameWon(result)
+    {
+        $scope.showModal = true;
+        $scope.gameResult = result; 
     }
 });
 
