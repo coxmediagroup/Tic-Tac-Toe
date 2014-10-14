6e36c03d227599d8bdb49bf4488b7d46abbf5bb8
victoriana <rbasawa1@gmail.com>
2014-10-14 00:41:33 -0400

    tictactoe.py is now a functioning cgi app
    tested the ‘web app’

--- cgi-bin/tictactoe.py
+++ cgi-bin/tictactoe.py
@@ -1,14 +1,11 @@
-# tic tac toe game where AI never loses
+#!/usr/local/bin/python
+# tic tac toe app  where AI never loses
 # Ravi Basawa as part of Cox Communications Radio test assignment
 # assumption: AI takes first move
-# algorithm: a comprehensive state transition design deemed 
-#            practical because the above assumption and the
-#            characteristic board symmetry reduces the size
-#            of the table to manageable and intellible level
-# initial stage - naive tic tac toe
-# next stage - intelligent ai with rules to win and block and be naive as last resort
-# tested - ran 10000 moves within minute (54s), no errors or ai losses
-# 100 moves in 0.6 seconds so approx 6ms per op includes reads
+# one stop, works
+
+import cgi
+import cgitb; cgitb.enable()  # for troubleshooting
 import sys
 import os 
 import time 
@@ -17,20 +14,19 @@ import re
 from datetime import datetime
 
 
-
 board=""
 boardreset="0123A5678"
 validhumaninput=["0","1","2","3","4","5","6","7","8","9"]
 
-reset="GAME RESET AND RESTARTED, A IS AI WHILE H IS HUMAN, A HAS GONE FIRST ALREADY"
-invalid="INPUT NEEDS TO BE IN RANGE 1..8 TO MARK BOARD OR 9 TO RESET BOARD, BOARD UNCHANGED"
-occupied="SQUARE ALREADY FILLED, NO ENTRY MADE, BOARD UNCHANGED"
-entered="HUMAN 'H' ENTERED SUCCESFULLY INTO SQUARE "
-win="AI WINS GAME, RESET WITH 9 TO PLAY AGAIN"
-draw="AI DRAWS GAME, RESET WITH 9 TO PLAY AGAIN"
-loss="AI LOSES GAME, SHOULD NEVER HAPPEN, RESET WITH 9 TO PLAY AGAIN"
-block="AI MADE BLOCKING MOVE"
-naive="AI MADE NAIVE MOVE"
+reset="<h3>GAME RESET AND RESTARTED, A IS AI, H IS HUMAN, A HAS GONE FIRST</h3>"
+invalid="<h3>INPUT NEEDS TO BE IN RANGE 1..8 OR BE 9 TO RESET BOARD</h3>"
+occupied="<h3>SQUARE ALREADY FILLED</h3>"
+entered="<h3>HUMAN 'H' ENTERED SUCCESFULLY INTO SQUARE "
+win="<h3>AI WINS GAME</h3>"
+draw="<h3>AI DRAWS GAME</h3>"
+loss="<h3>AI LOSES GAME, SHOULD NEVER HAPPEN</h3>"
+block="<h3>AI BLOCKS HUMAN</h3>"
+naive="<h3>AI MADE NAIVE MOVE</h3>"
 
 def tictactoeboard(humaninput):
 
@@ -45,7 +41,7 @@ def tictactoeboard(humaninput):
    # invalid
    if humaninput not in validhumaninput:
       print invalid
-      print board[0:3] + "\n" + board[3:6] + "\n" + board[6:9]
+      print "<h3>" + board[0:3] + "<br>\n" + board[3:6] + "<br>\n" + board[6:9] + "</h3>"
       return
    # reset
    elif humaninput == '9':
@@ -54,12 +50,12 @@ def tictactoeboard(humaninput):
       boardfile.write(boardreset)
       boardfile.close()
       print reset
-      print board[0:3] + "\n" + board[3:6] + "\n" + board[6:9]
+      print "<h3>" + board[0:3] + "<br>\n" + board[3:6] + "<br>\n" + board[6:9] + "</h3>"
       return
    # occupied
    elif board[int(humaninput)] != humaninput:
    	  print occupied
-   	  print board[0:3] + "\n" + board[3:6] + "\n" + board[6:9]
+   	  print "<h3>" + board[0:3] + "<br>\n" + board[3:6] + "<br>\n" + board[6:9] + "</h3>"
    	  return
    else:
       # entered
@@ -67,8 +63,8 @@ def tictactoeboard(humaninput):
       boardfile = open('board', 'w')
       boardfile.write(board)
       boardfile.close()
-      print entered + humaninput
-      print board[0:3] + "\n" + board[3:6] + "\n" + board[6:9]
+      print entered + humaninput + "</h3>"
+      print "<h3>" + board[0:3] + "<br>\n" + board[3:6] + "<br>\n" + board[6:9] + "</h3>"
 
    # check if human won (should not occur, middle square is always ai A
    if ((re.search(r"HHH......",board)) or 
@@ -79,13 +75,13 @@ def tictactoeboard(humaninput):
       boardfile.write(board)
       boardfile.close()
       print loss
-      print board[0:3] + "\n" + board[3:6] + "\n" + board[6:9]
+      print "<h3>" + board[0:3] + "<br>\n" + board[3:6] + "<br>\n" + board[6:9] + "</h3>"
       boardfile = open('board', 'w')
       boardfile.write(boardreset)
       boardfile.close()
       board = open('board').read()
       print reset
-      print board[0:3] + "\n" + board[3:6] + "\n" + board[6:9]
+      print "<h3>" + board[0:3] + "<br>\n" + board[3:6] + "<br>\n" + board[6:9] + "</h3>"
       return
 
    # check if board is full for draw
@@ -98,13 +94,13 @@ def tictactoeboard(humaninput):
    	  boardfile.write(board)
    	  boardfile.close()
    	  print draw
-   	  print board[0:3] + "\n" + board[3:6] + "\n" + board[6:9]
+   	  print "<h3>" + board[0:3] + "<br>\n" + board[3:6] + "<br>\n" + board[6:9] + "</h3>"
    	  boardfile = open('board', 'w')
    	  boardfile.write(boardreset)
    	  boardfile.close()
    	  board = open('board').read()
    	  print reset
-   	  print board[0:3] + "\n" + board[3:6] + "\n" + board[6:9]
+   	  print "<h3>" + board[0:3] + "<br>\n" + board[3:6] + "<br>\n" + board[6:9] + "</h3>"
    	  return
    
    # ai win
@@ -186,13 +182,13 @@ def tictactoeboard(humaninput):
     	boardfile.write(board)
     	boardfile.close()
     	print win
-    	print board[0:3] + "\n" + board[3:6] + "\n" + board[6:9]
+    	print "<h3>" + board[0:3] + "<br>\n" + board[3:6] + "<br>\n" + board[6:9] + "</h3>"
     	boardfile = open('board', 'w')
     	boardfile.write(boardreset)
     	boardfile.close()
     	board = open('board').read()
     	print reset
-    	print board[0:3] + "\n" + board[3:6] + "\n" + board[6:9]
+    	print "<h3>" + board[0:3] + "<br>\n" + board[3:6] + "<br>\n" + board[6:9] + "</h3>"
     	return 
    		
    # human block 
@@ -229,7 +225,7 @@ def tictactoeboard(humaninput):
     	boardfile.write(board)
     	boardfile.close()
     	print block
-    	print board[0:3] + "\n" + board[3:6] + "\n" + board[6:9]
+    	print "<h3>" + board[0:3] + "<br>\n" + board[3:6] + "<br>\n" + board[6:9] + "</h3>"
     	# check whether board full
         full=True
         for b in board:
@@ -237,13 +233,13 @@ def tictactoeboard(humaninput):
         		full=False
         if full == True:
             print draw
-            print board[0:3] + "\n" + board[3:6] + "\n" + board[6:9]
+            print "<h3>" + board[0:3] + "<br>\n" + board[3:6] + "<br>\n" + board[6:9] + "</h3>"
             boardfile = open('board', 'w')
             boardfile.write(boardreset)
             boardfile.close()
             board = open('board').read()
             print reset
-            print board[0:3] + "\n" + board[3:6] + "\n" + board[6:9]
+            print "<h3>" + board[0:3] + "<br>\n" + board[3:6] + "<br>\n" + board[6:9] + "</h3>"
         return 
 
    # ai naive move
@@ -258,7 +254,7 @@ def tictactoeboard(humaninput):
     	boardfile.write(board)
     	boardfile.close()
     	print naive
-    	print board[0:3] + "\n" + board[3:6] + "\n" + board[6:9]
+    	print "<h3>" + board[0:3] + "<br>\n" + board[3:6] + "<br>\n" + board[6:9] + "</h3>"
     	# check whether board full
         full=True
         for b in board:
@@ -266,31 +262,40 @@ def tictactoeboard(humaninput):
         		full=False
         if full == True:
             print draw
-            print board[0:3] + "\n" + board[3:6] + "\n" + board[6:9]
+            print "<h3>" + board[0:3] + "<br>\n" + board[3:6] + "<br>\n" + board[6:9] + "</h3>"
             boardfile = open('board', 'w')
             boardfile.write(boardreset)
             boardfile.close()
             board = open('board').read()
             print reset
-            print board[0:3] + "\n" + board[3:6] + "\n" + board[6:9]
+            print "<h3>" + board[0:3] + "<br>\n" + board[3:6] + "<br>\n" + board[6:9] + "</h3>"
     	return 
 
-   
-  
 
-# 1..8 : Mark Board ('H')
-# 9    : Reset Board
-myhumaninput = '9'
-print tictactoeboard(myhumaninput)
-a = datetime.now()
-print a
-i=0
-while i < 100:
-	myhumaninput=str(random.randint(0,9))
-	print myhumaninput
-	print tictactoeboard(myhumaninput)
-	print "\n"
-	i = i + 1
-b = datetime.now()
-print b
-print (b-a).seconds
+print "Content-type: text/html"
+print
+
+print """
+<html>
+
+<head><title>Tic Tac Toe</title></head>
+
+<body>
+
+  <h3> Tic Tac Toe </h3>
+"""
+
+form = cgi.FieldStorage()
+position = form.getvalue("position", "(no position)")
+print tictactoeboard(position)
+print """
+  <p>Last Entry: %s</p>
+  <p> Note: H is Human and A is AI, In this game, AI should never lose </p>
+  <form method="post" action="tictactoe.py">
+    <p><h3>Enter position [0-8] or enter 9 to Reset: <input type="text" name="position"/></h3></p>
+  </form>
+
+</body>
+
+</html>
+""" % position
