64c1c771f349dac4161d1e47f5f3d6e74a961d8b
Joe Koberg <jkoberg@gmail.com>
2014-12-12 22:09:37 -0500

    django boilerplate to get app routed

--- tictactoe_app/tictactoe/views.py
+++ tictactoe_app/tictactoe/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
 
 # Create your views here.
+
+def index(http_request):
+  return HttpResponse("hello there")
--- tictactoe_app/tictactoe_app/urls.py
+++ tictactoe_app/tictactoe_app/urls.py
@@ -7,4 +7,5 @@ urlpatterns = patterns('',
     # url(r'^blog/', include('blog.urls')),
 
     url(r'^admin/', include(admin.site.urls)),
+    url(r'^tictactoe/', 'tictactoe.views.index', name='index')
 )
