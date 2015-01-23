c58975dc660f00275d127c5f291772d57a03cd3c
Neil Shannon <neil.shannon@tstllc.net>
2015-01-22 21:06:15 -0500

    API works, building client

---
+++ public/js/board.js
@@ -0,0 +1,8 @@
+//If I had time I'd do this with Canvas
+function renderBoard(){
+    var board = JSON.parse(getBoard());
+}
+
+function getBoard(){
+    jQuery
+}
--- src/main/java/com/ntsdev/api/ApiController.java
+++ src/main/java/com/ntsdev/api/ApiController.java
@@ -1,14 +1,69 @@
 package com.ntsdev.api;
 
+import com.ntsdev.game.AI;
+import com.ntsdev.game.Board;
+import com.ntsdev.game.CellState;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
 
 @RestController
 @RequestMapping("/api")
 public class ApiController {
 
-    @RequestMapping("/board")
-    public String gameBoard() {
-        return "Here's the game board!";
+    private AI ai = new AI();
+    //this is actually OK to share across clients...it's stateless
+
+    @RequestMapping("/newgame")
+    public String newGame(HttpSession session, HttpServletResponse response) {
+        Board board = new Board();
+        session.setAttribute("board", board);
+        response.setContentType("application/json");
+        return board.toJSON();
     }
+
+    @RequestMapping("/board")
+    @ResponseBody
+    public String gameBoard(HttpSession session) {
+        Object maybeBoard = session.getAttribute("board");
+        if(maybeBoard == null){
+            Board newBoard = new Board();
+            session.setAttribute("board", newBoard);
+            return newBoard.toJSON();
+        }
+        else{
+            Board board = (Board) maybeBoard;
+            return board.toJSON();
+        }
+    }
+
+    @RequestMapping("/makemove")
+    @ResponseBody
+    public String playerMove(
+            HttpSession session,
+            @RequestParam("x") Integer x,
+            @RequestParam("y") Integer y,
+            HttpServletResponse response) throws IOException {
+
+        Board board = (Board) session.getAttribute("board");
+        board.makeMove(x,y, CellState.O); //player is always "o"
+        if(board.checkWin(CellState.O)){
+            response.sendRedirect("/winner.html");
+        }
+
+        board = ai.makeMove(board);
+        session.setAttribute("board", board);
+
+        if(board.checkWin(CellState.X)){
+            response.sendRedirect("/loser.html");
+        }
+
+        return board.toJSON();
+    }
+
 }
