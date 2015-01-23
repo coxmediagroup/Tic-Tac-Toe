e835aea7c7fddb198506e4f1cf873e8a05d2ccf4
Neil Shannon <neil.shannon@tstllc.net>
2015-01-22 21:40:31 -0500

    basic client added -- found bug in computer player

--- public/js/board.js
+++ public/js/board.js
@@ -1,8 +1,29 @@
 //If I had time I'd do this with Canvas
-function renderBoard(){
-    var board = JSON.parse(getBoard());
+function renderBoard(result){
+    var gameBoard = JSON.parse(result);
+    var boardElement = $('#board');
+    boardElement.empty();
+
+    for(var i=0;i<3;i++){
+        var row = gameBoard.board[i];
+        var rowElement = boardElement.append('<div class="row"></div>');
+        for(var j=0;j<3;j++){
+            var cellValue = row[j];
+            rowElement.append('<span class="column" onclick="makeMove(' + i + ',' + j + ');">' + cellValue + '</span>');
+        }
+    }
 }
 
 function getBoard(){
-    jQuery
+    $.get("/api/board", function(result) {
+        renderBoard(result);
+    });
+}
+
+function makeMove(x, y){
+    var url = '/api/makemove?x=' + x + "&y=" + y;
+    $.get(url, function(result){
+            renderBoard(result);
+        }
+    )
 }
