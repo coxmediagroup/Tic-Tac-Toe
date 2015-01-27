c4d42234b344d0fdd4323fed2979c04120924832
life <life@evolution.(none)>
2015-01-27 00:07:58 -0500

    Added a test for getGameState()

--- index.js
+++ index.js
@@ -3,8 +3,60 @@
 * Main server script.
 */
 
-var tttoe = require('./lib/tictactoeserver');
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
 
-var x = tttoe.getBoardStateSample();
-console.log(x);
+var t = require('./lib/tictactoeserver');
+
+
+var board = null;             // Current tic-tac-toc board state. Also keeps track of who played previously.
+
+app.get('/', function(req, res){
+  res.sendFile(__dirname + '/index.html');
+});
+
+io.on('connection', function(socket){
+  socket.emit('updateMsg', 'Welcome! Lets player a game of Tic-Tac-Toe!');
+  board = t.getBoardStateSample()
+  socket.emit('updateBoard', board);
+  setTimeout(function(){console.log('settimeout'); socket.emit('updateMsg', 'Your turn to make a move.');}, 3000);
+  
+  
+
+  socket.on('nextMove', function(nmove){
+    // 1) First determine whether the game is complete: [draw, humanWins, machineWins].
+
+    console.log('player made a move ' + nmove.cellIdx + ' ' + nmove.player);
+
+    // If nextMove valid, update the board:
+    board = t.updateBoard(board, nmove);
+
+    console.log('from server:'); console.log(board);
+
+    socket.emit('updateBoard', board);
+
+    if (nmove.player != 'machine') {
+      // Let the machine player make a move:
+      // 1) Machine makes a move.
+      var nmove2 = null;
+      nmove2 = t.getMPNextMoveRandom(board);
+      console.log(nmove2);
+
+      //   Update the board state on the server:
+      board = t.updateBoard(board, nmove2);
+
+      // 2) Update the boardUI on the client:
+      socket.emit('updateBoard', board);  // Here, board.playerBefore == 'machine'. 
+      // 3) Update msg:
+      socket.emit('updateMsg', 'Your turn to make a move.');
+    }
+  });
+
+
+});
+
+http.listen(4000, function(){
+  console.log('listening on *:4000');
+});
 
--- test/test-tictactoe_server.js
+++ test/test-tictactoe_server.js
@@ -1,19 +1,39 @@
 
-var tttoe = require('../lib/tictactoeserver');
+var t = require('../lib/tictactoeserver');
 
 
 /**
  * @desc Unit test for isGameOver()
  */
-exports['isGameOver'] = function (test) {
-  var x = getBoardStateSample();
-  var result = tttoe.isGameOver(x);
+exports['getGameState'] = function (test) {
+  var board = null; 
+  var gstate = null; 
 
-  console.log('x:');
-  console.log(x);
-  console.log('result:');
-  console.log(result);
+  var blank = ''
+  console.log('board:');
+  console.log(board);
+  console.log('gstate:');
+  console.log(gstate);
 
-  test.equal(result.gameOver, false);
+  board = t.getBoardStateSample('initial');
+  gstate = t.getGameState(board);
+  test.equal(gstate.isGameOver, false);
+  test.equal(gstate.playerWinner, blank);
+
+  board = t.getBoardStateSample('a');
+  gstate = t.getGameState(board);
+  test.equal(gstate.isGameOver, true);
+  test.equal(gstate.playerWinner, 'X');
+
+  board = t.getBoardStateSample('b');
+  gstate = t.getGameState(board);
+  test.equal(gstate.isGameOver, true);
+  test.equal(gstate.playerWinner, 'O');
+
+  board = t.getBoardStateSample('c');
+  gstate = t.getGameState(board);
+  test.equal(gstate.isGameOver, true);
+  test.equal(gstate.playerWinner, 'draw');
+
   test.done();
 };
