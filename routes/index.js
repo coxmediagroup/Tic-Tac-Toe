3ad3d92bcd0aee3339efc9a48224585533dd084b
Kyle Watson <dev.kyle.watson@gmail.com>
2015-03-09 10:03:21 -0400

    send winner in response

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ var router = express.Router();
 
 // select a tile
 router.get('/select/:tile/:player', function(req, res) {
-	var tile = req.params.tile,
-		player = req.params.player;
-	console.log('tile: ' + tile + ', player: ' + player)
-	var success = game.selectTile(tile, player);
-	console.log('success: ' + success);
-	res.json({success: success});
+	var tile = req.params.tile;
+	var player = req.params.player;
+
+	var response = game.selectTile(tile, player);
+	console.log('response: ' + response);
+	res.json(response);
 });
 
 module.exports = router;
--- server/game.js
+++ server/game.js
@@ -15,57 +15,53 @@ module.exports = (function() {
 		return board[tile] === undefined;
 	}
 	
-	/*
-	 horizontal
-	 0 == 1 == 2
-	 3 == 4 == 5
-	 6 == 7 == 8
+	function hasWinner() {
+		function checkHorizontal() {
+			var b = board;
+			return (b[0] != undefined && b[0] === b[1] && b[0] === b[2]) ||
+				(b[3] != undefined && b[3] === b[4] && b[3] === b[5]) ||
+				(b[6] != undefined && b[6] === b[7] && b[6] === b[8]);
+		}
+		function checkVertical() {
+			var b = board;
+			return (b[0] != undefined && b[0] === b[3] && b[0] === b[6]) ||
+				(b[1] != undefined && b[1] === b[4] && b[1] === b[7]) ||
+				(b[2] != undefined && b[2] === b[5] && b[2] === b[8]);
+		}
+		function checkAcross() {
+			var b = board;
+			return (b[0] != undefined && b[0] === b[4] && b[0] === b[8]) ||
+				(b[2] != undefined && b[2] === b[4] && b[2] === b[6]);
+		}
 
-	 vertical
-	 0 == 3 == 6
-	 1 == 4 == 7
-	 2 == 5 == 8
-
-	 diagonal
-	 0 == 4 == 8
-	 2 == 4 == 6
-	*/
-	return {
-		hasWinner: function() {
-			function checkHorizontal() {
-				var b = board;
-				return (b[0] != undefined && b[0] === b[1] && b[0] === b[2]) ||
-					(b[3] != undefined && b[3] === b[4] && b[3] === b[5]) ||
-					(b[6] != undefined && b[6] === b[7] && b[6] === b[8]);
-			}
-			function checkVertical() {
-				var b = board;
-				return (b[0] != undefined && b[0] === b[3] && b[0] === b[6]) ||
-					(b[1] != undefined && b[1] === b[4] && b[1] === b[7]) ||
-					(b[2] != undefined && b[2] === b[5] && b[2] === b[8]);
-			}
-			function checkAcross() {
-				var b = board;
-				return (b[0] != undefined && b[0] === b[4] && b[0] === b[8]) ||
-					(b[2] != undefined && b[2] === b[4] && b[2] === b[6]);
-			}
+		return checkHorizontal() || checkVertical() || checkAcross();
+	}
 	
-			return checkHorizontal() || checkVertical() || checkAcross();
-		},
-		hasMoves: function() {
-			for (var i in board) {
-				if (board[i] === undefined) {
-					return true;
-				}
-			}
-			return false;
-		},
-		selectTile: function(tile, player) {
-			if (canMove(tile)) {
-				board[tile] = player;
+	function hasMoves() {
+		for (var i in board) {
+			if (board[i] === undefined) {
 				return true;
 			}
-			return false;
 		}
+		return false;
+	}
+	
+	function selectTile(tile, player) {
+		if (canMove(tile)) {
+			board[tile] = player;
+		}
+		var resp = {
+			player: player,
+			winner: hasWinner(),
+			movesLeft: hasMoves(),
+			board: board
+		}
+		return resp;
+	}
+	
+	return {
+		hasWinner: hasWinner,
+		hasMoves: hasMoves,
+		selectTile: selectTile
 	};
 }())
