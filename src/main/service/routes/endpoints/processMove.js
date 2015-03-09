6b1cba4999b82e580ebde2861ec7df65ea2412ad
ryemyster <ryan.mcdonald.us@gmail.com>
2015-03-09 00:56:10 -0400

    more

--- src/main/service/routes/endpoints/processMove.js
+++ src/main/service/routes/endpoints/processMove.js
@@ -18,7 +18,7 @@ module.exports = {
             res.json({
                 currentTimeMs : new Date().getTime() / 1000
             })
-        }
+        };
     },
     '/more' : {
         methods : [ 'get' ],
