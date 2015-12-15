c06a3df1d9a3f0d8692b3503a2e03b64f557ecb9
marty_trujillo <marty@ilikeemail.com>
2015-12-15 01:20:08 -0500

    Makes the icons a bit bigger and wires the UI to the controller.

--- js/all.js
+++ js/all.js
@@ -40615,4 +40615,18 @@ angular.module("ticTacToeApp").controller("gameCtrl", function($scope){
 						'','','',
 						'','','',
 						];
+	$scope.userClick = function(index){
+		if($scope.gameState[index] === ""){
+			// this logic needs to be moved into a service...
+			if($scope.turn === 1){
+				$scope.gameState[index] = "x";
+				if(index === 4){
+					$scope.gameState[0] = "o";
+				} else {
+					$scope.gameState[4] = "o";					
+				}
+				$scope.turn = $scope.turn + 2;
+			}
+		}
+	}
 });
