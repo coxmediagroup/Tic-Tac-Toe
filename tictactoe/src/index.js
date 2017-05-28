cb83f2321cd499db34fd6be48613898e893a0dd0
aleczander <afigueroa81@ufl.edu>
2017-05-28 16:51:38 -0400

    removing routes and adding materialui and heroku

--- tictactoe/src/index.js
+++ tictactoe/src/index.js
@@ -1,10 +1,25 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEnventPlugin from 'react-tap-event-plugin'
+import RaisedButton from 'material-ui/RaisedButton'
 
+injectTapEnventPlugin()
+
+// const customHistory = createBrowserHistory()
 
 ReactDOM.render(
-  <div>
-    <h1>TicTacToe</h1>
-  </div>
-  , document.getElementById('root')
+  <MuiThemeProvider>
+    <div>
+      <h1>
+        Hello World!
+      </h1>
+      <RaisedButton
+        label={'test button'}
+        primary={true}
+        onTouchTap={()=>{console.log('hello, i work')}}
+      />
+    </div>
+  </MuiThemeProvider>
+   ,document.getElementById('root')
 )
