ba40592506240e052ec42b3c167f120ceae4bdaa
aleczander <afigueroa81@ufl.edu>
2017-05-31 02:06:34 -0400

    attempted minimax functionality instead I added ability to choose between x or o and added reset button for fun

--- tictactoe/src/containers/Template.js
+++ tictactoe/src/containers/Template.js
@@ -1,10 +1,12 @@
 import React, {Component} from 'react'
 // import ReactDOM from 'react-dom'
-// import injectTapEnventPlugin from 'react-tap-event-plugin'
-// import RaisedButton from 'material-ui/RaisedButton'
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider'
+import injectTapEnventPlugin from 'react-tap-event-plugin'
+import RaisedButton from 'material-ui/RaisedButton'
 import {Stage} from 'react-konva'
 import {Board, Squares} from '../styled/tictactoe'
 
+injectTapEnventPlugin()
 
 class Template extends Component {
 
@@ -54,6 +56,50 @@ class Template extends Component {
     })
   }
 
+  chooseO = (index, marker) => {
+    this.setState( (chooseMark, prop) => {
+      let {ownMark, otherMark} = chooseMark
+      ownMark = 'O'
+      otherMark = 'X'
+      return{
+        ownMark: 'O' ,
+        otherMark: 'X'
+      }
+    })
+  }
+
+  chooseX = (index, marker) => {
+    this.setState( (chooseMark, prop) => {
+      let {ownMark, otherMark} = chooseMark
+      ownMark = 'X'
+      otherMark = 'O'
+      return{
+        ownMark: 'X' ,
+        otherMark: 'O'
+      }
+    })
+  }
+
+  resetButton = (index, marker) => {
+    let openSquares = []
+
+    this.setState( (chooseReset, prop) => {
+      let {gameState} = chooseReset
+      let openSquares = []
+      if (openSquares.length === 0){
+       return{
+       gameState: new Array(9).fill(false)
+     }
+   }
+  })
+}
+
+
+  // this.chooseX = (index, marker) => {
+  //   this.state.ownMark = 'X'
+  //   this.state.otherMark = 'O'
+  // }
+
   move = (index, marker) => {
     this.setState( (prevState, prop) => {
         let {gameState, yourTurn, gameOver, winner} = prevState
@@ -76,21 +122,40 @@ class Template extends Component {
           win: foundWin || false,
           winner
         }
-    })
-  }
+      })
+    }
 
   makeAiMove = (gameState) => {
+
+    // let bestMoveScore = 100;
     let otherMark = this.state.otherMark
     let openSquares = []
+    // let score = null
+
     gameState.forEach( (square, index)=> {
       if(!square) {
         openSquares.push(index)
       }
     })
+
     let aiMove = openSquares[this.random(0, openSquares.length)]
     setTimeout(()=>{
       this.move(aiMove, otherMark)
-    }, 1000)
+    })
+
+//minimax loop function
+    // for (var i = 0; i < openSquares.length; i++){
+    //   let newGameState = this.move(i, otherMark);
+    //   if (newGameState){
+    //     var moveScore = this.maxScore(newGameState);
+    //     if(moveScore < bestMoveScore){
+    //       bestMoveScore = moveScore;
+    //       score = i;
+    //     }
+    //   }
+    // }
+    //
+    // return score;
   }
 
   random = (min, max) => {
@@ -107,6 +172,201 @@ class Template extends Component {
     })
   }
 
+//beginning of minimax function
+
+  // winner = (gameState) => {
+
+  //   let player = this.state.ownMark || this.state.otherMark;
+  //   if(
+  //     gameState[0] === player && gameState[1] === player && gameState[2] === player ||
+  //     gameState[3] === player && gameState[4] === player && gameState[5] === player ||
+  //     gameState[6] === player && gameState[7] === player && gameState[8] === player ||
+  //     gameState[0] === player && gameState[3] === player && gameState[6] === player ||
+  //     gameState[1] === player && gameState[4] === player && gameState[7] === player ||
+  //     gameState[2] === player && gameState[5] === player && gameState[8] === player ||
+  //     gameState[0] === player && gameState[4] === player && gameState[8] === player ||
+  //     gameState[2] === player && gameState[4] === player && gameState[6] === player
+  //   ){
+  //     return true;
+  //   } else {
+  //     return false;
+  //   }
+  // }
+
+  // copyGameState = (gameState, index, marker) => {
+  //   return gameState
+  //   return gameState.splice(index, 1, marker)
+  //   let {gameState, yourTurn, gameOver, winner} = prevState
+  // }
+  //
+  // validMove(move, mark, gameState){
+  //
+  //   let combo = this.combos
+  //   let [a,b,c] = combo
+  //
+  //   let newGameState = this.copyGameState(gameState);
+  //   if (newGameState[move] === (gameState[a] === gameState[b] && gameState[a] === gameState[c] && gameState[a])){
+  //     newGameState[move] = mark;
+  //     return newGameState;
+  //   }
+  // }
+
+  // findAiMove = (gameState) => {
+  //   let move = null
+  //   let otherMark = this.state.otherMark;
+  //   let ownMark = this.state.ownMark;
+  //   let openSquares = [];
+  //   let bestMoveScore = 100;
+  //   if(this.checkWin(gameState, ownMark) || this.checkWin(gameState, otherMark) || this.tie(gameState)){
+  //     return false;
+  //   }
+  //   for (var i = 0; i < openSquares.length; i++){
+  //     let newBoard = this.validMove(i, this.state.otherMark, gameState);
+  //     if(newBoard) {
+  //       let moveScore = this.maxScore(newBoard);
+  //       if(moveScore < bestMoveScore) {
+  //         bestMoveScore = moveScore;
+  //          move = i;
+  //       }
+  //     }
+  //   }
+  //   return move;
+  // }
+
+  tie(gameState){
+    let openSquares = []
+    // var move = combos.join(combos).replace('');
+    if (openSquares.length === 0) {
+      return true;
+    }
+    return false;
+  }
+
+  // minimax(index, gamestate){
+  //   let openSquares = [];
+  //
+  //   if(this.move(gameState, ownMark)){
+  //     return {score:10};
+  //   } else if (this.move(gameState, otherMark)) {
+  //     return {score:-10};
+  //   } else if (openSquares.length === 0){
+  //     return {score:0};
+  //   }
+  //
+  //
+  //
+  //   for (var i = 0; i < openSquares.length; i++){
+  //     var moveScore = {score};
+  //
+  //
+  //     moveScore.index =
+  //     let newGameState = this.move(i, marker);
+  //
+  //     if (marker === otherMark){
+  //       var moveScore = minimax(index, ownMark)
+  //     }
+  //
+  //
+  //     if (newGameState){
+  //       var moveScore = minimax(index, gamestate);
+  //       if(moveScore < bestMoveScore){
+  //         bestMoveScore = moveScore;
+  //         score = i;
+  //       }
+  //     }
+  //   }
+  //
+  // }
+
+  minScore(gameState) {
+    let openSquares = [];
+    let otherMark = this.state.otherMark;
+    let ownMark = this.state.ownMark;
+
+    if(this.move(gameState, ownMark)){
+      return 10;
+    } else if (this.move(gameState, otherMark)) {
+      return -10;
+    } else if (openSquares.length === 0){
+      return 0;
+    } else {
+      let bestMoveValue = 100;
+      for(var i = 0; i < openSquares.length; i++){
+        let newGameState = this.move(i, otherMark);
+        if(newGameState){
+          let predictedMoveValue = this.maxScore(newGameState);
+          if(predictedMoveValue < bestMoveValue){
+            bestMoveValue = predictedMoveValue;
+          }
+        }
+      }
+      return bestMoveValue;
+    }
+  }
+
+  maxScore(gameState) {
+    let openSquares = [];
+    let otherMark = this.state.otherMark;
+    let ownMark = this.state.ownMark;
+
+    if(this.move(gameState, ownMark)){
+      return 10;
+    } else if (this.move(gameState, otherMark)) {
+      return -10;
+    } else if (openSquares.length === 0){
+      return 0;
+    } else {
+      let bestMoveValue = -100;
+      for(var i = 0; i < openSquares.length; i++){
+        let newGameState = this.move(i, ownMark);
+        if(newGameState){
+          let predictedMoveValue = this.maxScore(newGameState);
+          if(predictedMoveValue > bestMoveValue){
+            bestMoveValue = predictedMoveValue;
+          }
+        }
+      }
+      return bestMoveValue;
+    }
+  }
+
+// gameLoop(index){
+//   if(this.move(index, marker)){
+//     this.setState({
+//
+//     });
+//     return;
+//   }
+//   if(this.tie(currentGameBoard)){
+//     this.setState({
+//       currentGameBoard,
+//       player,
+//     });
+//     return;
+//   }
+//   player = this.otherMark;
+//   currentGameBoard = this.validMove(this.findAiMove(currentGameBoard), player, currentGameBoard);
+//   if(this.winner(currentGameBoard, player)){
+//     this.setState({
+//       currentGameBoard,
+//       player
+//     });
+//     return;
+//   }
+//   if(this.tie(currentGameBoard)){
+//     this.setState({
+//       currentGameBoard,
+//       player,
+//     });
+//     return;
+//   }
+//   this.setState({
+//     currentGameBoard
+//   });
+// }
+
+//End of minimax function
+
   render() {
     let {
       size,
@@ -120,6 +380,7 @@ class Template extends Component {
       ownMark
     } = this.state
     return (
+      <MuiThemeProvider>
       <div>
 
         <h1>
@@ -149,13 +410,26 @@ class Template extends Component {
 
         </Stage>
 
-        {/* <RaisedButton
-          label={'test button'}
+        <RaisedButton
+          label={'O'}
           primary={true}
-          onTouchTap={()=>{console.log('hello, i work')}}
-        /> */}
+          onTouchTap={this.chooseO}
+        />
+
+        <RaisedButton
+          label={'X'}
+          secondary={true}
+          onTouchTap={this.chooseX}
+        />
+
+        <RaisedButton
+          label={'Reset'}
+          default={true}
+          onTouchTap={this.resetButton}
+        />
 
       </div>
+    </MuiThemeProvider>
     )
   }
 }
