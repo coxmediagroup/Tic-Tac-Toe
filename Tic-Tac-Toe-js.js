ba588130056c7d446a98956dc1ad7471e96c8d37
cpacheco <cpacheco4983@gmail.com>
2017-05-31 05:50:39 -0400

    started implementing minimax for comp ai. unexpected end of input error on line 195 for some reason

--- Tic-Tac-Toe-js.js
+++ Tic-Tac-Toe-js.js
@@ -2,16 +2,20 @@
 var painted;//array to check if canvas !empty
 var content;//array for what is on canvas
 var winCombo;//for possible winning combinations
-var turn = 0;
+var turn;
 var canvasID;//for canvas id
 var sqr;//hold actual canvas
 var context;//for drawing on canvas
 var sqrsFilled;
 var y;//for user answer to playAgain?
 var userXO;//user symbol choice
+var compXO;
+var score;//board score for miniMax
 
 window.onload = function() {//initailize vars
   sqrsFilled = 0;
+  score = 0;
+  turn = 0;
   painted = new Array();
   content = new Array();
   winCombo = [[0, 1, 2], [3,4,5], [6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8], [2,4,6]];
@@ -77,7 +81,7 @@ function canvasClicked(sqrNum) {
 
     checkWinners(content[sqrNum - 1]);//check for winner after each move
 
-    if(sqrsFilled === 9) {//check if board is full. if so, ask to play again
+    if(!movesLeft()) {//check if board is full. if so, ask to play again
       alert("Game Over!");
       playAgain();
     }
@@ -117,9 +121,75 @@ function userChoice(choice) {//checks which symbol user wants to use
   var choiceDiv = document.getElementById("XorO");
   if(choice === "x") {
     userXO = "x";
+    compXO = "o";
   }
   else if(choice === "o") {
     userXO = "o";
+    compXO = "x";
   }
   choiceDiv.style.display = 'none';
+}
+
+function miniMax(board, depth) {
+  var mmScore = score();
+
+  if(mmScore === 10) {
+    return mmScore;
+  }
+
+  if(mmScore === -10) {
+    return mmScore;
+  }
+
+  if(!movesLeft()) {
+    return 0;
+  }
+
+  if(turn % 2 !== 0) {
+    var best = -1000;
+
+    for(var i = 0; i < 9; i++) {
+      if(board[i] === '') {
+        board[i] = compXO.toUpperCase();
+        best = Math.max(best, miniMax(board, depth+1));
+        board[i] = '';
+      }
+    }
+    return best;
+  }
+  else {
+    var best = 1000;
+
+    for(var i = 0; i < 9; i++) {
+      if(board[i] === '') {
+        board[i] = userXO.toUpperCase();
+        best = Math.min(best, miniMax(board, depth+1));
+        board[i] = '';
+      }
+    }
+    return best;
+  }
+}
+
+function score() {
+  for(var i = 0; i < winCombo.length; i++) {//go through and check if either X or O has winning combo
+    if(content[winCombo[i][0]] == symbol && content[winCombo[i][1]] == symbol && content[winCombo[i][2]] == symbol) {
+      if(symbol === userXO.toUpperCase()) {
+        return -10;
+      }
+      else if(symbol === compXO.toUpperCase()) {
+        return 10;
+      }
+      else {
+        return 0;
+      }
+}
+
+function movesLeft() {
+  if(sqrsFilled !== 9) {
+    return true;
+  }
+  else {
+    return false;
+  }
 }
