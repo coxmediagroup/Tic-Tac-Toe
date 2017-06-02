255615ccc1f29addde26edd0ba05f425915f2d4e
alan <alwat91@uga.edu>
2017-06-02 07:19:43 -0400

    Functions planned out

--- script/main.js
+++ script/main.js
@@ -1,1 +1,47 @@
-console.log("Script working");
+const GameEngine = {
+  board: Array(9).fill(null)
+  currentPlayer: "X",
+  gameOver: false,
+  resetGame: function() {
+
+  },
+  toggleCurrentPlayer: function() {
+
+  },
+  isValidMove: function(position) {
+
+  },
+  checkForVictory: function() {
+
+  },
+  makeMove: function(position) {
+
+  }
+};
+
+const ViewEngine = {
+  refreshBoardView: function(boardState) {
+
+  },
+  flashMessage: function(msg) {
+
+  },
+  clearFlash: function() {
+
+  }
+};
+
+const GameController = {
+  onClickNewGame: function(event) {
+
+  },
+  onClickBoardSpace: function(event) {
+
+  }
+};
+
+
+window.onload = function() {
+  const board = document.querySelectorAll(".board div");
+  console.log(board);
+}
