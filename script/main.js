71a7c7674e08e8f233c1594b3d3681a0d4632107
alan <alwat91@uga.edu>
2017-06-02 07:34:25 -0400

    Game Engine complete

--- script/main.js
+++ script/main.js
@@ -1,3 +1,5 @@
+// Basic organization based on an assignment in General Assembly's Web Development Immersive course
+
 const GameEngine = {
   board: Array(9).fill(null),
   currentPlayer: "X",
@@ -15,13 +17,51 @@ const GameEngine = {
     }
   },
   isValidMove: function(position) {
-
+    // A move is valid if the game is not over, the position is in the board, and the position is not occupied
+    if (this.gameOver == false) {
+      if (this.board.length > position) {
+        if (this.board[position] == null) {
+          return true;
+        }
+        return false;
+      }
+      return false;
+    }
+    return false;
   },
   checkForVictory: function() {
-
+    // Check for column victory
+    for (var i = 0; i < 3; i++) {
+      if (this.currentPlayer == this.board[i] && this.currentPlayer == this.board[i + 3] && this.currentPlayer == this.board[i + 6]) {
+        return true;
+      }
+    }
+    // Check for row victory
+    for (var i = 0; i < 3; i += 3) {
+      if (this.currentPlayer == this.board[i] && this.currentPlayer == this.board[i + 1] && this.currentPlayer == this.board[i + 2]) {
+        return true;
+      }
+    }
+    // Check for diagonal victory
+    if (this.currentPlayer == this.board[0] && this.currentPlayer == this.board[4] && this.currentPlayer == this.board[8]) {
+      return true;
+    }
+    if (this.currentPlayer == this.board[2] && this.currentPlayer == this.board[4] && this.currentPlayer == this.board[6]) {
+      return true;
+    }
+    return false;
   },
   makeMove: function(position) {
-
+    if (this.isValidMove(position)) {
+      this.board[position] = this.currentPlayer;
+      if (this.checkForVictory()) {
+        this.gameOver = true;
+      } else {
+        this.toggleCurrentPlayer();
+      }
+      return true;
+    }
+    return false;
   }
 };
 
