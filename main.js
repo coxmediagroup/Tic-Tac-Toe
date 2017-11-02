ad03a81cc8622ed2aa5543ad5cda54aa6c7d29c2
rush <myersrush1986@gmail.com>
2017-11-01 22:04:22 -0400

    Add JavaScript logic for player move and detecting a winning scenario

--- main.js
+++ main.js
@@ -1,4 +1,57 @@
 $(document).ready(function(){
+  var playerToken = '';
+  var computerToken = '';
+  var playerMoves = [];
+  var computerMoves = [];
+  var playerWins = 0;
+  var computerWins = 0;
+  var winningCombinations = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 5],
+                             [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+  $('.selectToken').click(selectToken);
+  $('.tile').click(playerMove);
 
+  function selectToken(event) {
+    playerToken = event.target.dataset.id;
+    $('.chooseToken').addClass('hide');
+    playerToken === 'X' ? computerToken = 'O' : computerToken = 'X';
+  };
 
+  function playerMove(event) {
+    if (event.target.classList.contains('played')) {
+      alert("Square already played")
+    } else if ( playerToken === '' ) {
+      alert('select X or O');
+      } else {
+          var tile = event.target
+          tile.innerHTML = playerToken;
+          tile.classList.add("played", playerToken);
+          playerMoves.push(parseInt(tile.dataset.id));
+          checkForWin(playerMoves);
+          setTimeout(computerMove, 2000);
+        };
+  };
+
+  function computerMove(){
+
+    // if we have two in a row, choose the third(if we have two and the third is unplayed, choose the third)
+    // if player has two in a row and third is unplayed, block them
+    // if we have center and opponent has two opposing corners, play side piece
+    // if center is available, play center
+    // if player has a corner and opposite corner is available, play it
+    // if there's an unplayed corner, play it
+    // if there's an empty side, play it
+    // check for win
+  };
+
+  function checkForWin(moves){
+    winningCombinations.forEach(function(combination) {
+      var gameWon = combination.every(function(value) {
+        return moves.indexOf(value) !== -1;
+      });
+      if (gameWon) {
+        alert('you won');
+        playerWins++;
+      };
+    });
+  };
 });
