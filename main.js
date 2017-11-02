9c0a915413886ca0811d9eaca623ae3bb5d78929
rush <myersrush1986@gmail.com>
2017-11-02 14:55:46 -0400

    Incorporate game reset functionality

--- main.js
+++ main.js
@@ -1,20 +1,21 @@
 $(document).ready(function(){
-  var playerToken = '';
-  var computerToken = '';
-  var playerMoves = [];
-  var computerMoves = [];
-  var playerWins = 0;
-  var computerWins = 0;
-  var ties = 0;
-  var unplayedTiles = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-  var winningCombinations = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 5],
-                             [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+    var playerToken = '';
+    var computerToken = '';
+    var playerMoves = [];
+    var computerMoves = [];
+    var playerWins = 0;
+    var computerWins = 0;
+    var ties = 0;
+    var unplayedTiles = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    var winningCombinations = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 5],
+                               [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+
   $('.selectToken').click(selectToken);
+  $('.playAgain').click(newGame);
   $('.tile').click(playerMove);
   $('#selectTokenModal').modal('show');
   $('#computerScore').html(computerWins);
   $('#tieScore').html(ties)
-
 
   function selectToken(event) {
     playerToken = event.target.dataset.id;
@@ -32,9 +33,9 @@ $(document).ready(function(){
           tile.innerHTML = playerToken;
           tile.classList.add("played", playerToken);
           playerMoves.push(parseInt(tile.dataset.id));
-          console.log(unplayedTiles.indexOf(parseInt(tile.dataset.id)));
           unplayedTiles.splice(unplayedTiles.indexOf(parseInt(tile.dataset.id)), 1);
           checkForWin(playerMoves);
+          checkForTie();
           setTimeout(computerMove, 2000);
         };
   };
@@ -47,14 +48,8 @@ $(document).ready(function(){
     computerMoves.push(tileNumber);
     unplayedTiles.splice(unplayedTiles.indexOf(tileNumber), 1);
     checkForWin(computerMoves);
-    // if we have two in a row, choose the third (if we have two and the third is unplayed, choose the third) - OR if by adding any of the unplayed tiles, we could have an array with all the same values as any of the winning combinations, play this tile
-    // if player has two in a row and third is unplayed, block them
-    // if we have center and opponent has two opposing corners, play side piece
-    // if center is available, play center
-    // if player has a corner and opposite corner is available, play it
-    // if there's an unplayed corner, play it
-    // if there's an empty side, play it
-    // check for win
+    checkForTie();
+
   };
 
   function checkForWin(moves){
@@ -74,9 +69,27 @@ $(document).ready(function(){
       $("#winModal").modal()
       return
     } else if (moves === computerMoves) {
-      computerMoves++
-      alert('You lost');
+      computerWins++;
+      $('#computerScore').html(computerWins);
+      $("#loseGame").modal()
     }
+  };
+  function checkForTie(){
+    if (unplayedTiles.length == 0) {
+      ties++;
+      $("#tieGame").modal();
+      $('#tieScore').html(ties);
+    }
+  };
+  function newGame() {
+    playerToken = '';
+    computerToken = '';
+    playerMoves = [];
+    computerMoves = [];
+    unplayedTiles = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+    winningCombinations = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 5],
+                               [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
+    $('.tile').html('').removeClass('played');
+    $('#selectTokenModal').modal('show');
   }
-
 });
