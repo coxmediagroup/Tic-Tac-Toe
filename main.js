66b58a290caf0cd89f9c6efde2b145ac8edbf303
rush <myersrush1986@gmail.com>
2017-11-02 01:18:57 -0400

    Incorporate computer move logic and detecting computer winning scenario

--- main.js
+++ main.js
@@ -5,10 +5,16 @@ $(document).ready(function(){
   var computerMoves = [];
   var playerWins = 0;
   var computerWins = 0;
+  var ties = 0;
+  var unplayedTiles = [1, 2, 3, 4, 5, 6, 7, 8, 9];
   var winningCombinations = [[1, 2, 3], [4, 5, 6], [7, 8, 9], [1, 4, 5],
                              [2, 5, 8], [3, 6, 9], [1, 5, 9], [3, 5, 7]];
   $('.selectToken').click(selectToken);
   $('.tile').click(playerMove);
+  $('#selectTokenModal').modal('show');
+  $('#computerScore').html(computerWins);
+  $('#tieScore').html(ties)
+
 
   function selectToken(event) {
     playerToken = event.target.dataset.id;
@@ -26,14 +32,22 @@ $(document).ready(function(){
           tile.innerHTML = playerToken;
           tile.classList.add("played", playerToken);
           playerMoves.push(parseInt(tile.dataset.id));
+          console.log(unplayedTiles.indexOf(parseInt(tile.dataset.id)));
+          unplayedTiles.splice(unplayedTiles.indexOf(parseInt(tile.dataset.id)), 1);
           checkForWin(playerMoves);
           setTimeout(computerMove, 2000);
         };
   };
 
   function computerMove(){
-
-    // if we have two in a row, choose the third(if we have two and the third is unplayed, choose the third)
+    console.log(unplayedTiles);
+    var tileNumber = unplayedTiles[Math.floor(Math.random() * unplayedTiles.length)]
+    var tile = $('[data-id = ' + tileNumber.toString() + ']');
+    tile.addClass("played").html(computerToken);
+    computerMoves.push(tileNumber);
+    unplayedTiles.splice(unplayedTiles.indexOf(tileNumber), 1);
+    checkForWin(computerMoves);
+    // if we have two in a row, choose the third (if we have two and the third is unplayed, choose the third) - OR if by adding any of the unplayed tiles, we could have an array with all the same values as any of the winning combinations, play this tile
     // if player has two in a row and third is unplayed, block them
     // if we have center and opponent has two opposing corners, play side piece
     // if center is available, play center
@@ -49,9 +63,20 @@ $(document).ready(function(){
         return moves.indexOf(value) !== -1;
       });
       if (gameWon) {
-        alert('you won');
-        playerWins++;
+        winGame(moves);
       };
     });
   };
+  function winGame(moves){
+    if (moves === playerMoves) {
+      playerWins++;
+      $('#playerScore').html(playerWins);
+      $("#winModal").modal()
+      return
+    } else if (moves === computerMoves) {
+      computerMoves++
+      alert('You lost');
+    }
+  }
+
 });
